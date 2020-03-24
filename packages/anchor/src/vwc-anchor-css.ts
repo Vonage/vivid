38ee389d8ecd2ab568824381379ec4adfaf84dff
yinonov <yinon@hotmail.com>
2020-03-25 01:37:55 +0200

    button classMap issue

--- packages/anchor/src/vwc-anchor-css.ts
+++
@@ -1,3 +0,0 @@
-import {css} from 'lit-element';
-
-export const style = css`body{color:blue}`;
--- packages/button/src/vwc-button.ts
+++ packages/button/src/vwc-button.ts
@@ -1,15 +1,14 @@
 import { Button as MwcButton } from '@material/mwc-button';
 import { customElement } from 'lit-element';
 
-// import { scheme } from '@vivid/scheme';
-// import { style as customStyle } from './vwc-button.css';
+import { scheme } from '@vivid/scheme';
 
 @customElement('vwc-button')
 // @ts-ignore: Unreachable code error
 export class Button extends MwcButton {
-  // static get styles() {
-  //   return [super.styles, scheme];
-  // }
+  static get styles() {
+    return [super.styles, scheme];
+  }
 }
 
 declare global {
