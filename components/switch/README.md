55ba6cf160acbfc770f53496a7f1b6daf810fbc7
yinonov <yinon@hotmail.com>
2020-04-20 17:43:29 +0300

    update to stories

---
+++ components/switch/README.md
@@ -0,0 +1,15 @@
+# `button`
+
+## vwc-button
+
+hosted on [github packages](https://help.github.com/en/packages/using-github-packages-with-your-projects-ecosystem/configuring-npm-for-use-with-github-packages#installing-a-package) at @vonage/vwc-button
+
+> TODO: description
+
+## Usage
+
+```
+const button = require('button');
+
+// TODO: DEMONSTRATE API
+```
---
+++ components/switch/src/vwc-switch.ts
@@ -0,0 +1,19 @@
+import { Switch as MwcSwitch } from '@material/mwc-switch/mwc-switch';
+// import { style } from '@material/mwc-switch/mwc-switch-css.js';
+import { customElement } from 'lit-element';
+
+// import { theme } from '@vivid/theme';
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'vwc-switch': Switch;
+  }
+}
+
+@customElement('vwc-switch')
+export class Switch extends MwcSwitch {
+  // static get styles() {
+  //   // return [super.styles /*, theme*/];
+  //   return [style /*, theme*/];
+  // }
+}
