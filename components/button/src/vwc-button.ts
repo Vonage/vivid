f5f38e75d1863caacb19d36969d2f48476f99c66
Yuri Guller <gullerya@gmail.com>
2020-05-13 14:26:20 +0300

    wip

--- components/button/src/vwc-button.ts
+++ components/button/src/vwc-button.ts
@@ -1,9 +1,6 @@
 import '@vonage/vvd-core';
 import { Button as MwcButton } from '@material/mwc-button/mwc-button';
-// import { style } from '@material/mwc-button/mwc-button-css.js';
 import { customElement } from 'lit-element';
-
-// import { theme } from '@vivid/theme';
 
 declare global {
   interface HTMLElementTagNameMap {
@@ -12,9 +9,4 @@ declare global {
 }
 
 @customElement('vwc-button')
-export class Button extends MwcButton {
-  // static get styles() {
-  //   // return [super.styles /*, theme*/];
-  //   return [style /*, theme*/];
-  // }
-}
+export class Button extends MwcButton { }
