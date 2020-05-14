63cc1bad750d87b7f6e7afd547d8e857b7830127
yinonov <yinon@hotmail.com>
2020-05-14 11:25:51 +0300

    lit html approach to handle events

--- demos/components/scheme.js
+++ demos/components/scheme.js
@@ -1,5 +1,5 @@
 import { html } from 'lit-html';
-import Element from '../modules/Element.js';
+import Element from '../modules/element.js';
 import '@vonage/vwc-scheme-select';
 
 export default class Scheme extends Element {
@@ -29,39 +29,40 @@ export default class Scheme extends Element {
             </template>
           </code-sample>
         </dd>
-        <dt>Add scheme selector</dt>
+        <dt>Add scheme selector (works in conjunction with vvd-scheme)</dt>
         <dd>
-          <vwc-scheme-select></vwc-scheme-select>
+          <vwc-scheme-select @vvdschemeselect="${this.handleSelect}"></vwc-scheme-select>
           <code-sample copy-clipboard-button>
             <template preserve-content>
-
+              (async () => { await customElements.whenDefined('vwc-scheme-select'); const
+              schemeSelect = document.querySelector('vwc-scheme-select');
+              schemeSelect.addEventListener('vvdschemeselect', ({ detail: { scheme } }) =>
+              console.log(\`vwc-scheme-select output is: \${scheme}\`) ) })();
             </template>
           </code-sample>
-          <iframe
-            src="https://carbon.now.sh/embed?bg=rgba(171%2C%20184%2C%20195%2C%201)&t=dracula&wt=none&l=javascript&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=0px&ph=0px&ln=false&fl=1&fm=Hack&fs=16.5px&lh=133%25&si=false&es=2x&wm=false&code=(async%2520()%2520%253D%253E%2520%257B%250A%2520%2520await%2520customElements.whenDefined('vwc-scheme-select')%253B%250A%2520%2520const%2520todoListElement%2520%253D%2520document.querySelector('vwc-scheme-select')%253B%250A%2520%2520todoListElement.addEventListener('vvd-scheme-select'%252C%2520(%257B%2520detail%253A%2520%257B%2520scheme%2520%257D%2520%257D)%2520%253D%253E%250A%2520%2520%2520%2520console.log(%2560vwc-scheme-select%2520output%2520is%253A%2520%2524%257Bscheme%257D%2560)%252C%250A%2520%2520)%253B%250A%257D)()%253B"
-            style="transform:scale(0.7); width:1024px; height:473px; border:0; overflow:hidden;"
-            sandbox="allow-scripts allow-same-origin"
-          >
-          </iframe>
         </dd>
       </dl>
     `;
   }
 
-  installed() {
-    console.log(code)(
-      // Developers should ensure that the component is
-      // defined by using the whenDefined method of the
-      // custom element registry before attempting to call
-      // public methods.
-
-      async () => {
-        await customElements.whenDefined('vwc-scheme-select');
-        const todoListElement = document.querySelector('vwc-scheme-select');
-        todoListElement.addEventListener('vvd-scheme-select', ({ detail: { scheme } }) =>
-          console.log(`vwc-scheme-select output is: ${scheme}`),
-        );
-      },
-    )();
+  handleSelect({ detail: { scheme } }) {
+    console.log(`vwc-scheme-select output is: ${scheme}`);
   }
+
+  // installed() {
+  //   console.log(code)(
+  //     // Developers should ensure that the component is
+  //     // defined by using the whenDefined method of the
+  //     // custom element registry before attempting to call
+  //     // public methods.
+
+  //     async () => {
+  //       await customElements.whenDefined('vwc-scheme-select');
+  //       const schemeSelect = document.querySelector('vwc-scheme-select');
+  //       schemeSelect.addEventListener('vvdschemeselect', ({ detail: { scheme } }) =>
+  //         console.log(`vwc-scheme-select output is: ${scheme}`),
+  //       );
+  //     },
+  //   )();
+  // }
 }
