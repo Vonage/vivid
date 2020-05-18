d935e82f7eee64d77dc94dacbd169d38fb273235
yinonov <yinon@hotmail.com>
2020-05-18 09:09:40 +0300

    update on menu import collision

--- components/menu/src/vwc-menu.ts
+++ components/menu/src/vwc-menu.ts
@@ -1,15 +1,11 @@
 import '@vonage/vvd-core';
-import { MenuBase } from '@material/mwc-menu/mwc-menu-base';
-import { style } from '@material/mwc-menu/mwc-menu-css.js';
+import { Menu } from '@material/mwc-menu';
 import { customElement } from 'lit-element';
 
 // if customization is need, it must be done in the mwc
 // component scope as some components integrate other
 // mwc components
-@customElement('mwc-menu')
-export class Menu extends MenuBase {
-  static styles = style;
-}
+// Menu.styles = ...
 
 @customElement('vwc-menu')
 export class VWCMenu extends Menu {}
