0b3b9ef9f0af84f0f66e8e7e9ea8c249952e3216
yinonov <yinon@hotmail.com>
2020-05-24 18:08:41 +0300

    style(demo): demo adjustments

--- components/button/src/vwc-button.ts
+++ components/button/src/vwc-button.ts
@@ -1,12 +1,12 @@
 import '@vonage/vvd-core';
-import { customElement } from 'lit-element';
+import { customElement, css } from 'lit-element';
 import { Button as MWCButton } from '@material/mwc-button';
 
 declare global {
-	interface HTMLElementTagNameMap {
-		'vwc-button': VWCButton;
-	}
+  interface HTMLElementTagNameMap {
+    'vwc-button': VWCButton;
+  }
 }
 
 @customElement('vwc-button')
-export class VWCButton extends MWCButton { }
+export class VWCButton extends MWCButton {}
