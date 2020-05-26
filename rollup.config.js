03b44aa15ff3fb1674d48ec289da768cff406298
Yuri Guller <gullerya@gmail.com>
2020-05-26 13:41:53 +0300

    preparations for build and deploy the demo

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,31 @@
-import { createDefaultConfig } from '@open-wc/building-rollup';
-import typescript from '@rollup/plugin-typescript';
+import merge from 'deepmerge';
+// use createSpaConfig for bundling a Single Page App
+import { createSpaConfig } from '@open-wc/building-rollup';
 
-// if you need to support IE11 use "modern-and-legacy-config" instead.
-// import { createCompatibilityConfig } from '@open-wc/building-rollup';
-// export default createCompatibilityConfig({ input: './index.html' });
+// use createBasicConfig to do regular JS to JS bundling
+// import { createBasicConfig } from '@open-wc/building-rollup';
 
-export default createDefaultConfig({
-  input: './src/button/src/button.ts',
-  output: {
-    dir: 'output',
-    format: 'cjs',
-  },
-  plugins: [typescript({ lib: ['es5', 'es6', 'dom'], target: 'es5' })],
+const baseConfig = createSpaConfig({
+  // use the outputdir option to modify where files are output
+  // outputDir: 'dist',
+
+  // if you need to support older browsers, such as IE11, set the legacyBuild
+  // option to generate an additional build just for this browser
+  // legacyBuild: true,
+
+  // development mode creates a non-minified build for debugging or development
+  developmentMode: process.env.ROLLUP_WATCH === 'true',
+
+  // set to true to inject the service worker registration into your index.html
+  injectServiceWorker: false,
+});
+
+export default merge(baseConfig, {
+  // if you use createSpaConfig, you can use your index.html as entrypoint,
+  // any <script type="module"> inside will be bundled by rollup
+  input: './index.html',
+
+  // alternatively, you can use your JS as entrypoint for rollup and
+  // optionally set a HTML template manually
+  // input: './app.js',
 });
