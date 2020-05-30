e4bf7f509ca80c16c525c1ef3f3feb73a3bae197
yinonov <yinon@hotmail.com>
2020-05-30 08:34:47 +0300

    refactor(demos): refactored demo deploymeny process
    refactored demo build with rollup & changed github action to deploy
    rollup bundle
    
    BREAKING CHANGE: storybook isn't used for Demoing

--- identities/interfaces/shapes.d.ts
+++
@@ -1,5 +0,0 @@
-
-
-export interface Shape {
-  ['border-radius']: Shape;
-}
--- identities/interfaces/variable-types.d.ts
+++
@@ -1,8 +0,0 @@
-export type ColorBackground = 'black' | 'lightblue';
-export type ColorForeground = 'black' | 'white';
-
-export const enum Shape{
-	pill= '50%',
-	round= '8px',
-	square= '2px'
-}
