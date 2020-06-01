e953ed25cbd1adadaf85edaf8255d73b331d75f5
Yuri Guller <gullerya@gmail.com>
2020-06-01 12:41:58 +0300

    enhancing the fonts part demo

--- demos/components/fonts.js
+++ demos/components/fonts.js
@@ -8,6 +8,7 @@ export default class Home extends Element {
 			<style>
 				.preview {
 					--font-size: 24px;
+					--font-weight: 400;
 				}
 
 				.preview {
@@ -18,15 +19,28 @@ export default class Home extends Element {
 					justify-content: space-evenly;
 					margin: 24px 0;
 				}
+
 				.preview > .part {
 					flex: 1;
 					max-width: 320px;
 				}
-				.preview > .part.text {
+
+				.preview .text {
 					text-align: center;
 					font-size: var(--font-size);
+					font-weight: var(--font-weight);
 					overflow: hidden;
 					white-space: nowrap;
+				}
+
+				.preview .resizer {
+					display: flex;
+					align-items: center;
+					justify-content: space-between;
+				}
+
+				.preview .selector {
+					flex-basis: 240px;
 				}
 			</style>
 
@@ -39,16 +53,53 @@ export default class Home extends Element {
 			</p>
 			<div class="preview">
 				<span class="part text">Lorem ipsum...</span>
-				<vwc-slider class="part selector" min="16" max="40" value="24" step="1" pin></vwc-slider>
+				<div class="part resizers">
+					<div class="resizer">
+						<span>size:</span>
+						<vwc-slider class="size selector" min="16" max="40" value="24" step="1" pin></vwc-slider>
+					</div>
+					<div class="resizer">
+						<span>weight:</span>
+						<vwc-slider class="weight selector" min="100" max="900" value="400" step="100" pin></vwc-slider>
+					</div>
+				</div>
 			</div>
+
+			<h3>Usage example</h3>
+			<p>
+				Fonts are being fetched and 'installed' by the following API call flow:
+			</p>
+			<code-sample type="js" copy-clipboard-button>
+				<template preserve-content>
+					import fonts from 'vvd-fonts.js';
+
+
+					fonts.init().then(() => {});
+				</template>
+			</code-sample>
+
+			<p>
+				In your CSS part consume the fonts as following:
+			</p>
+			<code-sample type="css" copy-clipboard-button>
+				<template preserve-content>
+					body {
+						font-family: 'VonageMain';	/* for IE11 only, omit this line if not relevant */
+						font-family: var(--vvd-font-family-spezia);
+					}
+				</template>
+			</code-sample>
     `;
 	}
 
 	connectedCallback() {
 		super.connectedCallback();
 		setTimeout(() => {
-			this.querySelector('.part.selector').addEventListener('input', event => {
+			this.querySelector('.size.selector').addEventListener('input', event => {
 				this.querySelector('.preview').style.setProperty('--font-size', `${event.target.value}px`);
+			});
+			this.querySelector('.weight.selector').addEventListener('input', event => {
+				this.querySelector('.preview').style.setProperty('--font-weight', `${event.target.value}`);
 			});
 		}, 0);
 	}
