8318cf689682daf916bb53e5cc21eb3a76bab25a
Yuri Guller <gullerya@gmail.com>
2020-06-01 11:06:41 +0300

    issue #24 adding more demo slider

--- demos/components/slider.js
+++ demos/components/slider.js
@@ -8,7 +8,9 @@ export default class Home extends Element {
 			<h3>Plain slider</h3>
 			<vwc-slider value="30" min="0" max="100"></vwc-slider>
 			<h3>Stepped slider</h3>
-			<vwc-slider value="10" min="0" max="70" step="10" markers pin></vwc-slider>
+			<vwc-slider value="10" min="0" max="70" step="10" markers></vwc-slider>
+			<h3>Pinned slider</h3>
+			<vwc-slider value="70" min="0" max="99" step="5" pin></vwc-slider>
     `;
 	}
 }
