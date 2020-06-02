ae08df949072eb8f6a1099d1a2da55910ec4fda5
Yuri Guller <gullerya@gmail.com>
2020-06-02 11:05:34 +0300

    aligning this branch with develop as of now

--- demos/app.js
+++ demos/app.js
@@ -1,13 +1,7 @@
 import Router from 'navigo';
 import { html, render } from 'lit-html';
+
 import * as components from './components/index.js';
-
-import fonts from '../node_modules/@vonage/vvd-fonts/vvd-fonts.js';
-import scheme from '../node_modules/@vonage/vvd-scheme/vvd-scheme.js';
-
-Promise.all([fonts.init(), scheme.init('light')]).then(() =>
-  document.body.classList.add('scheme-loaded'),
-);
 
 const router = new Router();
 
@@ -23,126 +17,51 @@ Object.keys(components).forEach(key => {
 // });
 const componentDemoContainer = document.querySelector('.comp-demo');
 router
-	.on({
-		'/': function () {
-			render(
-				html`
-          <home-route></home-route>
-        `,
-				componentDemoContainer,
-			);
-		},
-		'button': function () {
-			render(
-				html`
-          <button-route></button-route>
-        `,
-				componentDemoContainer,
-			);
-		},
-		'checkbox': function () {
-			render(
-				html`
-          <checkbox-route></checkbox-route>
-        `,
-				componentDemoContainer,
-			);
-		},
-		'chip': function () {
-			render(
-				html`
-          <chip-route></chip-route>
-        `,
-				componentDemoContainer,
-			);
-		},
-		'drawer': function () {
-			render(
-				html`
-          <drawer-route></drawer-route>
-        `,
-				componentDemoContainer,
-			);
-		},
-		'fonts': function () {
-			render(
-				html`
-          <fonts-route></fonts-route>
-        `,
-				componentDemoContainer,
-			);
-		},
-		'formfield': function () {
-			render(
-				html`
-          <formfield-route></formfield-route>
-        `,
-				componentDemoContainer,
-			);
-		},
-		'home': function () {
-			render(
-				html`
-          <home-route></home-route>
-        `,
-				componentDemoContainer,
-			);
-		},
-		'list': function () {
-			render(
-				html`
-          <list-route></list-route>
-        `,
-				componentDemoContainer,
-			);
-		},
-		'scheme': function () {
-			render(
-				html`
-          <scheme-route></scheme-route>
-        `,
-				componentDemoContainer,
-			);
-		},
-		'select': function () {
-			render(
-				html`
-          <select-route></select-route>
-        `,
-				componentDemoContainer,
-			);
-		},
-		'slider': function () {
-			render(
-				html`
-          <slider-route></slider-route>
-        `,
-				componentDemoContainer,
-			);
-		},
-		'textarea': function () {
-			render(
-				html`
-          <textarea-route></textarea-route>
-        `,
-				componentDemoContainer,
-			);
-		},
-		'textfield': function () {
-			render(
-				html`
-          <textfield-route></textfield-route>
-        `,
-				componentDemoContainer,
-			);
-		},
-		'vwcapp': function () {
-			render(
-				html`
-          <vwcapp-route></vwcapp-route>
-        `,
-				componentDemoContainer,
-			);
-		}
-	})
-	.resolve();
+  .on({
+    '/': function () {
+      render(html` <home-route></home-route> `, componentDemoContainer);
+    },
+    button: function () {
+      render(html` <button-route></button-route> `, componentDemoContainer);
+    },
+    checkbox: function () {
+      render(html` <checkbox-route></checkbox-route> `, componentDemoContainer);
+    },
+    chip: function () {
+      render(html` <chip-route></chip-route> `, componentDemoContainer);
+    },
+    drawer: function () {
+      render(html` <drawer-route></drawer-route> `, componentDemoContainer);
+    },
+    fonts: function () {
+      render(html` <fonts-route></fonts-route> `, componentDemoContainer);
+    },
+    formfield: function () {
+      render(html` <formfield-route></formfield-route> `, componentDemoContainer);
+    },
+    home: function () {
+      render(html` <home-route></home-route> `, componentDemoContainer);
+    },
+    list: function () {
+      render(html` <list-route></list-route> `, componentDemoContainer);
+    },
+    scheme: function () {
+      render(html` <scheme-route></scheme-route> `, componentDemoContainer);
+    },
+    select: function () {
+      render(html` <select-route></select-route> `, componentDemoContainer);
+    },
+    slider: function () {
+      render(html` <slider-route></slider-route> `, componentDemoContainer);
+    },
+    textarea: function () {
+      render(html` <textarea-route></textarea-route> `, componentDemoContainer);
+    },
+    textfield: function () {
+      render(html` <textfield-route></textfield-route> `, componentDemoContainer);
+    },
+    vwcapp: function () {
+      render(html` <vwcapp-route></vwcapp-route> `, componentDemoContainer);
+    },
+  })
+  .resolve();
