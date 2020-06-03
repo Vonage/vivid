6a683c91cbbaf8df058e2bf3b957fdfa4ea41eab
Yuri Guller <gullerya@gmail.com>
2020-06-03 23:40:01 +0300

    issue #11 fixed linting and configs

--- common/scheme/src/os-sync.utils.ts
+++ common/scheme/src/os-sync.utils.ts
@@ -2,10 +2,10 @@ import { not } from 'ramda';
 
 export const pcs = window.matchMedia('(prefers-color-scheme: dark)');
 
-export function getPreferedColorScheme() {
+export function getPreferedColorScheme(): string {
 	return pcs.matches ? 'dark' : 'light';
 }
 
-export function prefersColorSchemeSupported() {
+export function prefersColorSchemeSupported(): boolean {
 	return not(pcs.media === 'not all');
 }
--- components/app/src/vwc-app.ts
+++ components/app/src/vwc-app.ts
@@ -1,26 +1,26 @@
 import '@vonage/vvd-core';
-import { customElement, LitElement, html } from 'lit-element';
+import { customElement, LitElement, html, TemplateResult, CSSResult } from 'lit-element';
 import { style } from './vwc-app.css';
 
 declare global {
-  interface HTMLElementTagNameMap {
-    'vwc-app': App;
-  }
+	interface HTMLElementTagNameMap {
+		'vwc-app': App;
+	}
 }
 
 @customElement('vwc-app')
 export class App extends LitElement {
-  static get styles() {
-    // return [super.styles /*, theme*/];
-    return [
-      style,
-      /*, theme*/
-    ];
-  }
+	static get styles(): Array<CSSResult> {
+		// return [super.styles /*, theme*/];
+		return [
+			style,
+			/*, theme*/
+		];
+	}
 
-  render() {
-    return html`
+	render(): TemplateResult {
+		return html`
       <slot></slot>
     `;
-  }
+	}
 }
