e468f8d3f6f731fd51f3f6aee41901d1ac503f96
Yuri Guller <gullerya@gmail.com>
2020-06-03 21:09:21 +0300

    merging with develop

--- karma.conf.js
+++ karma.conf.js
@@ -2,36 +2,36 @@ const { createDefaultConfig } = require('@open-wc/testing-karma');
 const merge = require('deepmerge');
 
 module.exports = config => {
-  //	merging our stuff into default config
-  const defaultConfig = createDefaultConfig(config);
-  const extendedDefaultConfig = merge(defaultConfig, {
-    files: [
-      // runs all files ending with .test in the test folder,
-      // can be overwritten by passing a --grep flag. examples:
-      //
-      // npm run test -- --grep test/foo/bar.test.js
-      // npm run test -- --grep test/bar/*
-      { pattern: config.grep ? config.grep : 'components/**/test/**/*.test.js', type: 'module' },
-    ],
-    esm: {
-      nodeResolve: true,
-    },
-    frameworks: ['esm', 'mocha', 'chai'],
-    browserDisconnectTimeout: 300000,
-    browserNoActivityTimeout: 360000,
-    captureTimeout: 420000,
-    coverageIstanbulReporter: {
-      thresholds: {
-        global: {
-          statements: 30,
-          lines: 30,
-          branches: 3,
-          functions: 13,
-        },
-      },
-    },
-  });
+	//	merging our stuff into default config
+	const defaultConfig = createDefaultConfig(config);
+	const extendedDefaultConfig = merge(defaultConfig, {
+		files: [
+			// runs all files ending with .test in the test folder,
+			// can be overwritten by passing a --grep flag. examples:
+			//
+			// npm run test -- --grep test/foo/bar.test.js
+			// npm run test -- --grep test/bar/*
+			{ pattern: config.grep ? config.grep : 'components/**/test/**/*.test.js', type: 'module' },
+		],
+		esm: {
+			nodeResolve: true,
+		},
+		frameworks: ['esm', 'mocha', 'chai'],
+		browserDisconnectTimeout: 300000,
+		browserNoActivityTimeout: 360000,
+		captureTimeout: 420000,
+		coverageIstanbulReporter: {
+			thresholds: {
+				global: {
+					statements: 10,
+					lines: 10,
+					branches: 3,
+					functions: 10,
+				},
+			},
+		},
+	});
 
-  config.set(extendedDefaultConfig);
-  return config;
+	config.set(extendedDefaultConfig);
+	return config;
 };
