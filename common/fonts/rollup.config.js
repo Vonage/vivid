538dbbe12981d4e5163bec4f359c891c55dae8cd
Yuri Guller <gullerya@gmail.com>
2020-06-05 15:50:02 +0300

    viv 40 / issue #32 build packages - developing and verifying the concept (#33)
    * viv #40, issue #32 - initial commit of bundling configuration of components
    
    * viv #40, issue #32 - adding centralized building for all components
    
    * issue #32 packing the components
    
    * issue #32 packing the components
    
    * issue #32 packing the components
    
    * issue #32 - adjusting to the new releasing stuff
    
    * documentation updated
    
    * issue #32 - fixing the tests!!!!
    
    * updated the part of the prettier
    
    * fixing linting issue
    
    * fixing lint
    
    * fixing lint and prettier
    
    * fixing stuff
    
    * building the prod correctly

---
+++ common/fonts/rollup.config.js
@@ -0,0 +1,28 @@
+import cleaner from 'rollup-plugin-cleaner';
+import { terser } from 'rollup-plugin-terser';
+import copy from 'rollup-plugin-copy';
+
+export default {
+	external: ['lit-element'],
+	plugins: [
+		cleaner({
+			targets: ['common/fonts/dist']
+		}),
+		terser(),
+		copy({
+			targets: [
+				{ src: 'common/fonts/vvd-fonts.d.ts', dest: 'common/fonts/dist' },
+				{ src: 'common/fonts/src/fonts', dest: 'common/fonts/dist' },
+				{ src: 'common/fonts/src/vvd-fonts.css', dest: 'common/fonts/dist' }
+			]
+		})
+	],
+	input: [
+		'common/fonts/vvd-fonts.js'
+	],
+	output: {
+		dir: 'common/fonts/dist',
+		format: 'es',
+		compact: true
+	}
+};
