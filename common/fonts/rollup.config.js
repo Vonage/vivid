85d26e220a98b18c6e914fa86209cc9ed5b5011c
Yuri Guller <gullerya@gmail.com>
2020-06-05 10:35:05 +0300

    issue #32 packing the components

--- common/fonts/rollup.config.js
+++ common/fonts/rollup.config.js
@@ -1,8 +1,19 @@
+import cleaner from 'rollup-plugin-cleaner';
 import { terser } from 'rollup-plugin-terser';
+import copy from 'rollup-plugin-copy';
 
 export default {
 	plugins: [
-		terser()
+		cleaner({
+			targets: ['common/fonts/dist']
+		}),
+		terser(),
+		copy({
+			targets: [
+				{ src: 'common/fonts/src/fonts', dest: 'common/fonts/dist' },
+				{ src: 'common/fonts/src/vvd-fonts.css', dest: 'common/fonts/dist' }
+			]
+		})
 	],
 	input: [
 		'common/fonts/vvd-fonts.js'
