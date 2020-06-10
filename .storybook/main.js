107aaa34aa602b69db0a52e5e7f764f6b2563d23
Yuri Guller <gullerya@gmail.com>
2020-06-10 14:28:22 +0300

    iss-43 viv-44 initial commit

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,18 @@
 module.exports = {
-  stories: ['../components/*/stories/*.stories.{js,mdx}'],
-  addons: [
-    'storybook-prebuilt/addon-knobs/register.js',
-    'storybook-prebuilt/addon-docs/register.js',
-    'storybook-prebuilt/addon-viewport/register.js',
-  ],
-  esDevServer: {
-    // custom es-dev-server options
-    nodeResolve: true,
-    watch: true,
-    open: true,
-  },
+	stories: [
+		'../common/*/stories/*.stories.{js,mdx}',
+		'../components/*/stories/*.stories.{js,mdx}'
+	],
+	addons: [
+		'@storybook/addon-a11y/register.js',
+		'@storybook/addon-docs/register.js',
+		'@storybook/addon-knobs/register.js',
+		'@storybook/addon-viewport/register.js',
+		'storybook-addon-playroom/register.js'
+	],
+	esDevServer: {
+		nodeResolve: true,
+		watch: true,
+		open: true,
+	},
 };
---
+++ .storybook/vivid-theme.js
@@ -0,0 +1,14 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+	base: 'light',
+	colorSecondary: 'black',
+	appBorderRadius: 6,
+	fontBase: '"Spezia", sans-serif',
+	fontCode: 'monospace',
+	textColor: '#131415',
+	barSelectedColor: 'black',
+	brandTitle: 'Vivid, the Vonage Design System',
+	brandUrl: 'http://vivid.vonage.com',
+	brandImage: './assets/images/vvd-logo.svg',
+});
