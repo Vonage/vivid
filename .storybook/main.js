4e7b114e575b31a680d9ae8fd9f6914271995880
Yuri Guller <gullerya@gmail.com>
2020-06-11 13:46:21 +0300

    wip

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,10 +3,10 @@ module.exports = {
 		'../components/**/stories/*.stories.js'
 	],
 	addons: [
-		'@storybook/addon-a11y/register.js',
-		'@storybook/addon-docs/register.js',
-		'@storybook/addon-knobs/register.js',
-		'@storybook/addon-viewport/register.js',
-		'storybook-addon-playroom/register.js'
+		'@storybook/addon-a11y',
+		'@storybook/addon-docs',
+		'@storybook/addon-knobs',
+		'@storybook/addon-viewport',
+		'storybook-addon-playroom'
 	]
 };
--- .storybook/vivid-theme.js
+++ .storybook/vivid-theme.js
@@ -1,11 +1,5 @@
 import { create } from '@storybook/theming/create';
-import scheme from '@vonage/vvd-scheme/vvd-scheme.js';
-import initFonts from '@vonage/vvd-fonts/vvd-fonts.js';
-
-Promise.all([
-	scheme.init(),
-	initFonts.init()
-]).then(() => console.info('init Vivid theme for main Storybook app done'));
+import './vivid-storybook-utils.js';
 
 export default create({
 	base: 'light',
@@ -21,5 +15,5 @@ export default create({
 
 	brandTitle: 'Vivid, the Vonage Design System',
 	brandImage: './assets/vvd-logo.svg',
-	brandUrl: 'http://vivid.vonage.com'
+	brandUrl: 'https://vivid.vonage.com'
 });
