20a13714b4fd8ec08aea426a8364211b79aab5b9
Yuri Guller <gullerya@gmail.com>
2020-06-14 17:42:27 +0300

    adding select to the stories

--- components/select/src/vwc-select.ts
+++ components/select/src/vwc-select.ts
@@ -1,5 +1,7 @@
 import { customElement } from 'lit-element';
 import { Select as MWCSelect } from '@material/mwc-select';
+//import { style as styleCoupling } from '@vonage/vvd-style-coupling/vvd-style-coupling.css.js';
+import { style as mwcSelectStyle } from '@material/mwc-select/mwc-select-css.js';
 
 declare global {
 	interface HTMLElementTagNameMap {
@@ -7,5 +9,12 @@ declare global {
 	}
 }
 
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-ignore
+MWCSelect.styles = [mwcSelectStyle];
+
+/**
+ * This component is an extension of [<mwc-select>](https://github.com/material-components/material-components-web-components/tree/master/packages/select)
+ */
 @customElement('vwc-select')
 export class VWCSelect extends MWCSelect {}
