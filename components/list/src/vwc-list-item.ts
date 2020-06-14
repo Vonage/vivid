ed38eb19be5230049a72c5ff0af41441b7cfe87e
Yuri Guller <gullerya@gmail.com>
2020-06-14 15:49:33 +0300

    added list stories, adjusted styling

--- components/list/src/vwc-list-item.ts
+++ components/list/src/vwc-list-item.ts
@@ -1,5 +1,7 @@
 import { customElement } from 'lit-element';
 import { ListItem as MWCListItem } from '@material/mwc-list/mwc-list-item';
+import { style as styleCoupling } from '@vonage/vvd-style-coupling/vvd-style-coupling.css.js';
+import { style as mwcListItemStyle } from '@material/mwc-list/mwc-list-item-css.js';
 
 declare global {
 	interface HTMLElementTagNameMap {
@@ -7,5 +9,12 @@ declare global {
 	}
 }
 
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-ignore
+MWCListItem.styles = [styleCoupling, mwcListItemStyle];
+
+/**
+ * This component is an extension of [<mwc-list-item>](https://github.com/material-components/material-components-web-components/tree/master/packages/list)
+ */
 @customElement('vwc-list-item')
 export class VWCListItem extends MWCListItem {}
---
+++ components/list/stories/list-basic.stories.js
@@ -0,0 +1,19 @@
+import '@vonage/vwc-list/vwc-list.js';
+import '@vonage/vwc-list/vwc-list-item.js';
+import { withA11y } from '@storybook/addon-a11y';
+import { html } from 'lit-element';
+
+export default {
+	title: 'Atomic/List',
+	component: 'vwc-list',
+	decorators: [withA11y]
+}
+
+export const basic = () => html`
+	<vwc-list>
+		<vwc-list-item>Item 0</vwc-list-item>
+		<vwc-list-item>Item 1</vwc-list-item>
+		<vwc-list-item>Item 2</vwc-list-item>
+		<vwc-list-item>Item 3</vwc-list-item>
+	</vwc-list>
+`;
