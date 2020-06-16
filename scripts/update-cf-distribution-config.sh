35e2ffde34f14baf086f614c31ca1be0d64df2ee
Yuri Guller <gullerya@gmail.com>
2020-06-16 15:17:21 +0300

    almost there

--- scripts/update-cf-distribution-config.sh
+++ scripts/update-cf-distribution-config.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 aws cloudfront get-distribution-config --id $AWS_CF_DISTRIBUTION_ID > cfd-config-source.json
-less cfd-config-source.json
 jq 'del(.ETag)' cfd-config-source.json > cfd-config.tmp
-jq '(.DistributionConfig.Origins.Items[0].OriginPath = "/$STORYBOOK_DEPLOY_VERSION")' cfd-config.tmp > cfd-config.json
+jq '(.DistributionConfig.Origins.Items[0].OriginPath = "/$newVal")' --arg newVal $STORYBOOK_DEPLOY_VERSION cfd-config.tmp > cfd-config.json
 less cfd-config.json
