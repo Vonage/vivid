a968c7bb0f400264dfb013c4548d58e24ae095e5
Yuri Guller <gullerya@gmail.com>
2020-06-16 15:25:22 +0300

    one more

--- scripts/update-cf-distribution-config.sh
+++ scripts/update-cf-distribution-config.sh
@@ -4,5 +4,5 @@
 # echo newOriginPath
 aws cloudfront get-distribution-config --id $AWS_CF_DISTRIBUTION_ID > cfd-config-source.json
 jq 'del(.ETag)' cfd-config-source.json > cfd-config.tmp
-jq --arg nv "\/$STORYBOOK_DEPLOY_VERSION" '(.DistributionConfig.Origins.Items[0].OriginPath = $nv)' cfd-config.tmp > cfd-config.json
+jq --arg nv '/$STORYBOOK_DEPLOY_VERSION' '(.DistributionConfig.Origins.Items[0].OriginPath = $nv)' cfd-config.tmp > cfd-config.json
 less cfd-config.json
