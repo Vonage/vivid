a2fe80ec22ff1595d12145ca84020345ef7f18a9
Yuri Guller <gullerya@gmail.com>
2020-06-16 15:39:30 +0300

    wuo

--- scripts/update-cf-distribution-config.sh
+++ scripts/update-cf-distribution-config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 aws cloudfront get-distribution-config --id $AWS_CF_DISTRIBUTION_ID > cfd-config-source.json
-AWS_CF_DISTRIBUTION_ETAG=$(echo jq -r '.ETAG' cfd-config-source.json)
-echo $AWS_CF_DISTRIBUTION_ETAG
+AWS_CF_DISTRIBUTION_ETAG=$(jq -r '.ETAG' cfd-config-source.json)
+echo ${AWS_CF_DISTRIBUTION_ETAG}
 
 #jq 'del(.ETag)' cfd-config-source.json > cfd-config.tmp
 #jq --arg nv "/$STORYBOOK_DEPLOY_VERSION" '(.DistributionConfig | .Origins.Items[0].OriginPath = $nv)' cfd-config.tmp > cfd-config.json
