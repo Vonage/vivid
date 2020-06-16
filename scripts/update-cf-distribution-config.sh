c3485a3177d7141d5cfcbc3ff4c82989fcbf6795
Yuri Guller <gullerya@gmail.com>
2020-06-16 15:41:14 +0300

    test

--- scripts/update-cf-distribution-config.sh
+++ scripts/update-cf-distribution-config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 aws cloudfront get-distribution-config --id $AWS_CF_DISTRIBUTION_ID > cfd-config-source.json
-AWS_CF_DISTRIBUTION_ETAG=$(jq -r '.ETAG' cfd-config-source.json)
+AWS_CF_DISTRIBUTION_ETAG=$( echo jq -r '.ETAG' cfd-config-source.json)
 echo $AWS_CF_DISTRIBUTION_ETAG
 
 #jq 'del(.ETag)' cfd-config-source.json > cfd-config.tmp
