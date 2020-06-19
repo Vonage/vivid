352839d4c58a5bee0487e6f4410e926389305366
Matteo Zuffolini <matteozuffolini@Matteos-MacBook-Pro-2.local>
2020-06-19 13:02:53 +0100

    style for carousel pagination

--- demos/components/carousel.js
+++ demos/components/carousel.js
@@ -8,24 +8,25 @@ export default class Carousel extends Element {
 		return html`
 			<style></style>
 			<vwc-carousel style="width:500px;height: 200px;">
-			<vwc-carousel-item>
-						<img
-							src="https://source.unsplash.com/IUPiWRNKNm8"
-							style="max-width:100%;object-fit:cover;"
-						/>
-					</vwc-carousel-item>
-					<vwc-carousel-item>
-						<img
-							src="https://source.unsplash.com/q_M6B9_cajM"
-							style="max-width:100%;object-fit:cover;"
-						/>
-					</vwc-carousel-item>
-					<vwc-carousel-item>
-						<img
-							src="https://source.unsplash.com/CVBQdww5JZU"
-							style="max-width:100%;object-fit:cover;"
-						/>
-					</vwc-carousel-item>
+				<vwc-carousel-item>
+					<img
+						src="https://source.unsplash.com/IUPiWRNKNm8"
+						style="max-width:100%;object-fit:cover;"
+					/>
+				</vwc-carousel-item>
+				<vwc-carousel-item>
+					<img
+						src="https://source.unsplash.com/q_M6B9_cajM"
+						style="max-width:100%;object-fit:cover;"
+					/>
+				</vwc-carousel-item>
+				<vwc-carousel-item>
+					<img
+						src="https://source.unsplash.com/CVBQdww5JZU"
+						style="max-width:100%;object-fit:cover;"
+					/>
+				</vwc-carousel-item>
+			</vwc-carousel>
 		`;
 	}
 }
