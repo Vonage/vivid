4a505078a3d67cbf19108241090ab6dd784b224e
yinonov <yinon@hotmail.com>
2020-06-19 15:42:44 +0300

    style

--- components/carousel/src/vwc-carousel.ts
+++ components/carousel/src/vwc-carousel.ts
@@ -1,18 +1,18 @@
 import {
-	customElement,
-	LitElement,
-	html,
-	query,
-	CSSResult,
-	TemplateResult,
+  customElement,
+  LitElement,
+  html,
+  query,
+  CSSResult,
+  TemplateResult,
 } from 'lit-element';
 import { style } from './vwc-carousel.css';
 import Swiper from 'swiper';
 
 declare global {
-	interface HTMLElementTagNameMap {
-		'vwc-carousel': VWCCarousel;
-	}
+  interface HTMLElementTagNameMap {
+    'vwc-carousel': VWCCarousel;
+  }
 }
 
 /**
@@ -20,73 +20,80 @@ declare global {
  */
 @customElement('vwc-carousel')
 export class VWCCarousel extends LitElement {
-	static get styles(): CSSResult {
-		return style;
-	}
+  static get styles(): CSSResult {
+    return style;
+  }
 
-	@query('.swiper-container')
-	private swiperContainer?: Element;
+  @query('.swiper-container')
+  private swiperContainer?: Element;
 
-	@query('.swiper-button-next')
-	private swiperButtonNext?: Element;
+  @query('.swiper-button-next')
+  private swiperButtonNext?: Element;
 
-	@query('.swiper-button-prev')
-	private swiperButtonPrev?: Element;
+  @query('.swiper-button-prev')
+  private swiperButtonPrev?: Element;
 
-	@query('.swiper-pagination')
-	private swiperPagination?: Element;
+  @query('.swiper-pagination')
+  private swiperPagination?: Element;
 
-	firstUpdated(): void {
-		try {
-			const mySwiper = new Swiper(this.swiperContainer as HTMLElement, {
-				// Optional parameters
-				// direction: "horizontal",
-				loop: true,
+  firstUpdated(): void {
+    try {
+      // const mySwiper =
+      this.swiperize(this.swiperContainer as HTMLElement, this.swiperOptions);
+    } catch (e) {
+      console.log(e);
+    }
+  }
 
-				autoplay: {
-					delay: 2500,
-					disableOnInteraction: true,
-				},
+  private get swiperOptions(): any {
+    return {
+      // Optional parameters
+      // direction: "horizontal",
+      loop: true,
 
-				cssMode: true,
-				navigation: {
-					nextEl: this.swiperButtonNext as HTMLElement,
-					prevEl: this.swiperButtonPrev as HTMLElement,
-				},
-				pagination: {
-					el: this.swiperPagination as HTMLElement,
-					clickable: true,
-					renderBullet: function (index, className) {
-						return '<span class="' + className + '">' + (index + 1) + '</span>';
-					},
-				},
-				mousewheel: true,
-				keyboard: true,
-			});
-			console.log(mySwiper, this.swiperContainer, Swiper);
-			console.log(this.style);
-		} catch (e) {
-			console.log(e);
-		}
-	}
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: true,
+      },
 
-	render(): TemplateResult {
-		return html`<!-- Slider main container -->
-			<div class="swiper-container">
-				<!-- Additional required wrapper -->
-				<ol class="swiper-wrapper">
-					<!-- Slides -->
-					<slot></slot>
-				</ol>
-				<!-- If we need pagination -->
-				<div class="swiper-pagination"></div>
+      cssMode: true,
+      navigation: {
+        nextEl: this.swiperButtonNext as HTMLElement,
+        prevEl: this.swiperButtonPrev as HTMLElement,
+      },
+      pagination: {
+        el: this.swiperPagination as HTMLElement,
+        clickable: true,
+        renderBullet: function (index: number, className: string) {
+          return `<span class="${className}">${index + 1}</span>`;
+        },
+      },
+      mousewheel: true,
+      keyboard: true,
+    };
+  }
 
-				<!-- If we need navigation buttons -->
-				<div class="swiper-button-prev"></div>
-				<div class="swiper-button-next"></div>
+  private swiperize(el: HTMLElement, options: any) {
+    return new Swiper(el, options);
+  }
 
-				<!-- If we need scrollbar -->
-				<!--     <div class="swiper-scrollbar"></div> -->
-			</div>`;
-	}
+  render(): TemplateResult {
+    return html`<!-- Slider main container -->
+      <div class="swiper-container">
+        <!-- Additional required wrapper -->
+        <ol class="swiper-wrapper">
+          <!-- Slides -->
+          <slot></slot>
+        </ol>
+        <!-- If we need pagination -->
+        <div class="swiper-pagination"></div>
+
+        <!-- If we need navigation buttons -->
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
+
+        <!-- If we need scrollbar -->
+        <!--     <div class="swiper-scrollbar"></div> -->
+      </div>`;
+  }
 }
