83a21026a9bdc74081fef9772d562d642e2bd43c
Yuri Guller <gullerya@gmail.com>
2020-06-22 21:06:00 +0300

    added version and time to the demo site

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -17,10 +17,15 @@ async function processCustomUI() {
 		if (vvc && vve) {
 			const bde = vve.content.cloneNode(true);
 			bde.querySelector('.build-version').textContent = buildDetails.version;
-			bde.querySelector('.build-timestamp').textContent = buildDetails.timestamp;
-			vvc.parentElement.insertBefore(vve.content, vvc.nextElementSibling);
+			bde.querySelector('.build-timestamp').textContent = formatBuildTime(new Date(buildDetails.timestamp));
+			vvc.parentElement.insertBefore(bde, vvc.nextElementSibling);
 		}
 	} else {
 		console.error(`failed to fetch build details, status ${response.status}`);
 	}
+}
+
+function formatBuildTime(input) {
+	let d = new Date();
+	return `${String(input.getHours()).padStart(2, '0')}:${String(input.getMinutes()).padStart(2, '0')} ${String(input.getDate()).padStart(2, '0')}-${String(input.getMonth() + 1).padStart(2, '0')}-${input.getFullYear()}`;
 }
