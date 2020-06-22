bec7341dd7d7df02b23f11c4c2b81c2d434932c3
Yuri Guller <gullerya@gmail.com>
2020-06-22 21:33:32 +0300

    fixing the build details preps

--- scripts/prepare-sb-details.js
+++ scripts/prepare-sb-details.js
@@ -13,9 +13,11 @@ run();
 //	definitions path
 //
 function run() {
+	console.info('starting build details preparation...');
 	ensureOutDir();
 	const details = buildDetails();
 	writeDetails(details);
+	console.info('build details are ready');
 }
 
 function ensureOutDir() {
@@ -31,6 +33,7 @@ function buildDetails() {
 		timestamp: new Date().toISOString(),
 		version: externalDescription.version
 	});
+	console.info('\tbuild details constructed');
 	return result;
 }
 
@@ -39,4 +42,5 @@ function writeDetails(details) {
 		throw new Error(`details to write expected to be a non-null object; got ${details}`);
 	}
 	fs.writeFileSync(path.join(OUT_PATH, OUT_FILE), JSON.stringify(details));
+	console.info('\tbuild details JSON has been written');
 }
