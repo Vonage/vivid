61c34f289c5f5bc6dc2c2c6f5620b06f58183a06
joelgraham93 <j.r.g@hotmail.com>
2020-06-29 18:31:30 +0100

    vwc-select styles

--- components/select/stories/select-basic.stories.js
+++ components/select/stories/select-basic.stories.js
@@ -11,14 +11,14 @@ export default {
 }
 
 export const basic = () => html`
-	<vwc-select label="filled">
+	<vwc-select outlined label="VWC Select">
 		<vwc-list-item></vwc-list-item>
 		<vwc-list-item value="0">Item 0</vwc-list-item>
 		<vwc-list-item value="1">Item 1</vwc-list-item>
 		<vwc-list-item value="2">Item 2</vwc-list-item>
 		<vwc-list-item value="3">Item 3</vwc-list-item>
 		<vwc-list-item noninteractive>
-			<vwc-button raised style="cursor: inherit; pointer-events: auto;">Click me</vwc-button>
+			<vwc-button unelevated style="cursor: inherit; pointer-events: auto;">Click me</vwc-button>
 		</vwc-list-item>
 	</vwc-select>
 `;
