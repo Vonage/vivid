4eb157ea94bd7991cc5edb7d8e080eb468d0d4a8
Yuri Guller <gullerya@gmail.com>
2020-07-08 19:32:05 +0300

    issue #126: all fixed, tests and polishing

--- components/carousel/src/vwc-carousel.ts
+++ components/carousel/src/vwc-carousel.ts
@@ -47,9 +47,15 @@ export class VWCCarousel extends LitElement {
 
 	private swiper?: Swiper;
 
+	private slideRefs: HTMLElement[] = [];
+
 	firstUpdated(): void {
 		this.swiper = new Swiper(this.swiperContainer as HTMLElement, this.swiperOptions);
-		this.postInit();
+		this.collectSlideRefs(this.swiper);
+		this.postSlideToNext();
+		this.postSlideToPrev();
+		this.renderPagination(this.swiper);
+		this.updatePagination(this.swiper);
 		this.addEventListener('mouseenter', () => {
 			if (this.autoplay) {
 				this.swiper?.autoplay?.stop();
@@ -60,6 +66,9 @@ export class VWCCarousel extends LitElement {
 				this.swiper?.autoplay?.start();
 			}
 		});
+		if (this.autoplay) {
+			this.swiper?.autoplay?.start();
+		}
 	}
 
 	protected createRenderRoot(): HTMLElement {
@@ -88,11 +97,9 @@ export class VWCCarousel extends LitElement {
 		});
 	}
 
-	private postInit(): void {
-		this.postSlideToNext();
-		this.postSlideToPrev();
-		if (this.autoplay) {
-			this.swiper?.autoplay?.start();
+	private collectSlideRefs(swiper: Swiper): void {
+		for (let i = 0, l = swiper.slides.length; i < l; i++) {
+			this.slideRefs[i] = swiper.slides[i];
 		}
 	}
 
@@ -112,6 +119,49 @@ export class VWCCarousel extends LitElement {
 		}
 	}
 
+	private renderPagination(_swiper: Swiper) {
+		if (this.swiperPagination) {
+			const df = document.createDocumentFragment();
+			this.slideRefs.forEach(_slide => {
+				const bullet = document.createElement('span');
+				bullet.classList.add('swiper-pagination-bullet');
+				bullet.addEventListener('click', this.goToSlide.bind(this));
+				df.appendChild(bullet);
+			});
+			this.swiperPagination.appendChild(df);
+		}
+	}
+
+	private updatePagination(swiper: Swiper): void {
+		if (this.swiperPagination) {
+			const activeIndex = this.calculateActiveIndex(swiper);
+			Array.from(this.swiperPagination.children).forEach((bullet, index) => {
+				bullet.classList[index === activeIndex ? 'add' : 'remove']('swiper-pagination-bullet-active');
+			});
+		}
+	}
+
+	private onSlideChange(): void {
+		if (this.swiper) {
+			this.updatePagination(this.swiper);
+		}
+	}
+
+	private goToSlide(event: Event): void {
+		if (this.swiper && this.swiperPagination) {
+			const logicalIndex = Array.from(this.swiperPagination.children).indexOf(event.target as HTMLElement);
+			const domIndex = Array.from(this.swiper.wrapperEl.children).indexOf(this.slideRefs[logicalIndex]);
+			if (typeof domIndex === 'number') {
+				this.swiper.slideTo(domIndex);
+			}
+		}
+	}
+
+	private calculateActiveIndex(swiper: Swiper): number {
+		const nai = swiper.activeIndex;
+		return this.slideRefs.indexOf(swiper.slides[nai]);
+	}
+
 	private get swiperOptions(): SwiperOptions {
 		return {
 			loop: false,
@@ -119,24 +169,17 @@ export class VWCCarousel extends LitElement {
 				delay: 2500,
 				disableOnInteraction: true,
 			} : false,
-
 			cssMode: false,
 			navigation: {
 				prevEl: this.swiperButtonPrev as HTMLElement,
 				nextEl: this.swiperButtonNext as HTMLElement,
 			},
-			pagination: {
-				el: this.swiperPagination as HTMLElement,
-				clickable: true,
-				renderBullet: (index: number, className: string) => {
-					return `<span class="${className}">${index}</span>`;
-				},
-			},
 			mousewheel: true,
 			keyboard: true,
 			on: {
 				slideNextTransitionEnd: this.postSlideToNext.bind(this),
-				slidePrevTransitionEnd: this.postSlideToPrev.bind(this)
+				slidePrevTransitionEnd: this.postSlideToPrev.bind(this),
+				slideChange: this.onSlideChange.bind(this)
 			}
 		};
 	}
