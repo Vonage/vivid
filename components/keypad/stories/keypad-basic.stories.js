a84ca66beead9b8efe1e9d5d8a7d31809bae8ff1
Dwane <hey@dwane.io>
2020-07-08 16:59:11 -0400

    Uses CSF for Storybook stories to align with other components.

---
+++ components/keypad/stories/keypad-basic.stories.js
@@ -0,0 +1,26 @@
+import '@vonage/vwc-keypad/vwc-keypad.js';
+import { withA11y } from '@storybook/addon-a11y';
+import { html } from 'lit-element';
+
+export default {
+	title: 'Atoms/Keypad',
+	component: 'vwc-keypad',
+	decorators: [withA11y]
+}
+
+export const basic = () => html`
+	<h3>Default</h3>
+  <vwc-keypad></vwc-keypad>
+
+  <h3>Without Display</h3>
+  <vwc-keypad no-display></vwc-keypad>
+
+  <h3>Only Numeric Buttons</h3>
+  <vwc-keypad no-asterisk no-hash></vwc-keypad>
+
+  <h3>Custom Action Text</h3>
+  <vwc-keypad actionText="Start"></vwc-keypad>
+
+  <h3>Custom Cancel Text</h3>
+  <vwc-keypad cancelText="Stop"></vwc-keypad>
+`;
