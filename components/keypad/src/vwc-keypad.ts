818fb81fdc44844ee345fc086801a3c4a0075977
Dwane <hey@dwane.io>
2020-07-09 10:57:20 -0400

    Worked out component dependency issue

--- components/keypad/src/vwc-keypad.ts
+++ components/keypad/src/vwc-keypad.ts
@@ -1,6 +1,8 @@
 import {html, LitElement, property, customElement, TemplateResult, CSSResult} from 'lit-element';
 import { style as vwcKeypadStyle } from './vwc-keypad.css';
 import { style as styleCoupling } from '@vonage/vvd-style-coupling/vvd-style-coupling.css.js';
+import '@vonage/vwc-button/vwc-button';
+import '@vonage/vwc-textfield/vwc-textfield';
 
 declare global {
 	interface HTMLElementTagNameMap {
