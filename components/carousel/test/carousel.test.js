25f758eebffcf373645fe826f4fdc5ed572034d6
yonatankra <yonatan.kra@vonage.com>
2020-07-13 17:27:50 +0300

    Refactor the Carousel tests
    Use the snapshot and the new component renderer helper

--- components/carousel/test/carousel.test.js
+++ components/carousel/test/carousel.test.js
@@ -1,6 +1,7 @@
 import '../vwc-carousel.js';
-import { htmlToDom, waitNextTask } from '../../../utils/js/test-helpers.js';
-
+import { htmlToDom, waitNextTask, activateComponent } from '../../../utils/js/test-helpers.js';
+import { chaiDomDiff } from '@open-wc/semantic-dom-diff';
+chai.use(chaiDomDiff);
 const
 	VWC_CAROUSEL = 'vwc-carousel',
 	VWC_CAROUSEL_ITEM = 'vwc-carousel-item';
@@ -11,33 +12,40 @@ describe('test vwc-carousel', () => {
 		assert.exists(customElements.get(VWC_CAROUSEL_ITEM, 'vwc-carousel-item element is not defined'));
 	});
 
-	it('vwc-carousel has internal contents', async () => {
-		await customElements.whenDefined(VWC_CAROUSEL);
-		const docFragContainer = htmlToDom(`<${VWC_CAROUSEL} id="carousel-a"></${VWC_CAROUSEL}>`);
-		const actualElement = docFragContainer.firstElementChild;
-		document.body.appendChild(docFragContainer);
-		await waitNextTask();
-		assert.equal(document.querySelector('#carousel-a'), actualElement);
-		assert.equal(actualElement.querySelectorAll('*').length, 11);
+	describe('HTML output', function() {
+		it('should have the required elements', async () => {
+			const actualElement = await activateComponent(`<${VWC_CAROUSEL} id="carousel-a"></${VWC_CAROUSEL}>`);
+			expect(actualElement).dom.to.equalSnapshot();
+		});
+	});
+
+	describe('slides order', function() {
+		it('should set the first slide in the middle and the last before it', async function() {
+
+			const elementsIds = ['a', 'b', 'c'];
+
+			const carouselElements = elementsIds.map((key) => `<${VWC_CAROUSEL_ITEM} id="carousel-b-slide-${key}">Slide ${key.toUpperCase()}</${VWC_CAROUSEL_ITEM}>`)
+
+			const elementTemplate = `
+				<${VWC_CAROUSEL} id="carousel-b" autoplay="false">
+					${carouselElements.join('\n')}
+				</${VWC_CAROUSEL}>
+			`;
+			const actualElement = await activateComponent(elementTemplate);
+
+			const carouselDOMElements = actualElement.querySelectorAll(VWC_CAROUSEL_ITEM);
+			expect(carouselDOMElements[0].id).to.equal(`carousel-b-slide-${elementsIds[elementsIds.length - 1]}`);
+			expect(carouselDOMElements[1].id).to.equal(`carousel-b-slide-${elementsIds[0]}`);
+		});
 	});
 
 	it('vwc-carousel with slides, autoplay = false', async () => {
-		await customElements.whenDefined(VWC_CAROUSEL);
-		const docFragContainer = htmlToDom(`
-			<${VWC_CAROUSEL} id="carousel-b" autoplay="false">
-				<${VWC_CAROUSEL_ITEM} id="carousel-b-slide-a">Slide A</${VWC_CAROUSEL_ITEM}>
-				<${VWC_CAROUSEL_ITEM} id="carousel-b-slide-b">Slide B</${VWC_CAROUSEL_ITEM}>
-				<${VWC_CAROUSEL_ITEM} id="carousel-b-slide-c">Slide C</${VWC_CAROUSEL_ITEM}>
-			</${VWC_CAROUSEL}>
-		`);
-		const actualElement = docFragContainer.firstElementChild;
-		document.body.appendChild(docFragContainer);
-		await waitNextTask();
+
 		assert.equal(document.querySelector('#carousel-b'), actualElement);
 		assert.equal(actualElement.querySelectorAll('*').length, 17);
 
 		//	DOM pivot index is the actual index AFTER the slides adjustements, in this case 1, since last one is pushed before the first
-		assertSlidesStrip(document.querySelector('#carousel-b .swiper-wrapper'), 1)
+		assertSlidesStrip(document.querySelector('#carousel-b .swiper-wrapper'), 1);
 	});
 
 	it('vwc-carousel with slides, autoplay = true', async () => {
