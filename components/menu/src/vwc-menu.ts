2859540ed912b9ab5a6e50912b6e2904d328d6d3
yonatankra <yonatan.kra@vonage.com>
2020-07-22 16:59:59 +0300

    fix: menu border radius to 6px

--- components/menu/src/vwc-menu.ts
+++ components/menu/src/vwc-menu.ts
@@ -3,6 +3,7 @@ import { Menu as MWCMenu } from '@material/mwc-menu';
 import { customElement } from 'lit-element';
 //import { style as styleCoupling } from '@vonage/vvd-style-coupling/vvd-style-coupling.css.js';
 import { style as mwcMenuStyle } from '@material/mwc-menu/mwc-menu-css.js';
+import {style as vwcMenuStyle} from './vwc-menu.css';
 
 declare global {
 	interface HTMLElementTagNameMap {
@@ -12,7 +13,7 @@ declare global {
 
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 // @ts-ignore
-MWCMenu.styles = [mwcMenuStyle];
+MWCMenu.styles = [mwcMenuStyle, vwcMenuStyle];
 
 /**
  * This component is an extension of [<mwc-menu>](https://github.com/material-components/material-components-web-components/tree/master/packages/menu)
