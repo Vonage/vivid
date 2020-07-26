2665bc6042ec3f520846bda0bb27a748f63b7a47
Tal Weinfeld <tal.weinfeld@vonage.com>
2020-07-26 09:20:06 +0300

    Hardcoded output file

--- common/fonts/build/generate-font-combos.js
+++ common/fonts/build/generate-font-combos.js
@@ -1,72 +1,77 @@
-const FONT_DESIGN_COMBINATIONS = {
-	"XLarge": {
-		"font-family": "var(--vvd-font-family-spezia)",
-		"font-weight": "500",
-		"font-stretch": "75%",
-		"font-size": "44px"
-	},
-	"Large": {
-		"font-family": "var(--vvd-font-family-spezia)",
-		"font-weight": "500",
-		"font-stretch": "75%",
-		"font-size": "32px"
-	},
-	"Medium": {
-		"font-family": "var(--vvd-font-family-spezia)",
-		"font-weight": "500",
-		"font-stretch": "75%",
-		"font-size": "24px"
-	},
-	"Heading": {
-		"font-family": "var(--vvd-font-family-spezia)",
-		"font-weight": "500",
-		"font-stretch": "75%",
-		"font-size": "20px"
-	},
-	"SubHeading": {
-		"font-family": "var(--vvd-font-family-spezia)",
-		"font-weight": "500",
-		"font-stretch": "75%",
-		"font-size": "16px"
-	},
-	"Body1": {
-		"font-family": "var(--vvd-font-family-spezia)",
-		"font-weight": "400",
-		"font-stretch": "50%",
-		"font-size": "16px"
-	},
-	"Body1Bold": {
-		"font-family": "var(--vvd-font-family-spezia)",
-		"font-weight": "600",
-		"font-stretch": "50%",
-		"font-size": "16px"
-	},
-	"Body2": {
-		"font-family": "var(--vvd-font-family-spezia)",
-		"font-weight": "400",
-		"font-stretch": "50%",
-		"font-size": "14px"
-	},
-	"Body2Bold": {
-		"font-family": "var(--vvd-font-family-spezia)",
-		"font-weight": "600",
-		"font-stretch": "50%",
-		"font-size": "14px"
-	},
-	"Caption": {
-		"font-family": "var(--vvd-font-family-spezia)",
-		"font-weight": "600",
-		"font-stretch": "50%",
-		"font-size": "12px"
-	}
-};
+const
+	fs = require('fs'),
+	path = require('path');
 
-process.stdout.end(`@mixin font-combo($combo-id){
+const
+	OUTPUT_SCSS = "../_typography.scss",
+	FONT_DESIGN_COMBINATIONS = {
+		"XLarge": {
+			"font-family": "var(--vvd-font-family-spezia)",
+			"font-weight": "500",
+			"font-stretch": "75%",
+			"font-size": "44px"
+		},
+		"Large": {
+			"font-family": "var(--vvd-font-family-spezia)",
+			"font-weight": "500",
+			"font-stretch": "75%",
+			"font-size": "32px"
+		},
+		"Medium": {
+			"font-family": "var(--vvd-font-family-spezia)",
+			"font-weight": "500",
+			"font-stretch": "75%",
+			"font-size": "24px"
+		},
+		"Heading": {
+			"font-family": "var(--vvd-font-family-spezia)",
+			"font-weight": "500",
+			"font-stretch": "75%",
+			"font-size": "20px"
+		},
+		"SubHeading": {
+			"font-family": "var(--vvd-font-family-spezia)",
+			"font-weight": "500",
+			"font-stretch": "75%",
+			"font-size": "16px"
+		},
+		"Body1": {
+			"font-family": "var(--vvd-font-family-spezia)",
+			"font-weight": "400",
+			"font-stretch": "50%",
+			"font-size": "16px"
+		},
+		"Body1Bold": {
+			"font-family": "var(--vvd-font-family-spezia)",
+			"font-weight": "600",
+			"font-stretch": "50%",
+			"font-size": "16px"
+		},
+		"Body2": {
+			"font-family": "var(--vvd-font-family-spezia)",
+			"font-weight": "400",
+			"font-stretch": "50%",
+			"font-size": "14px"
+		},
+		"Body2Bold": {
+			"font-family": "var(--vvd-font-family-spezia)",
+			"font-weight": "600",
+			"font-stretch": "50%",
+			"font-size": "14px"
+		},
+		"Caption": {
+			"font-family": "var(--vvd-font-family-spezia)",
+			"font-weight": "600",
+			"font-stretch": "50%",
+			"font-size": "12px"
+		}
+	};
+
+const outputSassFile = fs.createWriteStream(path.join(__dirname, OUTPUT_SCSS));
+outputSassFile.end(`@mixin font-combo($combo-id){
 	${Object
-	.entries(FONT_DESIGN_COMBINATIONS)
-	.map(([identifier, styles])=>{
-		return `\t@if($combo-id == "${identifier}"){${Object.entries(styles).map(([modifier, value])=>"\n\t\t"+[modifier, value].join(': ') + ";").join('')+"\n\t"}}`;
-	})
-	.join('\n')
+		.entries(FONT_DESIGN_COMBINATIONS)
+		.map(([identifier, styles])=> `\t@if($combo-id == "${identifier}"){${Object.entries(styles).map(([modifier, value])=>"\n\t\t"+[modifier, value].join(': ') + ";").join('')+"\n\t"}}`)
+		.join('\n')
 }
 }`);
