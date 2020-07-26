ce6646cea1a86903306a15fdcd421b8dc13c0947
yinonov <yinon@hotmail.com>
2020-07-26 14:31:20 +0300

    storybook updated

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,14 @@
 module.exports = {
-	stories: [
-		'../common/**/stories/*.stories.js',
-		'../components/**/stories/*.stories.js'
-	],
-	addons: [
-		'@storybook/addon-a11y',
-		'@storybook/addon-actions',
-		'@storybook/addon-docs',
-		'@storybook/addon-knobs',
-		'@storybook/addon-storysource',
-		'@storybook/addon-viewport'
-	]
+  stories: [
+    '../common/**/stories/*.stories.js',
+    '../components/**/stories/*.stories.js',
+  ],
+  addons: [
+    '@storybook/addon-a11y',
+    '@storybook/addon-actions',
+    '@storybook/addon-docs',
+    '@storybook/addon-knobs',
+    '@storybook/addon-storysource',
+    '@storybook/addon-viewport',
+  ],
 };
--- components/button/stories/button-basic.stories.js
+++ components/button/stories/button-basic.stories.js
@@ -1,60 +1,66 @@
 import '@vonage/vwc-button/vwc-button.js';
 import { withA11y } from '@storybook/addon-a11y';
 import { html } from 'lit-element';
+import { spread } from '@open-wc/lit-helpers';
 
 export default {
-  title: 'Atoms|Button',
+  title: 'Atoms/Button',
   component: 'vwc-button',
   decorators: [withA11y],
 };
+export const Basic = (args) => {
+  console.log(args);
+  return html`<vwc-button ...=${spread(args)}>Basic</vwc-button>`;
+};
+Basic.args = { label: 'hello' };
 
-export const basic = () => html`
-  <style>
-    .container {
-      display: inline-block;
-      padding: 24px;
-    }
-  </style>
+// export const basic = () => html`
+//   <style>
+//     .container {
+//       display: inline-block;
+//       padding: 24px;
+//     }
+//   </style>
 
-  <h3>Standard</h3>
-  <div class="container">
-    <vwc-button>Basic</vwc-button>
-    <vwc-button layout="text" icon="info">With icon</vwc-button>
-    <vwc-button disabled icon="code">Disabled</vwc-button>
-  </div>
+//   <h3>Standard</h3>
+//   <div class="container">
+//     <vwc-button>Basic</vwc-button>
+//     <vwc-button layout="text" icon="info">With icon</vwc-button>
+//     <vwc-button disabled icon="code">Disabled</vwc-button>
+//   </div>
 
-  <h3>Outlined</h3>
-  <div class="container">
-    <vwc-button layout="outlined">Basic</vwc-button>
-    <vwc-button layout="outlined" icon="info">With icon</vwc-button>
-    <vwc-button layout="outlined" disabled icon="code">Disabled</vwc-button>
-  </div>
+//   <h3>Outlined</h3>
+//   <div class="container">
+//     <vwc-button layout="outlined">Basic</vwc-button>
+//     <vwc-button layout="outlined" icon="info">With icon</vwc-button>
+//     <vwc-button layout="outlined" disabled icon="code">Disabled</vwc-button>
+//   </div>
 
-  <h3>Filled</h3>
-  <div class="container">
-    <vwc-button layout="filled">Normal</vwc-button>
-    <vwc-button layout="filled" connotation="cta" icon="info"
-      >Call to action</vwc-button
-    >
-    <vwc-button layout="filled" connotation="success" icon="code"
-      >Success</vwc-button
-    >
-    <vwc-button layout="filled" connotation="error" icon="code"
-      >Error</vwc-button
-    >
-    <vwc-button layout="filled" connotation="warning" icon="info"
-      >Warning</vwc-button
-    >
-    <vwc-button layout="filled" connotation="warning" disabled icon="code"
-      >Disabled</vwc-button
-    >
-  </div>
+//   <h3>Filled</h3>
+//   <div class="container">
+//     <vwc-button layout="filled">Normal</vwc-button>
+//     <vwc-button layout="filled" connotation="cta" icon="info"
+//       >Call to action</vwc-button
+//     >
+//     <vwc-button layout="filled" connotation="success" icon="code"
+//       >Success</vwc-button
+//     >
+//     <vwc-button layout="filled" connotation="error" icon="code"
+//       >Error</vwc-button
+//     >
+//     <vwc-button layout="filled" connotation="warning" icon="info"
+//       >Warning</vwc-button
+//     >
+//     <vwc-button layout="filled" connotation="warning" disabled icon="code"
+//       >Disabled</vwc-button
+//     >
+//   </div>
 
-  <h3>Pill shape</h3>
-  <div class="container">
-    <vwc-button layout="outlined" shape="pill" icon="code">Outlined</vwc-button>
-    <vwc-button layout="outlined" disabled shape="pill">Disabled</vwc-button>
-    <vwc-button layout="filled" shape="pill" icon="info">Unelevated</vwc-button>
-    <vwc-button layout="filled" disabled shape="pill">Disabled</vwc-button>
-  </div>
-`;
+//   <h3>Pill shape</h3>
+//   <div class="container">
+//     <vwc-button layout="outlined" shape="pill" icon="code">Outlined</vwc-button>
+//     <vwc-button layout="outlined" disabled shape="pill">Disabled</vwc-button>
+//     <vwc-button layout="filled" shape="pill" icon="info">Unelevated</vwc-button>
+//     <vwc-button layout="filled" disabled shape="pill">Disabled</vwc-button>
+//   </div>
+// `;
