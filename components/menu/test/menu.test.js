70c6ef0c8f39bae88b558136f871896c14eb42ea
yonatankra <yonatan.kra@vonage.com>
2020-07-30 10:40:50 +0300

    Linting a bit

--- components/menu/test/menu.test.js
+++ components/menu/test/menu.test.js
@@ -14,6 +14,5 @@ describe('test vwc-menu', () => {
 		await waitNextTask();
 		const actualElement = docFragContainer[0];
 		expect(actualElement.shadowRoot.innerHTML).to.equalSnapshot();
-
 	});
 });
