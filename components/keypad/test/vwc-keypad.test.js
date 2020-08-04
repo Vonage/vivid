52e3f468983551d863250a8ac2bcc49ff009f45c
Dwane <hey@dwane.io>
2020-08-04 03:29:11 -0400

    fix(vwc-keypad): keypad should maintain a cursor/caret position

--- components/keypad/test/vwc-keypad.test.js
+++ components/keypad/test/vwc-keypad.test.js
@@ -128,6 +128,38 @@ describe('VwcKeypad', () => {
     expect(el.cancelText).to.equal('Stop');
   });
 
+	it('maintains cursor position', async () => {
+		const el = await fixture(html`
+      <vwc-keypad></vwc-keypad>
+    `);
+
+		const digit1 = el.shadowRoot.getElementById('1-digit');
+		const digit2 = el.shadowRoot.getElementById('2-digit');
+		const digit3 = el.shadowRoot.getElementById('3-digit');
+		const digit4 = el.shadowRoot.getElementById('4-digit');
+		const digit5 = el.shadowRoot.getElementById('5-digit');
+		const digit6 = el.shadowRoot.getElementById('6-digit');
+		const digit7 = el.shadowRoot.getElementById('7-digit');
+		const digitsDisplay = el.shadowRoot.getElementById('digits-display');
+		digit1.click();
+		digit2.click();
+		digit3.click();
+		digit4.click();
+		await elementUpdated(digitsDisplay);
+		digitsDisplay.setSelectionRange(2,2);
+		el.displayBlur();
+		await elementUpdated(digitsDisplay);
+
+		expect(digitsDisplay.selectionStart).to.equal(2);
+		digit5.click();
+		digit6.click();
+		digit7.click();
+		await elementUpdated(digitsDisplay);
+
+		expect(el.digits).to.equal('1256734');
+	});
+
+
   it('passes the a11y audit', async () => {
     const el = await fixture(html`
       <vwc-keypad></vwc-keypad>
