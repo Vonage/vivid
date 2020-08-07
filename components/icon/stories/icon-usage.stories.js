4b79cdfcf9100a9a22433e34817a30e538397261
Tal Weinfeld <tal.weinfeld@vonage.com>
2020-08-07 16:17:56 +0300

    Removed redundant line

--- components/icon/stories/icon-usage.stories.js
+++ components/icon/stories/icon-usage.stories.js
@@ -5,8 +5,7 @@ import { html } from 'lit-element';
 export default {
 	title: 'Atoms|Icon',
 	component: 'vwc-icon',
-	decorators: [withA11y],
-	kind: "dfdf"
+	decorators: [withA11y]
 };
 
 export const usage = ()=> html`<strong>I <vwc-icon type="heart-full" style="vertical-align: middle; fill: red;"></vwc-icon> VIVID!</strong>`;
