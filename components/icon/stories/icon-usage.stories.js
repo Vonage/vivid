eace5276d1bc7cb3c354dda5402e53754369d90e
Tal Weinfeld <tal.weinfeld@vonage.com>
2020-08-07 16:12:15 +0300

    Fixed icon names Added dynamic list creation

---
+++ components/icon/stories/icon-usage.stories.js
@@ -0,0 +1,13 @@
+import '@vonage/vwc-icon';
+import { withA11y } from '@storybook/addon-a11y';
+import { html } from 'lit-element';
+
+export default {
+	title: 'Atoms|Icon',
+	component: 'vwc-icon',
+	decorators: [withA11y],
+	kind: "dfdf"
+};
+
+export const usage = ()=> html`<strong>I <vwc-icon type="heart-full" style="vertical-align: middle; fill: red;"></vwc-icon> VIVID!</strong>`;
+usage.story = { kind: "aaa" };
