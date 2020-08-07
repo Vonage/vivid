3f174c53eb986984b342bcb1f525eb992016ab72
Tal Weinfeld <tal.weinfeld@vonage.com>
2020-08-07 16:16:28 +0300

    Removed redundant line

--- components/icon/stories/icon-usage.stories.js
+++ components/icon/stories/icon-usage.stories.js
@@ -10,4 +10,3 @@ export default {
 };
 
 export const usage = ()=> html`<strong>I <vwc-icon type="heart-full" style="vertical-align: middle; fill: red;"></vwc-icon> VIVID!</strong>`;
-usage.story = { kind: "aaa" };
