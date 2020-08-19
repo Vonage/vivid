8ba43ed5b0c7a7af45659ca188415b27339f9305
yonatankra <yonatan.kra@vonage.com>
2020-08-19 16:19:08 +0300

    More robust cleanup mechanism

--- components/button/test/button.test.js
+++ components/button/test/button.test.js
@@ -5,10 +5,12 @@ import { chaiDomDiff } from '@open-wc/semantic-dom-diff';
 chai.use(chaiDomDiff);
 
 describe('test vwc-button', () => {
-	let addedElement;
+	let addedElements = [];
 
 	afterEach(() => {
-		addedElement ? addedElement.remove() : '';
+		while (addedElements.length) {
+			addedElements.pop().remove();
+		}
 	});
 
 	it('vwc-button is defined as a custom element', async () => {
@@ -16,8 +18,8 @@ describe('test vwc-button', () => {
 	});
 
 	it('vwc-button has internal contents', async () => {
-		const internalElements = textToDomToParent('<vwc-button id="button-a">Button Text</vwc-button>');
-		const actualElement = addedElement = internalElements[0];
+		addedElements = textToDomToParent('<vwc-button id="button-a">Button Text</vwc-button>');
+		const actualElement = addedElements[0];
 		await waitNextTask();
 		expect(actualElement.shadowRoot.innerHTML).to.equalSnapshot();
 	});
@@ -30,8 +32,8 @@ describe('test vwc-button', () => {
 
 		it(`should submit form when inside a form`, async function() {
 			let submitted = false;
-			const internalElements = textToDomToParent('<form onsubmit="return false" name="testForm" id="testForm"><vwc-button id="button-a">Button Text</vwc-button></form>');
-			const formElement = addedElement = internalElements[0];
+			addedElements = textToDomToParent('<form onsubmit="return false" name="testForm" id="testForm"><vwc-button id="button-a">Button Text</vwc-button></form>');
+			const formElement = addedElements[0];
 			const actualElement = formElement.firstChild;
 			await waitNextTask();
 			await waitNextTask();
@@ -44,8 +46,8 @@ describe('test vwc-button', () => {
 
 		it(`should submit form when of type submit`, async function() {
 			let submitted = false;
-			const internalElements = textToDomToParent('<form onsubmit="return false" name="testForm" id="testForm"><vwc-button id="button-a" type="submit">Button Text</vwc-button></form>');
-			const formElement = addedElement = internalElements[0];
+			addedElements = textToDomToParent('<form onsubmit="return false" name="testForm" id="testForm"><vwc-button id="button-a" type="submit">Button Text</vwc-button></form>');
+			const formElement = addedElements[0];
 			const actualElement = formElement.firstChild;
 			await waitNextTask();
 			await waitNextTask();
@@ -59,8 +61,8 @@ describe('test vwc-button', () => {
 		it(`should reset form when of type reset`, async function() {
 			let submitted = false;
 			let reset = false;
-			const internalElements = textToDomToParent('<form onsubmit="return false" name="testForm" id="testForm"><vwc-button id="button-a" type="reset">Button Text</vwc-button></form>');
-			const formElement = addedElement = internalElements[0];
+			addedElements = textToDomToParent('<form onsubmit="return false" name="testForm" id="testForm"><vwc-button id="button-a" type="reset">Button Text</vwc-button></form>');
+			const formElement = addedElements[0];
 			const actualElement = formElement.firstChild;
 			await waitNextTask();
 			await waitNextTask();
