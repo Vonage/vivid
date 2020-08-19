43c470ca4bf7cd382138a72a54104dcb723a2ed4
yonatankra <yonatan.kra@vonage.com>
2020-08-19 15:56:51 +0300

    Refactored test to work with snapshot and cleanup

--- components/button/test/button.test.js
+++ components/button/test/button.test.js
@@ -1,20 +1,24 @@
 import '../vwc-button.js';
-import { textToDocumentFragment, waitNextTask } from '../../../utils/js/test-helpers.js';
+import { waitNextTask, textToDomToParent } from '../../../utils/js/test-helpers.js';
+import { chaiDomDiff } from '@open-wc/semantic-dom-diff';
+
+chai.use(chaiDomDiff);
 
 describe('test vwc-button', () => {
+	let addedElement;
+
+	afterEach(() => {
+		addedElement ? addedElement.remove() : '';
+	});
+
 	it('vwc-button is defined as a custom element', async () => {
 		assert.exists(customElements.get('vwc-button', 'vwc-button element is not defined'));
 	});
 
 	it('vwc-button has internal contents', async () => {
-		await customElements.whenDefined('vwc-button');
-		const docFragContainer = textToDocumentFragment('<vwc-button id="button-a">Button Text</vwc-button>');
-		const actualElement = docFragContainer.firstElementChild;
-		document.body.appendChild(docFragContainer);
+		const internalElements = textToDomToParent('<vwc-button id="button-a">Button Text</vwc-button>');
+		const actualElement = addedElement = internalElements[0];
 		await waitNextTask();
-		assert.equal(document.querySelector('#button-a'), actualElement);
-		assert.exists(actualElement.shadowRoot);
-		assert.equal(actualElement.shadowRoot.childElementCount, 1);
-		assert.equal(actualElement.shadowRoot.querySelectorAll('*').length, 7);
+		expect(actualElement.shadowRoot.innerHTML).to.equalSnapshot();
 	});
 });
