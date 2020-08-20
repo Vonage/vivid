c6c0c11a57f15219fa56e22595ad06646f6f486f
yonatankra <yonatan.kra@vonage.com>
2020-08-20 15:19:05 +0300

    Clean DOM after each test

--- components/textfield/test/textfield.test.js
+++ components/textfield/test/textfield.test.js
@@ -7,14 +7,22 @@ chai.use(chaiDomDiff);
 const VWC_TEXTFIELD = 'vwc-textfield';
 
 describe.only('vwc-textfield', () => {
+	let addedElements = [];
+
+	afterEach(() => {
+		while (addedElements.length) {
+			addedElements.pop().remove();
+		}
+	});
+
 	it('should be defined as a custom element', async () => {
 		expect(Boolean(customElements.get(VWC_TEXTFIELD))).to.equal(true);
 	});
 
 	it('should have internal contents', async () => {
 		await customElements.whenDefined('vwc-textfield');
-		const docFragContainer = textToDomToParent('<vwc-textfield id="textfield-a"></vwc-textfield>');
-		const actualElement = docFragContainer[0];
+		addedElements = textToDomToParent('<vwc-textfield id="textfield-a"></vwc-textfield>');
+		const actualElement = addedElements[0];
 		await waitNextTask();
 		expect(actualElement.shadowRoot.innerHTML).to.equalSnapshot();
 	});
