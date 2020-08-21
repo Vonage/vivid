767fdff0fc29edf26b86519a8b515450aa90dd60
Tal Weinfeld <tal.weinfeld@vonage.com>
2020-08-21 18:04:20 +0300

    feat(announcer): revised release message format

--- scripts/release_announcer/index.js
+++ scripts/release_announcer/index.js
@@ -1,7 +1,6 @@
 const
     fs = require('fs'),
     fp = require('lodash/fp'),
-    path = require('path'),
     split = require('split'),
     kefir = require('kefir'),
     minimist = require('minimist'),
@@ -12,26 +11,26 @@ const
 const { file:fileName = "--", slackHookUrl } = minimist(process.argv.slice(2));
 
 const
-    slackDispatcher = slackDispatcherFactory({ hook_url: slackHookUrl }),
-    createLineStream = function(rawStream){
-        return kefir.stream(({ emit, error, end })=> {
-            let stream = pipeline(rawStream, split(), (err)=> end(err && error(err)));
-            stream.on('data', emit);
-            return ()=> stream.off('data', emit);
-        });
-    };
+	slackDispatcher = slackDispatcherFactory({ hook_url: slackHookUrl }),
+	createLineStream = function(rawStream){
+		return kefir.stream(({ emit, error, end })=> {
+			let stream = pipeline(rawStream, split(), (err)=> end(err && error(err)));
+			stream.on('data', emit);
+			return ()=> stream.off('data', emit);
+		});
+	};
 
 createLineStream(fileName === "--" ? process.stdin : fs.createReadStream(fileName))
-    .thru(gitLogAnalyzerFactory())
-    .filter(fp.pipe(fp.get('version'), fp.negate(fp.isUndefined)))
-    .take(1)
-    .flatMap(({ version, lines: log_lines })=>
-        kefir
-            .fromPromise(slackDispatcher(slackReleaseTemplate({ version, log_lines })))
-            .map(()=> `Message successfully sent to Slack`)
-    )
-    .onValue(console.log)
-    .onError((err)=> {
-    	console.log('Failed to send message to Slack');
-    	process.exit(1);
-		});
+	.thru(gitLogAnalyzerFactory())
+	.filter(fp.pipe(fp.get('version'), fp.negate(fp.isUndefined)))
+	.take(1)
+	.flatMap(({ version, log_lines })=>
+		kefir
+			.fromPromise(slackDispatcher(slackReleaseTemplate({ version, log_lines })))
+			.map(()=> `Message successfully sent to Slack`)
+	)
+	.onValue(console.log)
+	.onError(()=> {
+		console.log('Failed to send message to Slack');
+		process.exit(1);
+	});
