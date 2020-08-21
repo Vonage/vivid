6cf4bc45295ba3af8f1c9cce9be604b6b8107140
Yuri Guller <gullerya@gmail.com>
2020-08-21 10:47:36 +0300

    improving error stacktrace for readbility of test failures and adding tests for input elements

--- components/textfield/test/textfield.test.js
+++ components/textfield/test/textfield.test.js
@@ -1,20 +1,54 @@
 import '../vwc-textfield.js';
-import { textToDocumentFragment, waitNextTask } from '../../../utils/js/test-helpers.js';
+import { textToDomToParent, waitNextTask, assertComputedStyle } from '../../../utils/js/test-helpers.js';
+import { chaiDomDiff } from '@open-wc/semantic-dom-diff';
+
+chai.use(chaiDomDiff);
+
+const VWC_TEXTFIELD = 'vwc-textfield';
 
 describe('textfield', () => {
 	it('should be defined as a custom element', async () => {
-		assert.exists(customElements.get('vwc-textfield', 'vwc-textfield element is not defined'));
+		assert.exists(customElements.get(VWC_TEXTFIELD, 'vwc-textfield element is not defined'));
 	});
 
 	it('should have internal contents', async () => {
-		await customElements.whenDefined('vwc-textfield');
-		const docFragContainer = textToDocumentFragment('<vwc-textfield id="textfield-a"></vwc-textfield>');
-		const actualElement = docFragContainer.firstElementChild;
-		document.body.appendChild(docFragContainer);
+		const addedElements = textToDomToParent(`<${VWC_TEXTFIELD}></${VWC_TEXTFIELD}>`);
+		const actualElement = addedElements[0];
 		await waitNextTask();
-		assert.equal(document.querySelector('#textfield-a'), actualElement);
-		assert.exists(actualElement.shadowRoot);
-		assert.equal(actualElement.shadowRoot.childElementCount, 1);
-		assert.equal(actualElement.shadowRoot.querySelectorAll('*').length, 4);
+		expect(actualElement.shadowRoot.innerHTML).to.equalSnapshot();
+	});
+
+	describe('typography', () => {
+		it('should have set typography for a label', async () => {
+			const actualElements = textToDomToParent(`<${VWC_TEXTFIELD} outlined label="Vwc textarea"></${VWC_TEXTFIELD}>`);
+			await waitNextTask();
+			const labelElement = actualElements[0].shadowRoot.querySelector('.mdc-notched-outline').querySelector('#label');
+			expect(labelElement).to.exist;
+			assertComputedStyle(labelElement, {
+				fontFamily: 'SpeziaWebVariable',
+				fontSize: '14.2222px',
+				fontWeight: '400',
+				fontStretch: '50%',
+				lineHeight: '18.4px',
+				letterSpacing: '0.133333px',
+				textTransform: 'none'
+			});
+		});
+
+		it('should have set typography for an input', async () => {
+			const actualElements = textToDomToParent(`<${VWC_TEXTFIELD} outlined disabled label="Vwc textarea"></${VWC_TEXTFIELD}>`);
+			await waitNextTask();
+			const inputElement = actualElements[0].shadowRoot.querySelector('.mdc-text-field__input');
+			expect(inputElement).to.exist;
+			assertComputedStyle(inputElement, {
+				fontFamily: 'SpeziaWebVariable',
+				fontSize: '14.2222px',
+				fontWeight: '400',
+				fontStretch: '50%',
+				lineHeight: 'normal',
+				letterSpacing: '0.133333px',
+				textTransform: 'none'
+			});
+		});
 	});
 });
