fe98f06c72f546a04888060308295efbe9e6f6fa
Yuri Guller <gullerya@gmail.com>
2020-08-23 16:47:23 +0300

    adding the typography as dev deps

--- components/frame/stories/frame-basic.stories.js
+++ components/frame/stories/frame-basic.stories.js
@@ -3,7 +3,7 @@ import { withA11y } from '@storybook/addon-a11y';
 import { html } from 'lit-element';
 
 export default {
-	title: 'Cells|Frame',
+	title: 'Cells/Frame',
 	component: 'vwc-frame',
 	decorators: [withA11y]
 }
