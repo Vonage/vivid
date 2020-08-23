3f72ff937f4da7c5a6bc9ad2de722cac96931823
yonatankra <yonatan.kra@vonage.com>
2020-08-23 10:31:30 +0300

    First form association test

--- components/textfield/test/textfield.test.js
+++ components/textfield/test/textfield.test.js
@@ -26,4 +26,25 @@ describe.only('vwc-textfield', () => {
 		await waitNextTask();
 		expect(actualElement.shadowRoot.innerHTML).to.equalSnapshot();
 	});
+
+	describe(`form association`, function() {
+
+		it(`should attach to closest form`, function() {
+			const fieldValue = Math.random().toString();
+			const fieldName = 'test-field';
+			const addedElements = textToDomToParent(`<form onsubmit="return false" name="testForm" id="testForm"><${VWC_TEXTFIELD} name="${fieldName}" value="${fieldValue}">Button Text</${VWC_TEXTFIELD}></form>`);
+			const formElement = addedElements[0];
+			const actualElement = formElement.firstChild;
+
+			formElement.addEventListener('submit', () => {
+				const formData = new FormData(formElement);
+				for (let pair of formData.entries()) {
+					expect(pair[0]).to.equal(fieldName);
+					expect(pair[1]).to.equal(fieldValue);
+				}
+			});
+
+			formElement.requestSubmit();
+		});
+	});
 });
