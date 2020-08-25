99c9f57ad7baa7a85bd6eec18821f977902001d5
Yuri Guller <gullerya@gmail.com>
2020-08-25 15:00:17 +0300

    issue #240: added a global styling to the context init (typography only as of now)

--- common/context/src/vvd-context.ts
+++ common/context/src/vvd-context.ts
@@ -1,9 +1,20 @@
 import { init as initCore } from '@vonage/vvd-core';
+import { style as globalStyle } from './vvd-context.css';
 
 /**
  * Vivid context initialiser
+ * this API is a 'customer facing' one - meant to be used by a consuming application to initialise the Vivid context, services and resource for the application as a whole
+ *
  * @param services map of services to initialise; keys are a service names, values are an options to pass to those services upon initialisation
  */
 export default async function init(services: Record<string, unknown>): Promise<void> {
-	return initCore(services);
+	injectGlobalStyle();
+	await initCore(services);
+}
+
+function injectGlobalStyle() {
+	const ds = document.createElement('style');
+	ds.type = 'text/css';
+	ds.innerHTML = globalStyle.cssText;
+	document.head.appendChild(ds);
 }
