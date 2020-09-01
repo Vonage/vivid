703bdb828cb041b4fff14bf74d73a2dccb150abd
Tal Weinfeld <tal.weinfeld@vonage.com>
2020-09-01 13:09:20 +0300

    fix(storybook): fixed icons story following a destructive change in other component(s)

--- components/icon/stories/icon.story.css.js
+++ components/icon/stories/icon.story.css.js
@@ -2,15 +2,13 @@ const css = `.container {
 	display: flex;
 	flex-wrap: wrap;
 	justify-content: flex-start;
-	margin: 1rem;
+	margin: 1rem
 }
 
 .container > figure {
+	margin: 8px;
 	box-shadow: 0px 0px 3px 1px #12131411;
-	margin: 0.5rem;
-	width: 5rem;
-	height: 5rem;
-	padding: 1rem 0 0 0;
+	padding: 14px 25px 26px 25px;
 	text-align: center;
 	box-sizing: border-box;
 	position: relative;
@@ -25,7 +23,7 @@ const css = `.container {
 }
 
 .container > figure > figcaption {
-	font-size: 0.2rem;
+	font-size: 9px;
 	position: absolute;
 	bottom: 0;
 	left: 0;
@@ -37,7 +35,8 @@ const css = `.container {
 	font-weight: bold;
 	color: #121314;
 	background-color: #12131422;
-	padding: 0.2rem;
+	line-height: 18px;
+	padding: 0;
 }`;
 
 export default css;
