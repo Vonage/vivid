39671abf2cf46bd1b2d1e8b6687c4fcaa36c7bcf
yinonov <yinon@hotmail.com>
2020-09-02 16:07:02 +0300

    refine scheme select buttons

--- components/scheme-select/src/vwc-scheme-select.ts
+++ components/scheme-select/src/vwc-scheme-select.ts
@@ -3,38 +3,41 @@ import { SchemeOption } from '@vonage/vvd-scheme/vvd-scheme.js';
 import { SCHEME_SELECT_EVENT_TYPE } from '@vonage/vvd-scheme/scheme-change-listener.js';
 
 const createSelectionCustomEvent = (scheme: SchemeOption) =>
-	new CustomEvent(SCHEME_SELECT_EVENT_TYPE, {
-		detail: { scheme },
-		bubbles: true, // needed for bubbling up the shadow DOM
-		composed: true, // needed for bubbling up the shadow DOM
-	});
+  new CustomEvent(SCHEME_SELECT_EVENT_TYPE, {
+    detail: { scheme },
+    bubbles: true, // needed for bubbling up the shadow DOM
+    composed: true, // needed for bubbling up the shadow DOM
+  });
 
 declare global {
-	interface HTMLElementTagNameMap {
-		'vwc-scheme-select': SchemeSelect;
-	}
+  interface HTMLElementTagNameMap {
+    'vwc-scheme-select': SchemeSelect;
+  }
 }
 
 @customElement('vwc-scheme-select')
 export class SchemeSelect extends LitElement {
-	schemes: SchemeOption[] = ['syncWithOSSettings', 'light', 'dark'];
+  schemes: SchemeOption[] = ['syncWithOSSettings', 'light', 'dark'];
 
-	render(): TemplateResult {
-		return html`
-			${this.schemes.map(
-				(scheme) =>
-					html`
-						<vwc-button
-							unelevated
-							@click="${this.dispatchEvent.bind(
-								this,
-								createSelectionCustomEvent(scheme)
-							)}"
-						>
-							${scheme}
-						</vwc-button>
-					`
-			)}
-		`;
-	}
+  render(): TemplateResult {
+    return html`
+      ${this.schemes.map(
+        (scheme) =>
+          html`
+            <vwc-button
+              dense
+              unelevated
+              layout="filled"
+              connotation="cta"
+              @click="${this.dispatchEvent.bind(
+                this,
+                createSelectionCustomEvent(scheme)
+              )}"
+            >
+              ${scheme}
+            </vwc-button>
+          `
+      )}
+    `;
+  }
 }
