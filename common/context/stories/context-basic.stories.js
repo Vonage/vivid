1597f72ceafd8b363218862b230952a03df2e5bd
yinonov <yinon@hotmail.com>
2020-09-04 18:23:50 +0300

    use-semantic-html

--- common/context/stories/context-basic.stories.js
+++ common/context/stories/context-basic.stories.js
@@ -39,6 +39,70 @@ export const basic = () => html`
       Call Control Object). This scenario is generally used for forwarding
       calls.
     </p>
+
+    <h2>Checkboxes and radios</h2>
+    <div>
+      <label><input type="checkbox" />Sign me up!</label>
+    </div>
+    <div>
+      <label><input name="a" type="radio" />Mango</label>
+      <label><input name="a" type="radio" />Peach</label>
+    </div>
+    <h2>Actions</h2>
+    <div>
+      <button>Click to do something!</button>
+    </div>
+    <h2>Editable text</h2>
+    <div>
+      <input placeholder="Single line" />
+    </div>
+    <div>
+      <textarea placeholder="Multi line"></textarea>
+    </div>
+    <div>
+      <div contenteditable>Contenteditable</div>
+    </div>
+    <h2>Selects</h2>
+    <label
+      >Select one from drop-down:<br />
+      <select>
+        <option>Apple</option>
+        <option>Orange</option>
+        <option>Banana</option>
+      </select>
+    </label>
+    <label
+      >Select multiple:<br />
+      <select multiple="">
+        <option>Apple</option>
+        <option>Orange</option>
+        <option>Banana</option>
+      </select>
+    </label>
+    <h2>Navigation</h2>
+    <div>
+      <a href="google.com">Google.com</a>
+    </div>
+    <h2>Other</h2>
+    <label
+      >Range from 0 to 10:<br />
+      <input type="range" min="0" max="10" value="0" />
+    </label>
+    <div>
+      <input type="date" value="2008-09-01" />
+    </div>
+    <div>
+      <input type="time" value="00:00:00" />
+    </div>
+    <div>
+      <input type="number" placeholder="Number" />
+    </div>
+    <div>
+      <input type="color" title="Color" value="#00ff00" />
+    </div>
+    <div>
+      <input type="file" title="File" />
+    </div>
   </main>
 `;
 
