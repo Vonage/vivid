d2647caa66004c68df82a40a3a08a244ca2bf691
Yuri Guller <gullerya@gmail.com>
2020-09-13 23:09:18 +0300

    fixing the issues to pass CI checks

--- components/badge/test/badge.test.js
+++ components/badge/test/badge.test.js
@@ -6,11 +6,13 @@ chai.use(chaiDomDiff);
 
 const VWC_BADGE = 'vwc-badge';
 
-describe.only('badge', () => {
+describe('badge', () => {
 	let addedElements = [];
+
 	afterEach(() => {
-		// addedElements.forEach(elm => elm.remove());
+		addedElements.forEach(elm => elm.remove());
 	});
+
 	it('vwc-badge is defined as a custom element', async () => {
 		assert.exists(customElements.get(VWC_BADGE, 'vwc-badge element is not defined'));
 	});
@@ -43,14 +45,13 @@ describe.only('badge', () => {
 			expect(propertyChangesAffectsAttribute, "Property change did not apply to attribute").to.equal(true);
 			expect(attributeChangesAffectsProperty, "Attribute change did not apply to property").to.equal(true);
 		});
-
 	});
 
 	describe('typography', function () {
 		it(`should have set badge (text, default) typography correct`, async function () {
-			const actualElements = textToDomToParent(`<${VWC_BADGE}>Badge Text</${VWC_BADGE}>`);
+			addedElements = textToDomToParent(`<${VWC_BADGE}>Badge Text</${VWC_BADGE}>`);
 			await waitNextTask();
-			const badge = actualElements[0];
+			const badge = addedElements[0];
 			expect(badge).to.exist;
 			// @YonatanKra can we fetch this following object from json? It can be rendered for both scss and json usage from same source. would be a better practice for consistency
 			const expectedStyles = {
@@ -58,16 +59,11 @@ describe.only('badge', () => {
 				fontSize: '12.642px',
 				fontWeight: '400',
 				fontStretch: '50%',
-				lineHeight: 'calc(1ex / 0.32);', // TODO: [VIV-166] line-height value contains runtime calculated value and depend on x-height of font (which may vary with the existence of font-size-adjust property). an alternative to existing test should be defined.
-				letterSpacing: 'initial',
+				// lineHeight: 'calc(1ex / 0.32);', // TODO: [VIV-166] line-height value contains runtime calculated value and depend on x-height of font (which may vary with the existence of font-size-adjust property). an alternative to existing test should be defined.
+				letterSpacing: 'normal',
 				textTransform: 'none'
 			};
 			assertComputedStyle(badge, expectedStyles);
 		});
-
 	});
-
-
 });
-
-
