b41fa935a6387be0ef2343b287504eda025c5a40
Yuri Guller <gullerya@gmail.com>
2020-09-16 17:49:32 +0300

    improved the test of the core

--- common/core/test/core.test.js
+++ common/core/test/core.test.js
@@ -5,5 +5,13 @@ describe('vvd-core service', () => {
 		assert.isDefined(vvdCore, 'core service is defined');
 		assert.isObject(vvdCore, 'core service is a defaultly exported object');
 		assert.isFunction(vvdCore.set, 'core service has "set" API method');
+		assert.isDefined(
+			vvdCore.coreReady,
+			'core service has "coreReady" object (Promise)'
+		);
+		assert.isFunction(
+			vvdCore.coreReady.then,
+			'core service has "coreReady" object - ensure it is Promise'
+		);
 	});
 });
