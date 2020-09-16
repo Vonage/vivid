d3a43b2b36361ebd1bb1213ffbd9dabfd11bbf21
Yuri Guller <gullerya@gmail.com>
2020-09-16 16:21:32 +0300

    fixed the scheme logic flow and added tests to it

--- common/scheme/src/vvd-scheme.ts
+++ common/scheme/src/vvd-scheme.ts
@@ -1,7 +1,5 @@
-import { CSSResult } from 'lit-element';
-import { pipe } from 'ramda';
 import { onSchemeChange } from './scheme-change-listener';
-import { updateTagStyleCssText } from './vvd-scheme-style-tag-handler';
+import { applySchemeCSS } from './vvd-scheme-style-tag-handler';
 
 import {
 	pcs,
@@ -12,49 +10,23 @@ import {
 export type PredefinedScheme = 'light' | 'dark';
 export type SchemeOption = 'syncWithOSSettings' | PredefinedScheme;
 
-type ModuleType =
-	| typeof import('./scheme.dark.css')
-	| typeof import('./scheme.light.css'); // This is the import type!
-
-const getSchemeCssText = pipe(getSchemeModule, getStyleSheet, getCssText);
-
 let _selectedScheme: PredefinedScheme;
-export const getSelectedScheme = (): PredefinedScheme => _selectedScheme;
+function getSelectedScheme(): PredefinedScheme {
+	return _selectedScheme;
+}
 
 let _selectedSchemeOption: SchemeOption;
-export const getSelectedSchemeOption = (): SchemeOption =>
-	_selectedSchemeOption;
+function getSelectedSchemeOption(): SchemeOption {
+	return _selectedSchemeOption;
+}
 
-function schemeDefault(): SchemeOption {
-	// if no scheme chosen try 'prefers-color-scheme' and if not supported just return 'light
+function getDefaultSchemeOption(): SchemeOption {
+	// if no scheme chosen try 'prefers-color-scheme' and if not supported just return 'light'
 	return prefersColorSchemeSupported() ? 'syncWithOSSettings' : 'light';
 }
 
-function getSchemeModule(schemeOption: SchemeOption): Promise<ModuleType> {
-	switch (schemeOption) {
-		case 'dark':
-			return import('./scheme.dark.css');
-		case 'light':
-		default:
-			return import('./scheme.light.css');
-	}
-}
-
-async function getStyleSheet(ModulePromise: Promise<ModuleType>) {
-	return (await ModulePromise).style;
-}
-
-async function getCssText(
-	resultPromise: Promise<CSSResult>
-): Promise<CSSResult['cssText']> {
-	const { cssText } = await resultPromise;
-	return cssText;
-}
-
 async function syncWithOSSettings() {
-	updateTagStyleCssText(
-		await getSchemeCssText(getPreferedColorScheme() as SchemeOption)
-	);
+	applySchemeCSS(getPreferedColorScheme() as PredefinedScheme);
 }
 
 function init(): void {
@@ -63,39 +35,62 @@ function init(): void {
 	});
 }
 
-async function set(scheme: SchemeOption = schemeDefault()) {
-	console.log(`Vivid scheme requested to change to '${scheme}'...`);
+let setPromise: Promise<Record<string, unknown>> | null = null;
+async function set(
+	schemeOption: SchemeOption | null = null
+): Promise<Record<string, unknown>> {
+	console.log(`Vivid scheme requested to change to '${schemeOption}'...`);
 
-	if (scheme !== _selectedSchemeOption) {
-		_selectedSchemeOption = scheme;
-		let nextScheme: PredefinedScheme;
+	const effectiveOption = schemeOption
+		? schemeOption
+		: _selectedSchemeOption
+		? _selectedSchemeOption
+		: getDefaultSchemeOption();
 
-		if (scheme == 'syncWithOSSettings') {
-			pcs.addEventListener('change', syncWithOSSettings);
-			nextScheme = getPreferedColorScheme() as PredefinedScheme;
-		} else {
-			pcs.removeEventListener('change', syncWithOSSettings);
-			nextScheme = scheme;
-		}
-		if (_selectedScheme === nextScheme) {
-			return;
-		}
-		_selectedScheme = nextScheme;
-		updateTagStyleCssText(await getSchemeCssText(nextScheme));
-	} else {
-		console.log('new scheme equal to current, doing nothing');
+	console.log(`... which resolved effectively to '${effectiveOption}'...`);
+	if (effectiveOption === _selectedSchemeOption && setPromise) {
+		console.log('... new scheme option equal to current, done');
+		return setPromise;
 	}
 
-	console.log('... scheme change done');
+	_selectedSchemeOption = effectiveOption;
+	let effectiveNewScheme: PredefinedScheme;
+
+	if (_selectedSchemeOption === 'syncWithOSSettings') {
+		pcs.addEventListener('change', syncWithOSSettings);
+		effectiveNewScheme = getPreferedColorScheme() as PredefinedScheme;
+	} else {
+		pcs.removeEventListener('change', syncWithOSSettings);
+		effectiveNewScheme = _selectedSchemeOption;
+	}
+
+	let tmpPromise;
+	if (effectiveNewScheme !== _selectedScheme) {
+		_selectedScheme = effectiveNewScheme;
+		tmpPromise = applySchemeCSS(effectiveNewScheme);
+	} else {
+		tmpPromise = Promise.resolve();
+	}
+
+	setPromise = tmpPromise.then(() => {
+		console.log('... scheme changed');
+		return {
+			option: _selectedSchemeOption,
+			scheme: _selectedScheme,
+		};
+	});
+
+	return setPromise;
 }
 
 export default Object.freeze({
 	set,
+	getSelectedScheme,
+	getSelectedSchemeOption,
 });
 
 init();
 
 //TODO add the following tests:
-//!scheme init with/without arguments
 //!scheme change event
 //!add / remove Listener when toggling 'syncWithOSSettings' selected option
--- common/scheme/test/scheme.test.js
+++ common/scheme/test/scheme.test.js
@@ -1,45 +1,94 @@
-import fonts from '../vvd-fonts.js';
+import { randomAlpha } from '../../../test/test-helpers.js';
+import schemeService from '../vvd-scheme.js';
 
-describe('vvd-fonts service', () => {
-	it('should provide basic fonts API', async () => {
-		assert.isObject(fonts, 'imported "fonts" is object');
-		assert.isNotNull(fonts, 'imported "fonts" not null');
-		assert.isFrozen(fonts, 'imported "fonts" object should be frozen');
-		assert.isFunction(fonts.init, 'fonts has "init" method');
-	});
-
-	it('should affect the actual font', async () => {
-		//	create test element, to measure width of
-		const testElement = document.createElement('span');
-		testElement.textContent = 'www.iii.com';
-		testElement.style.fonSize = '16px';
-		testElement.style.fontStretch = '50%';
-		testElement.style.fontFamily = 'monospace';
-
-		//	first, append it as is, take the width (monospaced)
-		document.body.appendChild(testElement);
-		const monoWidth = testElement.offsetWidth;
-
-		//	second, set our font and then call init (to be sure, init might already ran)
-		testElement.style.fontFamily = 'var(--vvd-font-family-spezia), monospace';
-
-		//	second, init the fonts
-		await fonts.init();
-
-		//	third, measure the element again (now should have non-monospaced fonting)
-		const postWidth = testElement.offsetWidth;
-
-		assert.isTrue(
-			postWidth !== monoWidth,
-			'element width after should be other than before'
+describe('vvd-scheme service', () => {
+	it('should provide basic set scheme API', async () => {
+		assert.isObject(schemeService, 'imported "schemeService" is object');
+		assert.isNotNull(schemeService, 'imported "schemeService" not null');
+		assert.isFrozen(
+			schemeService,
+			'imported "schemeService" object should be frozen'
+		);
+		assert.isFunction(schemeService.set, 'schemeService has "set" method');
+		assert.isFunction(
+			schemeService.getSelectedScheme,
+			'schemeService has "getSelectedScheme" method'
+		);
+		assert.isFunction(
+			schemeService.getSelectedSchemeOption,
+			'schemeService has "getSelectedSchemeOption" method'
 		);
 	});
 
-	it('should provide the same Promise each new time after the initial run', async () => {
-		const r1 = await fonts.init();
-		const r2 = await fonts.init();
-		const r3 = await (await import('../vvd-fonts.js')).default.init();
-		expect(r2).equal(r1);
-		expect(r3).equal(r2);
+	it('should NOT auto init schemes upon import', async () => {
+		const r = randomAlpha();
+		const s = (await import(`../vvd-scheme.js?${r}`)).default;
+		const currentScheme = s.getSelectedScheme();
+		const currentSchemeOption = s.getSelectedSchemeOption();
+		assert.isUndefined(currentScheme);
+		assert.isUndefined(currentSchemeOption);
+	});
+
+	it('should be able to init to default when set to undefined', async () => {
+		const r = randomAlpha();
+		const s = (await import(`../vvd-scheme.js?${r}`)).default;
+		await s.set();
+		const currentScheme = s.getSelectedScheme();
+		const currentSchemeOption = s.getSelectedSchemeOption();
+		assert.equal(currentScheme, 'light');
+		assert.equal(currentSchemeOption, 'syncWithOSSettings');
+	});
+
+	it('should be able to init to default when set to null', async () => {
+		const r = randomAlpha();
+		const s = (await import(`../vvd-scheme.js?${r}`)).default;
+		await s.set(null);
+		const currentScheme = s.getSelectedScheme();
+		const currentSchemeOption = s.getSelectedSchemeOption();
+		assert.equal(currentScheme, 'light');
+		assert.equal(currentSchemeOption, 'syncWithOSSettings');
+	});
+
+	it('should be able to init to the given argument', async () => {
+		const r = randomAlpha();
+		const s = (await import(`../vvd-scheme.js?${r}`)).default;
+		await s.set('dark');
+		const currentScheme = s.getSelectedScheme();
+		const currentSchemeOption = s.getSelectedSchemeOption();
+		assert.equal(currentScheme, 'dark');
+		assert.equal(currentSchemeOption, 'dark');
+	});
+
+	it('should do nothing when set to the same argument', async () => {
+		const r = randomAlpha();
+		const s = (await import(`../vvd-scheme.js?${r}`)).default;
+		const r1 = await s.set('light');
+		assert.equal(r1.option, 'light');
+		assert.equal(r1.scheme, 'light');
+
+		const r2 = await s.set('light');
+		assert.equal(r2, r1);
+	});
+
+	it('should do nothing when set to undefined and already post-init', async () => {
+		const r = randomAlpha();
+		const s = (await import(`../vvd-scheme.js?${r}`)).default;
+		const r1 = await s.set('light');
+		assert.equal(r1.option, 'light');
+		assert.equal(r1.scheme, 'light');
+
+		const r2 = await s.set();
+		assert.equal(r2, r1);
+	});
+
+	it('should do nothing when set to null and already post-init', async () => {
+		const r = randomAlpha();
+		const s = (await import(`../vvd-scheme.js?${r}`)).default;
+		const r1 = await s.set('light');
+		assert.equal(r1.option, 'light');
+		assert.equal(r1.scheme, 'light');
+
+		const r2 = await s.set(null);
+		assert.equal(r2, r1);
 	});
 });
