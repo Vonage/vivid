6da7b22181350fede535072316cccdbec2d032b7
Yuri Guller <gullerya@gmail.com>
2020-09-23 21:44:39 +0300

    fixing typography browsers discrepancies in tests

--- test/test-helpers.js
+++ test/test-helpers.js
@@ -104,6 +104,14 @@ export function assertComputedStyle(element, expectedStyles) {
 				}
 				expectedValue = parseInt(expectedStyles[styleKey]);
 				break;
+			case 'letterSpacing':
+				expectedValue = parseFloat(expectedStyles[styleKey]).toFixed(3);
+				actualValue = parseFloat(computedStyle[styleKey]).toFixed(3);
+				break;
+			case 'lineHeight':
+				expectedValue = parseFloat(expectedStyles[styleKey]).toFixed(1);
+				actualValue = parseFloat(computedStyle[styleKey]).toFixed(1);
+				break;
 			default:
 				expectedValue = expectedStyles[styleKey];
 				actualValue = computedStyle[styleKey];
