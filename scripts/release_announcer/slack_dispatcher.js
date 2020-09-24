0fc03fa429e430aba8f919b5f4f1628d1945999d
yonatankra <yonatan.kra@vonage.com>
2020-09-24 16:10:39 +0300

    Accept conventional PR style

--- scripts/release_announcer/slack_dispatcher.js
+++ scripts/release_announcer/slack_dispatcher.js
@@ -29,7 +29,7 @@ exports.releaseTemplate = ({ version, log_lines: rawLogLines })=>{
 	const
 		validLog = rawLogLines.filter(({ comment })=> !/(branch)|(pull request)/i.test(comment)),
 		bugs = validLog.filter(({ comment })=> /^fix(e[ds])?:/i.test(comment)).map(formatLogLine(':beetle:')),
-		features = validLog.filter(({ comment })=> /feat(ures?)?:/i.test(comment)).map(formatLogLine(':star:'));
+		features = validLog.filter(({ comment })=> /^feat(ures?)?:/i.test(comment)).map(formatLogLine(':star:'));
 
 	return {
 		"blocks": [
