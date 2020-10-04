5bdf98776402a558454249ed92c66bfb7092e7d4
Yuri Guller <gullerya@gmail.com>
2020-10-04 17:23:00 +0300

    fixing (attempt) Sonar issues

--- components/keypad/test/vwc-keypad.test.js
+++ components/keypad/test/vwc-keypad.test.js
@@ -1,27 +1,34 @@
 import '../vwc-keypad.js';
-import { textToDomToParent, waitNextTask } from '../../../test/test-helpers.js';
+import {
+	textToDomToParent,
+	waitNextTask,
+	isolatedElementsCreation,
+} from '../../../test/test-helpers.js';
 import { chaiA11yAxe } from 'chai-a11y-axe';
 
 chai.use(chaiA11yAxe);
 
+const COMPONENT_NAME = 'vwc-keypad';
+
 describe('VwcKeypad', () => {
+	const addElement = isolatedElementsCreation();
+
 	it('has a default no-asterisk false, no-hash false, no-display false, actionText "Enter", cancelText "Cancel"', async () => {
-		const els = await textToDomToParent('<vwc-keypad></vwc-keypad>');
-		const el = els[0];
+		const el = addElement(
+			await textToDomToParent(`<${COMPONENT_NAME}></${COMPONENT_NAME}>`)
+		)[0];
 
 		expect(el.noAsterisk).to.equal(false);
 		expect(el.noHash).to.equal(false);
 		expect(el.noDisplay).to.equal(false);
 		expect(el.actionText).to.equal('Enter');
 		expect(el.cancelText).to.equal('Cancel');
-
-		el.remove();
 	});
 
 	it('keypad buttons clicks add digits to display', async () => {
-		const els = await textToDomToParent('<vwc-keypad></vwc-keypad>');
-		const el = els[0];
-
+		const el = addElement(
+			await textToDomToParent(`<${COMPONENT_NAME}></${COMPONENT_NAME}>`)
+		)[0];
 		const digit1 = el.shadowRoot.getElementById('1-digit');
 		const digit2 = el.shadowRoot.getElementById('2-digit');
 		const digit3 = el.shadowRoot.getElementById('3-digit');
@@ -48,13 +55,14 @@ describe('VwcKeypad', () => {
 		digitHash.click();
 
 		expect(el.digits).to.equal('1234567890*#');
-
-		el.remove();
 	});
 
 	it('no-asterisk attribute removes * button', async () => {
-		const els = await textToDomToParent('<vwc-keypad no-asterisk></vwc-keypad>');
-		const el = els[0];
+		const el = addElement(
+			await textToDomToParent(
+				`<${COMPONENT_NAME} no-asterisk></${COMPONENT_NAME}>`
+			)
+		)[0];
 		const asteriskButton = el.shadowRoot.querySelector('#asterisk-digit');
 
 		expect(el.noAsterisk).to.equal(true);
@@ -62,8 +70,9 @@ describe('VwcKeypad', () => {
 	});
 
 	it('no-hash attribute removes # button', async () => {
-		const els = await textToDomToParent('<vwc-keypad no-hash></vwc-keypad>');
-		const el = els[0];
+		const el = addElement(
+			await textToDomToParent(`<${COMPONENT_NAME} no-hash></${COMPONENT_NAME}>`)
+		)[0];
 		const hashButton = el.shadowRoot.querySelector('#hash-digit');
 
 		expect(el.noHash).to.equal(true);
@@ -71,8 +80,9 @@ describe('VwcKeypad', () => {
 	});
 
 	it('no-display attribute removes display input', async () => {
-		const els = await textToDomToParent('<vwc-keypad no-display></vwc-keypad>');
-		const el = els[0];
+		const el = addElement(
+			await textToDomToParent(`<${COMPONENT_NAME} no-display></${COMPONENT_NAME}>`)
+		)[0];
 		const digitsDisplay = el.shadowRoot.querySelector('#digits-display');
 
 		expect(el.noDisplay).to.equal(true);
@@ -80,17 +90,18 @@ describe('VwcKeypad', () => {
 	});
 
 	it('called createAction method sets actionStarted to true', async () => {
-		const els = await textToDomToParent('<vwc-keypad></vwc-keypad>');
-		const el = els[0];
+		const el = addElement(
+			await textToDomToParent(`<${COMPONENT_NAME}></${COMPONENT_NAME}>`)
+		)[0];
 
 		el.createAction();
 		expect(el.actionStarted).to.equal(true);
 	});
 
 	it('end action and clear digits on cancel button click', async () => {
-		const els = await textToDomToParent('<vwc-keypad></vwc-keypad>');
-		const el = els[0];
-
+		const el = addElement(
+			await textToDomToParent(`<${COMPONENT_NAME}></${COMPONENT_NAME}>`)
+		)[0];
 		const digit1 = el.shadowRoot.getElementById('1-digit');
 		const digit2 = el.shadowRoot.getElementById('2-digit');
 		const digit3 = el.shadowRoot.getElementById('3-digit');
@@ -111,25 +122,29 @@ describe('VwcKeypad', () => {
 	});
 
 	it('can change Action button text via attribute', async () => {
-		const els = await textToDomToParent(
-			'<vwc-keypad actionText="Start"></vwc-keypad>'
-		);
+		const el = addElement(
+			await textToDomToParent(
+				`<${COMPONENT_NAME} actionText="Start"></${COMPONENT_NAME}>`
+			)
+		)[0];
 
-		expect(els[0].actionText).to.equal('Start');
+		expect(el.actionText).to.equal('Start');
 	});
 
 	it('can change Cancel button text via attribute', async () => {
-		const els = await textToDomToParent(
-			'<vwc-keypad cancelText="Stop"></vwc-keypad>'
-		);
+		const el = addElement(
+			await textToDomToParent(
+				`<${COMPONENT_NAME} cancelText="Stop"></${COMPONENT_NAME}>`
+			)
+		)[0];
 
-		expect(els[0].cancelText).to.equal('Stop');
+		expect(el.cancelText).to.equal('Stop');
 	});
 
 	it('maintains cursor position', async () => {
-		const els = await textToDomToParent('<vwc-keypad></vwc-keypad>');
-		const el = els[0];
-
+		const el = addElement(
+			await textToDomToParent(`<${COMPONENT_NAME}></${COMPONENT_NAME}>`)
+		)[0];
 		const digit1 = el.shadowRoot.getElementById('1-digit');
 		const digit2 = el.shadowRoot.getElementById('2-digit');
 		const digit3 = el.shadowRoot.getElementById('3-digit');
@@ -157,9 +172,10 @@ describe('VwcKeypad', () => {
 	});
 
 	it('passes the a11y audit', async () => {
-		const els = await textToDomToParent('<vwc-keypad></vwc-keypad>');
+		const el = addElement(
+			await textToDomToParent(`<${COMPONENT_NAME}></${COMPONENT_NAME}>`)
+		)[0];
 
-		await waitNextTask();
-		expect(els[0]).shadowDom.to.be.accessible();
+		expect(el).shadowDom.to.be.accessible();
 	});
 });
