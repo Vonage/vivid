25ece6c5ed7ec7e9cb2338e1ddccb4d30c2c0406
yinonov <yinon@hotmail.com>
2020-10-07 10:34:19 +0300

    refactor(scheme): tag handler refactored
    tag handler dynamic imports changed back to switch case in order to
    prevent premature calls of schemes

--- common/scheme/src/vvd-scheme-style-tag-handler.ts
+++ common/scheme/src/vvd-scheme-style-tag-handler.ts
@@ -6,25 +6,30 @@ type ModuleType =
 	| typeof import('./scheme.dark.css')
 	| typeof import('./scheme.light.css');
 
-const importSchemeMap = new Map<PredefinedScheme, Promise<ModuleType>>([
-	['dark', import('./scheme.dark.css')],
-	['light', import('./scheme.light.css')],
-]);
+const getSchemeModule: (scheme: PredefinedScheme) => Promise<ModuleType> = (
+	scheme: PredefinedScheme
+) => {
+	switch (scheme) {
+		case 'dark':
+			return import('./scheme.dark.css');
+		case 'light':
+		default:
+			return import('./scheme.light.css');
+	}
+};
 
 const style = mountStyle();
 
 function mountStyle() {
-	const style = document.createElement('style');
-	style.innerHTML = preSchemeLoadingCssText;
-	document.head.appendChild(style);
-	return style;
+	const styleElement = document.createElement('style');
+	styleElement.innerHTML = preSchemeLoadingCssText;
+	document.head.appendChild(styleElement);
+	return styleElement;
 }
 
 export async function applySchemeCSS(scheme: PredefinedScheme): Promise<void> {
-	if (!importSchemeMap.has(scheme)) {
-		throw new Error('scheme not found');
-	}
-	const schemeModule = await importSchemeMap.get(scheme);
+	const schemeModule = await getSchemeModule(scheme);
+
 	const cssResult: CSSResult | undefined = schemeModule?.style;
 	if (cssResult) {
 		style.innerHTML = cssResult.cssText || '';
