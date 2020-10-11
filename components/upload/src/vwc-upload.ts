304783b446275e594a84e7b899b2a794272194be
Yuri Guller <gullerya@gmail.com>
2020-10-11 11:18:02 +0300

    upload main logic and bindings

--- components/upload/src/vwc-upload.ts
+++ components/upload/src/vwc-upload.ts
@@ -2,8 +2,8 @@ import '@vonage/vvd-core';
 import {
 	customElement,
 	html,
+	property,
 	LitElement,
-	query,
 	TemplateResult,
 } from 'lit-element';
 import { style } from './vwc-upload.css.js';
@@ -18,38 +18,98 @@ const INTERNAL_INPUT_SLOT_NAME = 'internal-input';
 
 @customElement('vwc-upload')
 export class VWCUpload extends LitElement {
-	static styles = [style];
-
 	#internalInput: HTMLInputElement = VWCUpload.createInternalInput();
 
-	@query('label') _label?: HTMLLabelElement;
+	static styles = [style];
+
+	@property({ type: String, reflect: true })
+	accept = '';
+
+	@property({ type: Boolean, reflect: true })
+	multiple = false;
+
+	@property({ type: String, reflect: true })
+	'aria-controls' = 'fileupload';
+
+	@property({ type: String, reflect: true })
+	buttonText = 'Upload';
+
+	@property({ type: String, reflect: true })
+	label = '';
 
 	connectedCallback(): void {
 		super.connectedCallback();
 		this.appendChild(this.#internalInput);
 	}
 
+	get files(): FileList | null {
+		return this.#internalInput.files;
+	}
+
 	triggerFileInput(): void {
 		this.#internalInput?.click();
 	}
 
+	protected updated(
+		changedProperties: Map<string | number | symbol, unknown>
+	): void {
+		super.update(changedProperties);
+		if (changedProperties.has('accept')) {
+			this.#internalInput.setAttribute('accept', this.accept);
+		}
+		if (changedProperties.has('multiple')) {
+			if (this.multiple) {
+				this.#internalInput.setAttribute('multiple', '');
+			} else {
+				this.#internalInput.removeAttribute('multiple');
+			}
+		}
+	}
+
 	protected render(): TemplateResult {
 		return html`
-			<slot name="${INTERNAL_INPUT_SLOT_NAME}"></slot>
-			${this.renderDragNDropSurface()}
-			<vwc-button
-				@click=${this.triggerFileInput}
-				layout="filled"
-				connotation="primary"
-				><slot></slot
-			></vwc-button>
+			<label>
+				${this.renderDragNDropSurface()} ${this.renderLabel()}
+				${this.renderButton()}
+				<slot name="${INTERNAL_INPUT_SLOT_NAME}"></slot>
+				${this.renderHelper()}
+			</label>
 		`;
 	}
 
 	private static createInternalInput() {
 		const result = document.createElement('input');
 		result.type = 'file';
+		// result.accept = this.accept;
+		// if (this.multiple) result.setAttribute('multiple', '');
 		return result;
+	}
+
+	private renderLabel(): TemplateResult {
+		let result;
+		if (this.label) {
+			result = html`<span class="vwc-upload-label">${this.label}</span>`;
+		} else {
+			result = html``;
+		}
+		return result;
+	}
+
+	private renderButton(): TemplateResult {
+		return html`
+			<vwc-button
+				class="vwc-upload-button"
+				@click=${this.triggerFileInput}
+				layout="filled"
+				connotation="primary"
+			>
+				<slot></slot>${this.buttonText}</vwc-button
+			>
+		`;
+	}
+
+	private renderHelper(): TemplateResult {
+		return html``;
 	}
 
 	private renderDragNDropSurface(): TemplateResult {
--- components/upload/stories/upload-basic.stories.js
+++ components/upload/stories/upload-basic.stories.js
@@ -1,23 +1,26 @@
-import '@vonage/vwc-upload/vwc-upload.js';
+import '@vonage/vwc-upload';
 import { html } from 'lit-element';
 import { spread } from '@open-wc/lit-helpers';
 import { argTypes } from './arg-types.js';
 
 export default {
-	title: 'Composite/Upload',
+	title: 'Cells/Upload',
 	component: 'vwc-upload',
 	argTypes
 }
 
-const Template = args => html`
-	<vwc-upload ...=${spread(args)}>
-		<input
-			slot="fileInput"
-			type="file"
-      name="avatar"
-      accept="image/png, image/jpeg"/>
-	</vwc-upload>
+const TemplateA = args => html`
+	<vwc-upload ...=${spread(args)}></vwc-upload>
 `;
 
-export const Basic = Template.bind({});
-Basic.args = { label: 'Upload your image file' };
+export const Basic = TemplateA.bind({});
+Basic.args = {};
+
+export const Label = TemplateA.bind({});
+Label.args = { label: 'Upload your image', buttonText: 'Upload' };
+
+export const Accept = TemplateA.bind({});
+Accept.args = { label: 'Upload your PDF', accept: '.pdf' };
+
+export const Multiple = TemplateA.bind({});
+Multiple.args = { label: 'Upload your PDFs', multiple: true };
