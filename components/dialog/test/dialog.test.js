8e689477bb007b23412d7489d6e463bdd184f551
yonatankra <yonatan.kra@vonage.com>
2020-10-25 13:56:41 +0200

    Pretify

--- components/dialog/test/dialog.test.js
+++ components/dialog/test/dialog.test.js
@@ -3,7 +3,7 @@ import {
 	waitNextTask,
 	textToDomToParent,
 	isolatedElementsCreation,
-	awaitEvent
+	awaitEvent,
 } from '../../../test/test-helpers.js';
 import { chaiDomDiff } from '@open-wc/semantic-dom-diff';
 
@@ -15,14 +15,14 @@ describe('Dialog', () => {
 	let addElement = isolatedElementsCreation();
 
 	it(`${COMPONENT_NAME} is defined as a custom element`, async () => {
-		assert.exists(
-			customElements.get(COMPONENT_NAME)
-		);
+		assert.exists(customElements.get(COMPONENT_NAME));
 	});
 
 	it('should have internal contents matching snapshot', async () => {
 		const [actualElement] = addElement(
-			textToDomToParent(`<${COMPONENT_NAME} open><div>Content</div></${COMPONENT_NAME}>`)
+			textToDomToParent(
+				`<${COMPONENT_NAME} open><div>Content</div></${COMPONENT_NAME}>`
+			)
 		);
 		await awaitEvent(actualElement, 'opened');
 		expect(actualElement.shadowRoot.innerHTML).to.equalSnapshot();
