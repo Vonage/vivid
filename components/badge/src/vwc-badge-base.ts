83cd2e168a9569798c805cc60b9277e0b8cf6087
yinonov <yinon@hotmail.com>
2020-10-25 17:43:19 +0200

    progress on badge

--- components/badge/src/vwc-badge-base.ts
+++ components/badge/src/vwc-badge-base.ts
@@ -1,10 +1,14 @@
 import { html, LitElement, property, TemplateResult } from 'lit-element';
-import { Connotation } from '@vonage/vvd-foundation/constants';
+import { Connotation, Shape } from '@vonage/vvd-foundation/constants';
 
 export class BadgeBase extends LitElement {
 	@property({ type: Boolean, reflect: true }) translucent = false;
+
 	@property({ type: String, reflect: true })
 	connotation?: Connotation;
+
+	@property({ type: String, reflect: true })
+	shape?: Shape | undefined;
 
 	protected render(): TemplateResult {
 		return html`<slot></slot>`;
