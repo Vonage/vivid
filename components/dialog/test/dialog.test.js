269ae59ee9816eb679d3a29e45d9d8b1af94089a
yonatankra <yonatan.kra@vonage.com>
2020-10-27 17:00:45 +0200

    Dialog style tests
    Supposed to fail cause using wrong variable names

--- components/dialog/test/dialog.test.js
+++ components/dialog/test/dialog.test.js
@@ -4,14 +4,16 @@ import {
 	textToDomToParent,
 	isolatedElementsCreation,
 	awaitEvent,
+	assertComputedStyle
 } from '../../../test/test-helpers.js';
 import { chaiDomDiff } from '@open-wc/semantic-dom-diff';
+import vvdCore from '@vonage/vvd-core';
 
 chai.use(chaiDomDiff);
 
 const COMPONENT_NAME = 'vwc-dialog';
 
-describe('Dialog', () => {
+describe.only('Dialog', () => {
 	let addElement = isolatedElementsCreation();
 
 	it(`${COMPONENT_NAME} is defined as a custom element`, async () => {
@@ -26,5 +28,40 @@ describe('Dialog', () => {
 		);
 		await awaitEvent(actualElement, 'opened');
 		expect(actualElement.shadowRoot.innerHTML).to.equalSnapshot();
+	});
+
+	describe(`styles`, function() {
+		it(`should have the wanted styles`, async function() {
+			await vvdCore
+				.set({
+					scheme: 'dark'
+				})
+				.then(() => {
+					//	do whatever after applying configuration
+				});
+
+			const surfaceWantedStyles = {
+				"background-color": 'rgb(255,255,255)',
+				"border-radius": `6px`,
+				"box-shadow": "0px 4px 20px rgba(19, 20, 21, 0.1)"
+			};
+
+			const contentWantedStyles = {
+				color: `rgb(0,0,0)`,
+			};
+
+			const [actualElement] = (
+				textToDomToParent(
+					`<${COMPONENT_NAME} open><div>Content</div></${COMPONENT_NAME}>`
+				)
+			);
+
+			await waitNextTask();
+			const surface = actualElement.shadowRoot.querySelector('.mdc-dialog__surface');
+			const content = actualElement.shadowRoot.querySelector('#content');
+
+			expect(assertComputedStyle(surface, surfaceWantedStyles)).to.eq(true);
+			expect(assertComputedStyle(content, contentWantedStyles)).to.eq(true);
+		});
 	});
 });
