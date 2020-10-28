7f082f7a5aa2aacf9e9e0f39919ef905c9c6cdac
Yuri Guller <gullerya@gmail.com>
2020-10-28 20:24:11 +0200

    fixing the issues and improving the story of the dialog

--- components/dialog/src/vwc-dialog.ts
+++ components/dialog/src/vwc-dialog.ts
@@ -1,33 +1,22 @@
 import '@vonage/vvd-core';
 import { customElement } from 'lit-element';
-import { Dialog as MwcDialog } from '@material/mwc-dialog';
-import { style as mwcDialogStyle } from '@material/mwc-dialog/mwc-dialog-css.js';
+import { Dialog as MWCDialog } from '@material/mwc-dialog';
 import { style as vwcDialogStyle } from './vwc-dialog.css';
+import { style as mwcDialogStyle } from '@material/mwc-dialog/mwc-dialog-css.js';
+import { style as styleCoupling } from '@vonage/vvd-style-coupling/vvd-style-coupling.css.js';
 
 declare global {
 	interface HTMLElementTagNameMap {
-		'vwc-dialog': VwcDialog;
+		'vwc-dialog': VWCDialog;
 	}
 }
 
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 // @ts-ignore
-MwcDialog.styles = [mwcDialogStyle, vwcDialogStyle];
+MWCDialog.styles = [styleCoupling, mwcDialogStyle, vwcDialogStyle];
 
 /**
  * This component is an extension of [<mwc-dialog>](https://github.com/material-components/material-components-web-components/tree/master/packages/dialog)
  */
 @customElement('vwc-dialog')
-export class VwcDialog extends MwcDialog {
-	protected updated(changes: Map<string, boolean>): void {
-		super.updated(changes);
-	}
-
-	connectedCallback(): void {
-		super.connectedCallback();
-	}
-
-	disconnectedCallback(): void {
-		super.disconnectedCallback();
-	}
-}
+export class VWCDialog extends MWCDialog {}
