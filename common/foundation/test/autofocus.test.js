827480f777609fbc8478c35a3c8a4a153a14496f
Yuri Guller <gullerya@gmail.com>
2020-10-28 16:25:26 +0200

    issue #401: tests for area and field

--- common/foundation/test/autofocus.test.js
+++ common/foundation/test/autofocus.test.js
@@ -1,13 +1,64 @@
 import { handleAutofocus } from '@vonage/vvd-foundation/general-utils.js';
+import { textToDomToParent, waitNextTask } from '../../../test/test-helpers';
+import '@vonage/vwc-textarea';
+import '@vonage/vwc-textfield';
 
-describe('autofocus', () => {
+const vwcElementsSupported = ['vwc-textarea', 'vwc-textfield'];
+
+describe.only('autofocus', () => {
 	it('should NOT throw on invalid input', async () => {
 		handleAutofocus();
 		handleAutofocus(null);
 		handleAutofocus('some');
 	});
 
-	it('should focus on target if none other focused in parent', () => {});
+	vwcElementsSupported.forEach((vwcElement) => {
+		describe(`autofocus behaviour of '${vwcElement}'`, () => {
+			it('should focus on target if no other element focused in parent', async () => {
+				clearAnyFocus();
 
-	it('should NOT "steal" focus from already focused element in parent in any', () => {});
+				const [e] = textToDomToParent(`<${vwcElement} autofocus></${vwcElement}>`);
+				await waitNextTask();
+
+				assertFocusStatus(e, true);
+				e.remove();
+			});
+
+			it('should NOT "steal" focus from already focused element in parent in any', async () => {
+				const [input] = textToDomToParent('<input/>');
+				input.focus();
+
+				const [e] = textToDomToParent(`<${vwcElement} autofocus></${vwcElement}>`);
+				await waitNextTask();
+
+				assertFocusStatus(e, false);
+				e.remove();
+				input.remove();
+			});
+
+			it('should NOT focus on target "autofocus" not specified', async () => {
+				clearAnyFocus();
+
+				const [e] = textToDomToParent(`<${vwcElement}></${vwcElement}>`);
+				await waitNextTask();
+
+				assertFocusStatus(e, false);
+				e.remove();
+			});
+		});
+	});
 });
+
+function clearAnyFocus() {
+	const currentlyFocused = document.querySelector(':focus');
+	if (currentlyFocused) {
+		currentlyFocused.blur();
+	}
+}
+
+function assertFocusStatus(vividInput, status) {
+	expect(vividInput.matches(':focus-within')).equal(status);
+	expect(
+		vividInput.shadowRoot.querySelector('input, textarea').matches(':focus')
+	).equal(status);
+}
