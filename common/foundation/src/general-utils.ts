30a82893f329d7e97c676441ab4175d923e5e9e1
Yuri Guller <gullerya@gmail.com>
2020-10-29 13:25:27 +0200

    issue #411: CR fixes

--- common/foundation/src/general-utils.ts
+++ common/foundation/src/general-utils.ts
@@ -3,15 +3,10 @@ export function handleAutofocus(targetElement: HTMLElement): void {
 		console.error(`HTMLElement target expected, got '${targetElement}'`);
 		return;
 	}
-	if (targetElement.hasAttribute('autofocus')) {
-		const rootNode = targetElement.getRootNode();
-		if (
-			('activeElement' in rootNode &&
-				!(rootNode as Document).activeElement?.matches(':focus')) ||
-			('matches' in rootNode &&
-				!(rootNode as HTMLElement).matches(':focus-within'))
-		) {
-			targetElement.focus();
-		}
+	if (
+		targetElement.hasAttribute('autofocus') &&
+		!document.body.matches(':focus-within')
+	) {
+		targetElement.focus();
 	}
 }
