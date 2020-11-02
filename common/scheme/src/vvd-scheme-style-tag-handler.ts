fbf956a92abd7fc389cf4a4c910da23dafd33ce7
Yuri Guller <gullerya@gmail.com>
2020-11-02 18:43:16 +0200

    issue #419: fixing scheme duplicate styles load

--- common/scheme/src/vvd-scheme-style-tag-handler.ts
+++ common/scheme/src/vvd-scheme-style-tag-handler.ts
@@ -18,13 +18,23 @@ const getSchemeModule: (scheme: PredefinedScheme) => Promise<ModuleType> = (
 	}
 };
 
-const style = mountStyle();
+const STYLE_ELEMENT_CLASS = 'vvd-scheme-style';
+const style = ensureStyleMount(STYLE_ELEMENT_CLASS);
 
-function mountStyle() {
-	const styleElement = document.createElement('style');
-	styleElement.innerHTML = preSchemeLoadingCssText;
-	document.head.appendChild(styleElement);
-	return styleElement;
+function ensureStyleMount(sseClass: string): HTMLStyleElement {
+	let result;
+	const existing = document.querySelectorAll(`.${sseClass}`);
+	if (existing.length) {
+		console.error(
+			`found ${existing.length} scheme styles upon init while expected for 1, check your dependencies configuration`
+		);
+		result = existing[0] as HTMLStyleElement;
+	} else {
+		result = document.createElement('style');
+		result.innerHTML = preSchemeLoadingCssText;
+		document.head.appendChild(result);
+	}
+	return result;
 }
 
 export async function applySchemeCSS(scheme: PredefinedScheme): Promise<void> {
