2213be62c4de37728d79dc2e49122261aaf79fd4
Yuri Guller <gullerya@gmail.com>
2020-11-10 11:53:40 +0200

    issue #430: work in progress

--- common/design-tokens/src/index.js
+++ common/design-tokens/src/index.js
@@ -1,10 +1,12 @@
 import { render as schemesRender } from './render-schemes.js';
 import { render as schemesCouplingRender } from './render-schemes-coupling.js';
+import { render as typographyRender } from './render-typography.js';
 
 console.log('Build started...');
 
 schemesRender();
 schemesCouplingRender();
+typographyRender();
 
 console.log('\n==============================================');
 console.log('\nBuild completed!');
