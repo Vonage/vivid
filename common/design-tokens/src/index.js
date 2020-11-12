0962b8fd098c147543e93d76bf25e633110e38b5
yinonov <yinon@hotmail.com>
2020-11-12 12:36:18 +0200

    typography list in 1 file

--- common/design-tokens/src/index.js
+++ common/design-tokens/src/index.js
@@ -1,6 +1,6 @@
 import { render as schemesRender } from './builders/render-schemes.js';
 import { render as schemesCouplingRender } from './builders/render-schemes-coupling.js';
-import { render as typographyRender } from './builders/render-typography.js';
+import { render as typographyRender } from './builders/render-typography-new.js';
 
 console.log('Build started...');
 
