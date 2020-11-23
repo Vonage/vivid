bdfc3c7dc4d01e1206fb59da9bc3b7f6994816fa
Yuri Guller <gullerya@gmail.com>
2020-11-23 19:35:27 +0200

    issue #454: added typography tests

--- components/note/test/note.typography.test.js
+++ components/note/test/note.typography.test.js
@@ -1,5 +1,36 @@
 import '../vwc-note.js';
+import {
+	assertComputedStyle,
+	isolatedElementsCreation,
+	textToDomToParent,
+	waitNextTask,
+} from '../../../test/test-helpers.js';
+import { getTypographyStyle } from '../../../test/typography-utils.js';
+
+const COMPONENT_NAME = 'vwc-note';
 
 describe('note typography', async () => {
-	//	TBD
+	let addElement = isolatedElementsCreation();
+
+	it('should have set typography for a header', async () => {
+		const [note] = addElement(
+			textToDomToParent(`<${COMPONENT_NAME} header="Header"></${COMPONENT_NAME}>`)
+		);
+		await waitNextTask();
+		assertComputedStyle(
+			note.shadowRoot.querySelector('.note-header'),
+			await getTypographyStyle('body-2-bold')
+		);
+	});
+
+	it('should have set typography for a message', async () => {
+		const [note] = addElement(
+			textToDomToParent(`<${COMPONENT_NAME}>Message</${COMPONENT_NAME}>`)
+		);
+		await waitNextTask();
+		assertComputedStyle(
+			note.shadowRoot.querySelector('.note-message'),
+			await getTypographyStyle('body-2')
+		);
+	});
 });
