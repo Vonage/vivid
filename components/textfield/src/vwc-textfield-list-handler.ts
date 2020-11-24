edf60a8908b87f21e9871673d8d6e2bb96e3be7a
yinonov <yinon@hotmail.com>
2020-11-25 01:08:42 +0200

    cleanup - remove comments

--- components/textfield/src/vwc-textfield-list-handler.ts
+++ components/textfield/src/vwc-textfield-list-handler.ts
@@ -3,9 +3,8 @@ import { VWCMenu } from '@vonage/vwc-menu';
 import { isEventMulti } from '@material/mwc-list';
 import { SelectedEvent } from '@material/mwc-list/mwc-list-foundation';
 import { VWCListItem } from '@vonage/vwc-list/vwc-list-item';
-// import { pipe } from 'ramda';
 import { fromEvent, merge } from 'rxjs';
-import { filter, map, tap } from 'rxjs/operators';
+import { filter, map } from 'rxjs/operators';
 import { not, pipe } from 'ramda';
 
 // TODO support for eager, lazy, debounce
@@ -26,127 +25,52 @@ export function handleList(this: VWCTextField): void {
 
 	configureMenuSettings(menu, this);
 
-	// const input = this.shadowRoot?.querySelector('input');
-	// input?.addEventListener('focus', onFocus.bind(null, menu));
-	// input?.addEventListener('blur', onBlur.bind(null, menu));
-
-	// const textfieldFocuses$ = fromEvent(this, 'focus');
-	// textfieldFocuses$.subscribe(console.log);
-
-	// const menuOpened$ = fromEvent(menu, 'opened').pipe(
-	// 	tap(() => console.log('menuOpened'))
-	// );
-	// const menuClosed$ = fromEvent(menu, 'closed');
-
+	// #region EVENT OBSERVABLES
+	const textfieldMousedowns$ = fromEvent(this, 'mousedown');
+	const textfieldFocuses$ = fromEvent(this, 'focus');
+	const textfieldBlurs$ = fromEvent(this, 'blur');
 	const textfieldBodyClicks$ = fromEvent(document.body, 'click', {
 		capture: true,
-	}).pipe(
-		filter((e) => (e?.target as HTMLElement) === this),
-		tap((e) => console.log('bodyClicks', e))
-	);
-	textfieldBodyClicks$.subscribe((e) => e.stopImmediatePropagation());
+	}).pipe(filter((e) => (e?.target as HTMLElement) === this));
+	const menuMousedowns$ = fromEvent(menu, 'mousedown');
+	const menuSelections$ = fromEvent<SelectedEvent>(menu, 'selected');
+	// #endregion
 
-	// // textfieldFocuses$.subscribe(() => console.log('textfieldFocuses'));
-	// const preventPrematureMenuClose$ = textfieldMousedowns$.pipe(
-	// 	// textfieldFocuses$,
-	// 	// switchMap(() => menuOpened$),
-	// 	// tap(() => console.log(menu.open)),
-	// 	switchMap(() =>
-	// 		bodyClicks$.pipe(
-	// 			tap(() => console.log('-- stopImmediatePropagation')),
-	// 			tap((e) => e.stopImmediatePropagation()),
-	// 			take(1)
-	// 		)
-	// 	)
-	// );
-
-	// preventPrematureMenuClose$.subscribe(() => {
-	// 	console.log('--- PREVENT BODY CLICKS ---');
-	// 	// e.stopImmediatePropagation();
-	// });
-
-	const textfieldMousedowns$ = fromEvent(this, 'mousedown').pipe(
-		tap(() => console.log('textfieldMousedowns'))
-	);
+	// #region COMPOSITE OBSERVABLES
 	const textfieldMousedownsWhileActive$ = textfieldMousedowns$.pipe(
 		filter(() => this === document.activeElement)
 	);
 
-	const textfieldFocuses$ = fromEvent(this, 'focus').pipe(
-		tap(() => console.log('textfieldFocuses'))
+	const menuOpenTriggers$ = merge(
+		textfieldFocuses$,
+		textfieldMousedownsWhileActive$
+	).pipe(
+		filter(Boolean.bind(menu?.items?.length)),
+		filter(() => !menu.open)
 	);
 
-	merge(textfieldFocuses$, textfieldMousedownsWhileActive$)
-		.pipe(
-			filter(Boolean.bind(menu?.items?.length)),
-			filter(() => !menu.open)
+	const menuSelectedItem$ = menuSelections$.pipe(
+		filter(pipe(isEventMulti, not)),
+		map(
+			({ target, detail: { index } }) =>
+				(target as VWCMenu)?.items[index as number]
 		)
-		.subscribe(() => menu.show());
-
-	const textfieldBlurs$ = fromEvent(this, 'blur').pipe(
-		tap(() => console.log('textfieldBlurs'))
 	);
+	// #endregion
+
+	// #region SUBSCRIPTIONS
+	textfieldBodyClicks$.subscribe((e) => e.stopImmediatePropagation());
+	menuOpenTriggers$.subscribe(() => menu.show());
 	textfieldBlurs$.pipe(filter(() => menu.open)).subscribe(() => menu.close());
 
-	// const preventPrematureClickClose = (e: MouseEvent) => {
-	// 	console.log('MUAA');
-	// 	if (menu.open) {
-	// 		e.stopImmediatePropagation();
-	// 	}
-	// };
-
-	// document.body.addEventListener('click', preventPrematureClickClose, {
-	// 	capture: true,
-	// 	once: true,
-	// });
-
-	const menuMousedowns$ = fromEvent(menu, 'mousedown');
 	// prevent blur of textfield on menu mousedown
-	menuMousedowns$.subscribe((e) => {
-		console.log('mousedown prevent blur of textfield on menu mousedown');
-		e.preventDefault();
-	});
-
-	const menuSelections$ = fromEvent<SelectedEvent>(menu, 'selected');
-	const menuSelectedItem$ = menuSelections$.pipe(
-		tap(() => console.log('menu select value')),
-		filter(pipe(isEventMulti, not)),
-		tap(console.log),
-		map(({ target, detail: { index } }) => (target as VWCMenu)?.items[index])
-	);
+	menuMousedowns$.subscribe((e) => e.preventDefault());
 	menuSelectedItem$.subscribe(({ value, textContent }: VWCListItem) => {
 		// TODO conditionally fallbacks to textContent - should be unit tested
 		value ||= textContent || '';
 		this.value = value;
 	});
-
-	// menuOpened$
-	// 	.pipe(switchMapTo(menuMousedowns$), takeUntil(menuClosed$))
-	// 	.subscribe(() => console.log('menuOpened and mouse down'));
-
-	// this.addEventListener('focus', openList.bind(null, menu));
-
-	// // prevent blur of textfield on menu mousedown
-	// // menu?.addEventListener(
-	// // 	'mousedown',
-	// // 	(e: MouseEvent) => {
-	// // 		console.log('ARGGG');
-	// // 		e.preventDefault();
-	// // 	},
-	// // 	{
-	// // 		capture: true,
-	// // 	}
-	// // );
-
-	// // this.addEventListener('blur', onBlur.bind(null, menu));
-
-	// (menu as any).addEventListener(
-	// 	'selected',
-	// 	pipe(onSelected, (value) => {
-	// 		this.value = value || '';
-	// 		menu.close();
-	// 	})
-	// );
+	// #endregion
 }
 
 // FUNCTIONS
@@ -160,66 +84,5 @@ function configureMenuSettings(menu: VWCMenu, anchor: HTMLElement) {
 	menu.defaultFocus = 'NONE';
 	menu.corner = 'BOTTOM_START';
 	menu.multi = false;
-	menu.quick = true;
+	// menu.quick = true;
 }
-
-// function openList(menu: VWCMenu): void {
-// 	if (!menu?.items?.length) {
-// 		return;
-// 	}
-
-// 	const preventPrematureClickClose = (e: MouseEvent) => {
-// 		console.log('MUAA');
-// 		if (menu.open) {
-// 			e.stopImmediatePropagation();
-// 		}
-// 	};
-
-// 	document.body.addEventListener('click', preventPrematureClickClose, {
-// 		capture: true,
-// 		once: true,
-// 	});
-
-// 	// menu.addEventListener(
-// 	// 	'closed',
-// 	// 	() => {
-// 	// 		document.body.removeEventListener('click', preventPrematureClickClose);
-// 	// 	},
-// 	// 	{ once: true }
-// 	// );
-
-// 	// setTimeout(() => {
-// 	menu.show();
-// 	// }, 500);
-// }
-
-// function preventSomething(): (this: HTMLElement, ev: MouseEvent) => any {
-// 	return (e) => e.stopImmediatePropagation();
-// }
-
-// function onBlur(menu: VWCMenu): void {
-// 	document.body.removeEventListener('click', preventMenuCloseByClick);
-// 	if (menu.open) {
-// 		menu.close();
-// 	}
-// }
-
-// function preventMenuCloseByClick(e: Event) {
-// 	e.stopImmediatePropagation();
-// }
-
-// function onSelected(e: SelectedEvent): string | undefined {
-// 	e.preventDefault();
-// 	if (isEventMulti(e)) {
-// 		// Typescript will infer evt as {index: Set<number>, diff: IndexDiff}
-// 		console.warn("multi property isn't supported for autocomplete");
-// 		return;
-// 	}
-// 	// Typescript will infer evt as {index: number, diff: undefined}
-// 	const {
-// 		target,
-// 		detail: { index },
-// 	} = e;
-// 	const { value } = (target as VWCMenu)?.items[index];
-// 	return value;
-// }
--- components/textfield/test/textfield-list.test.js
+++ components/textfield/test/textfield-list.test.js
@@ -11,124 +11,124 @@ const TEXTFIELD_LIST_SETUP_HTML_TAG = 'textfieldSetupTest';
 describe('textfield list', () => {
 	/* eslint-disable no-undef */
 	after(() => {
-		// cleanFrame(TEXTFIELD_LIST_SETUP_HTML_TAG);
+		cleanFrame(TEXTFIELD_LIST_SETUP_HTML_TAG);
 	});
 
 	describe('basic', () => {
-		// it(`should define ${VWC_TEXTFIELD} in custom elements registry`, async () => {
-		// 	assert.exists(
-		// 		customElements.get(VWC_TEXTFIELD, `${VWC_TEXTFIELD} element is not defined`)
-		// 	);
-		// });
+		it(`should define ${VWC_TEXTFIELD} in custom elements registry`, async () => {
+			assert.exists(
+				customElements.get(VWC_TEXTFIELD, `${VWC_TEXTFIELD} element is not defined`)
+			);
+		});
 
-		// it(`should define ${VWC_MENU} in custom elements registry`, async () => {
-		// 	assert.exists(
-		// 		customElements.get(VWC_MENU, `${VWC_MENU} element is not defined`)
-		// 	);
-		// });
+		it(`should define ${VWC_MENU} in custom elements registry`, async () => {
+			assert.exists(
+				customElements.get(VWC_MENU, `${VWC_MENU} element is not defined`)
+			);
+		});
 
-		// it(`should open ${VWC_MENU} on ${VWC_TEXTFIELD} focus`, async () => {
-		// 	await getFrameLoadedInjected(
-		// 		TEXTFIELD_LIST_SETUP_HTML_TAG,
-		// 		async (iframe) => {
-		// 			const iframeDocument = iframe.contentDocument;
+		it(`should open ${VWC_MENU} on ${VWC_TEXTFIELD} focus`, async () => {
+			await getFrameLoadedInjected(
+				TEXTFIELD_LIST_SETUP_HTML_TAG,
+				async (iframe) => {
+					const iframeDocument = iframe.contentDocument;
 
-		// 			const textfield = iframeDocument.querySelector(VWC_TEXTFIELD);
-		// 			const menu = iframeDocument.querySelector(VWC_MENU);
-		// 			expect(textfield.list).to.equal(menu.id);
-		// 			textfield.focus();
-		// 			expect(menu.open).to.equal(true);
-		// 		}
-		// 	);
-		// });
+					const textfield = iframeDocument.querySelector(VWC_TEXTFIELD);
+					const menu = iframeDocument.querySelector(VWC_MENU);
+					expect(textfield.list).to.equal(menu.id);
+					textfield.focus();
+					expect(menu.open).to.equal(true);
+				}
+			);
+		});
 
-		// it(`should keep ${VWC_TEXTFIELD} focus on ${VWC_MENU} open`, async () => {
-		// 	await getFrameLoadedInjected(
-		// 		TEXTFIELD_LIST_SETUP_HTML_TAG,
-		// 		async (iframe) => {
-		// 			const iframeDocument = iframe.contentDocument;
+		it(`should keep ${VWC_TEXTFIELD} focus on ${VWC_MENU} open`, async () => {
+			await getFrameLoadedInjected(
+				TEXTFIELD_LIST_SETUP_HTML_TAG,
+				async (iframe) => {
+					const iframeDocument = iframe.contentDocument;
 
-		// 			const textfield = iframeDocument.querySelector(VWC_TEXTFIELD);
-		// 			// adding a timeout for component to render shadowed html input
-		// 			await waitInterval(200);
-		// 			textfield.focus();
-		// 			expect(iframeDocument.activeElement).to.equal(textfield);
-		// 		}
-		// 	);
-		// });
+					const textfield = iframeDocument.querySelector(VWC_TEXTFIELD);
+					// adding a timeout for component to render shadowed html input
+					await waitInterval(200);
+					textfield.focus();
+					expect(iframeDocument.activeElement).to.equal(textfield);
+				}
+			);
+		});
 
-		// it(`should keep ${VWC_MENU} opened on document.body bubbled click`, async () => {
-		// 	await getFrameLoadedInjected(
-		// 		TEXTFIELD_LIST_SETUP_HTML_TAG,
-		// 		async (iframe) => {
-		// 			const iframeDocument = iframe.contentDocument;
+		it(`should keep ${VWC_MENU} opened on document.body bubbled click`, async () => {
+			await getFrameLoadedInjected(
+				TEXTFIELD_LIST_SETUP_HTML_TAG,
+				async (iframe) => {
+					const iframeDocument = iframe.contentDocument;
 
-		// 			const textfield = iframeDocument.querySelector(VWC_TEXTFIELD);
-		// 			const menu = iframeDocument.querySelector(VWC_MENU);
-		// 			textfield.focus();
+					const textfield = iframeDocument.querySelector(VWC_TEXTFIELD);
+					const menu = iframeDocument.querySelector(VWC_MENU);
+					textfield.focus();
 
-		// 			expect(menu.open).to.equal(true);
-		// 		}
-		// 	);
-		// });
+					expect(menu.open).to.equal(true);
+				}
+			);
+		});
 
-		// it(`should keep ${VWC_MENU} open on ${VWC_TEXTFIELD} focus`, async () => {
-		// 	await getFrameLoadedInjected(
-		// 		TEXTFIELD_LIST_SETUP_HTML_TAG,
-		// 		async (iframe) => {
-		// 			const iframeDocument = iframe.contentDocument;
+		it(`should keep ${VWC_MENU} open on ${VWC_TEXTFIELD} focus`, async () => {
+			await getFrameLoadedInjected(
+				TEXTFIELD_LIST_SETUP_HTML_TAG,
+				async (iframe) => {
+					const iframeDocument = iframe.contentDocument;
 
-		// 			const textfield = iframeDocument.querySelector(VWC_TEXTFIELD);
-		// 			const menu = iframeDocument.querySelector(VWC_MENU);
-		// 			textfield.focus();
+					const textfield = iframeDocument.querySelector(VWC_TEXTFIELD);
+					const menu = iframeDocument.querySelector(VWC_MENU);
+					textfield.focus();
 
-		// 			expect(menu.open).to.equal(true);
-		// 		}
-		// 	);
-		// });
+					expect(menu.open).to.equal(true);
+				}
+			);
+		});
 
-		// it(`should close ${VWC_MENU} on ${VWC_TEXTFIELD} blur`, async () => {
-		// 	await getFrameLoadedInjected(
-		// 		TEXTFIELD_LIST_SETUP_HTML_TAG,
-		// 		async (iframe) => {
-		// 			const iframeDocument = iframe.contentDocument;
+		it(`should close ${VWC_MENU} on ${VWC_TEXTFIELD} blur`, async () => {
+			await getFrameLoadedInjected(
+				TEXTFIELD_LIST_SETUP_HTML_TAG,
+				async (iframe) => {
+					const iframeDocument = iframe.contentDocument;
 
-		// 			const textfield = iframeDocument.querySelector(VWC_TEXTFIELD);
-		// 			const menu = iframeDocument.querySelector(VWC_MENU);
-		// 			expect(textfield.list).to.equal(menu.id);
-		// 			textfield.focus();
+					const textfield = iframeDocument.querySelector(VWC_TEXTFIELD);
+					const menu = iframeDocument.querySelector(VWC_MENU);
+					expect(textfield.list).to.equal(menu.id);
+					textfield.focus();
 
-		// 			textfield.blur();
+					textfield.blur();
 
-		// 			expect(menu.open).to.equal(false);
-		// 		}
-		// 	);
-		// });
+					expect(menu.open).to.equal(false);
+				}
+			);
+		});
 
-		// it(`should not open ${VWC_MENU} if length is less than 1`, async () => {
-		// 	await getFrameLoadedInjected(
-		// 		TEXTFIELD_LIST_SETUP_HTML_TAG,
-		// 		async (iframe) => {
-		// 			const iframeDocument = iframe.contentDocument;
+		it(`should not open ${VWC_MENU} if length is less than 1`, async () => {
+			await getFrameLoadedInjected(
+				TEXTFIELD_LIST_SETUP_HTML_TAG,
+				async (iframe) => {
+					const iframeDocument = iframe.contentDocument;
 
-		// 			const textfield = iframeDocument.querySelector(VWC_TEXTFIELD);
-		// 			const menu = iframeDocument.querySelector(VWC_MENU);
-		// 			menu.innerHTML = '';
-		// 			textfield.focus();
+					const textfield = iframeDocument.querySelector(VWC_TEXTFIELD);
+					const menu = iframeDocument.querySelector(VWC_MENU);
+					menu.innerHTML = '';
+					textfield.focus();
 
-		// 			expect(menu.open).to.equal(false);
-		// 		}
-		// 	);
-		// });
+					expect(menu.open).to.equal(false);
+				}
+			);
+		});
 
-		// it(`should keep ${VWC_TEXTFIELD} focus on vwc-list mousedown`, async () => {
-		// 	await getFrameLoadedInjected(
-		// 		TEXTFIELD_LIST_SETUP_HTML_TAG,
-		// 		async (iframe) => {
-		// 			expect(false).to.equal(true);
-		// 		}
-		// 	);
-		// });
+		it(`should keep ${VWC_TEXTFIELD} focus on vwc-list mousedown`, async () => {
+			await getFrameLoadedInjected(
+				TEXTFIELD_LIST_SETUP_HTML_TAG,
+				async (iframe) => {
+					expect(false).to.equal(true);
+				}
+			);
+		});
 
 		it(`should set ${VWC_TEXTFIELD}'s value as selected list option`, async () => {
 			await getFrameLoadedInjected(
@@ -138,20 +138,12 @@ describe('textfield list', () => {
 
 					const textfield = iframeDocument.querySelector(VWC_TEXTFIELD);
 
-					// await waitInterval(200);
-					// textfield.focus();
-					// const menu = iframeDocument.querySelector(VWC_MENU);
-					// const [firstItem] = menu.items;
-					// const { value: itemValue } = firstItem;
-					// console.log(firstItem)
-					// await waitInterval(1200);
-					// firstItem.select(0);
-
-					// textfield.value = itemValue;
-
-					// await waitInterval(200);
-					// menu.innerHTML = '';
-					// textfield.focus();
+					await waitInterval(200);
+					textfield.focus();
+					const menu = iframeDocument.querySelector(VWC_MENU);
+					menu.select(0);
+					const [firstItem] = menu.items;
+					const { value: itemValue } = firstItem;
 
 					expect(textfield.value).to.equal(itemValue);
 				}
