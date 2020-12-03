fcab1b9fa649283b11c9b2966c03e129ae5874f6
yinonov <yinon@hotmail.com>
2020-12-03 16:05:09 +0200

    dep-check issues fixed

--- components/icon-button-toggle/src/vwc-icon-button-toggle.ts
+++ components/icon-button-toggle/src/vwc-icon-button-toggle.ts
@@ -1,24 +1,12 @@
 import '@vonage/vvd-core';
-import '@vonage/vwc-icon';
 import { customElement, html, property, TemplateResult } from 'lit-element';
 import { IconButtonToggle as MWCIconButtonToggle } from '@material/mwc-icon-button-toggle';
-import { style as MWCIconButtonStyle } from '@material/mwc-icon-button/mwc-icon-button-css.js';
-import { style as VWCIconButtonStyle } from '@vonage/vwc-icon-button/vwc-icon-button.css';
-import { style as styleCoupling } from '@vonage/vvd-style-coupling/vvd-style-coupling.css.js';
 
 declare global {
 	interface HTMLElementTagNameMap {
 		'vwc-icon-button-toggle': VWCIconButtonToggle;
 	}
 }
-
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-// @ts-ignore
-MWCIconButtonToggle.styles = [
-	styleCoupling,
-	MWCIconButtonStyle,
-	VWCIconButtonStyle,
-];
 
 /**
  * This component is an extension of [<mwc-icon-button-toggle>](https://github.com/material-components/material-components-web-components/tree/master/packages/icon-button-toggle)
