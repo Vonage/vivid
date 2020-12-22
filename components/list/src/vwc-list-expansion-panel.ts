cef6552a2a98d8f04e4aaf684cbd34f43db2192f
yinonov <yinon@hotmail.com>
2020-12-22 16:22:03 +0200

    encapsulated list expansion panel

--- components/list/src/vwc-list-expansion-panel.ts
+++ components/list/src/vwc-list-expansion-panel.ts
@@ -1,13 +1,17 @@
+import '@vonage/vwc-icon';
 import {
 	customElement,
 	html,
 	LitElement,
 	property,
+	PropertyValues,
+	queryAssignedNodes,
 	TemplateResult,
 } from 'lit-element';
 import { observer } from '@material/mwc-base/observer';
 
 import { style } from './vwc-list-expansion-panel.css.js';
+import { ListItemBase } from '@material/mwc-list/mwc-list-item-base';
 
 declare global {
 	interface HTMLElementTagNameMap {
@@ -22,6 +26,8 @@ declare global {
 export class VWCListExpansionPanel extends LitElement {
 	static styles = style;
 
+	headerListItemIcon?: unknown;
+
 	@property({ type: Boolean, reflect: true })
 	@observer(function (
 		this: VWCListExpansionPanel,
@@ -35,20 +41,41 @@ export class VWCListExpansionPanel extends LitElement {
 			this.close();
 		}
 
-		this.notifyChange(isOpen);
+		this.onOpenChange(isOpen);
 	})
 	open = false;
 
-	@property({ type: Boolean }) quick = false;
+	// @property({ type: Boolean }) quick = false; // TODO add animation
+
+	@queryAssignedNodes('header', true, 'vwc-list-item')
+	headerNodes?: HTMLElement[] | null;
+
+	firstUpdated(changedProperties: PropertyValues): void {
+		super.firstUpdated(changedProperties);
+		const headerListItem = getHeaderListItem(this.headerNodes);
+		this.headerListItemIcon = getHeaderListItemIcon(headerListItem);
+
+		headerListItem.addEventListener(
+			'request-selected',
+			() => (this.open = !this.open)
+		);
+	}
+
+	private onOpenChange(isOpen: boolean): void {
+		(this.headerListItemIcon as Element).setAttribute(
+			'type',
+			isOpen ? 'up' : 'down'
+		);
+	}
 
 	close(): void {
-		this.notifyClose();
 		this.open = false;
+		this.notifyClose();
 	}
 
 	show(): void {
-		this.notifyOpen();
 		this.open = true;
+		this.notifyOpen();
 	}
 
 	notifyClose(): void {
@@ -65,17 +92,39 @@ export class VWCListExpansionPanel extends LitElement {
 		this.dispatchEvent(ev);
 	}
 
-	notifyChange(isOpen: boolean): void {
-		const init: CustomEventInit = {
-			bubbles: true,
-			composed: true,
-			detail: { open: isOpen },
-		};
-		const ev = new CustomEvent('changed', init);
-		this.dispatchEvent(ev);
-	}
-
 	render(): TemplateResult {
-		return html`<div class="expansion-panel"><slot></slot></div>`;
+		return html`<slot name="header"></slot>
+			<div class="expansion-panel"><slot></slot></div>`;
 	}
+}
+
+/// UTIL FNS
+function assert(condition: any, msg?: string): asserts condition {
+	if (!condition) {
+		throw new Error(msg);
+	}
+}
+
+function getHeaderListItem(headerNodes: unknown) {
+	assert(Array.isArray(headerNodes), `Not an array: ${headerNodes}`);
+	assert(headerNodes.length > 0, `is empty: ${headerNodes}`);
+	const [headerListItem] = headerNodes;
+	assert(
+		headerListItem instanceof ListItemBase,
+		`Not a list item: ${headerListItem}`
+	);
+	return headerListItem;
+}
+function mountIcon(headerListItem: ListItemBase) {
+	const icon = document.createElement('vwc-icon');
+	icon.setAttribute('slot', 'meta');
+	headerListItem.appendChild(icon);
+	headerListItem.setAttribute('hasMeta', ''); // side effect setting attribute to match icon usage
+	return icon;
+}
+
+function getHeaderListItemIcon(headerListItem: ListItemBase) {
+	let icon = headerListItem.querySelector('vwc-icon');
+	icon ||= mountIcon(headerListItem);
+	return icon;
 }
--- components/list/stories/sub-list-item.stories.js
+++ components/list/stories/sub-list-item.stories.js
@@ -1,8 +1,8 @@
+import '@vonage/vwc-list/vwc-list.js';
 import '@vonage/vwc-list/vwc-list-item.js';
 import '@vonage/vwc-list/vwc-list-expansion-panel.js';
 import '@vonage/vwc-icon';
 import { html } from 'lit-element';
-import { spread } from '@open-wc/lit-helpers';
 import { argTypes } from './arg-types-list-item.js';
 import { styleMap } from 'lit-html/directives/style-map';
 
@@ -16,28 +16,15 @@ const listStyles = {
 	'inline-size': '240px',
 };
 
-function toggleExpansion() {
-	const { nextElementSibling: panel } = this;
-	panel.open = !panel.open;
-	this.activated = panel.open ? true : false;
-}
-
-function onChange(e) {
-	console.log(e);
-	const { detail: { open } } = e;
-	const { target: { previousElementSibling: listItem } } = e;
-	const icon = listItem.querySelector('vwc-icon');
-	const iconType = open ? 'up-full' : 'down-full';
-	icon.setAttribute('type', iconType);
-}
-
-const Template = args => html`
+const Template = () => html`
 	<vwc-list style=${styleMap(listStyles)}>
-		<vwc-list-item ...=${spread(args)} @click="${toggleExpansion}">
+		<vwc-list-item>
 			Parent 1
-			<vwc-icon slot="meta"></vwc-icon>
 		</vwc-list-item>
-		<vwc-list-expansion-panel @changed="${onChange}">
+		<vwc-list-expansion-panel>
+			<vwc-list-item slot="header">
+				Parent 2
+			</vwc-list-item>
 			<vwc-list-item>
 				Child 1
 			</vwc-list-item>
@@ -45,24 +32,22 @@ const Template = args => html`
 				Child 2
 			</vwc-list-item>
 		</vwc-list-expansion-panel>
-		<vwc-list-item ...=${spread(args)} @click="${toggleExpansion}">
-			Parent 2
-			<vwc-icon slot="meta"></vwc-icon>
-		</vwc-list-item>
-		<vwc-list-expansion-panel @changed="${onChange}">
+		<vwc-list-expansion-panel>
+			<vwc-list-item slot="header">
+				Parent 3
+			</vwc-list-item>
 			<vwc-list-item>
 				Child 1
 			</vwc-list-item>
-			<vwc-list-item ...=${spread(args)} @click="${toggleExpansion}">
-				Child 2
-				<vwc-icon slot="meta"></vwc-icon>
-			</vwc-list-item>
-			<vwc-list-expansion-panel @changed="${onChange}">
-				<vwc-list-item>
-					Grand Child 1
+			<vwc-list-expansion-panel>
+				<vwc-list-item slot="header">
+					Child 2
 				</vwc-list-item>
 				<vwc-list-item>
-					Grand Child 2
+					Grand child 1
+				</vwc-list-item>
+				<vwc-list-item>
+					Grand child 2
 				</vwc-list-item>
 			</vwc-list-expansion-panel>
 		</vwc-list-expansion-panel>
@@ -70,4 +55,3 @@ const Template = args => html`
 `;
 
 export const Expandable = Template.bind({});
-Expandable.args = { hasMeta: '' }
