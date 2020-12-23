c772f68ddbc0d385684bc1af1876c044d1667957
yinonov <yinon@hotmail.com>
2020-12-23 14:25:19 +0200

    fix correct deps

--- components/list/test/list-item-expansion-panel.test.js
+++ components/list/test/list-item-expansion-panel.test.js
@@ -22,12 +22,32 @@ describe('list expansion panel', () => {
 		);
 	});
 
-	it('should have the expected internal contents', async () => {
+	it(`should have the expected custom element's internal contents`, async () => {
 		addElements = textToDomToParent(
 			`<${VWC_LIST_EXPANSION_PANEL}></${VWC_LIST_EXPANSION_PANEL}>`
 		);
 		const actualElement = addElements[0];
 		await waitNextTask();
 		expect(actualElement.shadowRoot.innerHTML).to.equalSnapshot();
+	});
+
+	it('should have the expected internal contents', async () => {
+		addElements = textToDomToParent(
+			`<${VWC_LIST_EXPANSION_PANEL}>
+				<vwc-list-item slot="header" graphic="icon">
+					<vwc-icon slot="graphic" type="profile"></vwc-icon>
+					Parent 1
+				</vwc-list-item>
+				<vwc-list-item>
+					Child 1
+				</vwc-list-item>
+				<vwc-list-item>
+					Child 2
+				</vwc-list-item>
+			</${VWC_LIST_EXPANSION_PANEL}>`
+		);
+		const actualElement = addElements[0];
+		await waitNextTask();
+		expect(actualElement.headerNodes.length).to.equal(1);
 	});
 });
