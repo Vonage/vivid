596526292e64cd9bea3728bdb8a1e6555ad357cf
Yuri Guller <gullerya@gmail.com>
2020-12-23 17:46:42 +0200

    adjusting storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,18 +2,11 @@ import '@vonage/vwc-theme-switch';
 import '@storybook/addon-console';
 import { addParameters, setCustomElements } from '@storybook/web-components';
 import customElements from '../custom-elements.json';
-import vvdCore from '@vonage/vvd-core';
+import vvdContext from '@vonage/vvd-context';
 
-vvdCore.settled
+vvdContext.install()
 	.then(() => console.info('init Vivid core done (preview frame)'))
 	.catch(e => console.error(e));
-
-const TOPIC_ORDERING = {
-	'Introduction': 0,
-	'Guides': 1,
-	'Design System': 2,
-	'Components': 3
-};
 
 async function run() {
 	setCustomElements(customElements);
