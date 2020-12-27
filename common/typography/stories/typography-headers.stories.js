59ac1b43aafae3bb637513d99414ad5d456710e4
Yuri Guller <gullerya@gmail.com>
2020-12-27 16:20:53 +0200

    issue #520: consumeing ajusted headers tokens

--- common/typography/stories/typography-headers.stories.js
+++ common/typography/stories/typography-headers.stories.js
@@ -6,48 +6,48 @@ export default {
 	title: 'Core/Typography',
 };
 
-export const basic = () => html`
+export const Typography = () => html`
 	<div>
-		<h1>Headline</h1>
+		<h1>Headline 1</h1>
 		<vwc-note connotation="info" icon="info">
 			This is achievable by utilizing <code>h1</code> HTML element with conjunction with context.
 		</vwc-note>
 	</div>
 
 	<div>
-		<h2>Title</h2>
+		<h2>Headline 2</h2>
 		<vwc-note connotation="info" icon="info">
 			This is achievable by utilizing <code>h2</code> HTML element with conjunction with context.
 		</vwc-note>
 	</div>
 
 	<div>
-		<h3>Subtitle 1</h3>
+		<h3>Title 1</h3>
 		<vwc-note connotation="info" icon="info">
 			This is achievable by utilizing <code>h3</code> HTML element with conjunction with context.
 		</vwc-note>
 	</div>
 
 	<div>
-		<h4>Subtitle 2</h4>
+		<h4>Title 2</h4>
 		<vwc-note connotation="info" icon="info">
 			This is achievable by utilizing <code>h4</code> HTML element with conjunction with context.
 		</vwc-note>
 	</div>
 
 	<div>
-		<h5>Subtitle 3</h5>
+		<h5>Subtitle 1</h5>
 		<vwc-note connotation="info" icon="info">
 			This is achievable by utilizing <code>h5</code> HTML element with conjunction with context.
 		</vwc-note>
 	</div>
 
 	<div>
-		<h6>Body 2</h6>
+		<h6>Subtitle 2</h6>
 		<vwc-note connotation="info" icon="info">
 			This is achievable by utilizing <code>h6</code> HTML element with conjunction with context.
 		</vwc-note>
 	</div>
 `;
 
-vvdContext.install();
+vvdContext.mount();
