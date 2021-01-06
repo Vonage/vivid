6b07fb053a637cd80afdf437c96fb7e5118df840
yinonov <yinon@hotmail.com>
2021-01-06 14:57:35 +0200

    chore(workspace): tools for api extraction & documentation

--- components/badge/src/vwc-badge.ts
+++ components/badge/src/vwc-badge.ts
@@ -1,12 +1,24 @@
 import '@vonage/vvd-core';
-import { customElement } from 'lit-element';
+import { customElement, CSSResult } from 'lit-element';
 
 import { BadgeBase } from './vwc-badge-base.js';
 import { style } from './vwc-badge.css.js';
 
 @customElement('vwc-badge')
 export class Badge extends BadgeBase {
-	static styles = style;
+	/**
+	 * Returns the average of two numbers.
+	 *
+	 * @remarks
+	 * This method is part of the {@link core-library#Statistics | Statistics subsystem}.
+	 *
+	 * @param x - The first input number
+	 * @param y - The second input number
+	 * @returns The arithmetic mean of `x` and `y`
+	 *
+	 * @internal
+	 */
+	static styles: CSSResult = style;
 }
 
 declare global {
