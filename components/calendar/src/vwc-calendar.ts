038821ba7b2663363a8d986161d0fac06d9e88a2
yinonov <yinon@hotmail.com>
2021-01-11 09:17:21 +0200

    clean component boilerplate

--- components/calendar/src/vwc-calendar.ts
+++ components/calendar/src/vwc-calendar.ts
@@ -1,37 +1,20 @@
-import { customElement, LitElement, property } from 'lit-element';
-import { style as vwcCalendarStyle } from './vwc-calendar.css';
+import { customElement, LitElement } from 'lit-element';
+import { style } from './vwc-calendar.css';
 
 declare global {
 	interface HTMLElementTagNameMap {
-		'vwc-calendar': VwcCalendar;
+		'vwc-calendar': VWCCalendar;
 	}
 }
 
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-// @ts-ignore
-VwcCalendar.styles = [vwcCalendarStyle];
-
 /**
- * This component is an extension of [<mwc-button>](https://github.com/material-components/material-components-web-components/tree/master/packages/button)
- * Our button supports native features like the 'form' and 'type' attributes
+ * calendar component
+ *
  */
 @customElement('vwc-calendar')
-export class VwcCalendar extends LitElement {
-	@property({ type: Boolean, reflect: true })
-	enlarged = false;
-
-	@property({ attribute: 'some-attribute', reflect: true })
-	someAttribute = null;
-
-	protected updated(changes: Map<string, boolean>): void {
-		super.updated();
-	}
-
-	connectedCallback(): void {
-		super.connectedCallback();
-	}
-
-	disconnectedCallback(): void {
-		super.disconnectedCallback();
-	}
+export class VWCCalendar extends LitElement {
+	/**
+	 * @internal
+	 * */
+	static styles = style;
 }
