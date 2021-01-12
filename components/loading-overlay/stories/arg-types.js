1068a8e782bf003d95cc170350ae188c7049d4b3
Yuri Guller <gullerya@gmail.com>
2021-01-12 10:32:07 +0200

    adding few more bits to doc

--- components/loading-overlay/stories/arg-types.js
+++ components/loading-overlay/stories/arg-types.js
@@ -1,18 +1,4 @@
 export const argTypes = {
-	delay: {
-		control: {
-			type: 'number',
-			defaultValue: 360,
-			description: 'Number of millis to wait before showing \'entertaining\' content'
-		}
-	},
-	timeout: {
-		control: {
-			type: 'number',
-			defaultValue: 12000,
-			description: 'Number of millis of maximum time the overlay is shown'
-		}
-	},
 	floatingLabelFoundation: { table: { disable: true } },
 	lineRippleFoundation: { table: { disable: true } },
 	ripple: { table: { disable: true } }
