0c1ed04c7a40bb5ddfd989f5d7fab35106c00f25
yonatankra <yonatan.kra@vonage.com>
2021-01-17 10:48:16 +0200

    Setup the dialog in the storybook

--- components/dialog/stories/dialog.stories.js
+++ components/dialog/stories/dialog.stories.js
@@ -1,15 +1,35 @@
 import '@vonage/vwc-dialog/vwc-dialog.js';
+import '@vonage/vwc-button';
 import { html } from 'lit-element';
 import { spread } from '@open-wc/lit-helpers';
 import { argTypes } from './arg-types.js';
 
 export default {
-  title: 'Atoms/Dialog',
-  component: 'vwc-dialog',
-  argTypes
+	title: 'Cells/Dialog',
+	component: 'vwc-dialog',
+	argTypes
 };
 
-const Template = args => html`<vwc-dialog ...=${spread(args)}></vwc-dialog>`;
+const Template = args => html`
+	<vwc-button @click="${handleOpenDialogClick}">Open dialog</vwc-button>
+	<vwc-dialog ...=${spread(args)}>
+		<div>Discard draft?</div>
+		<mwc-button
+			slot="primaryAction"
+			dialogAction="discard">
+			Discard
+		</mwc-button>
+		<mwc-button
+			slot="secondaryAction"
+			dialogAction="cancel">
+			Cancel
+		</mwc-button>
+	</vwc-dialog>
+`;
 
 export const Basic = Template.bind({});
-Basic.args = { label: 'Basic', disabled: 'false' };
+Basic.args = { id: 'dialog-a' };
+
+function handleOpenDialogClick(e) {
+	e.target.parentNode.querySelector('#dialog-a').show();
+}
