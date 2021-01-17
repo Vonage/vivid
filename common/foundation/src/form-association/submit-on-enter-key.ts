db7c4cfb315c55c95ca6da7487d6ef08e5efb591
yonatankra <yonatan.kra@vonage.com>
2021-01-17 10:18:49 +0200

    Prettier

--- common/foundation/src/form-association/submit-on-enter-key.ts
+++ common/foundation/src/form-association/submit-on-enter-key.ts
@@ -2,18 +2,18 @@ import { requestSubmit } from '../form-association';
 import { getFormByIdOrClosest } from './common';
 
 export function submitOnEnter(element: HTMLInputElement): void {
-	element.addEventListener(
-		'keydown',
-		function (this: HTMLInputElement, event: KeyboardEvent) {
-			const form = getFormByIdOrClosest(this);
-			if (!form) {
-				return;
-			}
-
-			if (event.key === 'Enter') {
-				event.preventDefault();
-				requestSubmit(form);
-			}
+	element.addEventListener('keydown', function (
+		this: HTMLInputElement,
+		event: KeyboardEvent
+	) {
+		const form = getFormByIdOrClosest(this);
+		if (!form) {
+			return;
 		}
-	);
+
+		if (event.key === 'Enter') {
+			event.preventDefault();
+			requestSubmit(form);
+		}
+	});
 }
