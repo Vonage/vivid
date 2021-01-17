ca6c040716b1c6220dcf6e02f08d1a1c36c905a9
yonatankra <yonatan.kra@vonage.com>
2021-01-17 14:20:59 +0200

    Change naming conventions

--- components/dialog/src/vwc-dialog.ts
+++ components/dialog/src/vwc-dialog.ts
@@ -1,21 +1,21 @@
 import { customElement } from 'lit-element';
 import { style } from './vwc-dialog.css';
-import { Dialog as MwcDialog } from '@material/mwc-dialog';
+import { Dialog as MWCDialog } from '@material/mwc-dialog';
 import { style as mwcDialogStyle } from '@material/mwc-dialog/mwc-dialog-css';
 import { style as styleCoupling } from '@vonage/vvd-style-coupling';
 
 declare global {
 	interface HTMLElementTagNameMap {
-		'vwc-dialog': VwcDialog;
+		'vwc-dialog': VWCDialog;
 	}
 }
 
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 // @ts-ignore
-MwcDialog.styles = [styleCoupling, mwcDialogStyle, style];
+VWCDialog.styles = [styleCoupling, mwcDialogStyle, style];
 
 @customElement('vwc-dialog')
-export class VwcDialog extends MwcDialog {
+export class VWCDialog extends MWCDialog {
 	connectedCallback(): void {
 		super.connectedCallback();
 	}
