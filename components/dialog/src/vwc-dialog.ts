8d5b70bebfc1d0b052a80881cbad7007b4a5680c
yonatankra <yonatan.kra@vonage.com>
2021-01-17 08:50:10 +0200

    Dialog now extends mwc-dialog

--- components/dialog/src/vwc-dialog.ts
+++ components/dialog/src/vwc-dialog.ts
@@ -1,5 +1,9 @@
-import { customElement, LitElement, property } from 'lit-element';
+import { customElement } from 'lit-element';
 import { style } from './vwc-dialog.css';
+import { Dialog as MwcDialog } from '@material/mwc-dialog'
+import { style as mwcDialogStyle } from '@material/mwc-dialog/mwc-dialog-css';
+import { Button as MWCButton } from '@material/mwc-button';
+import { style as styleCoupling } from '@vonage/vvd-style-coupling';
 
 declare global {
 	interface HTMLElementTagNameMap {
@@ -7,22 +11,12 @@ declare global {
 	}
 }
 
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-ignore
+MWCButton.styles = [styleCoupling, mwcDialogStyle, style];
+
 @customElement('vwc-dialog')
-export class VwcDialog extends LitElement {
-	/**
-	 * @internal
-	 */
-	static styles = style;
-
-	@property({ type: Boolean, reflect: true })
-	enlarged = false;
-
-	@property({ attribute: 'some-attribute', reflect: true })
-	someAttribute = null;
-
-	protected updated(changes: Map<string, boolean>): void {
-		super.updated();
-	}
+export class VwcDialog extends MwcDialog {
 
 	connectedCallback(): void {
 		super.connectedCallback();
