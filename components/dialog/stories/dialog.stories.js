620543fb29c9b6aca4098f6589cd0f094b8dd0a9
Yonatan Kra <yonatan.kra@vonage.com>
2021-01-17 12:55:09 +0200

    Apply suggestions from code review
    Co-authored-by: yinon <yinon@hotmail.com>

--- components/dialog/stories/dialog.stories.js
+++ components/dialog/stories/dialog.stories.js
@@ -5,7 +5,7 @@ import { spread } from '@open-wc/lit-helpers';
 import { argTypes } from './arg-types.js';
 
 export default {
-	title: 'Cells/Dialog',
+	title: 'Components/Atoms/Calendar',
 	component: 'vwc-dialog',
 	argTypes
 };
