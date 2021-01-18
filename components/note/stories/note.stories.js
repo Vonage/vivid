8ecef1534548ae691c8536387a0bb8cf83123c4b
Yuri Guller <gullerya@gmail.com>
2021-01-18 16:43:34 +0200

    issue #587: fixing note message layout

--- components/note/stories/note.stories.js
+++ components/note/stories/note.stories.js
@@ -38,7 +38,7 @@ const TemplateShort = args => html`
 export const ShortMessage = TemplateShort.bind({});
 ShortMessage.args = { connotation: 'info', icon: 'megaphone' };
 
-const TemplateComplex = args => html`
+const TemplateComplexA = args => html`
 	<vwc-note ...=${spread(args)}>
 		<div>
 			Pascal argues that a rational person should live as though God exists and seek to believe in God. If God does not actually exist, such a person will have only a finite loss (some pleasures, luxury, etc.), whereas if God does exist, he stands to receive infinite gains (as represented by eternity in Heaven) and avoid infinite losses (eternity in Hell).
@@ -49,5 +49,14 @@ const TemplateComplex = args => html`
 	</vwc-note>
 `;
 
-export const ComplexMessage = TemplateComplex.bind({});
-ComplexMessage.args = { connotation: 'info', icon: 'megaphone', header: `Pascal's theological argument` };
+export const ComplexMessageBlocks = TemplateComplexA.bind({});
+ComplexMessageBlocks.args = { connotation: 'info', icon: 'megaphone', header: `Pascal's theological argument` };
+
+const TemplateComplexB = args => html`
+	<vwc-note ...=${spread(args)}>
+		Pascal arguesthat a rational person should live as though God exists and seek to believe in God. If God does not actually exist, such a person will have only a finite loss (some pleasures, luxury, etc.), whereas if God does exist, he stands to receive infinite gains (as represented by eternity in Heaven) and avoid infinite losses (eternity in Hell). Read more <a href="https://en.wikipedia.org/wiki/Pascal%27s_wager#:~:text=Pascal's%20wager%20is%20an%20argument,Pascal%20(1623%E2%80%931662).&text=Pascal%20argues%20that%20a%20rational,seek%20to%20believe%20in%20God.">here</a>.
+	</vwc-note>
+`;
+
+export const ComplexMessageInlines = TemplateComplexB.bind({});
+ComplexMessageInlines.args = { connotation: 'alert', icon: 'megaphone', header: `Pascal's theological argument` };
