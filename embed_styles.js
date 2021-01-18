fcd3ff5ce1886b9ef69dd36a7ed7309f9a0d45ee
Tal Weinfeld <tal.weinfeld@vonage.com>
2021-01-18 14:03:09 +0200

    temporary build script

---
+++ embed_styles.js
@@ -0,0 +1,65 @@
+const
+	_ = require('lodash'),
+	fp = require('lodash/fp'),
+	glob = require('glob'),
+	kefir = require('kefir'),
+	{ join, dirname } = require('path'),
+	{ readFile, writeFile } = require('fs');
+
+const UMBRELLA_PACKAGE_NAME = "@vonage/vvd-umbrella";
+
+const stringify = (json)=> JSON.stringify(json, null, 2);
+
+const globStream = (globPattern)=> kefir.fromNodeCallback((cb)=> glob(globPattern, { absolute: true }, cb)).flatten();
+
+const packageStream = kefir
+	.merge(["common", "components"].map((folder)=> globStream(join(folder, '**', 'package.json'))))
+	.filter((filepath)=> !filepath.includes('node_modules'))
+	.flatMap((filepath)=> kefir
+		.fromNodeCallback((cb)=> readFile(filepath, 'utf8', cb))
+		.map(fp.pipe(JSON.parse, (packageJson)=> ({ filename: filepath, package: packageJson })))
+	)
+	.filter(fp.pipe(fp.get('package.name'), fp.eq(UMBRELLA_PACKAGE_NAME), fp.negate(fp.identity)));
+
+const tsStream = packageStream
+	.flatMap(({ filename, package })=> {
+		const tsConfigFilename = join(dirname(filename), 'tsconfig.json');
+		return kefir
+			.fromNodeCallback((cb)=> readFile(tsConfigFilename, 'utf8', cb))
+			.ignoreErrors()
+			.map(fp.pipe(JSON.parse, (json)=> ({ filename: tsConfigFilename, tsconfig: json })));
+	});
+
+kefir
+	.merge([
+		tsStream.map(({ filename, tsconfig: { references, "extends": ext, ...rest}})=> ({ filename, content: { "extends": [UMBRELLA_PACKAGE_NAME, 'configs', 'tsconfig.json'].join('/'), ...rest } })),
+		packageStream
+			.map(({ package, filename })=> {
+				return {
+					content: _.merge(package, {
+						"devDependencies": {
+							[UMBRELLA_PACKAGE_NAME]: "^1.0.2",
+							"typescript": "^4.1.3",
+						},
+						"scripts": {
+							"build:typescript": "tsc",
+							"build:style-modules": "umbrella-style-modules",
+							"build": _([
+								"yarn run build:style-modules",
+								"yarn run build:typescript",
+								_.get(package, 'scripts.build')
+							]).compact().join(' && '),
+						},
+					}),
+					filename
+				};
+			})
+	])
+	.log();
+
+	/*.flatMap(()=> {
+		return kefir
+			.fromNodeCallback((cb) => writeFile(filename, JSON.stringify(package, null, '  '), cb))
+			.map(fp.always(filename));
+	})*/
+	//.log();
