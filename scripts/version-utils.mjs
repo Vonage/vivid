68b8fbe30938cbc4cae14894db3e294f0538d912
Yuri Guller <gullerya@gmail.com>
2021-01-19 21:36:31 +0200

    issue #591: added script to get suggestion

--- scripts/version-utils.mjs
+++ scripts/version-utils.mjs
@@ -1,17 +1,23 @@
-import os from 'os';
-import { exec } from 'child_process';
 import standardVersion from 'standard-version';
+
+const log = [];
+const originalConsoleInfo = console.info;
+console.info = message => log.push(message);
 
 standardVersion({
 	bumpFiles: [{ filename: 'lerna.json', type: 'json' }],
 	packageFiles: [{ filename: 'lerna.json', type: 'json' }],
+	dryRun: true,
 	skip: {
-		bump: true,
+		changelog: true,
 		commit: true,
 		tag: true
 	}
-}).then(r => {
-	console.log(r);
-}).catch(err => {
-	console.error(`standard-version failed with message: ${err.message}`)
-})
+}).then(() => {
+	console.info = originalConsoleInfo;
+
+	const proposalLogLine = log.find(logLine => logLine.includes('bumping version in lerna.json'));
+	const proposedVersion = proposalLogLine.match(/.*(to.+m)(?<candidate>[0-9.]+)(.+m)$/).groups.candidate;
+
+	console.info(proposedVersion);
+});
