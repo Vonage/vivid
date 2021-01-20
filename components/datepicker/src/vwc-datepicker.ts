cf468194fe1a943f5c4bb1756e4273e87a0c1e22
joelgraham93 <j.r.g@hotmail.com>
2021-01-20 20:16:54 +0000

    Fix bug preventing all days to be enabled by default

--- components/datepicker/src/vwc-datepicker.ts
+++ components/datepicker/src/vwc-datepicker.ts
@@ -12,4 +12,51 @@ declare global {
  * This component is an extension of [<lit-flatpickr>](https://github.com/Matsuuu/lit-flatpickr)
  */
 @customElement('vwc-datepicker')
-export class VWCDatepicker extends LitFlatpickr {}
+export class VWCDatepicker extends LitFlatpickr {
+	// override LitFlatpickr to work with flatpickr dependency
+	// enable key needs to be undefined to enable all days by default
+	getOptions() {
+		return {
+			altFormat: this.altFormat,
+			altInput: this.altInput,
+			altInputClass: this.altInputClass,
+			allowInput: this.allowInput,
+			ariaDateFormat: this.ariaDateFormat,
+			clickOpens: this.clickOpens,
+			dateFormat: this.dateFormat,
+			defaultDate: this.defaultDate,
+			defaultHour: this.defaultHour,
+			defaultMinute: this.defaultMinute,
+			disable: this.disable,
+			disableMobile: this.disableMobile,
+			// enable: this.enable,
+			enableTime: this.enableTime,
+			enableSeconds: this.enableSeconds,
+			formatDate: this.formatDateFn,
+			hourIncrement: this.hourIncrement,
+			inline: this.inline,
+			maxDate: this.maxDate,
+			minDate: this.minDate,
+			minuteIncrement: this.minuteIncrement,
+			mode: this.mode,
+			nextArrow: this.nextArrow,
+			prevArrow: this.prevArrow,
+			noCalendar: this.noCalendar,
+			onChange: this.onChange,
+			onClose: this.onClose,
+			onOpen: this.onOpen,
+			onReady: this.onReady,
+			onMonthChange: this.onMonthChange,
+			onYearChange: this.onYearChange,
+			onValueUpdate: this.onValueUpdate,
+			parseDate: this.parseDateFn,
+			position: this.position,
+			shorthandCurrentMonth: this.shorthandCurrentMonth,
+			showMonths: this.showMonths,
+			static: this.static,
+			time_24hr: this.time_24hr,
+			weekNumbers: this.weekNumbers,
+			wrap: this.wrap,
+		};
+	}
+}
