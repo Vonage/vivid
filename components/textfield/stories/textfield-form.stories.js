33ba5f79a2b9427ade4d0244fe2b4c38d25753a4
yinonov <yinon@hotmail.com>
2021-01-24 15:23:15 +0200

    native input layout style coverage 90%

--- components/textfield/stories/textfield-form.stories.js
+++ components/textfield/stories/textfield-form.stories.js
@@ -5,23 +5,19 @@ import { spread } from '@open-wc/lit-helpers';
 
 const Template = args => html`
 	<style>
-		.login-form {
-			display: flex;
-			flex-direction: column;
-		}
-
-		.login-field {
-			min-height: 60px;
+		form {
+			display: grid;
+			gap: 20px;
 		}
 	</style>
-	<form class="login-form">
-		<vwc-textfield class="login-field" ...=${spread(args.username)}></vwc-textfield>
-		<vwc-textfield class="login-field" ...=${spread(args.password)}></vwc-textfield>
+	<form>
+		<vwc-textfield ...=${spread(args.username)}></vwc-textfield>
+		<vwc-textfield ...=${spread(args.password)}></vwc-textfield>
 	</form>
 `;
 
 export const LoginForm = Template.bind({});
 LoginForm.args = {
-	username: { id: 'username', label: 'username' },
-	password: { id: 'password', label: 'password', type: 'password' }
+	username: { id: 'username', label: 'username', icon: 'user' },
+	password: { id: 'password', label: 'password', type: 'password', icon: 'lock' }
 };
