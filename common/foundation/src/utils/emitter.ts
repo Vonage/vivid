f2f1d4ee7de1c546a886eb338d474a9297eafb6f
yinonov <yinon@hotmail.com>
2021-01-24 13:02:43 +0200

    refactor(vvd-foundation): private member, from convention to syntax

--- common/foundation/src/utils/emitter.ts
+++ common/foundation/src/utils/emitter.ts
@@ -1,16 +1,16 @@
 export class Emitter {
-	private _delegate = document.createDocumentFragment();
+	#delegate = document.createDocumentFragment();
 
 	addEventListener(
 		type: string,
 		listener: EventListener | EventListenerObject | null,
 		options?: boolean | AddEventListenerOptions | undefined
 	): void {
-		this._delegate.addEventListener(type, listener, options);
+		this.#delegate.addEventListener(type, listener, options);
 	}
 
 	dispatchEvent(event: Event): void {
-		this._delegate.dispatchEvent(event);
+		this.#delegate.dispatchEvent(event);
 	}
 
 	removeEventListener(
@@ -18,6 +18,6 @@ export class Emitter {
 		callback: EventListener | EventListenerObject | null,
 		options?: boolean | EventListenerOptions | undefined
 	): void {
-		this._delegate.removeEventListener(type, callback, options);
+		this.#delegate.removeEventListener(type, callback, options);
 	}
 }
