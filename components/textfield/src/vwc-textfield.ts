284a5f003ca98a26ba12cd91a13772c902635766
Yuri Guller <gullerya@gmail.com>
2021-01-25 12:33:13 +0200

    feat: autofill solution (#599)
    * Return a slot on render
    
    * Make the binding between input.value and this.value
    
    * some fixes to the event handling
    
    * fixed some styles, to see we are on trach - only positioning left
    
    * finalized CSS adjust
    
    * last touches, removing that beige stuff
    
    * style(textfiled): input padding - cover all cases
    
    * keep cursor's agent style sheet definition
    
    * fixing the calendar snapshot
    
    * fixing the tests: snapshot + typography
    
    * fixing the autofocus tests
    
    * fixing autofocus tests - remove only and use addElement
    
    * native input layout style coverage 90%
    
    * Fix all except the reset form
    
    * Fix autofocus stuff
    
    * fixing the reset?
    
    * added test validating the light DOM
    
    * css slotted affect
    
    * adding form logic for convenience
    
    * removed 'value' getter & setter
    
    * temp - revert to get/set - not final
    
    * fixing the floating label
    
    * call layout after value change
    
    * duplicate selector
    
    * fixed the form story
    
    * fixing the Sonar remarks
    
    * textfield with icons styling
    
    * back the 14px to be aligned
    
    * adding last attributes and arrangin the clode
    
    * z-index as autofill-only concern
    
    * get background for autofill ready
    
    * Test elements cleanup
    
    * fixing Yonatan remark
    
    Co-authored-by: yonatankra <yonatan.kra@vonage.com>
    Co-authored-by: yinonov <yinon@hotmail.com>

--- components/textfield/src/vwc-textfield.ts
+++ components/textfield/src/vwc-textfield.ts
@@ -9,15 +9,10 @@ import {
 	TemplateResult,
 	PropertyValues,
 } from 'lit-element';
-import { mapToClasses } from '@vonage/vvd-foundation/class-utils.js';
 import { TextField as MWCTextField } from '@material/mwc-textfield';
 import { style as styleCoupling } from '@vonage/vvd-style-coupling/vvd-style-coupling.css.js';
 import { style as vwcTextFieldStyle } from './vwc-textfield.css';
 import { style as mwcTextFieldStyle } from '@material/mwc-textfield/mwc-textfield-css.js';
-import {
-	associateWithForm,
-	submitOnEnter,
-} from '@vonage/vvd-foundation/form-association';
 import { Shape } from '@vonage/vvd-foundation/constants';
 import { handleAutofocus } from '@vonage/vvd-foundation/general-utils';
 export { TextFieldType } from '@material/mwc-textfield';
@@ -34,6 +29,7 @@ type TextfieldShape = Extract<Shape, Shape.Rounded | Shape.Pill>;
 // @ts-ignore
 MWCTextField.styles = [styleCoupling, mwcTextFieldStyle, vwcTextFieldStyle];
 
+const INPUT_ELEMENT_SLOT_NAME = 'formInputElement';
 @customElement('vwc-textfield')
 export class VWCTextField extends MWCTextField {
 	@property({ type: Boolean, reflect: true })
@@ -45,14 +41,37 @@ export class VWCTextField extends MWCTextField {
 	@property({ type: String, reflect: true })
 	form: string | undefined;
 
+	constructor() {
+		super();
+		Object.defineProperty(this, 'formElement', {
+			value: this.createInputElement(),
+		});
+		Object.defineProperty(this, 'value', {
+			get: function () {
+				return this.formElement.value;
+			},
+			set: function (newValue: string) {
+				this.formElement.value = newValue;
+				this.layout();
+			},
+		});
+	}
+
+	connectedCallback(): void {
+		super.connectedCallback();
+		if (!this.hasAttribute('outlined')) {
+			this.outlined = true;
+		}
+		this.formElement.value = this.value;
+		this.appendChild(this.formElement);
+	}
+
 	async firstUpdated(): Promise<void> {
 		await super.firstUpdated();
 		this.shadowRoot
 			?.querySelector('.mdc-notched-outline')
 			?.shadowRoot?.querySelector('.mdc-notched-outline')
 			?.classList.add('vvd-notch');
-		associateWithForm(this, this.formElement);
-		submitOnEnter((this as unknown) as HTMLInputElement);
 		handleAutofocus(this);
 	}
 
@@ -63,25 +82,24 @@ export class VWCTextField extends MWCTextField {
 		}
 	}
 
+	protected renderInput(shouldRenderHelperText: boolean): TemplateResult {
+		this.updateInputElement(shouldRenderHelperText);
+		return html`
+			<div class="mdc-text-field__input"></div>
+			<slot name="${INPUT_ELEMENT_SLOT_NAME}"></slot>
+		`;
+	}
+
 	protected renderIcon(icon: string, isTrailingIcon = false): TemplateResult {
-		const classes = {
-			'mdc-text-field__icon--leading': !isTrailingIcon,
-			'mdc-text-field__icon--trailing': isTrailingIcon,
-		};
+		const iconClass = isTrailingIcon
+			? 'mdc-text-field__icon--trailing'
+			: 'mdc-text-field__icon--leading';
 
 		return html`<vwc-icon
 			type="${icon}"
 			size="small"
-			class="${mapToClasses(classes).join(' ')}"
+			class="${iconClass}"
 		></vwc-icon>`;
-	}
-
-	protected renderRipple(): TemplateResult {
-		return html``;
-	}
-
-	protected renderLineRipple(): TemplateResult {
-		return html``;
 	}
 
 	protected renderOutline(): TemplateResult | string {
@@ -92,7 +110,9 @@ export class VWCTextField extends MWCTextField {
 			  </vwc-notched-outline>`;
 	}
 
-	renderHelperText(shouldRenderHelperText: boolean): TemplateResult | string {
+	protected renderHelperText(
+		shouldRenderHelperText: boolean
+	): TemplateResult | string {
 		if (!shouldRenderHelperText) {
 			return '';
 		}
@@ -107,5 +127,85 @@ export class VWCTextField extends MWCTextField {
 			?is-error="${isError}"
 			>${text}</vwc-helper-message
 		>`;
+	}
+
+	protected renderRipple(): TemplateResult {
+		return html``;
+	}
+
+	protected renderLineRipple(): TemplateResult {
+		return html``;
+	}
+
+	private createInputElement(): HTMLInputElement {
+		const element = document.createElement('input');
+		const defaultValue = this.getAttribute('value');
+		element.defaultValue = defaultValue ? defaultValue : '';
+		element.setAttribute('slot', INPUT_ELEMENT_SLOT_NAME);
+		return element;
+	}
+
+	private updateInputElement(shouldRenderHelperText: boolean): void {
+		const fe = this.formElement;
+
+		//	event listeners
+		fe.onfocus = this.onInputFocus.bind(this);
+		fe.onblur = this.onInputBlur.bind(this);
+
+		//	attributes
+		setAttributeIfDefined('id', this.id, fe);
+		setAttributeIfDefined('name', this.name, fe);
+		setAttributeIfDefined('type', this.type, fe);
+		setAttributeIfDefined('form', this.form, fe);
+		setAttributeIfDefined('placeholder', this.placeholder, fe);
+
+		setAttributeIfDefined('min', this.min, fe);
+		setAttributeIfDefined('max', this.max, fe);
+		setAttributeIfDefined('step', this.step, fe);
+		setAttributeIfDefined('size', this.size, fe);
+
+		const autoCapOrNone = this.autocapitalize ? this.autocapitalize : undefined;
+		const minOrNone = this.minLength === -1 ? undefined : this.minLength;
+		const maxOrNone = this.maxLength === -1 ? undefined : this.maxLength;
+		setAttributeIfDefined('autocapitalize', autoCapOrNone, fe);
+		setAttributeIfDefined('minlength', minOrNone, fe);
+		setAttributeIfDefined('maxlength', maxOrNone, fe);
+		setAttributeIfDefined('pattern', this.pattern, fe);
+		setAttributeIfDefined('inputmode', this.inputMode, fe);
+
+		addAttributeByCondition('disabled', this.disabled, fe);
+		addAttributeByCondition('readonly', this.readOnly, fe);
+		addAttributeByCondition('required', this.required, fe);
+
+		const ariaLabel = shouldRenderHelperText ? 'helper-text' : undefined;
+		const ariaError =
+			this.validationMessage && !this.isUiValid ? 'helper-text' : undefined;
+		setAttributeIfDefined('aria-controls', ariaLabel, fe);
+		setAttributeIfDefined('aria-describedby', ariaLabel, fe);
+		setAttributeIfDefined('aria-errortext', ariaError, fe);
+	}
+}
+
+function addAttributeByCondition(
+	attributeName: string,
+	condition: boolean,
+	target: HTMLInputElement
+): void {
+	if (condition) {
+		target.setAttribute(attributeName, '');
+	} else {
+		target.removeAttribute(attributeName);
+	}
+}
+
+function setAttributeIfDefined(
+	attributeName: string,
+	value: unknown,
+	target: HTMLInputElement
+): void {
+	if (value) {
+		target.setAttribute(attributeName, String(value));
+	} else {
+		target.removeAttribute(attributeName);
 	}
 }
