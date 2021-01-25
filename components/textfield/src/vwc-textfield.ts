89154c89ec4dd8dc1a242451de200008e8946618
Yuri Guller <gullerya@gmail.com>
2021-01-25 08:11:41 +0200

    adding last attributes and arrangin the clode

--- components/textfield/src/vwc-textfield.ts
+++ components/textfield/src/vwc-textfield.ts
@@ -9,7 +9,6 @@ import {
 	TemplateResult,
 	PropertyValues,
 } from 'lit-element';
-import { mapToClasses } from '@vonage/vvd-foundation/class-utils.js';
 import { TextField as MWCTextField } from '@material/mwc-textfield';
 import { style as styleCoupling } from '@vonage/vvd-style-coupling/vvd-style-coupling.css.js';
 import { style as vwcTextFieldStyle } from './vwc-textfield.css';
@@ -42,70 +41,6 @@ export class VWCTextField extends MWCTextField {
 	@property({ type: String, reflect: true })
 	form: string | undefined;
 
-	async firstUpdated(): Promise<void> {
-		await super.firstUpdated();
-		this.shadowRoot
-			?.querySelector('.mdc-notched-outline')
-			?.shadowRoot?.querySelector('.mdc-notched-outline')
-			?.classList.add('vvd-notch');
-		handleAutofocus(this);
-	}
-
-	updated(changedProperties: PropertyValues): void {
-		super.updated(changedProperties);
-		if (this.shape === 'pill') {
-			this.dense = true;
-		}
-	}
-
-	protected renderIcon(icon: string, isTrailingIcon = false): TemplateResult {
-		const classes = {
-			'mdc-text-field__icon--leading': !isTrailingIcon,
-			'mdc-text-field__icon--trailing': isTrailingIcon,
-		};
-
-		return html`<vwc-icon
-			type="${icon}"
-			size="small"
-			class="${mapToClasses(classes).join(' ')}"
-		></vwc-icon>`;
-	}
-
-	protected renderRipple(): TemplateResult {
-		return html``;
-	}
-
-	protected renderLineRipple(): TemplateResult {
-		return html``;
-	}
-
-	protected renderOutline(): TemplateResult | string {
-		return !this.outlined
-			? ''
-			: html`<vwc-notched-outline class="mdc-notched-outline vvd-notch">
-					${this.renderLabel()}
-			  </vwc-notched-outline>`;
-	}
-
-	renderHelperText(shouldRenderHelperText: boolean): TemplateResult | string {
-		if (!shouldRenderHelperText) {
-			return '';
-		}
-
-		const isError = this.validationMessage && !this.isUiValid;
-		const text = isError ? this.validationMessage : this.helper;
-
-		return html`<vwc-helper-message
-			id="helper-text"
-			class="helper-message"
-			?disabled="${this.disabled}"
-			?is-error="${isError}"
-			>${text}</vwc-helper-message
-		>`;
-	}
-
-	// autofill refactor enhancements
-
 	constructor() {
 		super();
 		Object.defineProperty(this, 'formElement', {
@@ -131,12 +66,75 @@ export class VWCTextField extends MWCTextField {
 		this.appendChild(this.formElement);
 	}
 
-	protected renderInput(): TemplateResult {
-		this.updateInputElement();
+	async firstUpdated(): Promise<void> {
+		await super.firstUpdated();
+		this.shadowRoot
+			?.querySelector('.mdc-notched-outline')
+			?.shadowRoot?.querySelector('.mdc-notched-outline')
+			?.classList.add('vvd-notch');
+		handleAutofocus(this);
+	}
+
+	updated(changedProperties: PropertyValues): void {
+		super.updated(changedProperties);
+		if (this.shape === 'pill') {
+			this.dense = true;
+		}
+	}
+
+	protected renderInput(shouldRenderHelperText: boolean): TemplateResult {
+		this.updateInputElement(shouldRenderHelperText);
 		return html`
 			<div class="mdc-text-field__input"></div>
 			<slot name="${INPUT_ELEMENT_SLOT_NAME}"></slot>
 		`;
+	}
+
+	protected renderIcon(icon: string, isTrailingIcon = false): TemplateResult {
+		const iconClass = isTrailingIcon
+			? 'mdc-text-field__icon--trailing'
+			: 'mdc-text-field__icon--leading';
+
+		return html`<vwc-icon
+			type="${icon}"
+			size="small"
+			class="${iconClass}"
+		></vwc-icon>`;
+	}
+
+	protected renderOutline(): TemplateResult | string {
+		return !this.outlined
+			? ''
+			: html`<vwc-notched-outline class="mdc-notched-outline vvd-notch">
+					${this.renderLabel()}
+			  </vwc-notched-outline>`;
+	}
+
+	protected renderHelperText(
+		shouldRenderHelperText: boolean
+	): TemplateResult | string {
+		if (!shouldRenderHelperText) {
+			return '';
+		}
+
+		const isError = this.validationMessage && !this.isUiValid;
+		const text = isError ? this.validationMessage : this.helper;
+
+		return html`<vwc-helper-message
+			id="helper-text"
+			class="helper-message"
+			?disabled="${this.disabled}"
+			?is-error="${isError}"
+			>${text}</vwc-helper-message
+		>`;
+	}
+
+	protected renderRipple(): TemplateResult {
+		return html``;
+	}
+
+	protected renderLineRipple(): TemplateResult {
+		return html``;
 	}
 
 	private createInputElement(): HTMLInputElement {
@@ -147,19 +145,14 @@ export class VWCTextField extends MWCTextField {
 		return element;
 	}
 
-	//	not yet implemented:
-	// aria-controls="${ifDefined(ariaControlsOrUndef)}"
-	// aria-describedby="${ifDefined(ariaDescribedbyOrUndef)}"
-	// aria-errortext="${ifDefined(ariaErrortextOrUndef)}"
-	// autocapitalize="${ifDefined(autocapitalizeOrUndef)}"
-	private updateInputElement(): void {
+	private updateInputElement(shouldRenderHelperText: boolean): void {
 		const fe = this.formElement;
 
 		//	event listeners
 		fe.onfocus = this.onInputFocus.bind(this);
 		fe.onblur = this.onInputBlur.bind(this);
 
-		//	attributes - basic
+		//	attributes
 		setAttributeIfDefined('id', this.id, fe);
 		setAttributeIfDefined('name', this.name, fe);
 		setAttributeIfDefined('type', this.type, fe);
@@ -171,22 +164,25 @@ export class VWCTextField extends MWCTextField {
 		setAttributeIfDefined('step', this.step, fe);
 		setAttributeIfDefined('size', this.size, fe);
 
-		setAttributeIfDefined('inputmode', this.inputMode, fe);
-		setAttributeIfDefined(
-			'minlength',
-			this.minLength === -1 ? undefined : this.minLength,
-			fe
-		);
-		setAttributeIfDefined(
-			'maxlength',
-			this.maxLength === -1 ? undefined : this.maxLength,
-			fe
-		);
+		const autoCapOrNone = this.autocapitalize ? this.autocapitalize : undefined;
+		const minOrNone = this.minLength === -1 ? undefined : this.minLength;
+		const maxOrNone = this.maxLength === -1 ? undefined : this.maxLength;
+		setAttributeIfDefined('autocapitalize', autoCapOrNone, fe);
+		setAttributeIfDefined('minlength', minOrNone, fe);
+		setAttributeIfDefined('maxlength', maxOrNone, fe);
 		setAttributeIfDefined('pattern', this.pattern, fe);
+		setAttributeIfDefined('inputmode', this.inputMode, fe);
 
 		addAttributeByCondition('disabled', this.disabled, fe);
 		addAttributeByCondition('readonly', this.readOnly, fe);
 		addAttributeByCondition('required', this.required, fe);
+
+		const ariaLabel = shouldRenderHelperText ? 'helper-text' : undefined;
+		const ariaError =
+			this.validationMessage && !this.isUiValid ? 'helper-text' : undefined;
+		setAttributeIfDefined('aria-controls', ariaLabel, fe);
+		setAttributeIfDefined('aria-describedby', ariaLabel, fe);
+		setAttributeIfDefined('aria-errortext', ariaError, fe);
 	}
 }
 
