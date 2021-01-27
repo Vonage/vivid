c4e30582ea4b0c2e06a2ca54ddaba21889e9015c
yonatankra <yonatan.kra@vonage.com>
2021-01-27 18:05:19 +0200

    Some design stuff

--- components/dialog/src/vwc-dialog.ts
+++ components/dialog/src/vwc-dialog.ts
@@ -1,4 +1,4 @@
-import { customElement } from 'lit-element';
+import { customElement, PropertyValues } from 'lit-element';
 import { style } from './vwc-dialog.css';
 import { Dialog as MWCDialog } from '@material/mwc-dialog';
 import { style as mwcDialogStyle } from '@material/mwc-dialog/mwc-dialog-css';
@@ -10,12 +10,28 @@ declare global {
 	}
 }
 
+const iconTemplate = document.createElement('template');
+iconTemplate.innerHTML =
+	`
+	<div id="dialog_icon">
+		<slot name="icon"></slot>
+</div>
+`;
+
+
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 // @ts-ignore
 MWCDialog.styles = [styleCoupling, mwcDialogStyle, style];
 
 @customElement('vwc-dialog')
 export class VWCDialog extends MWCDialog {
+	protected updated(_changedProperties: PropertyValues) {
+		super.updated(_changedProperties);
+		if (!this.renderRoot.querySelector('#dialog_icon')) {
+			this.renderRoot.querySelector('.mdc-dialog__surface')?.prepend(iconTemplate.content.cloneNode(true));
+		}
+	}
+
 	connectedCallback(): void {
 		super.connectedCallback();
 	}
