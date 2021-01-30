db28dba6444bb680f646417f6c058a04255b713d
yinonov <yinon@hotmail.com>
2021-01-30 13:16:32 +0200

    configs sync

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,10 +4,6 @@ module.exports = {
     "extends": [
         "stylelint-config-idiomatic-order",
         "./node_modules/prettier-stylelint/config.js"
-    ],
-    "rules": {
-        "indentation": 4,
-        "string-quotes": "single"
-    }
-}
+    ]
+	}
 };
