67dbda5f6634a94c0fabaab90704dddf468e88fb
Yuri Guller <gullerya@gmail.com>
2021-02-03 00:01:25 +0200

    feat(vwc-menu): issue #592 menu docs and better stories (#596)
    * issue #592: menu docs and better stories
    
    * issue #592: fixing some typos
    
    * adding the docs to the select
    
    * adding select documentation
    
    * adding select documentation
    
    * issue #592: docs for menu and more
    
    * issue #592: docs for menu and more
    
    * adding an initial content fo volta to vivid migration

---
+++ docs/stories/guides/context-volta-to-vivid.config.mjs
@@ -0,0 +1,24 @@
+import {
+	hrefLinkProcessing
+} from '../../../.storybook/build-scripts/create-stories-from-md.mjs';
+
+export default {
+	sourcePath: '../../volta-to-vivid.md',
+	outputName: 'context-volta-to-vivid',
+	story: {
+		title: 'Guides/Volta To Vivid Migration',
+		name: 'VoltaToVividMigration',
+		parameters: {
+			options: {
+				showPanel: false,
+				isToolshown: false
+			}
+		}
+	},
+	htmlPostProcess: htmlText => {
+		return hrefLinkProcessing(htmlText, [
+			['href="../readme.md"', '?path=/story/introduction-meet-vivid--meet-vivid'],
+			['href="./architecture.md"', '?path=/story/introduction-architecture--architecture']
+		]);
+	}
+};
