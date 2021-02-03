145ba687f361c7bea59b596710e82cec982c0ff2
Tal Weinfeld <tal.weinfeld@vonage.com>
2021-02-03 12:07:15 +0200

    updated version and build scripts

--- .storybook/icon-manifest-loader.js
+++ .storybook/icon-manifest-loader.js
@@ -2,6 +2,6 @@ const got = require('got');
 
 module.exports = async function(source){
 	const VERSION = JSON.parse(source) || "1.0.0";
-	return got({ url: `https://icons.resources.vonage.com/3f7739a0-a898-4f69-a82b-ad9d743170b6/v${VERSION}/manifest.json`, responseType: 'text', resolveBodyOnly: true })
+	return got({ url: `https://icon.resources.vonage.com/v${VERSION}/manifest.json`, responseType: 'text', resolveBodyOnly: true })
 		.catch(()=> JSON.stringify([]));
 };
