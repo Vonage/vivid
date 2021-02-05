36806c5530b3d24e46e481273d2f9afb78735f4c
yinonov <yinon@hotmail.com>
2021-02-05 16:21:43 +0200

    prettier format

--- docs/stories/introduction-meet-vivid.config.mjs
+++ docs/stories/introduction-meet-vivid.config.mjs
@@ -1,6 +1,6 @@
 import {
 	hrefLinkProcessing,
-	relocateStaticResources
+	relocateStaticResources,
 } from '../../.storybook/build-scripts/create-stories-from-md.mjs';
 
 export default {
@@ -12,24 +12,40 @@ export default {
 		parameters: {
 			options: {
 				showPanel: false,
-				isToolshown: false
-			}
-		}
+				isToolshown: false,
+			},
+		},
 	},
-	mdPreProcess: mdText => {
-		return mdText
-			.replace(/^[^#]*/, '');
+	mdPreProcess: (mdText) => {
+		return mdText.replace(/^[^#]*/, '');
 	},
-	htmlPostProcess: htmlText => {
-		let r = relocateStaticResources(htmlText, ['docs/assets/images/meet-vivid.svg']);
+	htmlPostProcess: (htmlText) => {
+		let r = relocateStaticResources(htmlText, [
+			'docs/assets/images/meet-vivid.svg',
+		]);
 		r = hrefLinkProcessing(r, [
-			['href="docs/getting-started.md"', '?path=/story/introduction-getting-started--getting-started'],
-			['href="docs/architecture.md"', '?path=/story/introduction-architecture--architecture'],
+			[
+				'href="docs/getting-started.md"',
+				'?path=/story/introduction-getting-started--getting-started',
+			],
+			[
+				'href="docs/architecture.md"',
+				'?path=/story/introduction-architecture--architecture',
+			],
 			['href="docs/roadmap.md"', '?path=/story/introduction-roadmap--roadmap'],
-			['href="docs/contact-us.md"', '?path=/story/introduction-contact-us--contact-us'],
-			['href="docs/installation.md"', '?path=/story/guides-installation--installation'],
-			['href="docs/contribution.md"', '?path=/story/guides-contribution--contribution']
+			[
+				'href="docs/contact-us.md"',
+				'?path=/story/introduction-contact-us--contact-us',
+			],
+			[
+				'href="docs/installation.md"',
+				'?path=/story/guides-installation--installation',
+			],
+			[
+				'href="docs/contribution.md"',
+				'?path=/story/guides-contribution--contribution',
+			],
 		]);
 		return r;
-	}
+	},
 };
