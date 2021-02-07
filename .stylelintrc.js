1b9177c57ef3ec715e5ee11dd9ffed5657ffb01d
yinonov <yinon@hotmail.com>
2021-02-07 17:25:29 +0200

    last issue

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -17,6 +17,7 @@ module.exports = {
 		"selector-max-id": null,
 		"selector-class-pattern": null,
 		"max-nesting-depth": null,
+		"selector-max-compound-selectors": null,
 		"a11y/selector-pseudo-class-focus": null,
 		"a11y/no-outline-none": null
 	}
