905b607078afec914571f1086101322d33613e31
yinon <yinon@hotmail.com>
2021-02-10 12:23:17 +0200

    ci(workspace): lit-analyzer linter (#650)
    * chore(workspace): husky update & run 'lint' pre-commit
    
    * format on commit
    
    * lint fix only staged
    
    * lit analyzer 1st catch
    
    Co-authored-by: Yuri Guller <gullerya@gmail.com>

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,6 @@
 module.exports = {
-	'{common,components}/**/*.scss':['stylelint --fix'],
+	'{common,components}/**/*.scss':['stylelint --quiet --fix'],
 	'{common,components}/**': ['eslint --fix --no-error-on-unmatched-pattern'],
-	'{{common,components}/**/*.{ts,scss},**/stories,**/test/*.test.js}': 'prettier --write'
+	'{common,components}/**/src/*.ts': ["lit-analyzer  --strict --quiet --format"],
+	'{{common,components}/**/*.{ts,scss},**/stories,**/test/*.test.js}': ['prettier --write']
 }
