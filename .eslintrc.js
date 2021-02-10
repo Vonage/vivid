0cecd4c46afaf386d44397220a31173d586300c5
yinonov <yinon@hotmail.com>
2021-02-10 14:28:27 +0200

    setup conflicts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-	'extends': [
+	extends: [
 		'eslint:recommended',
 		'plugin:wc/recommended',
 		'@open-wc/eslint-config',
@@ -9,29 +9,26 @@ module.exports = {
 		'plugin:@typescript-eslint/eslint-recommended',
 		// the followings should always be last
 		'prettier',
-		'prettier/@typescript-eslint'
 	],
-	'env': {
-		'browser': true,
-		'node': true
+	env: {
+		browser: true,
+		node: true,
 	},
-	'parser': '@typescript-eslint/parser',
-	'parserOptions': {
-		'ecmaVersion': 12,
-		'sourceType': 'module'
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 12,
+		sourceType: 'module',
 	},
-	'plugins': ['@typescript-eslint', 'mocha', 'wc', 'no-only-tests', 'lit-a11y'],
-	'settings': {
-		'wc': {
-			'elementBaseClasses': ['BaseElement', 'LitElement', 'FormElement']
-		}
+	plugins: ['@typescript-eslint', 'mocha', 'wc', 'no-only-tests', 'lit-a11y'],
+	settings: {
+		wc: {
+			elementBaseClasses: ['BaseElement', 'LitElement', 'FormElement'],
+		},
 	},
-	'rules': {
+	rules: {
 		'no-shadow': 'error',
 		'no-mixed-spaces-and-tabs': 'off',
-		'indent': 'off',
-		'linebreak-style': ['error', 'unix'],
-		'semi': 'off', // ['error', 'always'], //! should ENABLE after PR merge
+		indent: 'off',
 		'no-use-before-define': 'off', //! should remove after PR merge
 		'import/no-extraneous-dependencies': 'off', //! should remove after PR merge
 		'object-shorthand': 'off', //! should remove after PR merge
@@ -55,7 +52,7 @@ module.exports = {
 		'no-restricted-syntax': 'off', //! should remove after PR merge
 		'guard-for-in': 'off', //! should remove after PR merge
 		'no-cond-assign': 'off', //! should remove after PR merge
-		'camelcase': 'off', //! should remove after PR merge
+		camelcase: 'off', //! should remove after PR merge
 		'no-return-assign': 'off', //! should remove after PR merge
 		'no-plusplus': 'off', //! should remove after PR merge
 		'no-nested-ternary': 'off', //! should remove after PR merge
@@ -65,10 +62,10 @@ module.exports = {
 		'no-multi-assign': 'off', //! should remove after PR merge
 		'import/first': 'off', //! should remove after PR merge
 		'no-unused-expressions': 'off', //! should remove after PR merge
-		'eqeqeq': 'off', //! should remove after PR merge
+		eqeqeq: 'off', //! should remove after PR merge
 		'wc/guard-super-call': 'off', //! should remove after PR merge
 		'no-bitwise': 'off', //! should remove after PR merge
-		'radix': 'off', //! should remove after PR merge
+		radix: 'off', //! should remove after PR merge
 		'lit-a11y/anchor-is-valid': 'off', //! should remove after PR merge
 		'no-else-return': 'off', //! should remove after PR merge
 		'lit-a11y/click-events-have-key-events': 'off', //! should remove after PR merge
@@ -78,25 +75,24 @@ module.exports = {
 		'@typescript-eslint/no-unused-vars': 'off', //! should remove after PR merge
 		'wc/attach-shadow-constructor': 'off', //! should remove after PR merge
 		'no-void': 'off', //! should remove after PR merge
-		'no-unused-vars': 'off'/*[
+		'no-unused-vars': [
 			'error',
 			{
-				'varsIgnorePattern': '^(?:(?:MDC(?:(?:[A-Z][a-z0-9]+)+)Adapter)|(?:(?:(?:[A-Z][a-z0-9]+)+)Type))$'
-			}
-		]*/, //! should ENABLE after PR merge
+				varsIgnorePattern:
+					'^(?:(?:MDC(?:(?:[A-Z][a-z0-9]+)+)Adapter)|(?:(?:(?:[A-Z][a-z0-9]+)+)Type))$',
+			},
+		],
+		'@typescript-eslint/no-extra-semi': 'off',
 		'@typescript-eslint/explicit-function-return-type': 'off',
 		'@typescript-eslint/indent': 'off',
 		'max-len': 'off',
 		'block-spacing': 'off',
 		'@typescript-eslint/explicit-member-accessibility': [
 			'error',
-			{ 'accessibility': 'no-public' }
+			{ accessibility: 'no-public' },
 		],
 		'no-new': 'warn',
-		'quotes': ['error', 'single', { 'avoidEscape': true }],
 		'no-var': 'error',
-		'curly': 'error',
-		'no-floating-decimal': 'error',
 		'require-jsdoc': 'off',
 		'valid-jsdoc': 'off',
 		'prefer-const': 'error',
@@ -106,40 +102,40 @@ module.exports = {
 		'mocha/no-identical-title': 'error',
 		'mocha/no-nested-tests': 'error',
 		'mocha/no-pending-tests': 'error',
-		'mocha/no-skipped-tests': 'error'
+		'mocha/no-skipped-tests': 'error',
 	},
-	'overrides': [
+	overrides: [
 		{
-			'files': ['{common,components}/**/*.ts'],
-			'rules': {
+			files: ['{common,components}/**/*.ts'],
+			rules: {
 				'no-unused-vars': 'off',
 				'no-invalid-this': 'off',
-				'new-cap': 'off'
-			}
+				'new-cap': 'off',
+			},
 		},
 		{
-			'files': ['**/test/**/*.js', '**/stories/**/*.js'],
-			'rules': {
+			files: ['**/test/**/*.js', '**/stories/**/*.js'],
+			rules: {
 				'@typescript-eslint/explicit-module-boundary-types': 'off',
 				'@typescript-eslint/no-non-null-assertion': 'off',
 				'no-only-tests/no-only-tests': 'error',
-				'quotes': 'off',
+				quotes: 'off',
 				'no-var': 'off',
-				'curly': 'off',
+				curly: 'off',
 				'no-floating-decimal': 'off',
 				'no-unused-vars': 'off',
 				'prefer-const': 'off',
-				'comma-dangle': 'off'
+				'comma-dangle': 'off',
 			},
-			'globals': {
-				'chai': false,
-				'it': false,
-				'describe': false,
-				'expect': false,
-				'assert': false,
-				'afterEach': false,
-				'beforeEach': false
-			}
-		}
-	]
-}
+			globals: {
+				chai: false,
+				it: false,
+				describe: false,
+				expect: false,
+				assert: false,
+				afterEach: false,
+				beforeEach: false,
+			},
+		},
+	],
+};
