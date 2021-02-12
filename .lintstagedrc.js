98687155da7068ef346bc4d3bb2040605a07ba82
yinonov <yinon@hotmail.com>
2021-02-12 11:33:42 +0200

    temporary flag for mwc-chip clearance

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,5 +3,5 @@ module.exports = {
 	"{common,components}/*/src/**/*.{js,ts}": [
 		"eslint --no-error-on-unmatched-pattern --quiet --fix",
 	],
-	'{common,components}/*/src/**/*.ts': ["lit-analyzer  --strict --quiet --format"],
+	'{common,components}/*/src/**/*.ts': ["lit-analyzer  --rules.no-incompatible-type-binding off --strict --quiet --format"],
 };
