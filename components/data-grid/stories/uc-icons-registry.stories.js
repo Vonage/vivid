dfd33c5fe5adf977662f65bf1eadf994f28b3b15
Alexander Mazuruk <alexander.mazuruk@gmail.com>
2021-02-12 16:58:26 +0100

    optimize icons registry configuration

--- components/data-grid/stories/uc-icons-registry.stories.js
+++ components/data-grid/stories/uc-icons-registry.stories.js
@@ -10,6 +10,12 @@ export default {
 
 class IconsDataSource {
     getRows(params) {
+      const idSort = params.sortModel.length > 0 ? params.sortModel[0].sort : undefined;
+      if (idSort) {
+        icons.sort((a, b) => idSort === 'desc' ? a.id.localeCompare(b.id) : b.id.localeCompare(a.id))
+      } else {
+        icons.sort((a, b) => a.id.localeCompare(b.id))
+      }
       params.successCallback(icons.slice(params.startRow, params.endRow), icons.length);
     }
 }
@@ -21,8 +27,9 @@ function IconCellRenderer () {
 // gets called once before the renderer is used
 IconCellRenderer.prototype.init = function(params) {
     // create the cell
-    this.eGui = document.createElement('div');
-    this.eGui.innerHTML = `<vwc-icon type="${params.value}" size="medium"></vwc-icon>`;
+    this.eGui = document.createElement('vwc-icon');
+    this.eGui.type = params.value;
+    this.eGui.size = 'small';
 };
 
 // gets called once (assuming destroy hasn't been called first) when grid ready to insert the element
@@ -33,7 +40,7 @@ IconCellRenderer.prototype.getGui = function() {
 // gets called whenever the user gets the cell to refresh
 IconCellRenderer.prototype.refresh = function(params) {
     // set value into cell again
-    this.eGui.innerHTML = `<vwc-icon type="${params.value}" size="medium"></vwc-icon>`;
+    this.eGui.type = params.value;
     // return true to tell the grid we refreshed successfully
     return true;
 };
@@ -41,13 +48,22 @@ IconCellRenderer.prototype.refresh = function(params) {
 export const IconsRegistry = () => {
     const options = {
         rowModelType: 'infinite',
-        datasource: new IconsDataSource()
+        datasource: new IconsDataSource(),
+        defaultColDef: {
+            minWidth: 60,
+            resizable: true,
+            unSortIcon: true,
+            wrapText: true,
+            filter: 'agTextColumnFilter'
+        }
     }
     return html`
 <div style='width: 900px; height: 300px'>
     <vwc-data-grid .options=${options}>
+        <vwc-data-grid-column headerName="Icon" field="id" width="65" filter=""
+        .cellStyle=${{'text-align': '-webkit-center', 'padding-top': '4px'}} 
+        .cellRenderer=${IconCellRenderer}></vwc-data-grid-column>
         <vwc-data-grid-column headerName="Id" field="id" sortable="true"></vwc-data-grid-column>
-        <vwc-data-grid-column headerName="Icon" field="id" width="65" .cellRenderer=${IconCellRenderer}></vwc-data-grid-column>
         <vwc-data-grid-column headerName="Keywords" field="keyword"></vwc-data-grid-column>
         <vwc-data-grid-column headerName="Tags" field="tag"></vwc-data-grid-column>
         <vwc-data-grid-column headerName="Alias" field="alias"></vwc-data-grid-column>
