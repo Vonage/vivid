0a969717f714b8ef20118ee1a63f3438626aacbb
yinonov <yinon@hotmail.com>
2021-02-12 15:39:05 +0200

    fix(scheme): restore pre-scheme components style generator

---
+++ common/scheme/scripts/generate-pre-scheme-style.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+console.log(
+  '\x1b[33m%s\x1b[0m',
+  '** generating css selectors from custom-elements.json **'
+);
+
+const customElementsJson = fs.readFileSync(
+  __dirname + '/../../../custom-elements.json',
+  { encoding: 'utf8' }
+);
+
+const { tags } = JSON.parse(customElementsJson);
+const cssSelector = tags.reduce(
+  (acc, { name }, i) => (acc += `${name}${i < tags.length - 1 ? ', ' : ''}`),
+  ''
+);
+
+fs.writeFileSync(
+  'src/pre-scheme-loading.scss',
+  `
+		export const preSchemeLoadingCssText = \`${cssSelector} {
+			visibility: hidden;
+		}\`
+	`
+);
