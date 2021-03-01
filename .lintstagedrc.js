426d16919a495693e97290329d0ef4de81cc1a89
yinonov <yinon@hotmail.com>
2021-03-01 10:07:09 +0200

    test(workspace): (es)lint test files

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,7 @@
 module.exports = {
 	"{common,components}/**/*.scss": ["stylelint --quiet --fix"],
-	"{common,components}/*/src/**/*.{js,ts}": [
-		"eslint --no-error-on-unmatched-pattern --quiet --fix",
+	"{common,components}/*/{src,test}/**/*": [
+		"eslint --ext .js .ts --no-error-on-unmatched-pattern --quiet --fix",
 	],
 	'{common,components}/*/src/**/*.ts': ["lit-analyzer --rules.no-incompatible-type-binding off --strict --quiet --format"],
 };
