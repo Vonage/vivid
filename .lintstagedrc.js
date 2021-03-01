17590dd461f940eee19aa69629b44e1dbc9889f1
yinonov <yinon@hotmail.com>
2021-03-01 10:27:17 +0200

    removed --ext in lint staged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,7 @@
 module.exports = {
 	"{common,components}/**/*.scss": ["stylelint --quiet --fix"],
 	"{common,components}/*/{src,test}/**/*.{js,ts}": [
-		"eslint --ext .js .ts --no-error-on-unmatched-pattern --quiet --fix",
+		"eslint --no-error-on-unmatched-pattern --quiet --fix",
 	],
 	'{common,components}/*/src/**/*.ts': ["lit-analyzer --rules.no-incompatible-type-binding off --strict --quiet --format"],
 };
