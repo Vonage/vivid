09792e35288bd049d5f1b0b602805a6d8498adb2
yinon <yinon@hotmail.com>
2021-03-09 17:12:14 +0200

    feat(vwc-circular-progress): added connotation support (#694)
    * feat(vwc-circular-progress): added connotation support
    
    * fixing the color comparator - adding stroke property for the cured ones
    
    * fixing the color comparator - adding stroke property for the cured ones
    
    * dep-check @vonage/vvd-design-tokens
    
    Co-authored-by: Yuri Guller <gullerya@gmail.com>

--- components/circular-progress/src/vwc-circular-progress.ts
+++ components/circular-progress/src/vwc-circular-progress.ts
@@ -1,8 +1,10 @@
 import '@vonage/vvd-core';
-import { customElement } from 'lit-element';
+import { customElement, property } from 'lit-element';
 import { CircularProgress as MWCCircularProgress } from '@material/mwc-circular-progress';
 import { style as mwcCircularProgressStyle } from '@material/mwc-circular-progress/mwc-circular-progress-css.js';
+import { style as vwcCircularProgressStyle } from './vwc-circular-progress.css';
 import { style as styleCoupling } from '@vonage/vvd-style-coupling/vvd-style-coupling.css.js';
+import { Connotation } from '@vonage/vvd-foundation/constants';
 
 declare global {
 	interface HTMLElementTagNameMap {
@@ -10,9 +12,20 @@ declare global {
 	}
 }
 
+type CircularProgressConnotation = Extract<
+	Connotation,
+	| Connotation.Primary
+	| Connotation.CTA
+	| Connotation.Success
+	| Connotation.Alert
+	>;
+
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 // @ts-ignore
-MWCCircularProgress.styles = [styleCoupling, mwcCircularProgressStyle];
+MWCCircularProgress.styles = [styleCoupling, mwcCircularProgressStyle, vwcCircularProgressStyle];
 
 @customElement('vwc-circular-progress')
-export class VWCCircularProgress extends MWCCircularProgress {}
+export class VWCCircularProgress extends MWCCircularProgress {
+	@property({ type: String, reflect: true })
+	connotation?: CircularProgressConnotation | null;
+}
--- components/circular-progress/test/circular-progress.test.js
+++ components/circular-progress/test/circular-progress.test.js
@@ -5,6 +5,11 @@ import {
 	textToDomToParent,
 	waitNextTask,
 } from '../../../test/test-helpers';
+import {
+	assertConnotationAttribute,
+	assertConnotationProperty,
+} from '@vonage/vvd-foundation/test/connotation.test.js';
+import { Connotation } from '@vonage/vvd-foundation/constants';
 
 chai.use(chaiDomDiff);
 
@@ -29,5 +34,64 @@ describe('circular progress', () => {
 		const actualElement = addedElements[0];
 		await waitNextTask();
 		expect(actualElement.shadowRoot.innerHTML).to.equalSnapshot();
+	});
+
+	describe('Connotation', () => {
+		const CONNOTATIONS_SUPPORTED = [Connotation.Primary, Connotation.CTA, Connotation.Success, Connotation.Alert];
+
+		it(`should sync linear progress class member 'connotation' and html attribute 'connotation'`, async function () {
+			const [linearProgress] = addElement(
+				textToDomToParent(`<${COMPONENT_NAME}></${COMPONENT_NAME}>`)
+			);
+			await waitNextTask();
+
+			const syncMatchFn = connotation => linearProgress.connotation == connotation &&
+				linearProgress.getAttribute('connotation') == connotation;
+
+			let connotationValue = 'cta';
+			linearProgress.connotation = connotationValue;
+			await waitNextTask();
+			const propertyChangesAffectsAttribute = syncMatchFn(connotationValue);
+
+			connotationValue = 'primary';
+			linearProgress.setAttribute('connotation', connotationValue);
+			await waitNextTask();
+			const attributeChangesAffectsProperty = syncMatchFn(connotationValue);
+
+			expect(
+				propertyChangesAffectsAttribute,
+				'Property change did not apply to attribute'
+			).to.equal(true);
+			expect(
+				attributeChangesAffectsProperty,
+				'Attribute change did not apply to property'
+			).to.equal(true);
+		});
+
+		for (const connotation of CONNOTATIONS_SUPPORTED) {
+			it(`should reflect '${connotation}' connotation (attribute) visually`, async () => {
+				const [linearProgress] = addElement(
+					textToDomToParent(`<${COMPONENT_NAME}></${COMPONENT_NAME}>`)
+				);
+				await assertConnotationAttribute({
+					element: linearProgress,
+					connotation: connotation,
+					childrenAffected: ['.mdc-circular-progress__determinate-circle', '.mdc-circular-progress__indeterminate-circle-graphic'],
+					stylesAffected: ['stroke'],
+				});
+			});
+
+			it(`should reflect '${connotation}' connotation (property) visually`, async () => {
+				const [linearProgress] = addElement(
+					textToDomToParent(`<${COMPONENT_NAME}></${COMPONENT_NAME}>`)
+				);
+				await assertConnotationProperty({
+					element: linearProgress,
+					connotation: connotation,
+					childrenAffected: ['.mdc-circular-progress__determinate-circle', '.mdc-circular-progress__indeterminate-circle-graphic'],
+					stylesAffected: ['stroke'],
+				});
+			});
+		}
 	});
 });
