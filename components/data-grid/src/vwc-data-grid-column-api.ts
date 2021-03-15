addbc25b8fe006383e20c3c31d4625efb86d372b
Yuri Guller <gullerya@gmail.com>
2021-03-15 13:25:42 +0200

    added docs

--- components/data-grid/src/vwc-data-grid-column-api.ts
+++ components/data-grid/src/vwc-data-grid-column-api.ts
@@ -20,13 +20,49 @@ const COLUMN_DEFINITION_COMPONENT = 'vwc-data-grid-column',
  * - this API is to be applied to each column in the data grid
  */
 interface DataGridColumn {
+	/**
+	 * path into the item to retrieve the data to be shown
+	 * - won't be relevant for `selector` column
+	 */
 	path?: string;
 
+	/**
+	 * designates the column as a collapsible tree / groupable in case of a single level
+	 */
 	tree: boolean;
+
+	/**
+	 * hides / shows the column
+	 */
 	hidden: boolean;
+
+	/**
+	 * prevents columns from being scrolled / moved
+	 */
 	frozen: boolean;
+
+	/**
+	 * turns the column into sortable and vice-versa
+	 * - when sortable and no custom header renderer, defaul sorting renderer will be shown and effective
+	 */
 	sortable: boolean;
+
+	/**
+	 * makes columns resizable an vice-versa
+	 * - when resizable, a resize handle is available in UI to resize the column's width
+	 */
 	resizable: boolean;
+
+	/**
+	 * makes the column `selector`
+	 * - won't react on features like `header`, `path`, `sortable`, `resizable` etc
+	 * - unless overridden, will provide an OOTB UI for selecting the rows
+	 * - if the values is `single`
+	 * 	 - only one item will be selected, other unselected automatically
+	 *   - no select all UI in the header will be provided
+	 * - if `multi`
+	 *   - header managing the select / deselect all will be shown
+	 */
 	selector?: string;
 
 	autoWidth: boolean;
