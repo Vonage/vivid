2e54c258a6d259284bf60b50ef934c9fb9cd4ff5
Yuri Guller <gullerya@gmail.com>
2021-03-15 22:09:12 +0200

    issue #677: added selection API tests

--- components/data-grid/test/data-grid-selection.test.js
+++ components/data-grid/test/data-grid-selection.test.js
@@ -14,12 +14,126 @@ const COMPONENT_NAME = 'vwc-data-grid';
 describe('data grid selection API', () => {
 	let addElement = isolatedElementsCreation();
 
-	it('vwc-note is defined as a custom element', async () => {
+	it('should set item selected on select item', async () => {
+		let fires = 0;
 		const [g] = addElement(
 			textToDomToParent(`<${COMPONENT_NAME}></${COMPONENT_NAME}>`)
 		);
 		await waitNextTask();
+		g.addEventListener('selected-items-changed', () => fires++);
 		g.columns = getColumns();
 		g.items = getItems(3);
+		expect(g.selectedItems).exist;
+		expect(g.selectedItems.length).equal(0);
+
+		g.selectItem(g.items[0]);
+		expect(g.selectedItems).exist;
+		expect(g.selectedItems.length).equal(1);
+		expect(g.selectedItems[0]).equal(g.items[0]);
+
+		expect(fires).equal(1);
+	});
+
+	it('should unset item selected on de-select item', async () => {
+		let fires = 0;
+		const [g] = addElement(
+			textToDomToParent(`<${COMPONENT_NAME}></${COMPONENT_NAME}>`)
+		);
+		await waitNextTask();
+		g.addEventListener('selected-items-changed', () => fires++);
+		g.columns = getColumns();
+		g.items = getItems(3);
+		g.selectItem(g.items[0]);
+
+		g.deselectItem(g.items[0]);
+		expect(g.selectedItems).exist;
+		expect(g.selectedItems.length).equal(0);
+
+		expect(fires).equal(2);
+	});
+
+	it('should set all items selected on select all', async () => {
+		let fires = 0;
+		const [g] = addElement(
+			textToDomToParent(`<${COMPONENT_NAME}></${COMPONENT_NAME}>`)
+		);
+		await waitNextTask();
+		g.addEventListener('selected-items-changed', () => fires++);
+		g.columns = getColumns();
+		g.items = getItems(3);
+
+		await waitNextTask();
+		g.selectAll();
+		expect(g.selectedItems.length).equal(3);
+
+		expect(fires).equal(1);
+	});
+
+	it('should unset all items selected on de-select all', async () => {
+		let fires = 0;
+		const [g] = addElement(
+			textToDomToParent(`<${COMPONENT_NAME}></${COMPONENT_NAME}>`)
+		);
+		await waitNextTask();
+		g.addEventListener('selected-items-changed', () => fires++);
+		g.columns = getColumns();
+		g.items = getItems(3);
+
+		await waitNextTask();
+		g.selectAll();
+		g.deselectAll();
+		expect(g.selectedItems.length).equal(0);
+
+		expect(fires).equal(2);
+	});
+
+	it('should set all items selected when select item used with default mode', async () => {
+		let fires = 0;
+		const [g] = addElement(
+			textToDomToParent(`<${COMPONENT_NAME}></${COMPONENT_NAME}>`)
+		);
+		await waitNextTask();
+		g.addEventListener('selected-items-changed', () => fires++);
+		g.columns = getColumns();
+		g.items = getItems(3);
+
+		await waitNextTask();
+		g.selectItem(g.items[0]);
+		g.selectItem(g.items[1], false);
+		expect(g.selectedItems.length).equal(2);
+
+		expect(fires).equal(2);
+	});
+
+	it('should set only one item selected when select item used with single mode', async () => {
+		let fires = 0;
+		const [g] = addElement(
+			textToDomToParent(`<${COMPONENT_NAME}></${COMPONENT_NAME}>`)
+		);
+		await waitNextTask();
+		g.addEventListener('selected-items-changed', () => fires++);
+		g.columns = getColumns();
+		g.items = getItems(3);
+
+		await waitNextTask();
+		g.selectItem(g.items[0]);
+		g.selectItem(g.items[1], true);
+		expect(g.selectedItems.length).equal(1);
+		expect(g.selectedItems[0]).equal(g.items[1]);
+
+		expect(fires).equal(2);
+	});
+
+	it('should throw when select all used with data provider', async () => {
+		const [g] = addElement(
+			textToDomToParent(`<${COMPONENT_NAME}></${COMPONENT_NAME}>`)
+		);
+		await waitNextTask();
+		const i = getItems(3);
+		g.dataProvider = (_params, cb) => cb(i, i.length);
+		g.columns = getColumns();
+
+		await waitNextTask();
+		expect(g.selectAll.bind(g)).throw(`'selectAll' is NOT supported when grid data supplied by 'dataProvider' method`);
 	});
 });
