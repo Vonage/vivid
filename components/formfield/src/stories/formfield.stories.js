3d10f48630a951636b85dfee359ef4f55f827605
yonatankra <yonatan.kra@vonage.com>
2021-03-29 10:52:17 +0300

    More formfield examples for all the APIs

--- components/formfield/src/stories/formfield.stories.js
+++ components/formfield/src/stories/formfield.stories.js
@@ -11,6 +11,53 @@ export default {
 	argTypes
 };
 
+const CheckboxTemplate = args => html`
+	<mwc-formfield label="Tomato">
+		<mwc-checkbox checked></mwc-checkbox>
+	</mwc-formfield>`;
+
+const SwitchTemplate = args => html`
+	<mwc-formfield label="Airplane mode">
+		<mwc-switch checked></mwc-switch>
+	</mwc-formfield>`;
+
+const RadioTemplate = args => html`
+	<mwc-formfield label="Home">
+		<mwc-radio name="location" checked></mwc-radio>
+	</mwc-formfield>
+
+	<mwc-formfield label="Work">
+		<mwc-radio name="location"></mwc-radio>
+	</mwc-formfield>`;
+
+const CheckboxNoWrapTemplate = args => html`
+	<style>
+		mwc-formfield {
+			width: 150px;
+		}
+	</style>
+	<mwc-formfield label="really really long label with wrap">
+		<mwc-checkbox></mwc-checkbox>
+	</mwc-formfield>
+	<mwc-formfield label="really really long label without wrap" nowrap>
+		<mwc-checkbox></mwc-checkbox>
+	</mwc-formfield>`;
+
+const CheckboxSpaceBetweenTemplate = args => html`
+	<style>
+		mwc-formfield {
+			width: 250px;
+			outline: 2px solid black;
+			margin: 2px;
+		}
+	</style>
+	<mwc-formfield label="No spaceBetween">
+		<mwc-checkbox></mwc-checkbox>
+	</mwc-formfield>
+	<mwc-formfield label="With spaceBetween" spaceBetween>
+		<mwc-checkbox></mwc-checkbox>
+	</mwc-formfield>`;
+
 const Template = args => html`
 	<main style="width: 400px; border: 5px black dotted; padding: 5px;">
 		<vwc-formfield label="This is a checkbox.">
@@ -42,3 +89,18 @@ const Template = args => html`
 
 export const FormfieldExamples = Template.bind({});
 FormfieldExamples.args = {};
+
+export const WithCheckbox = CheckboxTemplate.bind({});
+WithCheckbox.args = {};
+
+export const WrapVsNowrap = CheckboxNoWrapTemplate.bind({});
+WrapVsNowrap.args = {};
+
+export const Spacebetween = CheckboxSpaceBetweenTemplate.bind({});
+Spacebetween.args = {};
+
+export const WithRadio = RadioTemplate.bind({});
+WithRadio.args = {};
+
+export const WithSwitch = SwitchTemplate.bind({});
+WithSwitch.args = {};
