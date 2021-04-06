a459a9d4d05b860740cb4b1f2dcc3599f944a88c
Yuri Guller <gullerya@gmail.com>
2021-04-06 15:14:50 +0300

    issue #747: initial commit

---
+++ components/pagination/src/vwc-pagination.ts
@@ -0,0 +1,38 @@
+import '@vonage/vvd-core';
+import '@vonage/vwc-icon';
+import {
+	customElement, property, LitElement, CSSResult
+} from 'lit-element';
+import { style } from './vwc-pagination.css';
+import { html, TemplateResult } from 'lit-element';
+
+export const COMPONENT_NAME = 'vwc-pagination';
+
+declare global {
+	interface HTMLElementTagNameMap {
+		[COMPONENT_NAME]: VWCPagination;
+	}
+}
+
+/**
+ * `vwc-pagination` component is designated to reflect and 'manage' state of the paged content views
+ *
+ * `vwc-pagination` exposes APIs to set the `total` of pages and `selected` page
+ */
+@customElement('vwc-pagination')
+export class VWCPagination extends LitElement {
+	static get styles(): CSSResult[] {
+		return [style];
+	}
+
+	@property({ type: Number, reflect: true })
+	total = 0;
+
+	@property({ type: Number, reflect: true })
+	selected = null;
+
+	protected render(): TemplateResult {
+		return html`
+		`;
+	}
+}
