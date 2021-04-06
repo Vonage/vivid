eed15d5c18f8c4a4f244aab6c094913a5cea0379
yinonov <yinon@hotmail.com>
2021-04-06 14:01:40 +0300

    inline tests

--- components/inline/test/inline.test.js
+++ components/inline/test/inline.test.js
@@ -9,26 +9,35 @@ import { chaiDomDiff } from '@open-wc/semantic-dom-diff';
 chai.use(chaiDomDiff);
 
 const VWC_INLINE = 'vwc-inline';
+const inlineHtmlStr = `<${VWC_INLINE}>
+	<span>1</span>
+	<span>2</span>
+	<span>3</span>
+	<span>4</span>
+</${VWC_INLINE}>`;
 
-describe('inline', () => {
-	const addElement = isolatedElementsCreation();
+const getNewElement = () => isolatedElementsCreation()(textToDomToParent(inlineHtmlStr))[0];
 
-	it(`${VWC_INLINE} is defined as a custom element`, async () => {
-		assert.exists(
-			customElements.get(VWC_INLINE, `${VWC_INLINE} element is not defined`)
-		);
+describe(VWC_INLINE, () => {
+	describe('basics', () => {
+		it(`${VWC_INLINE} is defined as a custom element`, async () => {
+			assert.exists(
+				customElements.get(VWC_INLINE, `${VWC_INLINE} element is not defined`)
+			);
+		});
+
+		it('should have internal contents', async () => {
+			const actualElement = getNewElement();
+			await waitNextTask();
+			expect(actualElement.shadowRoot.innerHTML).to.equalSnapshot();
+		});
 	});
 
-	it('should have internal contents', async () => {
-		const [actualElement] = addElement(
-			textToDomToParent(`<${VWC_INLINE}>
-				<span>1</span>
-				<span>2</span>
-				<span>3</span>
-				<span>4</span>
-			</${VWC_INLINE}>`)
-		);
-		await waitNextTask();
-		expect(actualElement.shadowRoot.innerHTML).to.equalSnapshot();
+	describe('API', () => {
+		it(`should be set to auto-fit`, async () => {
+			const actualElement = getNewElement();
+			await waitNextTask();
+			expect(actualElement.shadowRoot.innerHTML).to.equalSnapshot();
+		});
 	});
 });
