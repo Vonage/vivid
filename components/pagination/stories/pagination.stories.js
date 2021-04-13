49243e120c740047697c6b8468e110aea27eb115
Yuri Guller <gullerya@gmail.com>
2021-04-13 13:59:01 +0300

    issue #747: moving the pagination to the beta area

--- components/pagination/stories/pagination.stories.js
+++ components/pagination/stories/pagination.stories.js
@@ -4,7 +4,7 @@ import { spread } from '@open-wc/lit-helpers';
 import { argTypes } from './arg-types.js';
 
 export default {
-	title: 'Components/Composite/Pagination',
+	title: 'Components/Beta/Pagination',
 	component: 'vwc-pagination',
 	argTypes
 };
