5aaf2b831b71ad87eaf7eded06071979ce0f73af
Yuri Guller <gullerya@gmail.com>
2021-04-18 12:47:42 +0300

    issue #747: fixing the storybook mark

--- components/pagination/stories/pagination-introduction.config.mjs
+++ components/pagination/stories/pagination-introduction.config.mjs
@@ -2,7 +2,7 @@ export default {
 	sourcePath: '../readme.md',
 	outputName: 'pagination-introduction',
 	story: {
-		title: 'Components/Composite/Pagination/Introduction',
+		title: 'Components/Alpha/Pagination/Introduction',
 		name: 'Introduction',
 		parameters: {
 			options: {
