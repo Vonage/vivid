b49c6c63f2bb1d63002fa4e3d91993d6d874b51a
Yonatan Kra <yonatan.kra@vonage.com>
2021-04-19 09:33:13 +0300

    Update components/toggle-button-group/stories/toggle-buttons-group.stories.js
    Co-authored-by: yinon <yinon@hotmail.com>

--- components/toggle-button-group/stories/toggle-buttons-group.stories.js
+++ components/toggle-button-group/stories/toggle-buttons-group.stories.js
@@ -4,7 +4,7 @@ import { spread } from '@open-wc/lit-helpers';
 import { argTypes } from './arg-types.js';
 
 export default {
-	title: 'Components/Beta/Toggle Buttons Group',
+	title: 'Alpha/Components/Toggle Buttons Group',
 	component: 'vwc-toggle-button-group',
 	argTypes
 };
