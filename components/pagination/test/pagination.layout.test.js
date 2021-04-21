9400b52476411b45cba70af35333c90ca1a2b4c8
Yuri Guller <gullerya@gmail.com>
2021-04-21 17:22:43 +0300

    issue #793: fixing pagination layout

--- components/pagination/test/pagination.layout.test.js
+++ components/pagination/test/pagination.layout.test.js
@@ -2,6 +2,7 @@ import { COMPONENT_NAME } from '@vonage/vwc-pagination';
 import {
 	waitNextTask,
 	textToDomToParent,
+	assertDistancePixels,
 } from '../../../test/test-helpers.js';
 import { chaiDomDiff } from '@open-wc/semantic-dom-diff';
 import { isolatedElementsCreation } from '../../../test/test-helpers';
@@ -51,4 +52,26 @@ describe('pagination layout', () => {
 			expect(pagination.shadowRoot.innerHTML).to.equalSnapshot();
 		});
 	}
+
+	it('has correct distance between the items', async () => {
+		const pages = 10;
+		const [pagination] = addElement(
+			textToDomToParent(
+				`<${COMPONENT_NAME} total="${pages}"></${COMPONENT_NAME}>`
+			)
+		);
+		await waitNextTask();
+		const prevItem = pagination.shadowRoot.querySelector('.item.prev');
+		const nextItem = pagination.shadowRoot.querySelector('.item.next');
+		const pageItems = pagination.shadowRoot.querySelectorAll('.pages .item');
+		expect(prevItem).exist;
+		expect(nextItem).exist;
+		expect(pageItems).exist;
+		expect(pageItems.length).equal(7);
+		assertDistancePixels(prevItem, pageItems[0], 'x', 32);
+		assertDistancePixels(nextItem, pageItems[6], 'x', 32);
+		for (let i = 0; i < 6; i++) {
+			assertDistancePixels(pageItems[i], pageItems[i + 1], 'x', 28);
+		}
+	});
 });
