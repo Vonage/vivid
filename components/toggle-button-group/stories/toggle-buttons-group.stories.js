7cdcf7749dbaadb8e9924a63d0057fc357d86ac5
yonatankra <yonatan.kra@vonage.com>
2021-04-22 14:58:03 +0300

    Add multi and accent examples

--- components/toggle-button-group/stories/toggle-buttons-group.stories.js
+++ components/toggle-button-group/stories/toggle-buttons-group.stories.js
@@ -34,3 +34,24 @@ const TemplateIcons = args => html`<vwc-toggle-button-group ...=${spread(args)}>
 
 export const Iconed = TemplateIcons.bind({});
 Iconed.args = { };
+
+const AccentTemplate = args => html`<vwc-toggle-button-group accent ...=${spread(args)}>
+	<vwc-button label="Standard" layout="filled"></vwc-button>
+	<vwc-button label="Hybrid" layout="filled"></vwc-button>
+	<vwc-button label="Satellite" layout="filled"></vwc-button>
+</vwc-toggle-button-group>`;
+
+export const Accent = AccentTemplate.bind({});
+Accent.args = { };
+
+const MultiTemplate = args => html`<vwc-toggle-button-group multi
+																														accent
+																														...=${spread(args)}>
+	<vwc-button label="Standard" layout="filled"></vwc-button>
+	<vwc-button label="Hybrid" layout="filled"></vwc-button>
+	<vwc-icon-button icon="home" layout="filled"></vwc-icon-button>
+	<vwc-button label="Satellite" layout="filled"></vwc-button>
+</vwc-toggle-button-group>`;
+
+export const Multi = MultiTemplate.bind({});
+Multi.args = { };
