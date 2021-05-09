5b34c8236dbf1e3364827bec6d432aa954e527d4
yonatankra <yonatan.kra@vonage.com>
2021-05-09 15:16:59 +0300

    Serve the page

--- ui-tests/index.ts
+++ ui-tests/index.ts
@@ -1,10 +1,31 @@
 import { webkit } from 'playwright';
 
-(async () => {
-	const browser = await webkit.launch();
-	const page = await browser.newPage();
-	await page.goto('http://vivid.vonage.com');
-	await page.screenshot({ path: './ui-tests/image.png' });
+const handler = require('serve-handler');
+const http = require('http');
 
-	await browser.close();
-})();
+const server = http.createServer((request, response) => {
+	// You pass two more arguments for config and middleware
+	// More details here: https://github.com/vercel/serve-handler#options
+	return handler(request, response, {
+		public: 'ui-tests/dist'
+	});
+});
+
+server.listen(3000, () => {
+	console.log('Running at http://localhost:3000');
+
+	(async () => {
+		const browser = await webkit.launch();
+		const page = await browser.newPage();
+		await page.goto('http://localhost:3000');
+		await page.screenshot({ path: './ui-tests/image.png' });
+
+		await browser.close();
+
+		server.close();
+	})();
+});
+
+
+
+
