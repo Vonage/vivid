f5a1043a7581f519e6ce504bd8cad1b02cd28fe8
Yuri Guller <gullerya@gmail.com>
2021-05-16 10:55:16 +0300

    fixing the checkbox disposition in check list item

--- components/list/stories/list-check.stories.js
+++ components/list/stories/list-check.stories.js
@@ -1,42 +1,13 @@
 import '@vonage/vwc-list/vwc-list.js';
 import '@vonage/vwc-list/vwc-check-list-item.js';
 import { html } from 'lit-element';
-import { spread } from '@open-wc/lit-helpers';
-import { argTypes } from './arg-types-list-item.js';
 
 export default {
 	title: 'Components/List (check list item)',
-	component: 'vwc-check-list-item',
-	argTypes: {
-		...argTypes,
-		left: {
-			control: {
-				type: 'inline-radio',
-				options: { 'true': '', 'false': undefined }
-			}
-		}
-	}
+	component: 'vwc-check-list-item'
 }
 
-const Template = args => html`
-	<style>
-	vwc-list {
-		width: 240px;
-	}
-	</style>
-
-	<vwc-list multi>
-		<vwc-check-list-item ...=${spread(args)}>Item 0</vwc-check-list-item>
-		<vwc-check-list-item ...=${spread(args)}>Item 1</vwc-check-list-item>
-		<li divider role="separator" padded></li>
-		<vwc-check-list-item left ...=${spread(args)}>Item 2 (left)</vwc-check-list-item>
-		<vwc-check-list-item left ...=${spread(args)}>Item 3 (left)</vwc-check-list-item>
-	</vwc-list>`;
-
-export const Checklist = Template.bind({});
-Checklist.args = { selected: '', left: '' }
-
-export const checklist = args => html`
+const TemplateOneLine = () => html`
 	<style>
 		vwc-list {
 			width: 240px;
@@ -44,10 +15,40 @@ export const checklist = args => html`
 	</style>
 
 	<vwc-list multi>
-		<vwc-check-list-item selected ...=${spread(args)}>Item 0</vwc-check-list-item>
-		<vwc-check-list-item selected ...=${spread(args)}>Item 1</vwc-check-list-item>
+		<vwc-check-list-item selected>Item 0</vwc-check-list-item>
+		<vwc-check-list-item>Item 1</vwc-check-list-item>
 		<li divider role="separator" padded></li>
-		<vwc-check-list-item left selected ...=${spread(args)}>Item 2 (left)</vwc-check-list-item>
-		<vwc-check-list-item left ...=${spread(args)}>Item 3 (left)</vwc-check-list-item>
-	</vwc-list>
-`;
+		<vwc-check-list-item left selected>Item 2 (left)</vwc-check-list-item>
+		<vwc-check-list-item left>Item 3 (left)</vwc-check-list-item>
+	</vwc-list>`;
+
+export const Checklist = TemplateOneLine.bind({});
+
+const TemplatTwoLines = () => html`
+	<style>
+		vwc-list {
+			width: 240px;
+		}
+	</style>
+
+	<vwc-list multi>
+		<vwc-check-list-item selected twoline>
+			Item 0
+			<span slot="secondary">Additional info</span>
+		</vwc-check-list-item>
+		<vwc-check-list-item twoline>
+			Item 1
+			<span slot="secondary">Additional info</span>
+		</vwc-check-list-item>
+		<li divider role="separator" padded></li>
+		<vwc-check-list-item left selected twoline>
+			Item 2 (left)
+			<span slot="secondary">Additional info</span>
+		</vwc-check-list-item>
+		<vwc-check-list-item left twoline>
+			Item 3 (left)
+			<span slot="secondary">Additional info</span>
+		</vwc-check-list-item>
+	</vwc-list>`;
+
+export const ChecklistTwoLines = TemplatTwoLines.bind({});
