0cabf329eb30a14fc4a68749161a4c6f82033da9
Yonatan Kra <yonatan.kra@vonage.com>
2021-05-20 17:34:36 +0300

    chore(list-item): remove performance patch and test (#850)
    * Remove performance patch and test
    
    * proofing the test more
    
    * proofing the test more
    
    * fixing the snapshots
    
    * fixing back
    
    * added predictable container sizes
    
    * wip
    
    * fixing test again
    
    * fixing them again and again
    
    * fixing
    
    * fixing
    
    Co-authored-by: Yuri Guller <gullerya@gmail.com>

--- components/list/test/list-item.test.js
+++ components/list/test/list-item.test.js
@@ -100,26 +100,4 @@ describe('list item', () => {
 			assertComputedStyle(actualElement, shapeStyles('rounded'));
 		});
 	});
-
-	describe(`performance issue`, function () {
-		function createElement(index) {
-			return `<vwc-list-item value=${index}>Item ${index}</vwc-list-item>`;
-		}
-		const selectItems = new Array(300)
-			.fill(0)
-			.map((_, index) => index)
-			.reduce((last, next) => (last += createElement(next)), '');
-
-		it(`should not take more than 50ms to remove the list from the DOM`, async function () {
-			const [actualElement] = addElement(
-				textToDomToParent(`<vwc-list>${selectItems}</vwc-list>`)
-			);
-			await waitNextTask();
-			const startTime = performance.now();
-			actualElement.remove();
-			const endTime = performance.now();
-
-			expect(endTime - startTime).to.be.lessThan(50);
-		});
-	});
 });
