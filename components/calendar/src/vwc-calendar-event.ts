d7f2f3f4165064b245ee31acb4fc3b4502edd5d8
yinonov <yinon@hotmail.com>
2021-05-26 15:39:28 +0300

    fix(vwc-calendar): cells wrong grid columns

--- components/calendar/src/vwc-calendar-event.ts
+++ components/calendar/src/vwc-calendar-event.ts
@@ -1,8 +1,9 @@
 import '@vonage/vvd-core';
 import {
-	customElement, html, LitElement, property, TemplateResult, unsafeCSS
+	customElement, html, LitElement, property, TemplateResult
 } from 'lit-element';
 import { style } from './vwc-calendar-event.css';
+import { styleMap } from 'lit-html/directives/style-map';
 
 declare global {
 	interface HTMLElementTagNameMap {
@@ -23,31 +24,74 @@ export class VWCCalendarEvent extends LitElement {
 	static styles = [style];
 
 	/**
-	 * @prop the label of the event
+	 * @prop the heading of the event
 	 * @public
 	 * */
-	@property({ type: String, reflect: true })
-	label?: string;
+	@property({ type: String, reflect: false })
+	heading?: string;
+
+	/**
+	 * @prop the description of the event
+	 * @public
+	 * */
+	@property({ type: String, reflect: false })
+	description?: string;
 
 	/**
 	 * @prop day - day of the week (starts from 0)
 	 * @public
 	 * */
-	@property({ type: Number, reflect: true })
-	day?: 0 | 1 | 2 | 3 | 4 | 5 | 6;
+	@property({ type: Number, reflect: false })
+	day: 1 | 2 | 3 | 4 | 5 | 6 | 7 = 1;
+
+	/**
+	 * @prop color - color of event card
+	 * @public
+	 * */
+	@property({ type: String, reflect: false })
+	color?: string;
+
+	/**
+	 * @prop sets card display precendence and indentation
+	 * @public
+	 * */
+	@property({ type: String, reflect: false, attribute: 'overlap-count' })
+	overlapCount?: string;
+
+	/**
+	 * @prop start - time of day event starts
+	 * @public
+	 * */
+	@property({ type: Number, reflect: false })
+	start = 0; // TODO should be converted to allowed range
+
+	/**
+	 * @prop duration - event's time duration in hours
+	 * @public
+	 * */
+	@property({ type: Number, reflect: false })
+	duration = 1; // TODO should be converted to allowed range
 
 	/**
 	 * the html markup
 	 * @internal
 	 * */
 	protected render(): TemplateResult {
+		const styles = {
+			...this.color && { '--vvd-calendar-event--primary-color': this.color },
+			...this.overlapCount && { '--vvd-calendar-event--overlap-count': this.overlapCount },
+			'--vvd-calendar-event--start': (this.start + 1).toString(),
+			'--vvd-calendar-event--duration': (this.duration).toString(),
+			'--vvd-calendar-event--day': this.day.toString()
+		};
 		return html`
 			<section
 				role="button"
 				tabindex="0"
-				style="--vvd-calendar-event-event-day: ${unsafeCSS(this.day)}"
+				style="${styleMap(styles)}"
 			>
-				<label>${this.label}</label>
+				<h2><strong>${this.heading}</strong></h2>
+				<p>${this.description}</p>
 			</section>
 		`;
 	}
