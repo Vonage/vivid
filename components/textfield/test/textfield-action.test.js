cc7372bc8bf8e6bce24c4def460a79860aa60104
yinonov <yinon@hotmail.com>
2021-05-27 19:35:33 +0300

    test coverage

--- components/textfield/test/textfield-action.test.js
+++ components/textfield/test/textfield-action.test.js
@@ -28,37 +28,107 @@ describe('textfield action', () => {
 		return actualElement;
 	}
 
-	describe(`a11y`, function () {
-		it(`should enforce disable on child nodes`, async function () {
-			const actualElement = await createElement();
-			actualElement.disabled = true;
-			await waitNextTask();
-			const buttons = Array.from(actualElement.querySelectorAll(iconButton));
+	it(`should enforce disable on child nodes`, async function () {
+		const actualElement = await createElement();
+		actualElement.disabled = true;
+		await waitNextTask();
+		const buttons = Array.from(actualElement.querySelectorAll(iconButton));
 
-			const allDisabled = buttons.every(button => button.disabled);
+		const allDisabled = buttons.every(button => button.disabled);
 
-			actualElement.disabled = false;
-			await waitNextTask();
-			const allEnabled = buttons.every(button => !button.disabled);
+		actualElement.disabled = false;
+		await waitNextTask();
+		const allEnabled = buttons.every(button => !button.disabled);
 
-			expect(allDisabled, `Children not disabled on initialization`)
-				.to
-				.equal(true);
+		expect(allDisabled, `Child nodes are not disabled on initialization`)
+			.to
+			.equal(true);
 
-			expect(allEnabled, `Children not enabled on change`)
-				.to
-				.equal(true);
-		});
+		expect(allEnabled, `Child nodes are not enabled on change`)
+			.to
+			.equal(true);
+	});
 
-		it(`should disable dynamically added child node's`, async function () {
-			const actualElement = await createElement();
-			actualElement.disabled = true;
-			const newIconButton = document.createElement(iconButton);
-			newIconButton.setAttribute('slot', 'action');
-			actualElement.appendChild(newIconButton);
+	it(`should enforce shape on child nodes`, async function () {
+		const actualElement = await createElement();
+		actualElement.shape = 'pill';
+		await waitNextTask();
+		const buttons = Array.from(actualElement.querySelectorAll(iconButton));
 
-			await waitNextTask();
-			expect(newIconButton.hasAttribute('disabled')).to.equal(true);
-		});
+		const allCircled = buttons.every(button => button.shape === 'circled');
+
+		actualElement.shape = 'rounded';
+		await waitNextTask();
+		const allRounded = buttons.every(button => button.shape === 'rounded');
+
+		expect(allCircled, `Child nodes are not shaped properly on initialization`)
+			.to
+			.equal(true);
+
+		expect(allRounded, `Child nodes are not shaped properly on change`)
+			.to
+			.equal(true);
+	});
+
+	it(`should enforce density on child nodes`, async function () {
+		const actualElement = await createElement();
+		actualElement.dense = true;
+		await waitNextTask();
+		const buttons = Array.from(actualElement.querySelectorAll(iconButton));
+
+		const allDense = buttons.every(button => button.dense === true);
+
+		actualElement.dense = false;
+		await waitNextTask();
+		const allNormal = buttons.every(button => button.dense === false);
+
+		expect(allDense, `Child nodes are not sized properly on initialization`)
+			.to
+			.equal(true);
+
+		expect(allNormal, `Child nodes are not sized properly on change`)
+			.to
+			.equal(true);
+	});
+
+	it(`should disable dynamically added child node's`, async function () {
+		const actualElement = await createElement();
+		const newIconButton = document.createElement(iconButton);
+		newIconButton.setAttribute('slot', 'action');
+		actualElement.appendChild(newIconButton);
+
+		actualElement.disabled = true;
+		await waitNextTask();
+		expect(newIconButton.disabled).to.equal(true);
+	});
+
+	it(`should shape dynamically added child node's`, async function () {
+		const actualElement = await createElement();
+		const newIconButton = document.createElement(iconButton);
+		newIconButton.setAttribute('slot', 'action');
+		actualElement.appendChild(newIconButton);
+
+		actualElement.shape = 'rounded';
+		await waitNextTask();
+		expect(newIconButton.shape).to.equal('rounded');
+
+		actualElement.shape = 'pill';
+		await waitNextTask();
+		expect(newIconButton.shape).to.equal('circled');
+	});
+
+	it(`should set density dynamically to added child node's`, async function () {
+		const actualElement = await createElement();
+		const newIconButton = document.createElement(iconButton);
+		newIconButton.setAttribute('slot', 'action');
+		actualElement.appendChild(newIconButton);
+
+		actualElement.dense = true;
+		await waitNextTask();
+		expect(newIconButton.dense).to.equal(true);
+
+		actualElement.dense = false;
+		await waitNextTask();
+		expect(newIconButton.dense).to.equal(false);
 	});
 });
