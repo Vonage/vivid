ab48093d77783053970348b7bb7d09a1e9fe48cd
yinonov <yinon@hotmail.com>
2021-05-31 16:52:32 +0300

    move to alpha state

--- components/icon-button-toggle/stories/icon-button-toggle.stories.js
+++ components/icon-button-toggle/stories/icon-button-toggle.stories.js
@@ -4,7 +4,7 @@ import { spread } from '@open-wc/lit-helpers';
 import { argTypes } from './arg-types.js';
 
 export default {
-	title: 'Components/Icon Button Toggle',
+	title: 'Alpha/Components/Icon Button Toggle',
 	component: 'vwc-icon-button-toggle',
 	argTypes
 };
