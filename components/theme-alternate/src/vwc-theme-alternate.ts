0bb2106fba6c3cdd3e705a9a3170bb6be9e19602
yinonov <yinon@hotmail.com>
2021-05-31 12:05:55 +0300

    feat(vwc-theme-alternate): component 1st draft

---
+++ components/theme-alternate/src/vwc-theme-alternate.ts
@@ -0,0 +1,25 @@
+import '@vonage/vvd-core';
+import {
+	customElement,
+	html,
+	LitElement,
+	TemplateResult,
+} from 'lit-element';
+
+export const COMPONENT_NAME = 'vwc-theme-alternate';
+
+declare global {
+	interface HTMLElementTagNameMap {
+		[COMPONENT_NAME]: VWCThemeAlternate;
+	}
+}
+
+/**
+ * `vwc-theme-alternate` component is designated to wrap html scope in an ui-contrasting region
+ */
+@customElement('vwc-theme-alternate')
+export class VWCThemeAlternate extends LitElement {
+	protected render(): TemplateResult {
+		return html`<slot part="vvd-scheme-alternate"></slot>`;
+	}
+}
---
+++ components/theme-alternate/test/theme-alternate.test.js
@@ -0,0 +1,9 @@
+import { COMPONENT_NAME } from '@vonage/vwc-theme-alternate';
+import '@vonage/vwc-button';
+import { chaiDomDiff } from '@open-wc/semantic-dom-diff';
+
+chai.use(chaiDomDiff);
+
+describe('Theme Alternate', () => {
+	console.log(COMPONENT_NAME);
+});
