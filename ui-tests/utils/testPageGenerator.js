da7e1292eb5a3f6eefc4bec99a7ef08de9378bf3
yonatankra <yonatan.kra@vonage.com>
2021-06-09 17:52:12 +0300

    Allow for tests exclusion from the snapshot

--- ui-tests/utils/testPageGenerator.js
+++ ui-tests/utils/testPageGenerator.js
@@ -14,14 +14,19 @@ function getTestFolders(workingFolder) {
 }
 
 const template = fs.readFileSync(path.join(__dirname, '../testPage.js.tmpl')).toString();
-const testsNames = getTestFolders(path.join(__dirname, '../tests'));
-const testsImports = testsNames.reduce((result, testName) => {
-	return `${result}import { createElementVariations as ${pascalCase(testName)} } from './tests/${testName}';
+
+function generateTestPage(excludeList = []) {
+	const testsNames = getTestFolders(path.join(__dirname, '../tests')).filter(testName => !excludeList.includes(testName));
+	const testsImports = testsNames.reduce((result, testName) => {
+		return `${result}import { createElementVariations as ${pascalCase(testName)} } from './tests/${testName}';
 	`;
-}, '');
-const testsCalls = testsNames.reduce((result, testName) => {
-	return `${result}await ${pascalCase(testName)}(wrapper);
+	}, '');
+	const testsCalls = testsNames.reduce((result, testName) => {
+		return `${result}await ${pascalCase(testName)}(wrapper);
 	`;
-}, '');
-const fileContenst = (template.replace('${testImports}', testsImports)).replace('${testCalls}', testsCalls);
-fs.writeFileSync(path.join(__dirname, '../testPage.js'), fileContenst);
+	}, '');
+	const fileContents = (template.replace('${testImports}', testsImports)).replace('${testCalls}', testsCalls);
+	fs.writeFileSync(path.join(__dirname, '../testPage.js'), fileContents);
+}
+
+module.exports = generateTestPage;
