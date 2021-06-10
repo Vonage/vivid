771c84907f831e3be307e6754573cbf07fc3b184
Yonatan Kra <yonatan.kra@vonage.com>
2021-06-10 18:43:01 +0300

    feat(ci): visual tests auto discovery and documenation (#885)
    * The ui tests now prepopulates the tests from the tests folder
    
    * Allow for tests exclusion from the snapshot
    
    * Move files to assets folder
    
    * Documentation

---
+++ ui-tests/utils/testPageGenerator.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+const { pascalCase } = require('pascal-case');
+
+function getTestFolders(workingFolder) {
+	const testFolders = [];
+	fs.readdirSync(workingFolder).forEach((testFolder) => {
+		const absolutePath = path.join(workingFolder, testFolder);
+		if (fs.statSync(absolutePath).isDirectory()) {
+			testFolders.push(testFolder);
+		}
+	});
+	return testFolders;
+}
+
+const template = fs.readFileSync(path.join(__dirname, '../assets/testPage.js.tmpl')).toString();
+
+function generateTestPage(excludeList = []) {
+	const testsNames = getTestFolders(path.join(__dirname, '../tests')).filter(testName => !excludeList.includes(testName));
+	const testsImports = testsNames.reduce((result, testName) => {
+		return `${result}import { createElementVariations as ${pascalCase(testName)} } from './tests/${testName}';
+	`;
+	}, '');
+	const testsCalls = testsNames.reduce((result, testName) => {
+		return `${result}await ${pascalCase(testName)}(wrapper);
+	`;
+	}, '');
+	const fileContents = (template.replace('${testImports}', testsImports)).replace('${testCalls}', testsCalls);
+	fs.writeFileSync(path.join(__dirname, '../testPage.js'), fileContents);
+}
+
+module.exports = generateTestPage;
