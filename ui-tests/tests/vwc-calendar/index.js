04e4f6fdd38d24a284e20e69cdd74de4341299ef
yinonov <yinon@hotmail.com>
2021-06-12 10:18:53 +0300

    calendar ui test misses events

--- ui-tests/tests/vwc-calendar/index.js
+++ ui-tests/tests/vwc-calendar/index.js
@@ -1,6 +1,7 @@
 import vvdCore from '@vonage/vvd-core';
-import * as stories from '@vonage/vwc-calendar/stories/calendar.stories';
 import { VWCCalendar } from '@vonage/vwc-calendar';
+import '@vonage/vwc-calendar/vwc-calendar-event.js';
+import * as stories from '@vonage/vwc-calendar/stories/calendar.stories';
 import { storiesToElement } from '../../utils/storiesToElement';
 
 VWCCalendar;
@@ -8,6 +9,11 @@ VWCCalendar;
 export async function createElementVariations(wrapper) {
 	wrapper.style.transform = 'scale(.5)';
 	wrapper.appendChild(storiesToElement(stories));
+
+	await wrapper.querySelector('vwc-calendar').updateComplete;
+
+	const events = Array.from(wrapper.querySelectorAll('vwc-calendar-event'));
+	await Promise.allSettled(events.map(({ updateComplete }) => updateComplete));
 
 	await vvdCore.settled;
 }
