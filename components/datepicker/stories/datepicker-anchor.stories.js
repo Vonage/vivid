465ef146584f5e4511ea7b0aba784f71c96e66c9
tveinfeld <67224311+tveinfeld@users.noreply.github.com>
2021-06-23 12:15:31 +0300

    chore(date-picker): move back to alpha (#910)
    * chore(date-picker): move back to alpha
    
    * chore(date-picker): added deprecation message
    
    Co-authored-by: Yonatan Kra <yonatan.kra@vonage.com>

--- components/datepicker/stories/datepicker-anchor.stories.js
+++ components/datepicker/stories/datepicker-anchor.stories.js
@@ -1,5 +1,6 @@
 import '@vonage/vwc-datepicker';
 import '@vonage/vwc-icon-button';
+import '@vonage/vwc-banner';
 import { html } from 'lit-element';
 import { spread } from '@open-wc/lit-helpers';
 
@@ -8,8 +9,14 @@ const Template = args => html`
 		html, body {
 			height: 100%;
 		}
+		vwc-note {
+			margin-bottom: 1rem;
+		}
 	</style>
 
+	<vwc-note header="This component is being deprecated!" open icon="warning-solid" connotation="warning">
+		We're working on a newer and better solution. Sorry for the inconvenience.
+	</vwc-note>
   <vwc-textfield id="input" dense></vwc-textfield>
   <vwc-icon-button id="trigger" icon="calendar" layout="filled" @click="${handleOpenDatepicker}"></vwc-icon-button>
   <vwc-datepicker id="datepicker" ...=${spread(args)}></vwc-datepicker>
