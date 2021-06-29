da169f820de1508a9a354f8e0e951ef5e28ddd8f
yinonov <yinon@hotmail.com>
2021-06-29 15:43:41 +0300

    remove ui tests from this PR

--- ui-tests/tests/vwc-file-picker/index.js
+++
@@ -1,9 +0,0 @@
-import '@vonage/vwc-file-picker';
-import '@vonage/vwc-button';
-import * as stories from '@vonage/vwc-file-picker/stories/file-picker-presentation.stories';
-import { storiesToElement } from '../../utils/storiesToElement';
-
-
-export async function createElementVariations(wrapper) {
-	wrapper.appendChild(storiesToElement(stories));
-}
