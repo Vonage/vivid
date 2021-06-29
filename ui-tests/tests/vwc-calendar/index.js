7de81eda7723078bab0b3359911721682913b4b1
yinonov <yinon@hotmail.com>
2021-06-29 14:36:49 +0300

    restore all calendar ui tests

--- ui-tests/tests/vwc-calendar/index.js
+++ ui-tests/tests/vwc-calendar/index.js
@@ -1,11 +1,11 @@
 import '@vonage/vwc-calendar';
 import '@vonage/vwc-calendar/vwc-calendar-event.js';
-import { Basic as calendarBasic } from '@vonage/vwc-calendar/stories/calendar.stories';
+import * as stories from '@vonage/vwc-calendar/stories/calendar.stories';
 import { storiesToElement } from '../../utils/storiesToElement';
 
 
 export async function createElementVariations(wrapper) {
-	wrapper.appendChild(storiesToElement({ calendarBasic }));
+	wrapper.appendChild(storiesToElement(stories));
 }
 
 
