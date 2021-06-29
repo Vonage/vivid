3355e97b17839df8655e07f50009779ba5b9ca8a
Tal Weinfeld <tal.weinfeld@vonage.com>
2021-06-29 13:30:16 +0300

    updated stories and tests

--- components/audio/test/audio.test.js
+++ components/audio/test/audio.test.js
@@ -38,6 +38,12 @@ describe('vwc-audio', () => {
 		expect(vwcAudioEl.shadowRoot.querySelector('vwc-scrub-bar')).not.to.exist;
 	});
 
+	it(`should show timestamp indicator when "timestamp" is set`, async function () {
+		const [vwcAudioEl] = addElements(textToDomToParent(`<vwc-audio timestamp></vwc-audio>`));
+		await waitNextTask();
+		expect(vwcAudioEl.shadowRoot.querySelector('.playhead-position')).to.exist;
+	});
+
 	it('should cover audio methods', async function () {
 		const [audioElement] = addElements(textToDomToParent(`<vwc-audio></vwc-audio>`));
 		await waitNextTask();
