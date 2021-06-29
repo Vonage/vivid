fdb62bfee6c0cf41ebfa4c0ca5d9483a8ec4afdb
yinon <yinon@hotmail.com>
2021-06-29 12:09:28 +0300

    chore(vwc-calendar): accessible keyboard interactions (#905)
    * chore(vwc-calendar): accessible keyboard shortcuts
    
    * finalize arrow keys
    
    * remove duplicate tabindex attr
    
    * wrong import
    
    * calendar event focus case
    
    * check actions
    
    * /
    
    * testing keyboard arrow events
    
    * forgot gridcell
    
    * narrow down duplicates
    
    * unused import
    
    * reduce duplicates
    
    * arrowLeft usage in test
    
    * test arrow left
    
    * group vertical / horizontal arrows tests
    
    * group all keyboard interactions tests
    
    * partial review comments resolved
    
    * arrows consts
    
    * removed unnecessary condition
    
    * switch case indentation
    
    * improve tests readability
    
    Co-authored-by: tveinfeld <67224311+tveinfeld@users.noreply.github.com>

--- components/calendar/test/calendar.a11y.test.js
+++ components/calendar/test/calendar.a11y.test.js
@@ -1,7 +1,6 @@
 import '../vwc-calendar.js';
 import {
 	isolatedElementsCreation,
-	waitNextTask,
 	textToDomToParent
 } from '../../../test/test-helpers.js';
 import { chaiA11yAxe } from 'chai-a11y-axe';
@@ -13,14 +12,80 @@ const COMPONENT_NAME = 'vwc-calendar';
 describe('calendar a11y', () => {
 	const addElement = isolatedElementsCreation();
 
-	it('should have 0 accessibility violations', async () => {
+	const addCalendarElement = async (content) => {
 		const [actualElement] = addElement(
-			textToDomToParent(`<${COMPONENT_NAME}></${COMPONENT_NAME}>`)
+			textToDomToParent(`<${COMPONENT_NAME}>${content || ''}</${COMPONENT_NAME}>`)
 		);
-		await waitNextTask();
+		await actualElement.updateComplete;
 
-		await expect(actualElement).shadowDom.to.be.accessible({
-			ignoredRules: ['list'],
+		return actualElement;
+	};
+
+	const extractCalendarElements = (actualElement) => {
+		const { shadowRoot } = actualElement;
+		return {
+			actualElement,
+			shadowRoot,
+			grid: shadowRoot.querySelector('[role="grid"i]')
+		};
+	};
+
+	const createKEvent = key => new KeyboardEvent('keydown', { key });
+
+	it('should pass accessibility test', async () => {
+		const { actualElement } = extractCalendarElements(await addCalendarElement());
+		await expect(actualElement).shadowDom.to.be.accessible();
+	});
+
+	describe('keyboard events', () => {
+		it('should focus to default on initial keyboard interaction', async () => {
+			const { shadowRoot, grid } = extractCalendarElements(await addCalendarElement());
+
+			grid.dispatchEvent(createKEvent('ArrowDown'));
+
+			const defaultFocusElement = grid.querySelector('[role="columnheader"i]');
+
+			expect(shadowRoot.activeElement).to.equal(defaultFocusElement);
+		});
+
+		it('should change focus on keyboard arrow interactions', async () => {
+			const { shadowRoot, grid } = extractCalendarElements(await addCalendarElement());
+
+			grid.querySelector('[role="columnheader"i]:nth-child(3)').focus();
+
+			const getRole = (role, i) => grid.querySelector(`[role="${role}"i]:nth-child(${i})`);
+			const moveToElement = (key) => {
+				grid.dispatchEvent(createKEvent(key));
+				return shadowRoot.activeElement;
+			};
+
+			const focusedElementAfterMovingRight = moveToElement('ArrowRight');
+			const focusedElementAfterMovingLeft = moveToElement('ArrowLeft');
+			const focusedElementAfterMovingUp = moveToElement('ArrowUp');
+			const focusedElementAfterMovingDown = moveToElement('ArrowDown');
+
+			expect(focusedElementAfterMovingRight).to.equal(getRole('columnheader', 4));
+			expect(focusedElementAfterMovingLeft).to.equal(getRole('columnheader', 3));
+			expect(focusedElementAfterMovingUp).to.equal(getRole('gridcell', 3));
+			expect(focusedElementAfterMovingDown).to.equal(getRole('columnheader', 3));
+		});
+
+		it('should move focus from calendar event to containing column', async () => {
+			const eventComponent = 'vwc-calendar-event';
+
+			const { actualElement, shadowRoot, grid } = extractCalendarElements(await addCalendarElement(
+				`<${eventComponent} slot="day-2" start="4" duration="5"></${eventComponent}>`
+			));
+
+			actualElement.querySelector(eventComponent)
+				.shadowRoot.querySelector('section')
+				.focus();
+
+			grid.dispatchEvent(createKEvent('ArrowUp'));
+
+			expect(shadowRoot.activeElement).to.equal(
+				grid.querySelector('[role="gridcell"i]:nth-child(3)')
+			);
 		});
 	});
 });
