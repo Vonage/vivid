f036d9078d60249b0e5c7ecbdc8e2d0a57b24765
yonatankra <yonatan.kra@vonage.com>
2021-07-04 14:20:43 +0300

    Refactor a bit

--- ui-tests/utils/testPageGenerator.js
+++ ui-tests/utils/testPageGenerator.js
@@ -5,22 +5,37 @@ const { pascalCase } = require('pascal-case');
 
 const template = fs.readFileSync(path.join(__dirname, '../assets/testPage.js.tmpl')).toString();
 
-function generateTestPage(excludeList = []) {
-	const testsNames = getTestFolders(path.join(__dirname, '../tests')).filter(testName => !excludeList.includes(testName));
-	const testsImports = testsNames.reduce((result, testName) => {
-		return `${result}import { createElementVariations as ${pascalCase(testName)} } from './tests/${testName}';
-	`;
-	}, '');
-	const testsCalls = testsNames.reduce((result, testName) => {
-		return `${result}(() => {
+function generateComponentImport(testName) {
+	return `import { createElementVariations as ${pascalCase(testName)} } from './tests/${testName}';`;
+}
+
+function generateComponentContentFunction(testName) {
+	return `(() => {
 			const wrapperElement = document.createElement('div');
 			wrapperElement.id = "${pascalCase(testName)}";
 			wrapper.appendChild(wrapperElement);
 			return ${pascalCase(testName)}(wrapperElement);
 		})(),
+	`;
+}
+
+function generateTestPageFileContents(testsImports, testsCalls) {
+	const fileContents = (template.replace('${testImports}', testsImports)).replace('${testCalls}', testsCalls);
+	return fileContents;
+}
+
+function generateTestPage(excludeList = []) {
+	const testsNames = getTestFolders(path.join(__dirname, '../tests')).filter(testName => !excludeList.includes(testName));
+	const testsImports = testsNames.reduce((result, testName) => {
+		return `${result}${generateComponentImport(testName)}
+	`;
+	}, '');
+
+	const testsCalls = testsNames.reduce((result, testName) => {
+		return `${result}${generateComponentContentFunction(testName)}
 		`;
 	}, '');
-	const fileContents = (template.replace('${testImports}', testsImports)).replace('${testCalls}', testsCalls);
+	const fileContents = generateTestPageFileContents(testsImports, testsCalls);
 	saveFile('../testPage.js', fileContents);
 }
 
