f179b8512fd903dd4706bd6870d4fa46324bf030
yinonov <yinon@hotmail.com>
2021-07-06 09:59:58 +0300

    tests coverage

--- ui-tests/tests/vwc-accordion/index.js
+++ ui-tests/tests/vwc-accordion/index.js
@@ -1,4 +1,5 @@
 import '@vonage/vwc-accordion';
+import '@vonage/vwc-accordion';
 import '@vonage/vwc-expansion-panel';
 
 
@@ -6,10 +7,31 @@ export async function createElementVariations(wrapper) {
 	const accordionElementWrapper = document.createElement('div');
 	accordionElementWrapper.innerHTML =
 		`
+		  <vwc-expansion-panel header="Campaign Details"></vwc-expansion-panel>
+
+		  <vwc-expansion-panel header="Campaign Details" indicatorIconSet="binary"></vwc-expansion-panel>
+		  <vwc-expansion-panel header="Campaign Details" icon="chat-solid"></vwc-expansion-panel>
+		  <vwc-expansion-panel header="Campaign Details" leadingToggle></vwc-expansion-panel>
+		  <vwc-expansion-panel header="Campaign Details" dense></vwc-expansion-panel>
+
 		  <vwc-expansion-panel open header="Campaign Details">
 				A campaign describes a specific use case and details of the messages you will be sending through it.
 				Such as: sample messages, subscriber opt-in/out, and the associated numbers you will be sending with these messages.
 			</vwc-expansion-panel>
+
+			<vwc-accordion>
+				<vwc-expansion-panel open header="Item 1">lorem ipsum dolor amet</vwc-expansion-panel>
+				<vwc-expansion-panel header="Item 2">lorem ipsum dolor amet</vwc-expansion-panel>
+				<vwc-expansion-panel open header="Item 3">lorem ipsum dolor amet</vwc-expansion-panel>
+				<vwc-expansion-panel header="Item 4">lorem ipsum dolor amet</vwc-expansion-panel>
+			</vwc-accordion>
+
+			<vwc-accordion multi>
+				<vwc-expansion-panel open header="Item 1">lorem ipsum dolor amet</vwc-expansion-panel>
+				<vwc-expansion-panel header="Item 2">lorem ipsum dolor amet</vwc-expansion-panel>
+				<vwc-expansion-panel open header="Item 3">lorem ipsum dolor amet</vwc-expansion-panel>
+				<vwc-expansion-panel header="Item 4">lorem ipsum dolor amet</vwc-expansion-panel>
+			</vwc-accordion>
 		`;
 	wrapper.appendChild(accordionElementWrapper);
 }
