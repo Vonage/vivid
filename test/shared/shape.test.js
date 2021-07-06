dfa8a2b3d67cdb920fb6890f15158bf2369cc766
yonatankra <yonatan.kra@vonage.com>
2021-07-06 14:48:51 +0300

    Clear stray elements from the test

--- test/shared/shape.test.js
+++ test/shared/shape.test.js
@@ -11,13 +11,17 @@ import {
 
 export async function shapeRoundedTestCases(COMPONENT_NAME) {
 	const addElement = isolatedElementsCreation();
+	let formElement,
+		actualElement;
 
-	const addedElements = addElement(
-		textToDomToParent(`<${COMPONENT_NAME}></${COMPONENT_NAME}>`)
-	);
-	await waitNextTask();
-	const formElement = addedElements[0];
-	const actualElement = formElement.shadowRoot.querySelector(topLevelSelectors[COMPONENT_NAME]);
+	before(async () => {
+		const addedElements = addElement(
+			textToDomToParent(`<${COMPONENT_NAME}></${COMPONENT_NAME}>`)
+		);
+		await waitNextTask();
+		formElement = addedElements[0];
+		actualElement = formElement.shadowRoot.querySelector(topLevelSelectors[COMPONENT_NAME]);
+	});
 
 	it('should have rounded shape by default', async () => {
 		assertComputedStyle(actualElement, shapeStyles('rounded'));
@@ -32,12 +36,15 @@ export async function shapeRoundedTestCases(COMPONENT_NAME) {
 
 export async function shapePillTestCases(COMPONENT_NAME, assertDense = true) {
 	const addElement = isolatedElementsCreation();
+	let actualElement;
 
-	const [e] = addElement(
-		textToDomToParent(`<${COMPONENT_NAME} shape="pill"></${COMPONENT_NAME}>`)
-	);
-	await waitNextTask();
-	const actualElement = e.shadowRoot.querySelector(topLevelSelectors[COMPONENT_NAME]);
+	before(async () => {
+		const [e] = addElement(
+			textToDomToParent(`<${COMPONENT_NAME} shape="pill"></${COMPONENT_NAME}>`)
+		);
+		await waitNextTask();
+		actualElement = e.shadowRoot.querySelector(topLevelSelectors[COMPONENT_NAME]);
+	});
 
 	it(`${COMPONENT_NAME} should have pill shape when shape set to pill`, async () => {
 		assertComputedStyle(actualElement, shapeStyles('pill'));
@@ -53,12 +60,18 @@ export async function shapePillTestCases(COMPONENT_NAME, assertDense = true) {
 export async function shapeCircledTestCases(COMPONENT_NAME) {
 	const addElement = isolatedElementsCreation();
 
-	const addedElements = addElement(
-		textToDomToParent(`<${COMPONENT_NAME} shape="circled"></${COMPONENT_NAME}>`)
-	);
-	await waitNextTask();
-	const formElement = addedElements[0];
-	const actualElement = formElement.shadowRoot.querySelector(topLevelSelectors[COMPONENT_NAME]);
+	let formElement,
+		actualElement;
+
+	before(async () => {
+		const addedElements = addElement(
+			textToDomToParent(`<${COMPONENT_NAME} shape="circled"></${COMPONENT_NAME}>`)
+		);
+		await waitNextTask();
+		formElement = addedElements[0];
+		actualElement = formElement.shadowRoot.querySelector(topLevelSelectors[COMPONENT_NAME]);
+	});
+
 
 	it('should have circled shape when shape set to circled', async () => {
 		assertComputedStyle(actualElement, shapeStyles('circled'));
