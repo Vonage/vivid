bf84a5e06d2d18ae0a9cdbff30268b2c9ed579ca
yinonov <yinon@hotmail.com>
2021-07-06 13:39:46 +0300

    restore stories

---
+++ components/expansion-panel/stories/arg-types.js
@@ -0,0 +1,47 @@
+export const argTypes = {
+	header: {
+		control: {
+			type: 'text'
+		}
+	},
+	indicatorIconSet: {
+		control: {
+			type: 'select',
+			options: ['chevron', 'binary'],
+		}
+	},
+	dense: {
+		control: {
+			type: 'inline-radio',
+			options: { 'true': '', 'false': undefined }
+		}
+	},
+	leadingToggle: {
+		control: {
+			type: 'inline-radio',
+			options: { 'true': '', 'false': undefined }
+		}
+	},
+	icon: {
+		control: {
+			type: 'text'
+		}
+	},
+	noRipple: {
+		control: {
+			type: 'inline-radio',
+			options: { 'true': '', 'false': undefined }
+		}
+	},
+	open: {
+		control: {
+			type: 'inline-radio',
+			options: { 'true': '', 'false': undefined }
+		}
+	},
+	styles: { table: { disable: true } },
+	closeOnSelect: { table: { disable: true } },
+	headerListItemIcon: { table: { disable: true } },
+	trailingIcon: { table: { disable: true } },
+	ripple: { table: { disable: true } }
+}
