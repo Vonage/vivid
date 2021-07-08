04e69f78126b6acab2dd719b161a56a39176d3f2
Yonatan Kra <yonatan.kra@vonage.com>
2021-07-08 16:34:04 +0300

    chore(vwc-snackbar): fix flaky snackbar tests (#941)
    * Clear stray elements from the test
    
    * Clear out clatter caused by the shape tests
    
    * Set the snackbar layout test as fixed position

--- test/shared/shape.test.js
+++ test/shared/shape.test.js
@@ -11,13 +11,17 @@ import {
 
 export async function shapeRoundedTestCases(COMPONENT_NAME) {
 	const addElement = isolatedElementsCreation();
+	let formElement,
+		actualElement;
 
-	const addedElements = addElement(
-		textToDomToParent(`<${COMPONENT_NAME}></${COMPONENT_NAME}>`)
-	);
-	await waitNextTask();
-	const formElement = addedElements[0];
-	const actualElement = formElement.shadowRoot.querySelector(topLevelSelectors[COMPONENT_NAME]);
+	beforeEach(async () => {
+		const addedElements = addElement(
+			textToDomToParent(`<${COMPONENT_NAME}></${COMPONENT_NAME}>`)
+		);
+		await waitNextTask();
+		formElement = addedElements[0];
+		actualElement = formElement.shadowRoot.querySelector(topLevelSelectors[COMPONENT_NAME]);
+	});
 
 	it('should have rounded shape by default', async () => {
 		assertComputedStyle(actualElement, shapeStyles('rounded'));
@@ -32,12 +36,16 @@ export async function shapeRoundedTestCases(COMPONENT_NAME) {
 
 export async function shapePillTestCases(COMPONENT_NAME, assertDense = true) {
 	const addElement = isolatedElementsCreation();
+	let actualElement,
+		e;
 
-	const [e] = addElement(
-		textToDomToParent(`<${COMPONENT_NAME} shape="pill"></${COMPONENT_NAME}>`)
-	);
-	await waitNextTask();
-	const actualElement = e.shadowRoot.querySelector(topLevelSelectors[COMPONENT_NAME]);
+	beforeEach(async () => {
+		[e] = addElement(
+			textToDomToParent(`<${COMPONENT_NAME} shape="pill"></${COMPONENT_NAME}>`)
+		);
+		await waitNextTask();
+		actualElement = e.shadowRoot.querySelector(topLevelSelectors[COMPONENT_NAME]);
+	});
 
 	it(`${COMPONENT_NAME} should have pill shape when shape set to pill`, async () => {
 		assertComputedStyle(actualElement, shapeStyles('pill'));
@@ -53,12 +61,18 @@ export async function shapePillTestCases(COMPONENT_NAME, assertDense = true) {
 export async function shapeCircledTestCases(COMPONENT_NAME) {
 	const addElement = isolatedElementsCreation();
 
-	const addedElements = addElement(
-		textToDomToParent(`<${COMPONENT_NAME} shape="circled"></${COMPONENT_NAME}>`)
-	);
-	await waitNextTask();
-	const formElement = addedElements[0];
-	const actualElement = formElement.shadowRoot.querySelector(topLevelSelectors[COMPONENT_NAME]);
+	let formElement,
+		actualElement;
+
+	beforeEach(async () => {
+		const addedElements = addElement(
+			textToDomToParent(`<${COMPONENT_NAME} shape="circled"></${COMPONENT_NAME}>`)
+		);
+		await waitNextTask();
+		formElement = addedElements[0];
+		actualElement = formElement.shadowRoot.querySelector(topLevelSelectors[COMPONENT_NAME]);
+	});
+
 
 	it('should have circled shape when shape set to circled', async () => {
 		assertComputedStyle(actualElement, shapeStyles('circled'));
