08b60996e66c19d272d3a0c1cdcc7b1023f49057
yinonov <yinon@hotmail.com>
2021-07-11 01:24:58 +0300

    hardcoded html

--- ui-tests/tests/vwc-file-picker/index.js
+++ ui-tests/tests/vwc-file-picker/index.js
@@ -1,7 +1,11 @@
-import * as stories from '@vonage/vwc-file-picker/stories/file-picker-presentation.stories';
-import { storiesToElement } from '../../utils/storiesToElement';
+import '@vonage/vwc-file-picker';
+import '@vonage/vwc-button';
+import '@vonage/vwc-icon';
 
+import { filePickersHTML } from './html';
 
 export async function createElementVariations(wrapper) {
-	wrapper.appendChild(storiesToElement(stories));
+	const el = document.createElement('div');
+	el.innerHTML = filePickersHTML;
+	wrapper.appendChild(el);
 }
