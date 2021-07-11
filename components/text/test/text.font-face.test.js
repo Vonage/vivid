43514fe2324d6b530a526c8944b5e8b79100d335
yinonov <yinon@hotmail.com>
2021-07-11 11:53:45 +0300

    irrelevant FONT_FACES_SUPPORTED

--- components/text/test/text.font-face.test.js
+++ components/text/test/text.font-face.test.js
@@ -6,29 +6,6 @@ import {
 
 
 const VWC_TEXT = 'vwc-text';
-const FONT_FACES_SUPPORTED = [
-	'body-1',
-	'body-1-bold',
-	'body-1-code',
-	'body-1-link',
-	'body-2',
-	'body-2-bold',
-	'body-2-code',
-	'body-2-link',
-	'button',
-	'button-dense',
-	'button-enlarge',
-	'caption',
-	'caption-bold',
-	'caption-code',
-	'caption-link',
-	'headline-1',
-	'headline-2',
-	'subtitle-1',
-	'subtitle-2',
-	'title-1',
-	'title-2'
-];
 
 describe(`${VWC_TEXT} font face`, () => {
 	const addElement = isolatedElementsCreation();
