711b1ecd5378518ccdf57248708805b05203749b
yonatankra <yonatan.kra@vonage.com>
2021-07-13 08:34:18 +0300

    Add the documentation to the storybook

---
+++ components/button-group/stories/button-group-introduction.config.mjs
@@ -0,0 +1,14 @@
+export default {
+	sourcePath: '../readme.md',
+	outputName: 'button-group-introduction',
+	story: {
+		title: 'Alpha/Components/Button Group',
+		name: 'Introduction',
+		parameters: {
+			options: {
+				showPanel: false,
+				isToolshown: false
+			}
+		}
+	}
+};
