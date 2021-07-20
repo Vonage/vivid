0bb3d9de92011fa8e505982952d2a3d568a3f293
Yonatan Kra <yonatan.kra@vonage.com>
2021-07-20 16:26:52 +0300

    chore(list): improve list documentation in storybook (#975)
    * Add list ui-tests
    
    * Fix the two lines icon issue
    
    * Reduce padding of the list to 16px
    
    * Bottom border color API
    
    * Allow color change to all borders
    
    * Set the --vvd-list-color-divider API
    
    * fix(list): side margin set to 16px
    
    * fix(list): vertical center the left icon
    
    * fix(list): add documentation
    
    * Remove obsolete line
    
    * Change checkbox and radio padding
    
    * Fix css tests
    
    * Improve documentation
    
    * rename to align with calendar
    
    * Fix another test
    
    * Add component's header
    
    * chore(list): improve documentation
    
    * Improve docs
    
    * Update snapshots
    
    * Fix the list ui-test
    
    Co-authored-by: yinonov <yinon@hotmail.com>

--- ui-tests/tests/vwc-list/index.js
+++ ui-tests/tests/vwc-list/index.js
@@ -3,7 +3,7 @@ import { storiesToElement } from '../../utils/storiesToElement';
 import * as basicStories from '@vonage/vwc-list/stories/list-basic.stories';
 import * as checkStories from '@vonage/vwc-list/stories/list-check.stories';
 import * as expansionPanelStories from '@vonage/vwc-list/stories/list-expansion-panel.stories';
-import * as metaStories from '@vonage/vwc-list/stories/list-interactive-meta.stories';
+import * as metaStories from '@vonage/vwc-list/stories/list-meta-icon.stories';
 import * as listItemStories from '@vonage/vwc-list/stories/list-item.stories';
 import * as radioItemStories from '@vonage/vwc-list/stories/list-radio.stories';
 
