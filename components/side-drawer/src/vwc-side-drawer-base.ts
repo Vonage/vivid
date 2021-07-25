a22dba38ac55b2efa119bfb005e906d3ae5790ff
yinonov <yinon@hotmail.com>
2021-07-25 15:53:47 +0300

    alternate scheme

--- components/side-drawer/src/vwc-side-drawer-base.ts
+++ components/side-drawer/src/vwc-side-drawer-base.ts
@@ -4,8 +4,8 @@ import {
 
 export class VWCSideDrawerBase extends LitElement {
 	protected render(): TemplateResult {
-		return html`<div class="side-drawer">
-						<slot></slot>
-					</div>`;
+		return html`<div class="side-drawer" part="vvd-scheme-alternate">
+			<slot></slot>
+		</div>`;
 	}
 }
