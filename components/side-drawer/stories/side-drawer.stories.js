7d0d6febe8c6d02e575161bbe3a7dcff80ad1400
Rina <rina.oksman@vonage.com>
2021-07-25 15:32:04 +0300

    story added

--- components/side-drawer/stories/side-drawer.stories.js
+++ components/side-drawer/stories/side-drawer.stories.js
@@ -1,7 +1,12 @@
 import '@vonage/vwc-side-drawer/vwc-side-drawer.js';
+import '@vonage/vwc-expansion-panel/vwc-expansion-panel.js';
+import '@vonage/vwc-text/vwc-text.js';
+
 import { html } from 'lit-element';
 import { spread } from '@open-wc/lit-helpers';
 import { argTypes } from './arg-types.js';
+
+
 
 export default {
   title: 'Alpha/Components/Side Drawer',
@@ -9,7 +14,65 @@ export default {
   argTypes
 };
 
-const Template = args => html`<vwc-side-drawer ...=${spread(args)}></vwc-side-drawer>`;
+const Template = args => html`
+   <vwc-side-drawer ...=${spread(args)}>
+
+    <vwc-list-item shape="rounded" graphic="icon">
+      <vwc-icon slot="graphic" type="home-line"></vwc-icon>
+      1st level item
+    </vwc-list-item>
+
+    <vwc-text font-face="title-3">
+      SECTION TITLE
+    </vwc-text>
+
+    <vwc-list-item shape="rounded" graphic="icon">
+      <vwc-icon slot="graphic" type="chat-line"></vwc-icon>
+      1st level item
+    </vwc-list-item>
+    <vwc-list-item shape="rounded" graphic="icon">
+      <vwc-icon slot="graphic" type="chat-line"></vwc-icon>
+      1st level item
+    </vwc-list-item>
+		<vwc-list-expansion-panel open>
+			<vwc-list-item slot="header" shape="rounded" graphic="icon">
+				<vwc-icon slot="graphic" type="chat-line"></vwc-icon>
+				1st level item
+			</vwc-list-item>
+			<vwc-list-item shape="rounded">
+        2nd level item
+			</vwc-list-item>
+			<vwc-list-item shape="rounded">
+        2nd level item
+			</vwc-list-item>
+		</vwc-list-expansion-panel>
+
+    <vwc-text font-face="title-3">
+      SECTION TITLE
+    </vwc-text>
+  
+		<vwc-list-expansion-panel>
+			<vwc-list-item slot="header" shape="rounded" graphic="icon">
+				<vwc-icon slot="graphic" type="chat-line"></vwc-icon>
+				1st level item
+			</vwc-list-item>
+			<vwc-list-item shape="rounded">
+      2nd level item
+			</vwc-list-item>
+			<vwc-list-expansion-panel>
+				<vwc-list-item slot="header" shape="rounded">
+        2nd level item
+				</vwc-list-item>
+				<vwc-list-item shape="rounded">
+        3rd level item
+				</vwc-list-item>
+				<vwc-list-item shape="rounded">
+        3rd level item
+				</vwc-list-item>
+			</vwc-list-expansion-panel>
+		</vwc-list-expansion-panel>
+
+   </vwc-side-drawer> 
+`;
 
 export const Basic = Template.bind({});
-Basic.args = { label: 'Basic', disabled: 'false' };
