081ba7f6a4344342d40bb9ce235b1101b33c41df
yinonov <yinon@hotmail.com>
2021-07-25 22:53:43 +0300

    added icons tests

--- __snapshots__/badge.md
+++ __snapshots__/badge.md
@@ -1,10 +1,99 @@
 # `badge`
 
-#### `should internal contents`
+#### `should match internal contents`
 
 ```html
-<slot>
-</slot>
+<span class="vwc-badge">
+  <slot>
+    badge
+  </slot>
+</span>
+```
 
+#### `should match internal contents (legacy)`
+
+```html
+<span class="vwc-badge">
+  <slot>
+  </slot>
+</span>
+
+```
+
+## `icons`
+
+####   `should have icons when icons are set`
+
+```html
+<span class="vwc-badge">
+  <vwc-icon
+    class="icon icon--leading"
+    type="thumbs-down-line"
+  >
+  </vwc-icon>
+  <slot>
+  </slot>
+  <vwc-icon
+    class="icon icon--trailing"
+    type="thumbs-up-line"
+  >
+  </vwc-icon>
+</span>
+```
+
+####   `should have icons added when icons are set dynamically (property)`
+
+```html
+<span class="vwc-badge">
+  <vwc-icon
+    class="icon icon--leading"
+    type="thumbs-down-line"
+  >
+  </vwc-icon>
+  <slot>
+  </slot>
+  <vwc-icon
+    class="icon icon--trailing"
+    type="thumbs-down-line"
+  >
+  </vwc-icon>
+</span>
+```
+
+####   `should have icons added when icons are set dynamically (attribute)`
+
+```html
+<span class="vwc-badge">
+  <vwc-icon
+    class="icon icon--leading"
+    type="thumbs-down-line"
+  >
+  </vwc-icon>
+  <slot>
+  </slot>
+  <vwc-icon
+    class="icon icon--trailing"
+    type="thumbs-up-line"
+  >
+  </vwc-icon>
+</span>
+```
+
+####   `should have icons removed when icons are unset (property)`
+
+```html
+<span class="vwc-badge">
+  <slot>
+  </slot>
+</span>
+```
+
+####   `should have icon removed when icon is unset (attribute)`
+
+```html
+<span class="vwc-badge">
+  <slot>
+  </slot>
+</span>
 ```
 
--- components/badge/test/badge.test.js
+++ components/badge/test/badge.test.js
@@ -22,20 +22,27 @@ describe('badge', () => {
 		);
 	});
 
-	it('should internal contents', async () => {
-		const [actualElement] = addElement(
-			textToDomToParent(`<${VWC_BADGE}>Badge Text</${VWC_BADGE}>`)
+	it('should match internal contents', async () => {
+		const [badge] = addElement(
+			textToDomToParent(`<${VWC_BADGE} text="badge"></${VWC_BADGE}>`)
 		);
-		await waitNextTask();
-		expect(actualElement.shadowRoot.innerHTML).to.equalSnapshot();
+		expect(badge.shadowRoot.innerHTML).to.equalSnapshot();
+	});
+
+	it('should match internal contents (legacy)', async () => {
+		const [badge] = addElement(
+			textToDomToParent(`<${VWC_BADGE}>badge</${VWC_BADGE}>`)
+		);
+		await badge.updateComplete;
+		expect(badge.shadowRoot.innerHTML).to.equalSnapshot();
 	});
 
 	describe('typography', function () {
 		it(`should have set badge (text, default) typography correct`, async function () {
 			const [badge] = addElement(
-				textToDomToParent(`<${VWC_BADGE}>Badge Text</${VWC_BADGE}>`)
+				textToDomToParent(`<${VWC_BADGE} text="badge"></${VWC_BADGE}>`)
 			);
-			await waitNextTask();
+			await badge.updateComplete;
 			expect(badge).to.exist;
 			assertComputedStyle(badge, { ...(await getTypographyStyle('caption-bold')), lineHeight: '24px' });
 		});
@@ -46,62 +53,127 @@ describe('badge', () => {
 			const addedElements = addElement(
 				textToDomToParent(`<${VWC_BADGE}>I'm a badge</${VWC_BADGE}>`)
 			);
-			const actualElement = addedElements[0];
-			await waitNextTask();
-			assertComputedStyle(actualElement, { height: '24px' });
+			const badge = addedElements[0];
+			await badge.updateComplete;
+			assertComputedStyle(badge, { height: '24px' });
 		});
 
 		it('should have dense size when dense', async () => {
 			const addedElements = addElement(
 				textToDomToParent(`<${VWC_BADGE} dense>I'm a badge</${VWC_BADGE}>`)
 			);
-			const actualElement = addedElements[0];
-			await waitNextTask();
-			assertComputedStyle(actualElement, { height: '20px' });
+			const badge = addedElements[0];
+			await badge.updateComplete;
+			assertComputedStyle(badge, { height: '20px' });
 		});
 
 		it('should have enlarged size when enlarged', async () => {
 			const addedElements = addElement(
 				textToDomToParent(`<${VWC_BADGE} enlarged>I'm a badge</${VWC_BADGE}>`)
 			);
-			const actualElement = addedElements[0];
-			await waitNextTask();
-			assertComputedStyle(actualElement, { height: '28px' });
+			const badge = addedElements[0];
+			await badge.updateComplete;
+			assertComputedStyle(badge, { height: '28px' });
 		});
 	});
 
 	describe('shape', () => {
-		let actualElement;
+		let badge;
 		beforeEach(async () => {
 			const addedElements = addElement(
 				textToDomToParent(
 					`<${VWC_BADGE} layout="filled">I'm a badge</${VWC_BADGE}>`
 				)
 			);
-			await waitNextTask();
-			actualElement = addedElements[0];
+			await badge.updateComplete;
+			badge = addedElements[0];
 		});
 
 		it('should have rounded shape by default', async () => {
 			assertComputedStyle(
-				actualElement,
+				badge,
 				shapeStyles('rounded', 'badge')
 			);
 		});
 
 		it('should have rounded shape when shape set to rounded', async () => {
-			actualElement.shape = 'rounded';
-			await waitNextTask();
+			badge.shape = 'rounded';
+			await badge.updateComplete;
 			assertComputedStyle(
-				actualElement,
+				badge,
 				shapeStyles('rounded', 'badge')
 			);
 		});
 
 		it('should have pill shape when shape set to pill', async () => {
-			actualElement.shape = 'pill';
-			await waitNextTask();
-			assertComputedStyle(actualElement, shapeStyles('pill', 'badge'));
+			badge.shape = 'pill';
+			await badge.updateComplete;
+			assertComputedStyle(badge, shapeStyles('pill', 'badge'));
+		});
+	});
+
+	describe('icons', () => {
+		it('should have icons when icons are set', async () => {
+			const [badge] = addElement(
+				textToDomToParent(
+					`<${VWC_BADGE} icon="thumbs-down-line" iconTrailing="thumbs-up-line"></${VWC_BADGE}>`
+				)
+			);
+
+			await badge.updateComplete;
+			expect(badge.shadowRoot.innerHTML).to.equalSnapshot();
+		});
+
+		it('should have icons added when icons are set dynamically (property)', async () => {
+			const [badge] = addElement(
+				textToDomToParent(
+					`<${VWC_BADGE}></${VWC_BADGE}>`
+				)
+			);
+
+			badge.icon = 'thumbs-down-line';
+			badge.iconTrailing = 'thumbs-down-line';
+			await badge.updateComplete;
+			expect(badge.shadowRoot.innerHTML).to.equalSnapshot();
+		});
+
+		it('should have icons added when icons are set dynamically (attribute)', async () => {
+			const [badge] = addElement(
+				textToDomToParent(
+					`<${VWC_BADGE}></${VWC_BADGE}>`
+				)
+			);
+
+			badge.setAttribute('icon', 'thumbs-down-line');
+			badge.setAttribute('iconTrailing', 'thumbs-up-line');
+			await badge.updateComplete;
+			expect(badge.shadowRoot.innerHTML).to.equalSnapshot();
+		});
+
+		it('should have icons removed when icons are unset (property)', async () => {
+			const [badge] = addElement(
+				textToDomToParent(
+					`<${VWC_BADGE} icon="thumbs-down-line" iconTrailing="thumbs-up-line"></${VWC_BADGE}>`
+				)
+			);
+
+			badge.icon = undefined;
+			badge.iconTrailing = undefined;
+			await badge.updateComplete;
+			expect(badge.shadowRoot.innerHTML).to.equalSnapshot();
+		});
+
+		it('should have icon removed when icon is unset (attribute)', async () => {
+			const [badge] = addElement(
+				textToDomToParent(
+					`<${VWC_BADGE} icon="thumbs-down-line" iconTrailing="thumbs-up-line"></${VWC_BADGE}>`
+				)
+			);
+
+			badge.removeAttribute('icon');
+			badge.removeAttribute('iconTrailing');
+			await badge.updateComplete;
+			expect(badge.shadowRoot.innerHTML).to.equalSnapshot();
 		});
 	});
 });
