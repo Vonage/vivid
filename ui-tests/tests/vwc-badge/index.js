e0840686e64e1fe8e6def0365d877a569ca1f2c1
yinonov <yinon@hotmail.com>
2021-07-25 23:43:06 +0300

    update badge ui test

--- ui-tests/tests/vwc-badge/index.js
+++ ui-tests/tests/vwc-badge/index.js
@@ -11,12 +11,12 @@ export async function createElementVariations(wrapper) {
 		<vwc-badge layout="filled" shape="pill" text="my badge"></vwc-badge>
 		<vwc-badge layout="filled" dense text="my badge"></vwc-badge>
 		<vwc-badge layout="filled" enlarged text="my badge"></vwc-badge>
-		<vwc-badge connotation="cta" layout="filled" text="my badge" icon="thumbs-down-line" iconTrailing="thumbs-down-line"></vwc-badge>
-		<vwc-badge connotation="cta" layout="soft" text="my badge" icon="thumbs-down-line" iconTrailing="thumbs-down-line"></vwc-badge>
-		<vwc-badge layout="outlined" text="my badge" icon="thumbs-down-line" iconTrailing="thumbs-down-line"></vwc-badge>
-		<vwc-badge layout="filled" shape="pill" text="my badge" icon="thumbs-down-line" iconTrailing="thumbs-down-line"></vwc-badge>
-		<vwc-badge layout="filled" dense text="my badge" icon="thumbs-down-line" iconTrailing="thumbs-down-line"></vwc-badge>
-		<vwc-badge layout="filled" enlarged text="my badge" icon="thumbs-down-line" iconTrailing="thumbs-down-line"></vwc-badge>
+		<vwc-badge connotation="cta" layout="filled" text="my badge" icon="thumbs-down-line" iconTrailing="thumbs-up-line"></vwc-badge>
+		<vwc-badge connotation="cta" layout="soft" text="my badge" icon="thumbs-down-line" iconTrailing="thumbs-up-line"></vwc-badge>
+		<vwc-badge layout="outlined" text="my badge" icon="thumbs-down-line" iconTrailing="thumbs-up-line"></vwc-badge>
+		<vwc-badge layout="filled" shape="pill" text="my badge" icon="thumbs-down-line" iconTrailing="thumbs-up-line"></vwc-badge>
+		<vwc-badge layout="filled" dense text="my badge" icon="thumbs-down-line" iconTrailing="thumbs-up-line"></vwc-badge>
+		<vwc-badge layout="filled" enlarged text="my badge" icon="thumbs-down-line" iconTrailing="thumbs-up-line"></vwc-badge>
 	`;
 	wrapper.appendChild(badgeElementWrapper);
 
