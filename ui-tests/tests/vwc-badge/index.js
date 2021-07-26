0bd106020cabf8a4f87ad602537ea0fb6b1d37c9
yinonov <yinon@hotmail.com>
2021-07-26 09:30:30 +0300

    migrate connotation test to UI

--- ui-tests/tests/vwc-badge/index.js
+++ ui-tests/tests/vwc-badge/index.js
@@ -7,16 +7,16 @@ export async function createElementVariations(wrapper) {
 	badgeElementWrapper.innerHTML = `
 		<vwc-badge connotation="cta" layout="filled" text="my badge"></vwc-badge>
 		<vwc-badge connotation="cta" layout="soft" text="my badge"></vwc-badge>
-		<vwc-badge layout="outlined" text="my badge"></vwc-badge>
-		<vwc-badge layout="filled" shape="pill" text="my badge"></vwc-badge>
-		<vwc-badge layout="filled" dense text="my badge"></vwc-badge>
-		<vwc-badge layout="filled" enlarged text="my badge"></vwc-badge>
-		<vwc-badge connotation="cta" layout="filled" text="my badge" icon="thumbs-down-line" iconTrailing="thumbs-up-line"></vwc-badge>
-		<vwc-badge connotation="cta" layout="soft" text="my badge" icon="thumbs-down-line" iconTrailing="thumbs-up-line"></vwc-badge>
+		<vwc-badge connotation="alert" layout="outlined" text="my badge"></vwc-badge>
+		<vwc-badge connotation="alert" layout="filled" shape="pill" text="my badge"></vwc-badge>
+		<vwc-badge connotation="success" layout="filled" dense text="my badge"></vwc-badge>
+		<vwc-badge connotation="info" layout="filled" enlarged text="my badge"></vwc-badge>
+		<vwc-badge connotation="primary" layout="soft" text="my badge" icon="thumbs-down-line" iconTrailing="thumbs-up-line"></vwc-badge>
+		<vwc-badge connotation="warning" layout="outlined" text="my badge" icon="thumbs-down-line" iconTrailing="thumbs-up-line"></vwc-badge>
 		<vwc-badge layout="outlined" text="my badge" icon="thumbs-down-line" iconTrailing="thumbs-up-line"></vwc-badge>
 		<vwc-badge layout="filled" shape="pill" text="my badge" icon="thumbs-down-line" iconTrailing="thumbs-up-line"></vwc-badge>
-		<vwc-badge layout="filled" dense text="my badge" icon="thumbs-down-line" iconTrailing="thumbs-up-line"></vwc-badge>
-		<vwc-badge layout="filled" enlarged text="my badge" icon="thumbs-down-line" iconTrailing="thumbs-up-line"></vwc-badge>
+		<vwc-badge connotation="success" layout="soft" dense text="my badge" icon="thumbs-down-line" iconTrailing="thumbs-up-line"></vwc-badge>
+		<vwc-badge connotation="alert" layout="filled" enlarged text="my badge" icon="thumbs-down-line" iconTrailing="thumbs-up-line"></vwc-badge>
 	`;
 	wrapper.appendChild(badgeElementWrapper);
 
