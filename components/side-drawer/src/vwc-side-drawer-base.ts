22d7599b756975d943033dd8982f1a878820eb90
yinonov <yinon@hotmail.com>
2021-07-26 11:00:36 +0300

    this will break

--- components/side-drawer/src/vwc-side-drawer-base.ts
+++ components/side-drawer/src/vwc-side-drawer-base.ts
@@ -1,10 +1,15 @@
 import {
-	html, LitElement, TemplateResult
+	html, LitElement, TemplateResult, property
 } from 'lit-element';
+
+import { nothing } from 'lit-html';
 export class VWCSideDrawerBase extends LitElement {
+	@property({ type: Boolean, reflect: true })
+	alternate?: boolean;
+
 	protected render(): TemplateResult {
 		return html`
-			<div class="side-drawer" part=${this.hasAttribute('vvd-scheme-alternate') ? '' : 'vvd-scheme-alternate'}>
+			<div class="side-drawer" part=${this.alternate ? 'vvd-scheme-alternate' ? nothing }>
 				<vwc-list innerRole="navigation" innerAriaLabel="Primary navigation" itemRoles="link">
 					<slot name="navigation"></slot>
 				</vwc-list>
