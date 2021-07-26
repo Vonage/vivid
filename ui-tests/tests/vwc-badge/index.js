9ea592fbe20eb32d409d4c68d09c1db761fdd109
yinonov <yinon@hotmail.com>
2021-07-26 09:45:14 +0300

    update ui tests

--- ui-tests/tests/vwc-badge/index.js
+++ ui-tests/tests/vwc-badge/index.js
@@ -7,8 +7,8 @@ export async function createElementVariations(wrapper) {
 	badgeElementWrapper.innerHTML = `
 		<vwc-badge connotation="cta" layout="filled" text="my badge"></vwc-badge>
 		<vwc-badge connotation="cta" layout="soft" text="my badge"></vwc-badge>
-		<vwc-badge connotation="alert" layout="outlined" text="my badge"></vwc-badge>
-		<vwc-badge connotation="alert" layout="filled" shape="pill" text="my badge"></vwc-badge>
+		<vwc-badge connotation="info" layout="outlined" text="my badge"></vwc-badge>
+		<vwc-badge connotation="alert" layout="soft" shape="pill" text="my badge"></vwc-badge>
 		<vwc-badge connotation="success" layout="filled" dense text="my badge"></vwc-badge>
 		<vwc-badge connotation="info" layout="filled" enlarged text="my badge"></vwc-badge>
 		<vwc-badge connotation="primary" layout="soft" text="my badge" icon="thumbs-down-line" iconTrailing="thumbs-up-line"></vwc-badge>
