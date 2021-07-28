abeb4815901fcf444446311b4e48f546b201ed70
Alexander Mazuruk <18071010+k-paxian@users.noreply.github.com>
2021-07-28 10:04:32 +0200

    feat(data-grid): expose `heightByRows` field to control grid height d… (#985)
    * feat(data-grid): expose `heightByRows` field to control grid height dynamically guided by content  #983
    
    * calm lint
    
    * height-by-rows attribute convention
    
    * refine changes
    
    * add space in story
    
    * correct description
    
    * correct doc
    
    Co-authored-by: tveinfeld <67224311+tveinfeld@users.noreply.github.com>
    Co-authored-by: Yonatan Kra <yonatan.kra@vonage.com>
    Co-authored-by: yinonov <yinon@hotmail.com>

---
+++ components/data-grid/stories/data-grid-dynamic-height-js.js
@@ -0,0 +1,45 @@
+import '@vonage/vwc-data-grid';
+import '@vonage/vwc-button';
+import { html } from 'lit-element';
+import { sequentalData } from './data-grid-demo-data-provider';
+
+let count = 2;
+
+const Template = args => html`
+	<vwc-button layout="outlined" @click="${add}">Add item</vwc-button>
+	<vwc-button layout="outlined" @click="${remove}">Remove item</vwc-button>
+	<br>
+	<br>
+	<vwc-data-grid
+		class="vvd-scheme-alternate"
+		.heightByRows="${true}"
+		.columns="${args.columns}"
+		.items="${args.items}">
+	</vwc-data-grid>
+`;
+
+export const HeightByRows = Template.bind({});
+
+HeightByRows.args = {
+	columns: [
+		{ header: 'First Name', path: 'fname' },
+		{ header: 'Last Name', path: 'lname' }
+	],
+	items: sequentalData({ fname: 'A-{i}', lname: 'B-{i}' }, count)
+};
+
+const getGrid = () => document.querySelector('vwc-data-grid');
+const updateGrid = () => {
+	getGrid().heightByRows = count <= 5;
+	getGrid().items = sequentalData({ fname: 'A-{i}', lname: 'B-{i}' }, count);
+};
+
+function add() {
+	count++;
+	updateGrid();
+}
+
+function remove() {
+	count--;
+	updateGrid();
+}
