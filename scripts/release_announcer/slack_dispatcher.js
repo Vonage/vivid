c341cf438f9673edc3c65901f8d2dcc018ed810b
Tal Weinfeld <tal.weinfeld@vonage.com>
2021-07-28 19:26:14 +0300

    removed console

--- scripts/release_announcer/slack_dispatcher.js
+++ scripts/release_announcer/slack_dispatcher.js
@@ -58,8 +58,6 @@ exports.releaseTemplate = ({ version, log_lines: rawLogLines })=>{
 		bugs = validLog.filter(({ comment })=> /^fix(e[ds])?.*?:/i.test(comment)).map(formatLogLine(':beetle:')),
 		features = validLog.filter(({ comment })=> /^feat(ures?)?.*?:/i.test(comment)).map(formatLogLine(':star:'));
 
-	console.log(validLog);
-
 	return {
 		"blocks": [
 			{
