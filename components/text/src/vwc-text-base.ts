153885709b5310b01a9b6e8ef3c93f7f5773748e
yinonov <yinon@hotmail.com>
2021-07-30 12:12:46 +0300

    style(vwc-text): support connotation set

--- components/text/src/vwc-text-base.ts
+++ components/text/src/vwc-text-base.ts
@@ -3,11 +3,24 @@ import { VVDFontFace } from '@vonage/vvd-design-tokens/build/types/font-faces';
 import {
 	html, LitElement, property, TemplateResult
 } from 'lit-element';
+import { Connotation } from '@vonage/vvd-foundation/constants';
 
+type TextConnotation = Extract<
+	Connotation,
+	| Connotation.Primary
+	| Connotation.CTA
+	| Connotation.Announcement
+	| Connotation.Info
+	| Connotation.Success
+	| Connotation.Alert
+	>;
 
 export class VWCTextBase extends LitElement {
-	@property({ type: String, reflect: true, attribute: 'font-face' })
+	@property({ type: String, reflect: true })
 	fontFace?: VVDFontFace;
+
+	@property({ type: String, reflect: true })
+	connotation?: TextConnotation;
 
 	protected render(): TemplateResult {
 		return html`<slot></slot>`;
