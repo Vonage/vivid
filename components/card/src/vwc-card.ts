d0e6348a22e25e494a568d89b9264a4423ad8f7a
yonatankra <yonatan.kra@vonage.com>
2021-08-03 13:39:31 +0300

    Heading and header-icon

--- components/card/src/vwc-card.ts
+++ components/card/src/vwc-card.ts
@@ -1,5 +1,6 @@
-import { customElement, LitElement } from 'lit-element';
+import { customElement, html, LitElement } from 'lit-element';
 import { style } from './vwc-card.css';
+import { property } from 'lit-element/lib/decorators';
 
 declare global {
 	interface HTMLElementTagNameMap {
@@ -13,4 +14,44 @@ export class VWCCard extends LitElement {
 	 * @internal
 	 */
 	static styles = style;
+
+	@property({
+		reflect: true,
+		attribute: 'heading',
+		type: String
+	})
+	heading = '';
+
+	@property({
+		reflect: true,
+		attribute: 'header-icon',
+		type: String
+	})
+	headerIcon: string | null = null;
+
+	protected render(): unknown {
+		return html`
+			<div>
+				<div class="vwc-card-media">
+					<slot name="media"></slot>
+				</div>
+				<header>
+					<slot name="header-icon">
+						<vwc-icon type="${this.headerIcon}"></vwc-icon>
+					</slot>
+					<vwc-badge></vwc-badge>
+					<div class="vwc-card-header">
+						${this.heading}
+					</div>
+				</header>
+				<div class="vwc-card-content">
+					<slot></slot>
+				</div>
+				<div class="vwc-card-actions">
+					<slot name="actions">
+					</slot>
+				</div>
+			</div>
+		`;
+	}
 }
--- components/card/test/card.test.js
+++ components/card/test/card.test.js
@@ -5,6 +5,7 @@ import {
 	isolatedElementsCreation
 } from '../../../test/test-helpers.js';
 import { chaiDomDiff } from '@open-wc/semantic-dom-diff';
+import { LitElement } from 'lit-element';
 
 chai.use(chaiDomDiff);
 
@@ -26,5 +27,104 @@ describe('Card', () => {
 		const actualElement = addedElements[0];
 		await waitNextTask();
 		expect(actualElement.shadowRoot.innerHTML).to.equalSnapshot();
+
+	describe(`heading`, function () {
+		const headingText = 'This is the heading';
+
+		it(`should set the heading according to the attribute`, async function () {
+			const [actualElement] = addElement(
+				textToDomToParent(`<${COMPONENT_NAME} heading="${headingText}">Content</${COMPONENT_NAME}>`)
+			);
+
+			await actualElement.updateComplete;
+
+			const headerElement = actualElement.shadowRoot.querySelector('.vwc-card-header');
+
+			expect(headerElement.innerText).to.equal(headingText);
+		});
+
+		it(`should set the heading according to the property`, async function () {
+			const [actualElement] = addElement(
+				textToDomToParent(`<${COMPONENT_NAME}>Content</${COMPONENT_NAME}>`)
+			);
+
+			actualElement.heading = headingText;
+
+			await actualElement.updateComplete;
+
+			const headerElement = actualElement.shadowRoot.querySelector('.vwc-card-header');
+
+			expect(headerElement.innerText).to.equal(headingText);
+		});
+
+		it(`should reflect the heading property and attribute`, async function () {
+			const differentText = 'Diff';
+
+			const [actualElement] = addElement(
+				textToDomToParent(`<${COMPONENT_NAME}>Content</${COMPONENT_NAME}>`)
+			);
+
+			actualElement.heading = headingText;
+
+			await actualElement.updateComplete;
+
+			const attributeValue = actualElement.getAttribute('heading');
+
+			actualElement.setAttribute('heading', differentText);
+
+			expect(attributeValue).to.equal(headingText);
+			expect(actualElement.heading).to.equal(differentText);
+		});
+	});
+
+	describe(`header-icon`, function () {
+		const iconName = 'home';
+
+		it(`should set the icon according to the attribute`, async function () {
+			const [actualElement] = addElement(
+				textToDomToParent(`<${COMPONENT_NAME} header-icon="${iconName}">Content</${COMPONENT_NAME}>`)
+			);
+
+			await actualElement.updateComplete;
+
+			const headerIconElement = actualElement.shadowRoot.querySelector('header vwc-icon');
+
+			expect(headerIconElement.type).to.equal(iconName);
+		});
+
+		it(`should set the icon according to the property`, async function () {
+			const [actualElement] = addElement(
+				textToDomToParent(`<${COMPONENT_NAME}>Content</${COMPONENT_NAME}>`)
+			);
+
+			actualElement.headerIcon = iconName;
+
+			await actualElement.updateComplete;
+
+			const headerIconElement = actualElement.shadowRoot.querySelector('header vwc-icon');
+
+			expect(headerIconElement.type).to.equal(iconName);
+		});
+
+		it(`should reflect the icon property and attribute`, async function () {
+			const differentIconName = 'share';
+
+			const [actualElement] = addElement(
+				textToDomToParent(`<${COMPONENT_NAME}>Content</${COMPONENT_NAME}>`)
+			);
+
+			actualElement.headerIcon = iconName;
+
+			await actualElement.updateComplete;
+
+			const attributeValue = actualElement.getAttribute('header-icon');
+
+			actualElement.setAttribute('header-icon', differentIconName);
+
+			await actualElement.updateComplete;
+
+			expect(attributeValue).to.equal(iconName);
+			expect(actualElement.headerIcon).to.equal(differentIconName);
+		});
 	});
 });
