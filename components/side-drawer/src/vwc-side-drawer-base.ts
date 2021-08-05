5e149f63b27803bb0b354b939441b1c53b9fe996
yinonov <yinon@hotmail.com>
2021-08-05 10:39:38 +0300

    some modifications

--- components/side-drawer/src/vwc-side-drawer-base.ts
+++ components/side-drawer/src/vwc-side-drawer-base.ts
@@ -3,6 +3,7 @@ import {
 } from 'lit-element';
 import { ifDefined } from 'lit-html/directives/if-defined';
 import { classMap } from 'lit-html/directives/class-map';
+import { observer } from '@material/mwc-base/observer';
 
 /**
  * @slot header - The content of the header.
@@ -13,6 +14,20 @@ import { classMap } from 'lit-html/directives/class-map';
  */
 export class VWCSideDrawerBase extends LitElement {
 	@property({ type: Boolean, reflect: true })
+	@observer(function (
+		this: VWCSideDrawerBase,
+		isOpen: boolean,
+		wasOpen: boolean
+	) {
+		if (isOpen) {
+			this.open();
+			// wasOpen helps with first render (when it is `undefined`) perf
+		} else if (wasOpen !== undefined) {
+			this.close();
+		}
+
+		this.openChanged(isOpen);
+	})
 	open = true;
 
 	/**
@@ -44,23 +59,42 @@ export class VWCSideDrawerBase extends LitElement {
 		this.#close();
 	}
 
+	disconnectedCallback() {
+		document.removeEventListener('keydown', this.#onKeydown);
+	}
+
+	#open(): void {
+		this.open = true;
+		// TODO add notifyClose (dispatch event) // add @fires jsdoc
+		document.addEventListener('keydown', this.#onKeydown);
+	}
+
 	#close(): void {
 		this.open = false;
 		// TODO add notifyClose (dispatch event) // add @fires jsdoc
+		document.removeEventListener('keydown', this.#onKeydown);
 	}
 
-	 private renderTopBar(): TemplateResult {
-	 	return html`<div class="vvd-side-drawer--top-bar">
+	private renderTopBar(): TemplateResult {
+		return html`<div class="vvd-side-drawer--top-bar">
 			<slot name="top-bar"></slot>
 		</div>`;
-	 }
+	}
 
-	 private renderScrim(): TemplateResult {
-		 // eslint-disable-next-line lit-a11y/click-events-have-key-events
-		 return html`<div class="vvd-side-drawer--scrim" @click="${this.#handleScrimClick}"></div>`;
-	 }
+	private renderScrim(): TemplateResult {
+		// eslint-disable-next-line lit-a11y/click-events-have-key-events
+		return html`<div class="vvd-side-drawer--scrim" @click="${this.#handleScrimClick}"></div>`;
+	}
 
-	 /**
+	#onKeydown({ key }: KeyboardEvent): void {
+		console.log(this.type, this.open, key);
+		if (this.type === 'modal' && this.open && key === 'Escape') {
+			this.#close();
+		}
+	}
+
+
+	/**
 	 * the html markup
 	 * @internal
 	 * */
@@ -82,6 +116,7 @@ export class VWCSideDrawerBase extends LitElement {
 			<aside
 				part="${ifDefined(alternate)}"
 				class="side-drawer ${classMap(classes)}"
+				@keydown=${this.#onKeydown}
 			>
 				${topBar}
 
