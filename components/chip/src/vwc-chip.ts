5d7c331a3ad87bf38643b1edafe66c8d11f15f38
Tal Weinfeld <tal.weinfeld@vonage.com>
2021-08-11 11:10:43 +0300

    Chip component (wip)

--- components/chip/src/vwc-chip.ts
+++ components/chip/src/vwc-chip.ts
@@ -1,8 +1,10 @@
 import '@vonage/vvd-core';
-import '@vonage/vwd-icon';
+import '@vonage/vwc-icon';
 import {
 	customElement, LitElement, html, property, TemplateResult
 } from 'lit-element';
+import { nothing } from 'lit-html';
+import { defaultTo } from 'ramda';
 import { style } from './vwc-chip.css';
 
 declare global {
@@ -11,19 +13,43 @@ declare global {
 	}
 }
 
+const defaultToNothing = defaultTo(nothing);
+
 @customElement('vwc-chip')
 export class VWCChip extends LitElement {
 	static styles = style;
 	@property({ type: String, reflect: true })
-	text?: boolean;
+	text?: string;
 
 	@property({ type: String, reflect: true })
-	icon?: boolean;
+	icon?: string;
+
+	@property({ type: Boolean, reflect: true })
+	selected?: boolean;
+
+	@property({ type: Boolean, reflect: true })
+	dense?: boolean;
+
+	@property({ type: Boolean, reflect: true })
+	enlarged?: boolean;
+
+	@property({ type: String, reflect: true })
+	connotation?: boolean;
+
+	@property({ type: String, reflect: true })
+	shape?: string;
+
+	@property({ type: String, reflect: true })
+	layout?: string;
+
+	@property({ type: String, reflect: true })
+	trailingIcon?: string;
 
 	render():TemplateResult {
 		return html`<div>
-			<vwc-icon type='${this.icon}'></vwc-icon>
-			${this.text}
+			${defaultToNothing(this.icon && html`<vwc-icon class='icon' type='${this.icon}'></vwc-icon>`)}
+			<span>${this.text}</span>
+			${defaultToNothing(this.trailingIcon && html`<vwc-icon class='icon' type='${this.trailingIcon}'></vwc-icon>`)}
 		</div>`;
 	}
 }
