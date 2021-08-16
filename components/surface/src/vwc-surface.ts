29957d39a61888cd54027d39cdc36eb16073a7da
yinonov <yinon@hotmail.com>
2021-08-16 13:56:17 +0300

    revert

--- components/surface/src/vwc-surface.ts
+++ components/surface/src/vwc-surface.ts
@@ -1,8 +1,8 @@
 import '@vonage/vvd-core';
 import { customElement } from 'lit-element';
-import { MenuSurfaceBase } from '@material/mwc-menu/mwc-menu-surface-base.js';
-import { style as mwcSurfaceStyle } from '@material/mwc-menu/mwc-menu-surface-css.js';
-import { style as vwcSurfaceStyle } from './vwc-surface.css.js';
+import { MenuSurfaceBase } from '@material/mwc-menu/mwc-menu-surface-base';
+import { style as mwcSurfaceStyle } from '@material/mwc-menu/mwc-menu-surface-css';
+import { style as vwcSurfaceStyle } from './vwc-surface.css';
 
 export const COMPONENT_NAME = 'vwc-surface';
 
