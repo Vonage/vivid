709ec4a75c76c2aa9815f047cf9807b9a4f322a9
Yonatan Kra <yonatan.kra@vonage.com>
2021-08-16 11:07:29 +0300

    chore(button): layout css mixins in button (#1019)
    * Layout standards set
    
    * Fixed outline state of button and update ui-tests
    
    * useless arg
    
    Co-authored-by: yinonov <yinon@hotmail.com>

--- ui-tests/tests/vwc-button-toggle-group/index.js
+++ ui-tests/tests/vwc-button-toggle-group/index.js
@@ -1,13 +1,13 @@
-import vvdCore from '@vonage/vvd-core';
 import * as stories from '@vonage/vwc-button-toggle-group/stories/toggle-buttons-group.stories';
 import '@vonage/vwc-button-toggle-group';
 import '@vonage/vwc-button';
+import '@vonage/vwc-icon-button';
 import { storiesToElement } from '../../utils/storiesToElement';
 
 export async function createElementVariations(wrapper) {
 	wrapper.appendChild(storiesToElement(stories));
 
-	await vvdCore.settled;
+	await Promise.all([...wrapper.querySelectorAll('vwc-button-toggle-group')].map(element => element.updateComplete));
 }
 
 
