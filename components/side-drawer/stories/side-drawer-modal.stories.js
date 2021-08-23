e769c0da49ddb4f6186bd914831c2cc656f53fee
Rina <rina.oksman@vonage.com>
2021-08-23 17:10:44 +0300

    remove story duplications

--- components/side-drawer/stories/side-drawer-modal.stories.js
+++ components/side-drawer/stories/side-drawer-modal.stories.js
@@ -1,23 +1,13 @@
-import '@vonage/vwc-side-drawer/vwc-side-drawer.js';
-import '@vonage/vwc-list/vwc-list-expansion-panel.js';
-import { styleMap } from 'lit-html/directives/style-map';
 import { html } from 'lit-element';
-import { spread } from '@open-wc/lit-helpers';
 import { argTypes } from './arg-types.js';
 import { unsafeSVG } from 'lit-html/directives/unsafe-svg';
 import { pageContentMock } from '../../../scripts/storybook/svg_templates';
+import { Basic } from './side-drawer.stories.js';
 
 export default {
 	title: 'Alpha/Components/Side Drawer/Modal',
 	component: 'vwc-side-drawer',
 	argTypes
-};
-
-let prevActivatedItem;
-
-const titleStyles = {
-	color: "#C0C0C0",
-	fontWeight: "bold",
 };
 
 const Template = args => html`
@@ -30,74 +20,10 @@ const Template = args => html`
 			height: 100%;
 		}
 	</style>
-
 	<div id="demo">
-		<vwc-side-drawer id="side-drawer" ...=${spread(args)} @click="${onClick}">
-			<span slot="top-bar">
-				<vwc-icon type="vonage-mono"></vwc-icon> VONAGE
-			</span>
-
-			<vwc-list
-					innerRole="navigation"
-					innerAriaLabel="Primary navigation"
-					itemRoles="link"
-				>
-				<vwc-list-item shape="rounded" graphic="icon">
-					<vwc-icon slot="graphic" type="home-line"></vwc-icon>1st level item
-				</vwc-list-item>
-
-				<p style=${styleMap(titleStyles)}>SECTION TITLE</p>
-
-				<vwc-list-item shape="rounded" graphic="icon">
-					<vwc-icon slot="graphic" type="chat-line"></vwc-icon>1st level item
-				</vwc-list-item>
-
-				<vwc-list-expansion-panel open>
-					<vwc-list-item slot="header" shape="rounded" graphic="icon">
-						<vwc-icon slot="graphic" type="chat-line"></vwc-icon>1st level item
-					</vwc-list-item>
-					<vwc-list-expansion-panel open>
-						<vwc-list-item slot="header" shape="rounded"
-							>2nd level item</vwc-list-item
-						>
-						<vwc-list-item shape="rounded">3rd level item</vwc-list-item>
-						<vwc-list-item shape="rounded">3rd level item</vwc-list-item>
-					</vwc-list-expansion-panel>
-				</vwc-list-expansion-panel>
-
-				<p style=${styleMap(titleStyles)}>SECTION TITLE</p>
-
-				<vwc-list-expansion-panel >
-					<vwc-list-item slot="header" shape="rounded" graphic="icon">
-						<vwc-icon slot="graphic" type="chat-line"></vwc-icon>1st level item
-					</vwc-list-item>
-					<vwc-list-item shape="rounded">2nd level item</vwc-list-item>
-					<vwc-list-item shape="rounded">2nd level item</vwc-list-item>
-				</vwc-list-expansion-panel>
-			</vwc-list>
-
-		</vwc-side-drawer>
-
-		<div id="default"></div>
+		${Basic(args)}
 		${unsafeSVG(pageContentMock('100%', '100%', 'xMinYMin slice'))}
-
-	</div>
-`;
-
-function onClick(e) {
-	// only list items can be activated
-	if (e.target.localName !== "vwc-list-item") {
-		return;
-	}
-	if (e.target.slot === "header") {
-		return;
-	}
-	if (typeof prevActivatedItem !== 'undefined') {
-		prevActivatedItem.activated = false;
-	}
-	prevActivatedItem = e.target;
-	prevActivatedItem.activated = true;
-}
+	</div>`;
 
 export const Modal = Template.bind({});
 Modal.args = { type: 'modal', open: true };
--- components/side-drawer/stories/side-drawer.stories.js
+++ components/side-drawer/stories/side-drawer.stories.js
@@ -13,8 +13,6 @@ export default {
 	argTypes
 };
 
-let prevActivatedItem;
-
 const titleStyles = {
 	color: "#C0C0C0",
 	fontWeight: "bold",
@@ -43,8 +41,13 @@ const Template = args => html`
 			height: 100%;
 		}
 	</style>
-
 	<div id="demo">
+		${Basic(args)}
+		<div id="default"></div>
+		${unsafeSVG(pageContentMock())}
+	</div>`;
+
+const SideDrawerTemplate = args => html`
 		<vwc-side-drawer id="side-drawer" ...=${spread(args)} @click="${onClick}">
 			<span slot="top-bar">
 				<vwc-icon type="vonage-mono"></vwc-icon> VONAGE
@@ -88,15 +91,21 @@ const Template = args => html`
 					<vwc-list-item shape="rounded">2nd level item</vwc-list-item>
 				</vwc-list-expansion-panel>
 			</vwc-list>
+		</vwc-side-drawer>`;
 
-		</vwc-side-drawer>
+export const Basic = SideDrawerTemplate.bind({});
+Basic.args = { };
 
-		<div id="default"></div>
-		${unsafeSVG(pageContentMock())}
+export const Alternate = Template.bind({});
+Alternate.args = { alternate: true };
 
-	</div>
-`;
+export const TopBar = Template.bind({});
+TopBar.args = { hasTopBar: true };
 
+export const Dismissible = Template.bind({});
+Dismissible.args = { type: 'dismissible', open: true };
+
+let prevActivatedItem;
 function onClick(e) {
 	// only list items can be activated
 	if (e.target.localName !== "vwc-list-item") {
@@ -111,15 +120,3 @@ function onClick(e) {
 	prevActivatedItem = e.target;
 	prevActivatedItem.activated = true;
 }
-
-export const Basic = Template.bind({});
-Basic.args = { };
-
-export const Alternate = Template.bind({});
-Alternate.args = { alternate: true };
-
-export const TopBar = Template.bind({});
-TopBar.args = { hasTopBar: true };
-
-export const Dismissible = Template.bind({});
-Dismissible.args = { type: 'dismissible', open: true };
