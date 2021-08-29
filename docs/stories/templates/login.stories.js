9402bbe98751d45ff89a68f5cefc694df2575e63
Rina <rina.oksman@vonage.com>
2021-08-29 13:30:41 +0300

    remove css

--- docs/stories/templates/login.stories.js
+++ docs/stories/templates/login.stories.js
@@ -6,18 +6,11 @@ export default {
 
 export const Template = () => html`
   <style>
-    	div#demo {
-        width: 100%;
-        height: 100%;
-        display: flex;
-		  }
       form {
         display: grid;
         gap: 20px;
         margin: auto;
-      }
-      a {
-        margin: auto;
+        width: 800px;
       }
       .controls {
         display: flex;
@@ -25,21 +18,12 @@ export const Template = () => html`
         gap: 10px;
       }
       #password {
-        display: flex;
-      }
-      #remember {
-        flex: 1 0;
-      }
-      #forgot {
-        flex: 0 0 50vw;
-        padding-top: 20px;
+        text-align: end;
       }
 	</style>
-  <div id="demo">
     <form>
       <p><vwc-text font-face="title-2">Welcome back!</vwc-text></p>
       <p><vwc-text font-face="body-2">Don't have an account? <a href="#">Sign Up</a></vwc-text></p>
-      <div>
       <vwc-textfield
         name="username"
         label="username"
@@ -73,11 +57,10 @@ export const Template = () => html`
           placeholder=" "
         />
       </vwc-textfield>
-      <div id="password">
-        <vwc-formfield id="remember" label="Remember me"><vwc-checkbox></vwc-checkbox></vwc-formfield>
-        <vwc-text id="forgot" font-face="body-2"><a href="#">Forgot password?</a></vwc-text>
-      </div>
-      </div>
+      <vwc-inline>
+        <vwc-formfield label="Remember me"><vwc-checkbox></vwc-checkbox></vwc-formfield>
+        <vwc-text id="password" font-face="body-2"><a href="#">Forgot password?</a></vwc-text>
+      </vwc-inline>
       <div class="controls">
         <vwc-button layout="filled" type="submit" unelevated="">
           Log in
@@ -85,7 +68,6 @@ export const Template = () => html`
         </vwc-button>
       </div>
     </form>
-  </div>
 `;
 
 
