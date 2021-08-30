f59a91a504ceb0b1f41da66ce31fab2178292c44
Rina <rina.oksman@vonage.com>
2021-08-30 13:25:09 +0300

    split stories

---
+++ docs/stories/templates/login-page.stories.js
@@ -0,0 +1,41 @@
+import { html } from 'lit-element';
+import { LogIn } from './login.stories';
+
+export default {
+	title: 'Templates/Application',
+};
+
+export const LogInTemplate = () => html`
+<style>
+#login-page {
+    display: flex;
+}
+figure {
+    flex: 0 0 50vw;
+    background-color: var(--vvd-color-neutral-20);
+    position: relative;
+}
+#content {
+    position: absolute;
+    bottom: 20px;
+    left: 20px;
+}
+</style>
+<div id="login-page">
+    ${LogIn()}
+    <figure>
+        <div id="content">
+            <p><vwc-text font-face="subtitle-1">Lorem ipsum</vwc-text></p>
+            <p><vwc-text font-face="body-1">Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</vwc-text></p>
+            <div><vwc-button label="Apply Now →" layout="outlined" type="submit" outlined=""></vwc-button></div>
+        </div>
+    </figure>
+</div>
+`;
+
+LogInTemplate.parameters = {
+	options: {
+		showPanel: false,
+		isToolshown: false
+	}
+};
--- docs/stories/templates/login.stories.js
+++ docs/stories/templates/login.stories.js
@@ -1,84 +1,62 @@
 import { html } from 'lit-element';
 
 export default {
-	title: 'Templates/Log in',
+	title: 'Templates/Log In',
 };
 
-export const Template = () => html`
+export const LogIn = () => html`
   <style>
       form {
         display: grid;
         gap: 24px;
-        margin: auto;
-        width: 800px;
+        margin: 50px;
       }
-      p {
+      .vivid-scope h3, p{
           margin: 0;
           padding: 0;
       }
-      .controls {
+      .actions {
         display: flex;
         justify-content: start;
         gap: 10px;
       }
-      #password {
-        text-align: end;
-      }
       vwc-inline.sign-in-helper {
 	      align-items: center;
       }
+      .password {
+        text-align: end;
+      }
+      #side-drawer {
+        flex: 1 0;
+        --vvd-side-drawer--background-color: var(--vvd-color-on-primary);
+        --vvd-side-drawer--min-inline-size: 100%;
+      }
 	</style>
-    <form>
-      <p><vwc-text font-face="title-2">Welcome back!</vwc-text></p>
-      <p><vwc-text font-face="body-2">Don't have an account? <a href="#">Sign Up</a></vwc-text></p>
-      <vwc-textfield
-        name="username"
-        label="username"
-        icon="user"
-        placeholder=" "
-        outlined=""
-      >
-        <input
-          value=""
-          slot="formInputElement"
-          class="vivid-input-internal"
-          name="username"
-          type="text"
-          placeholder=" "
-        />
-      </vwc-textfield>
-      <vwc-textfield
-        name="password"
-        label="password"
-        icon="lock"
-        type="password"
-        placeholder=" "
-        outlined=""
-      >
-        <input
-          value=""
-          slot="formInputElement"
-          class="vivid-input-internal"
-          name="password"
-          type="password"
-          placeholder=" "
-        />
-      </vwc-textfield>
-      <vwc-inline class="sign-in-helper">
-        <vwc-formfield label="Remember me"><vwc-checkbox></vwc-checkbox></vwc-formfield>
-        <vwc-text id="password" font-face="body-2"><a href="#">Forgot password?</a></vwc-text>
-      </vwc-inline>
-      <div class="controls">
-        <vwc-button layout="filled" type="submit" unelevated="">
-          Log in
-          <button type="submit" style="display: none;"></button>
-        </vwc-button>
-      </div>
-    </form>
+    <vwc-side-drawer id="side-drawer" hasTopBar>
+      <span slot="top-bar"><vwc-icon type="vonage-mono"></vwc-icon><vwc-text font-face="body-1-bold"> VONAGE</vwc-text></span>
+      <section>
+      <form>
+        <h3><vwc-text font-face="title-2">Welcome back!</vwc-text></h3>
+        <p><vwc-text font-face="body-2">Don't have an account? <a href="#">Sign Up</a></vwc-text></p>
+        <vwc-textfield name="username" label="username" icon="user" placeholder=" " outlined="">
+          <input value="" slot="formInputElement" class="vivid-input-internal" name="username" type="text" placeholder=" "/>
+        </vwc-textfield>
+        <vwc-textfield name="password" label="password" icon="lock" type="password" placeholder=" " outlined="">
+          <input value="" slot="formInputElement" class="vivid-input-internal" name="password" type="password" placeholder=" "/>
+        </vwc-textfield>
+        <vwc-inline class="sign-in-helper">
+          <vwc-formfield label="Remember me"><vwc-checkbox></vwc-checkbox></vwc-formfield>
+          <vwc-text class="password" font-face="body-2"><a href="#">Forgot password?</a></vwc-text>
+        </vwc-inline>
+        <footer class="actions">
+          <vwc-button layout="filled">Log in</vwc-button>
+        </footer>
+      </form>
+      </section>
+    </vwc-side-drawer>
 `;
 
-
-Template.parameters = {
+LogIn.parameters = {
 	options: {
 		showPanel: false,
 		isToolshown: false
