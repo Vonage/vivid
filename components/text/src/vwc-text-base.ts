1bc7bf665d10cf2b4bc8ca447ae28a342d886900
yinonov <yinon@hotmail.com>
2021-08-31 22:21:46 +0300

    vwc-text

--- components/text/src/vwc-text-base.ts
+++ components/text/src/vwc-text-base.ts
@@ -3,7 +3,9 @@ import { VVDFontFace } from '@vonage/vvd-design-tokens/build/types/font-faces';
 import {
 	html, LitElement, property, TemplateResult
 } from 'lit-element';
+import { ClassInfo, classMap } from 'lit-html/directives/class-map';
 import { Connotation } from '@vonage/vvd-foundation/constants';
+import { renderPropertyToClass as renderClassFromProperty } from '@vonage/vvd-foundation/decorators';
 
 type TextConnotation = Extract<
 	Connotation,
@@ -22,7 +24,23 @@ export class VWCTextBase extends LitElement {
 	@property({ type: String, reflect: true })
 	connotation?: TextConnotation;
 
+	protected getRenderClassesFontFace(): ClassInfo {
+		return this.fontFace ? { [`vwc-text--font-face-${this.fontFace}`]: true }	: {};
+	}
+
+	protected getRenderClassesConnotation(): ClassInfo {
+		return this.connotation ? { [`vwc-text--connotation-${this.connotation}`]: true }	: {};
+	}
+
+	@renderClassFromProperty('this.connotation')
+	protected getRenderClasses(): ClassInfo {
+		return {
+			...this.getRenderClassesFontFace(),
+			...this.getRenderClassesConnotation(),
+		};
+	}
+
 	protected render(): TemplateResult {
-		return html`<slot></slot>`;
+		return html`<slot class="vwc-text ${classMap(this.getRenderClasses())}"></slot>`;
 	}
 }
