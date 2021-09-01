cc288b0f0d6048c0f88ce792d2038608b45ece66
Rina <rina.oksman@vonage.com>
2021-09-01 16:44:39 +0300

    vwc-top-app-bar

--- docs/stories/templates/application-page.stories.js
+++ docs/stories/templates/application-page.stories.js
@@ -6,67 +6,49 @@ export default {
 
 export const ApplicationTemplate = () => html`
 <style>
-    div#demo {
-  	position: fixed;
+  #div{
+    position: relative;
   	display: flex;
   }
-  vwc-side-drawer#side-drawer {
-  	flex: 0 0 auto;
-  	height: 100%;
-  }
+    vwc-side-drawer#side-drawer {
+      flex: 0 0 auto;
+  	  height: 100%;
+    }
+    vwc-top-app-bar#top-app-bar {
+    }
 </style>
 <div id="demo">
-<vwc-side-drawer id="side-drawer" alternate hasTopBar>
+
+  <vwc-side-drawer id="side-drawer" alternate hasTopBar>
     <span slot="top-bar"><vwc-icon type="vonage-mono"></vwc-icon>VONAGE</span>
 
-    <vwc-list
-        innerRole="navigation"
-        innerAriaLabel="Primary navigation"
-        itemRoles="link"
-    >
+    <vwc-list innerRole="navigation" innerAriaLabel="Primary navigation" itemRoles="link">
         <vwc-list-item shape="rounded" graphic="icon">
-        <vwc-icon slot="graphic" type="home-line"></vwc-icon>1st level item
+          <vwc-icon slot="graphic" type="home-line"></vwc-icon>1st level item
         </vwc-list-item>
 
         <p>SECTION TITLE</p>
 
         <vwc-list-item shape="rounded" graphic="icon">
-        <vwc-icon slot="graphic" type="chat-line"></vwc-icon>1st level item
+          <vwc-icon slot="graphic" type="chat-line"></vwc-icon>1st level item
         </vwc-list-item>
 
         <vwc-list-expansion-panel open>
-        <vwc-list-item slot="header" shape="rounded" graphic="icon">
-            <vwc-icon slot="graphic" type="chat-line"></vwc-icon>1st level
-            item
-        </vwc-list-item>
-        <vwc-list-expansion-panel open>
-            <vwc-list-item slot="header" shape="rounded"
-            >2nd level item</vwc-list-item
-            >
-            <vwc-list-item shape="rounded">3rd level item</vwc-list-item>
-            <vwc-list-item shape="rounded">3rd level item</vwc-list-item>
-        </vwc-list-expansion-panel>
+          <vwc-list-item slot="header" shape="rounded" graphic="icon"><vwc-icon slot="graphic" type="chat-line"></vwc-icon>1st level item</vwc-list-item>
+          <vwc-list-expansion-panel open>
+              <vwc-list-item slot="header" shape="rounded">2nd level item</vwc-list-item>
+              <vwc-list-item shape="rounded">3rd level item</vwc-list-item>
+              <vwc-list-item shape="rounded">3rd level item</vwc-list-item>
+          </vwc-list-expansion-panel>
         </vwc-list-expansion-panel>
     </vwc-list>
-</vwc-side-drawer>
 
-<vwc-top-app-bar-fixed dense>
-<vwc-icon-button
-    slot="actionItems"
-    icon="search-line"
-    layout="filled"
-  ></vwc-icon-button>
-  <vwc-icon-button
-    slot="actionItems"
-    icon="info-line"
-    layout="filled"
-  ></vwc-icon-button>
-  <vwc-icon-button
-    slot="actionItems"
-    icon="upload-line"
-    layout="filled"
-  ></vwc-icon-button>
-</vwc-top-app-bar-fixed>
+    <vwc-top-app-bar id="top-app-bar" dense>
+      <vwc-icon-button slot="actionItems" icon="search-line" layout="filled"></vwc-icon-button>
+      <vwc-icon-button slot="actionItems" icon="info-line" layout="filled"></vwc-icon-button>
+      <vwc-icon-button slot="actionItems" icon="upload-line" layout="filled"></vwc-icon-button>
+    </vwc-top-app-bar>
+  </vwc-side-drawer>
 </div>
 `;
 
