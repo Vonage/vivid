fc40e9212108f1622ad463548819b0bd28f0193e
yinonov <yinon@hotmail.com>
2021-09-12 19:04:46 +0300

    tags aligned

--- ui-tests/tests/vwc-tags/index.js
+++ ui-tests/tests/vwc-tags/index.js
@@ -3,14 +3,7 @@ import '@vonage/vwc-tags/vwc-tag.js';
 export async function createElementVariations(wrapper) {
 	const tagElementWrapper = document.createElement('div');
 	tagElementWrapper.innerHTML = `
-		<style>
-			.container {
-				display: flex;
-				gap: 5px;
-			}
-		</style>
-
-		<div class="container">
+		<div>
 			<vwc-tag text="Tag 1"></vwc-tag>
 			<vwc-tag text="Tag 2" connotation="cta"></vwc-tag>
 			<vwc-tag text="Tag 3" connotation="primary" selectable selected></vwc-tag>
@@ -20,6 +13,7 @@ export async function createElementVariations(wrapper) {
 			<vwc-tag text="Tag 7" layout="outlined" connotation="primary" selectable selected></vwc-tag>
 			<vwc-tag text="Tag 8" layout="outlined" connotation="cta" selectable selected></vwc-tag>
 		</div>
+
 		<vwc-tags>
 			<vwc-tag text="Python" selectable></vwc-tag>
 			<vwc-tag text="Javascript" selectable selected></vwc-tag>
