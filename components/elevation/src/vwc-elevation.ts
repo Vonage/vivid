c098c7720cb4489cacabb2263a0eee56ade1aa0b
yonatankra <yonatan.kra@vonage.com>
2021-09-13 16:01:12 +0300

    Setting up the attributes API

--- components/elevation/src/vwc-elevation.ts
+++ components/elevation/src/vwc-elevation.ts
@@ -1,5 +1,6 @@
 import { customElement, LitElement } from 'lit-element';
 import { style } from './vwc-elevation.css';
+import { property } from 'lit-element/lib/decorators';
 
 declare global {
 	interface HTMLElementTagNameMap {
@@ -13,4 +14,13 @@ export class VWCElevation extends LitElement {
 	 * @internal
 	 */
 	static styles = style;
+
+	@property({ type: Number })
+	dp = 2;
+
+	@property({ type: String, attribute: 'background-color' })
+	backgroundColor: string | null = null;
+
+	@property({ type: String, attribute: 'border-radius' })
+	borderRadius: string | null = null;
 }
--- components/elevation/test/elevation.test.js
+++ components/elevation/test/elevation.test.js
@@ -13,10 +13,137 @@ const COMPONENT_NAME = 'vwc-elevation';
 describe.only('Elevation', () => {
 	let addElement = isolatedElementsCreation();
 
+	function createElement(props) {
+		const propsAndValues = !props ? '' :
+			Object.keys(props)
+				.reduce((attributesString, attributeName) => {
+					return `${attributesString} ${attributeName}="${props[attributeName]}"`;
+				}, '');
+		const [actualElement] = addElement(
+			textToDomToParent(`<${COMPONENT_NAME}${propsAndValues}></${COMPONENT_NAME}>`)
+		);
+		return actualElement;
+	}
+
 	it(`${COMPONENT_NAME} is defined as a custom element`, async () => {
 		assert.exists(
 			customElements.get(COMPONENT_NAME)
 		);
+	});
+
+	describe(`dp`, function () {
+		const defaultValue = 2;
+		it(`should default to ${defaultValue}`, async function () {
+			const actualElement = createElement();
+			await actualElement.updateComplete;
+			expect(actualElement.dp)
+				.to
+				.equal(2);
+		});
+
+		it(`should change the property when the attribute changes`, async function () {
+			const startingDP = 8;
+			const actualElement = createElement({ dp: startingDP });
+			await actualElement.updateComplete;
+
+			const propertyValueBeforeChange = actualElement.dp;
+
+			actualElement.setAttribute('dp', '4');
+
+			expect(propertyValueBeforeChange)
+				.to
+				.equal(startingDP);
+			expect(actualElement.dp)
+				.to
+				.equal(4);
+		});
+	});
+
+	describe(`background-color`, function () {
+		const defaultValue = null;
+		it(`should default to ${defaultValue}`, async function () {
+			const actualElement = createElement();
+			await actualElement.updateComplete;
+			expect(actualElement.backgroundColor)
+				.to
+				.equal(defaultValue);
+		});
+
+		it(`should change the property when the attribute changes`, async function () {
+			const startingBackgroundColor = 'black';
+			const nextBackgroundColor = 'blue';
+			const actualElement = createElement({ 'background-color': startingBackgroundColor });
+			await actualElement.updateComplete;
+
+			const propertyValueBeforeChange = actualElement.backgroundColor;
+
+			actualElement.setAttribute('background-color', nextBackgroundColor);
+
+			expect(propertyValueBeforeChange)
+				.to
+				.equal(startingBackgroundColor);
+			expect(actualElement.backgroundColor)
+				.to
+				.equal(nextBackgroundColor);
+		});
+	});
+
+	describe(`background-color`, function () {
+		const defaultValue = null;
+		it(`should default to ${defaultValue}`, async function () {
+			const actualElement = createElement();
+			await actualElement.updateComplete;
+			expect(actualElement.backgroundColor)
+				.to
+				.equal(defaultValue);
+		});
+
+		it(`should change the property when the attribute changes`, async function () {
+			const startingBackgroundColor = 'black';
+			const nextBackgroundColor = 'blue';
+			const actualElement = createElement({ 'background-color': startingBackgroundColor });
+			await actualElement.updateComplete;
+
+			const propertyValueBeforeChange = actualElement.backgroundColor;
+
+			actualElement.setAttribute('background-color', nextBackgroundColor);
+
+			expect(propertyValueBeforeChange)
+				.to
+				.equal(startingBackgroundColor);
+			expect(actualElement.backgroundColor)
+				.to
+				.equal(nextBackgroundColor);
+		});
+	});
+
+	describe(`border-radius`, function () {
+		const defaultValue = null;
+		it(`should default to ${defaultValue}`, async function () {
+			const actualElement = createElement();
+			await actualElement.updateComplete;
+			expect(actualElement.borderRadius)
+				.to
+				.equal(defaultValue);
+		});
+
+		it(`should change the property when the attribute changes`, async function () {
+			const startingBackgroundColor = '16px';
+			const nextBackgroundColor = '24px';
+			const actualElement = createElement({ 'border-radius': startingBackgroundColor });
+			await actualElement.updateComplete;
+
+			const propertyValueBeforeChange = actualElement.borderRadius;
+
+			actualElement.setAttribute('border-radius', nextBackgroundColor);
+
+			expect(propertyValueBeforeChange)
+				.to
+				.equal(startingBackgroundColor);
+			expect(actualElement.borderRadius)
+				.to
+				.equal(nextBackgroundColor);
+		});
 	});
 
 	// it('should internal contents', async () => {
