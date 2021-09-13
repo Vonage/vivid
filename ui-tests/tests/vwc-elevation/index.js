c990413246b28b28facb578ace9fedd3cf262af4
yonatankra <yonatan.kra@vonage.com>
2021-09-13 17:51:51 +0300

    Add ui-tests

---
+++ ui-tests/tests/vwc-elevation/index.js
@@ -0,0 +1,68 @@
+import '@vonage/vwc-elevation';
+
+export async function createElementVariations(wrapper) {
+	const elementWrapper = document.createElement('div');
+	elementWrapper.innerHTML =
+		`
+			<style>
+				#VwcElevation {
+					display: inline-block;
+				}
+
+				vwc-elevation {
+					margin: 5px;
+					display: block;
+				}
+				.card {
+					width: 300px;
+					height: 30px;
+					padding: 20px;
+					text-align: center;
+				}
+			</style>
+		  <vwc-elevation border-radius="16px" background-color="lightblue">
+		  	<div class="card">
+					This is the content inside the elevation with background and radius
+				</div>
+			</vwc-elevation>
+
+			<vwc-elevation dp="2">
+		  	<div class="card">
+					This is the content inside the elevation with DP 2
+				</div>
+			</vwc-elevation>
+
+			<vwc-elevation dp="4">
+		  	<div class="card">
+					This is the content inside the elevation with DP 4
+				</div>
+			</vwc-elevation>
+
+			<vwc-elevation dp="8">
+		  	<div class="card">
+					This is the content inside the elevation with DP 4
+				</div>
+			</vwc-elevation>
+
+			<vwc-elevation dp="12">
+		  	<div class="card">
+					This is the content inside the elevation with DP 4
+				</div>
+			</vwc-elevation>
+
+			<vwc-elevation dp="16">
+		  	<div class="card">
+					This is the content inside the elevation with DP 4
+				</div>
+			</vwc-elevation>
+
+			<vwc-elevation dp="24">
+		  	<div class="card">
+					This is the content inside the elevation with DP 24
+				</div>
+			</vwc-elevation>
+		`;
+	wrapper.appendChild(elementWrapper);
+}
+
+
