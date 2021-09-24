be912c074649f2859e481470dd6df96de163e0d3
yinon <yinon@hotmail.com>
2021-09-24 09:50:16 +0300

    refactor(vwc-top-app-bar): application neccessary adjustments (#1064)

--- components/top-app-bar-fixed/src/vwc-top-app-bar-fixed.ts
+++ components/top-app-bar-fixed/src/vwc-top-app-bar-fixed.ts
@@ -1,9 +1,8 @@
 import '@vonage/vvd-core';
 import { customElement } from 'lit-element';
-import { TopAppBarFixed as MWCTopAppBarFixed } from '@material/mwc-top-app-bar-fixed';
-import { styles as MWCTopAppBarStyles } from '@material/mwc-top-app-bar/mwc-top-app-bar.css.js';
-import { style as VWCTopAppBarFixedStyle } from './vwc-top-app-bar-fixed.css.js';
-import { style as styleCoupling } from '@vonage/vvd-style-coupling/mdc-vvd-coupling.css';
+import { TopAppBarFixedBase as MWCTopAppBarFixedBase } from '@material/mwc-top-app-bar-fixed/mwc-top-app-bar-fixed-base.js';
+import { styles as MWCTopAppBarStyles } from '@material/mwc-top-app-bar/mwc-top-app-bar.css';
+import { style as VWCTopAppBarStyle } from '@vonage/vwc-top-app-bar/vwc-top-app-bar.css.js';
 
 declare global {
 	interface HTMLElementTagNameMap {
@@ -11,12 +10,10 @@ declare global {
 	}
 }
 
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-// @ts-ignore
-MWCTopAppBarFixed.styles = [styleCoupling, MWCTopAppBarStyles, VWCTopAppBarFixedStyle];
-
 /**
  * This component is an extension of [<mwc-top-app-bar-fixed>](https://github.com/material-components/material-components-web-components/tree/master/packages/top-app-bar-fixed)
  */
 @customElement('vwc-top-app-bar-fixed')
-export class VWCTopAppBarFixed extends MWCTopAppBarFixed { }
+export class VWCTopAppBarFixed extends MWCTopAppBarFixedBase {
+	static override styles = [MWCTopAppBarStyles, VWCTopAppBarStyle];
+}
