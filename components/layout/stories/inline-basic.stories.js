6b6d0237c4906016129ae654578d0b3bcd52d5d3
yinon <yinon@hotmail.com>
2021-10-06 11:50:31 +0300

    Update components/layout/stories/inline-basic.stories.js

--- components/layout/stories/inline-basic.stories.js
+++ components/layout/stories/inline-basic.stories.js
@@ -4,7 +4,7 @@ import { spread } from '@open-wc/lit-helpers';
 import { argTypes } from './arg-types.js';
 
 export default {
-	title: 'Alpha/Components/Layout',
+	title: 'Components/Layout',
 	component: 'vwc-layout',
 	argTypes
 };
