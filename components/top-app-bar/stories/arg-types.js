a5f0f1580c80c019cedcb4c2dcb78f277e83f4f4
Rina <rina.oksman@vonage.com>
2021-10-10 16:36:29 +0300

    add alternate

--- components/top-app-bar/stories/arg-types.js
+++ components/top-app-bar/stories/arg-types.js
@@ -1,21 +1,27 @@
 export const argTypes = {
-  centerTitle: {
-    control: {
-      type: 'inline-radio',
-      options: { true: '', false: undefined },
-    },
-  },
-  dense: {
-    control: {
-      type: 'inline-radio',
-      options: { true: '', false: undefined },
-    },
-  },
-  prominent: {
-    control: {
-      type: 'inline-radio',
-      options: { true: '', false: undefined },
-    },
-  },
-  styles: { table: { disable: true } },
+	centerTitle: {
+		control: {
+			type: 'inline-radio',
+			options: { true: '', false: undefined },
+		},
+	},
+	dense: {
+		control: {
+			type: 'inline-radio',
+			options: { true: '', false: undefined },
+		},
+	},
+	alternate: {
+		control: {
+			type: 'inline-radio',
+			options: { true: '', false: undefined },
+		},
+	},
+	prominent: {
+		control: {
+			type: 'inline-radio',
+			options: { true: '', false: undefined },
+		},
+	},
+	styles: { table: { disable: true } },
 };
