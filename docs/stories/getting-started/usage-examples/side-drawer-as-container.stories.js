600e860e93d02aabd3a5c5dbc255fac6e6b9d16e
Rina <rina.oksman@vonage.com>
2021-10-11 17:46:46 +0300

    layout fix

--- docs/stories/getting-started/usage-examples/side-drawer-as-container.stories.js
+++ docs/stories/getting-started/usage-examples/side-drawer-as-container.stories.js
@@ -50,15 +50,15 @@ const style = html`
 `;
 
 const loremIpsum = () => html`
-<vwc-layout gutters="xs">
-	<p>
+<vwc-layout gutters="md">
+	<section>
 		Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
 		standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
 		a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
 		remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
 		Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions
 		of Lorem Ipsum.
-	</p>
+	</section>
 </vwc-layout>
 `;
 
