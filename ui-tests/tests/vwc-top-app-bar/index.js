de8447c65e38d8d282cd2d0deef72f7a42a5e286
yinonov <yinon@hotmail.com>
2021-10-11 15:06:36 +0300

    better alignment

--- ui-tests/tests/vwc-top-app-bar/index.js
+++ ui-tests/tests/vwc-top-app-bar/index.js
@@ -7,6 +7,12 @@ export async function createElementVariations(wrapper) {
 	snapshotTheWholePage(wrapper);
 	const elementWrapper = document.createElement('div');
 	elementWrapper.innerHTML = `
+	<style>
+		body {
+			margin: 0;
+		}
+	</style>
+
 	<vwc-top-app-bar>
 	<vwc-icon-button slot="navigationIcon" icon="menu-line"></vwc-icon-button>
 	<span slot="title">Top App Bar</span>
@@ -14,8 +20,6 @@ export async function createElementVariations(wrapper) {
 	<vwc-icon-button slot="actionItems" icon="twitter-mono"></vwc-icon-button>
 	<vwc-icon-button slot="actionItems" icon="facebook-mono"></vwc-icon-button>
 	<vwc-icon-button slot="actionItems" icon="heart-solid"></vwc-icon-button>
-	<span slot="actionItems">Toggle theme:</span>
-	<vwc-theme-switch slot="actionItems"></vwc-theme-switch>
 
 	<div id="content">
 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
