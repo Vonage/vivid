6b397e86620199b99d56887ad016602fcae6b384
yonatankra <yonatan.kra@vonage.com>
2021-10-14 11:19:19 +0300

    Refactor the scheme coupling to themes scss variables

---
+++ common/design-tokens/src/builders/render-themes-sass.js
@@ -0,0 +1,56 @@
+import { resolve } from 'path';
+
+import StyleDictionaryPackage from 'style-dictionary';
+import fs from 'fs';
+import _ from 'lodash';
+
+const propertiesPath = resolve('../../node_modules/@vonage/vvd-design-tokens-properties/dist');
+
+
+StyleDictionaryPackage.registerFormat({
+	name: 'custom/format/css-to-scss',
+	formatter: _.template(
+		fs.readFileSync(resolve('templates/web-scss-variables.template'))
+	),
+});
+
+// HAVE THE STYLE DICTIONARY CONFIG DYNAMICALLY GENERATED
+function getStyleDictionaryConfig() {
+	return {
+		source: [
+			`${propertiesPath}/color/**/*.json`,
+			`${propertiesPath}/shadow/**/*.json`,
+			`${propertiesPath}/themes/light/**/main.json`,
+		],
+		platforms: {
+			web: {
+				prefix: 'vvd',
+				transformGroup: 'scss',
+				buildPath: `${resolve()}/`,
+				files: [
+					{
+						destination: 'build/scss/semantic-variables/_scheme-variables.scss',
+						format: 'custom/format/css-to-scss',
+						filter: {
+							attributes: {
+								category: 'color'
+							}
+						}
+					}
+				]
+			}
+		}
+	};
+}
+
+
+// PROCESS THE DESIGN TOKENS FOR THE DIFFERENT SCHEMES AND PLATFORMS
+// TODO: [VIV-41] add accessible colors scheme
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const render = () => {
+	console.log('\n🔁\x1b[2mProcessing scheme variables coupling\x1b[0m');
+
+	StyleDictionaryPackage.extend(getStyleDictionaryConfig()).buildPlatform('web');
+
+	console.log('\n\x1b[2m================================================================\x1b[0m');
+};
--- common/design-tokens/src/index.js
+++ common/design-tokens/src/index.js
@@ -1,12 +1,13 @@
 import { render as themesRender } from './builders/render-themes.js';
+import { render as themesSassRender } from './builders/render-themes-sass.js';
 import { render as elevationsRender } from './builders/elevation/render.js';
 import { renderTypographySassMixin, renderTypographyTypes } from './builders/typography/index.js';
 
 
 console.log('\n\x1b[32m=== Design tokens build started ====\x1b[0m');
 
-
 themesRender();
+themesSassRender();
 renderTypographySassMixin();
 renderTypographyTypes();
 elevationsRender();
