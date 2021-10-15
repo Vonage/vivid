75cedad71e2ff5fdb86f20fadc89166b8ec5d64b
yinonov <yinon@hotmail.com>
2021-10-15 09:27:21 +0300

    dependency check

--- components/data-grid/src/vwc-data-grid-column.ts
+++ components/data-grid/src/vwc-data-grid-column.ts
@@ -2,8 +2,9 @@ import {
 	COLUMN_DEFINITION_COMPONENT,
 	COLUMN_DEFINITION_UPDATE_EVENT,
 	SELECTOR_SINGLE,
-	SELECTOR_MULTI
+	SELECTOR_MULTI,
 } from './vwc-data-grid-column-api.js';
+import type {	DataGridColumn } from './vwc-data-grid-column-api.js';
 import { LitElement } from 'lit';
 import { customElement,	property } from 'lit/decorators';
 
