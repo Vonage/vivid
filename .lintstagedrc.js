cfcdfea75d2e450d826a80f6e5cec96b788931c5
yinonov <yinon@hotmail.com>
2021-10-19 23:27:16 +0300

    refine lint settings

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,6 +2,5 @@ module.exports = {
 	"{common,components}/**/*.scss": ["stylelint --quiet --fix"],
 	"{common,components}/*/{src,test}/**/*.{js,ts}": [
 		"eslint --no-error-on-unmatched-pattern --quiet --fix",
-	],
-	'{common,components}/*/src/**/*.ts': ["lit-analyzer --rules.no-nullable-attribute-binding off --rules.no-missing-import warning --rules.no-incompatible-type-binding off --strict"],
+	]
 };
