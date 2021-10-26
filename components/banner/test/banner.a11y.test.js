785f0b9e83b9c60bf5c3cc3acf7715a25a82dd7a
Rina <rina.oksman@vonage.com>
2021-10-26 15:43:16 +0300

    tests

--- components/banner/test/banner.a11y.test.js
+++ components/banner/test/banner.a11y.test.js
@@ -4,18 +4,21 @@ import { fixture } from '@open-wc/testing-helpers';
 
 describe('banner a11y', function () {
 	it('should adhere to accessibility guidelines', async function () {
-		const bannerEl = await fixture(html`<vwc-banner></vwc-banner>`);
+		const bannerEl = await fixture(html`<vwc-banner message="Hello" open></vwc-banner>`);
 		await expect(bannerEl).shadowDom.to.be.accessible();
 	});
 	it('should be with default role and aria-live values', async function () {
-		const bannerEl = await fixture(html`<vwc-banner></vwc-banner>`);
-		expect(bannerEl).to.have.attribute('role', 'status');
-		expect(bannerEl).to.have.attribute('aria-live', 'polite');
+		const bannerEl = await fixture(html`<vwc-banner message="Hello" open></vwc-banner>`);
+		expect(bannerEl.shadowRoot.querySelector('.banner--message')).to.have.attribute('role', 'status');
+		expect(bannerEl.shadowRoot.querySelector('.banner--message')).to.have.attribute('aria-live', 'polite');
 	});
 	it('should be with reflected role and aria-live values', async function () {
-		const bannerEl = await fixture(html`<vwc-banner role="alert" aria-live="assertive"></vwc-banner>`);
-		expect(bannerEl).to.have.attribute('role', 'alert');
-		expect(bannerEl).to.have.attribute('aria-live', 'assertive');	
+		const bannerEl = await fixture(html`<vwc-banner message="Hello" open role="alert" aria-live="assertive"></vwc-banner>`);
+		expect(bannerEl.shadowRoot.querySelector('.banner--message')).to.have.attribute('role', 'alert');
+		expect(bannerEl.shadowRoot.querySelector('.banner--message')).to.have.attribute('aria-live', 'assertive');
+	});
+	it('should be without role and aria-live values when closed', async function () {
+		const bannerEl = await fixture(html`<vwc-banner message="Hello"></vwc-banner>`);
+		expect(bannerEl.shadowRoot.querySelector('.banner--message')).to.equal(null);
 	});
 });
-
