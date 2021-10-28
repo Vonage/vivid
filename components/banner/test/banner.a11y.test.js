4f34e7b3fbc997579c90ff3452a508401d7694a2
rinaok <rina.oksman@vonage.com>
2021-10-28 15:11:23 +0300

    feat(vwc-banner): add aria live polite (#1093)
    * add aria live and role
    
    * story fix
    
    * add aria-live and role
    
    * render aria live
    
    * ui test
    
    * directive
    
    * inport accessible-snackbar-label-directive
    
    * ui test
    
    * dependency
    
    * ui test
    
    * directive
    
    * add dep
    
    * aria property
    
    * update versions
    
    * yarn
    
    * yarn
    
    * reflect
    
    * aria
    
    * set role attribute
    
    * add constants
    
    * merge
    
    * merge
    
    * test
    
    * test
    
    * tests
    
    * a11y test
    
    * test
    
    * test
    
    Co-authored-by: yinonov <yinon@hotmail.com>

--- components/banner/test/banner.a11y.test.js
+++ components/banner/test/banner.a11y.test.js
@@ -1,10 +1,30 @@
 import 'chai-a11y-axe';
 import { html } from 'lit-html';
-import { fixture } from '@open-wc/testing-helpers';
+import { fixture, aTimeout } from '@open-wc/testing-helpers';
 
 describe('banner a11y', function () {
+	const TRANSITION_TIME = 200;
 	it('should adhere to accessibility guidelines', async function () {
-		const bannerEl = await fixture(html`<vwc-banner></vwc-banner>`);
+		const bannerEl = await fixture(html`<vwc-banner message="Hello" open></vwc-banner>`);
 		await expect(bannerEl).shadowDom.to.be.accessible();
+	});
+	it('should be with default role and aria-live values', async function () {
+		const bannerEl = await fixture(html`<vwc-banner message="Hello" open></vwc-banner>`);
+		expect(bannerEl.shadowRoot.querySelector('.banner--message')).to.have.attribute('role', 'status');
+		expect(bannerEl.shadowRoot.querySelector('.banner--message')).to.have.attribute('aria-live', 'polite');
+	});
+	it('should be with reflected role and aria-live values', async function () {
+		const bannerEl = await fixture(html`<vwc-banner message="Hello" open role="alert" aria-live="assertive" dismissible></vwc-banner>`);
+		expect(bannerEl.shadowRoot.querySelector('.banner--message')).to.have.attribute('role', 'alert');
+		expect(bannerEl.shadowRoot.querySelector('.banner--message')).to.have.attribute('aria-live', 'assertive');
+		bannerEl.shadowRoot.querySelector('vwc-icon-button')?.click();
+		await aTimeout(TRANSITION_TIME * 1.1);
+		bannerEl.setAttribute('open', 'true');
+		expect(bannerEl.shadowRoot.querySelector('.banner--message')).to.equal(null);
+		expect(bannerEl.shadowRoot.querySelector('.banner--message')).to.equal(null);
+	});
+	it('should be without role and aria-live values when closed', async function () {
+		const bannerEl = await fixture(html`<vwc-banner message="Hello"></vwc-banner>`);
+		expect(bannerEl.shadowRoot.querySelector('.banner--message')).to.equal(null);
 	});
 });
