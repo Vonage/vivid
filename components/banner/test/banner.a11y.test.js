187174bf4a4d9c664c52c471553226cd1df38f63
Rina <rina.oksman@vonage.com>
2021-10-31 15:58:57 +0200

    fix test

--- components/banner/test/banner.a11y.test.js
+++ components/banner/test/banner.a11y.test.js
@@ -1,4 +1,6 @@
+import '../vwc-banner.js';
 import 'chai-a11y-axe';
+import 'chai-dom';
 import { aTimeout } from '@open-wc/testing-helpers';
 import {
 	isolatedElementsCreation,
@@ -22,27 +24,27 @@ describe('banner a11y', function () {
 			textToDomToParent(`<${COMPONENT_NAME} message="Hello" open></${COMPONENT_NAME}>`)
 		);
 		await actualElement.updateComplete;
-		expect(actualElement.shadowRoot.querySelector('.banner--message')).to.have.attribute('role', 'status');
-		expect(actualElement.shadowRoot.querySelector('.banner--message')).to.have.attribute('aria-live', 'polite');
+		expect(actualElement.shadowRoot?.querySelector('.banner--message')).to.have.attribute('role', 'status');
+		expect(actualElement.shadowRoot?.querySelector('.banner--message')).to.have.attribute('aria-live', 'polite');
 	});
 	it('should be with reflected role and aria-live values', async function () {
 		const [actualElement] = addElement(
 			textToDomToParent(`<${COMPONENT_NAME} message="Hello" open role="alert" aria-live="assertive" dismissible></${COMPONENT_NAME}>`)
 		);
 		await actualElement.updateComplete;
-		expect(actualElement.shadowRoot.querySelector('.banner--message')).to.have.attribute('role', 'alert');
-		expect(actualElement.shadowRoot.querySelector('.banner--message')).to.have.attribute('aria-live', 'assertive');
-		actualElement.shadowRoot.querySelector('vwc-icon-button')?.click();
+		expect(actualElement.shadowRoot?.querySelector('.banner--message')).to.have.attribute('role', 'alert');
+		expect(actualElement.shadowRoot?.querySelector('.banner--message')).to.have.attribute('aria-live', 'assertive');
+		actualElement.shadowRoot?.querySelector('vwc-icon-button')?.click();
 		await aTimeout(TRANSITION_TIME * 1.1);
 		actualElement.setAttribute('open', 'true');
-		expect(actualElement.shadowRoot.querySelector('.banner--message')).to.equal(null);
-		expect(actualElement.shadowRoot.querySelector('.banner--message')).to.equal(null);
+		expect(actualElement.shadowRoot?.querySelector('.banner--message')).to.equal(null);
+		expect(actualElement.shadowRoot?.querySelector('.banner--message')).to.equal(null);
 	});
 	it('should be without role and aria-live values when closed', async function () {
 		const [actualElement] = addElement(
 			textToDomToParent(`<${COMPONENT_NAME} message="Hello"></${COMPONENT_NAME}>`)
 		);
 		await actualElement.updateComplete;
-		expect(actualElement.shadowRoot.querySelector('.banner--message')).to.equal(null);
+		expect(actualElement.shadowRoot?.querySelector('.banner--message')).to.equal(null);
 	});
 });
