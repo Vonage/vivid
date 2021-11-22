4bb334588114bdc256870da34cc0783f8362e2df
rinaok <rina.oksman@vonage.com>
2021-11-22 16:13:08 +0200

    fix(vwc-banner): add-examples and fix prop (#1123)
    * story refactoring
    
    * add aria to readme file
    
    * dismissible = false
    
    * banner-open
    
    * banner-open
    
    * Delete vwc-banner.md
    
    * fix ui test
    
    * fix name typo

--- ui-tests/tests/vwc-banner/index.js
+++ ui-tests/tests/vwc-banner/index.js
@@ -1,15 +1,16 @@
-
 import '@vonage/vwc-banner';
-import {
-	Info, Announcement, Success, Warning, Alert
-} from '@vonage/vwc-banner/stories/banner.stories';
-import { storiesToElement } from '../../utils/storiesToElement';
 
 export async function createElementVariations(wrapper) {
-	wrapper.style.width = '800px';
-	wrapper.appendChild(storiesToElement({
-		Info, Announcement, Success, Warning, Alert
-	}));
+	const textElementWrapper = document.createElement('div');
+	const message = "test";
+	textElementWrapper.innerHTML = `
+	<vwc-banner open connotation="info" message=${message}></vwc-banner>
+	<vwc-banner open connotation="announcement" message=${message}></vwc-banner>
+	<vwc-banner open connotation="success" message=${message}></vwc-banner>
+	<vwc-banner open connotation="warning" message=${message}></vwc-banner>
+	<vwc-banner open connotation="alert" message=${message}></vwc-banner>
+		`;
+	wrapper.appendChild(textElementWrapper);
 }
 
 
