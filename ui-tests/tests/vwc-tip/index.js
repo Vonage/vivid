787e7da6214f6a9647c177c95a57326900961ee9
rtannenbaum <rachel.tannenbaum@vonage.com>
2021-12-06 16:31:25 +0200

    padding fix

--- ui-tests/tests/vwc-tip/index.js
+++ ui-tests/tests/vwc-tip/index.js
@@ -4,7 +4,7 @@ export async function createElementVariations(wrapper) {
 	const tipElementWrapper = document.createElement('div');
 	tipElementWrapper.innerHTML =
 		`
-		 <vwc-tip></vwc-tip>
+		 <vwc-tip dismissble></vwc-tip>
 		 <vwc-tip icon="info-line"></vwc-tip>
 		`;
 	wrapper.appendChild(tipElementWrapper);
