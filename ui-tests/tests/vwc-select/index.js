6b01e50258f9f5705d071a33ade243abf2372110
rtannenbaum <rachel.tannenbaum@vonage.com>
2021-12-07 16:33:29 +0200

    add ui test for dense with no padding

--- ui-tests/tests/vwc-select/index.js
+++ ui-tests/tests/vwc-select/index.js
@@ -101,6 +101,105 @@ export async function createElementVariations(wrapper) {
   </vwc-list-item>
 </vwc-select de>
 <hr>
+<div style="display: flex; align-items: flex-start;">
+<p style="margin: 0; padding-right: 1rem;">Dense-with label</p>
+	<vwc-select dense label="VWC Select">
+  <vwc-list-item
+    mwc-list-item=""
+    tabindex="0"
+    aria-disabled="false"
+    role="option"
+    aria-selected="true"
+    selected=""
+    activated=""
+  ></vwc-list-item>
+  <vwc-list-item
+    value="0"
+    mwc-list-item=""
+    tabindex="-1"
+    aria-disabled="false"
+    role="option"
+  >
+    Item 0
+  </vwc-list-item>
+  <vwc-list-item
+    value="1"
+    mwc-list-item=""
+    tabindex="-1"
+    aria-disabled="false"
+    role="option"
+  >
+    Item 1
+  </vwc-list-item>
+  <vwc-list-item
+    value="2"
+    mwc-list-item=""
+    tabindex="-1"
+    aria-disabled="false"
+    role="option"
+  >
+    Item 2
+  </vwc-list-item>
+  <vwc-list-item
+    value="3"
+    mwc-list-item=""
+    tabindex="-1"
+    aria-disabled="false"
+    role="option"
+  >
+    Item 3
+  </vwc-list-item>
+</vwc-select>
+<p style="margin: 0; padding: 0 1rem;">Dense-no label</p>
+	<vwc-select dense>
+  <vwc-list-item
+    mwc-list-item=""
+    tabindex="0"
+    aria-disabled="false"
+    role="option"
+    aria-selected="true"
+    selected=""
+    activated=""
+  ></vwc-list-item>
+  <vwc-list-item
+    value="0"
+    mwc-list-item=""
+    tabindex="-1"
+    aria-disabled="false"
+    role="option"
+  >
+    Item 0
+  </vwc-list-item>
+  <vwc-list-item
+    value="1"
+    mwc-list-item=""
+    tabindex="-1"
+    aria-disabled="false"
+    role="option"
+  >
+    Item 1
+  </vwc-list-item>
+  <vwc-list-item
+    value="2"
+    mwc-list-item=""
+    tabindex="-1"
+    aria-disabled="false"
+    role="option"
+  >
+    Item 2
+  </vwc-list-item>
+  <vwc-list-item
+    value="3"
+    mwc-list-item=""
+    tabindex="-1"
+    aria-disabled="false"
+    role="option"
+  >
+    Item 3
+  </vwc-list-item>
+</vwc-select>
+</div>
+<hr>
 	<vwc-select label="VWC Select" helper="Helper Text" ghost dense>
   <vwc-list-item
     mwc-list-item=""
