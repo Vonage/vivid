ce79c00174fa4c4e1d884f078e310b4faefda26d
Rina Oksman <rina.oksman@vonage.com>
2021-12-16 13:17:10 +0200

    chore(design-tokens): add sizing design tokens (#1138)
    * add sizing design tokens
    
    * update version
    
    * update vvd-design-tokens-properties version
    
    * update package.json
    
    * update yarn.lock
    
    * node-version
    
    * render themes
    
    Co-authored-by: yonatankra <yonatan.kra@vonage.com>
    Co-authored-by: yinonov <yinon@hotmail.com>

--- common/design-tokens/src/index.js
+++ common/design-tokens/src/index.js
@@ -2,7 +2,6 @@ import { render as themesRender } from './builders/render-themes.js';
 import { render as themesSassRender } from './builders/render-themes-sass.js';
 import { renderTypographySassMixin, renderTypographyTypes } from './builders/typography/index.js';
 
-
 console.log('\n\x1b[32m=== Design tokens build started ====\x1b[0m');
 
 themesRender();
