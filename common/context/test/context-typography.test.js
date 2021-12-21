288469f1519f2cb7b515d629931086c619142731
yonatankra <yonatan.kra@vonage.com>
2021-12-21 14:42:26 +0200

    Fix context tests

--- common/context/test/context-typography.test.js
+++ common/context/test/context-typography.test.js
@@ -11,22 +11,30 @@ import {
 } from './typography-definitions.test.js';
 
 describe('vvd-context typography', () => {
+	const wrapper = document.createElement('div');
+	document.body.appendChild(wrapper);
+
+	afterEach(function () {
+		document.body.classList.remove('vivid-scope');
+		wrapper.innerHTML = '';
+	});
+
 	for (const deviation of DEVIATIVE_ELEMENTS) {
 		describe(`'${deviation.name}' deviation`, () => {
+
 			for (const [contextElement, typographyContext] of Object.entries(
 				CONTEXT_PROVIDING_ELEMENTS
 			)) {
 				for (const deviatingElement of deviation.elements) {
 					it(`should have '${deviatingElement}' element styled within '${contextElement}'`, async () => {
-						const iframe = await setupLocalIframe();
-						const d = iframe.contentWindow.document;
+						const d = document;
 
 						d.body.classList.add('vivid-scope');
 
 						await vvdContext.mount(d);
 
 						const ce = d.createElement(contextElement);
-						d.body.appendChild(ce);
+						wrapper.appendChild(ce);
 
 						const te = d.createElement(deviatingElement);
 						te.textContent = 'sample';
@@ -38,8 +46,6 @@ describe('vvd-context typography', () => {
 							deviation.deviations
 						);
 						assertComputedStyle(te, expectedStyle);
-
-						iframe.remove();
 					});
 				}
 			}
@@ -49,19 +55,17 @@ describe('vvd-context typography', () => {
 	describe('padding layout', () => {
 		for (const [te, tp] of Object.entries(PADDING_DEFINITIONS)) {
 			it(`should have '${te}' element with correct paddings`, async () => {
-				const iframe = await setupLocalIframe();
-				const d = iframe.contentWindow.document;
+				const d = document;
 				d.body.classList.add('vivid-scope');
 				await vvdContext.mount(d);
 				const ce = d.createElement(te);
-				d.body.appendChild(ce);
+				wrapper.appendChild(ce);
 				assertComputedStyle(ce, {
 					paddingTop: tp[0],
 					paddingRight: tp[1],
 					paddingBottom: tp[2],
 					paddingLeft: tp[3]
 				});
-				iframe.remove();
 			});
 		}
 	});
@@ -69,19 +73,17 @@ describe('vvd-context typography', () => {
 	describe('margin layout', () => {
 		for (const [te, tm] of Object.entries(MARGIN_DEFINITIONS)) {
 			it(`should have '${te}' element with correct margins`, async () => {
-				const iframe = await setupLocalIframe();
-				const d = iframe.contentWindow.document;
+				const d = document;
 				d.body.classList.add('vivid-scope');
 				await vvdContext.mount(d);
 				const ce = d.createElement(te);
-				d.body.appendChild(ce);
+				wrapper.appendChild(ce);
 				assertComputedStyle(ce, {
 					marginTop: tm[0],
 					marginRight: tm[1],
 					marginBottom: tm[2],
 					marginLeft: tm[3]
 				});
-				iframe.remove();
 			});
 		}
 	});
