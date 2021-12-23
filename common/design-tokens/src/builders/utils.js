7a8ae89482252b23dcc9bf9a1148e7f568346a88
Yonatan Kra <yonatan.kra@vonage.com>
2021-12-23 11:22:45 +0200

    fix(docs): fix storybook build (#1166)
    * Fix SB build
    
    * fix(docs): fix storybook build
    
    * Fix linting issue

--- common/design-tokens/src/builders/utils.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-
-export function removeFolderSafely(outputFolder) {
-	if (fs.existsSync(outputFolder)) {
-		fs.rmdirSync(outputFolder, { recursive: true });
-		return true;
-	}
-	return false;
-}
