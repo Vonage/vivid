fb047cfc316489206939b75b9022fa3cb5e592bc
Rina <rina.oksman@vonage.com>
2021-12-27 17:53:52 +0200

    add ui test

--- ui-tests/tests/vwc-popup/index.js
+++ ui-tests/tests/vwc-popup/index.js
@@ -1,12 +1,37 @@
-import '../../../components/popup/vwc-popup';
+import '@vonage/vwc-popup';
+import '@vonage/vwc-button';
+import '@vonage/vwc-text';
 
 export async function createElementVariations(wrapper) {
 	const textElementWrapper = document.createElement('div');
 	textElementWrapper.innerHTML = `
-		<vwc-popup>Rachel</vwc-popup>
-		<vwc-popup tail dismissible><div style="height: 100px; width: 100px; background-color: #660000;"></div></vwc-popup>
-		`;
+		<style>
+			.popup-wrapper {
+				width: 100%;
+				height: 200px;
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				background-color: var(--vvd-color-neutral-10);
+			}
+		</style>
+		<div class="popup-wrapper">
+			<vwc-button id="button" layout="outlined" outlined aria-describedby="popup">Click to open popup</vwc-button>
+			<vwc-popup id="popup" corner="right" arrow><slot><vwc-text>I'm popup</vwc-text></slot></vwc-popup>
+		</div>`;
+
 	wrapper.appendChild(textElementWrapper);
+	const button = document.getElementById("button");
+	button.addEventListener("click", onClick);
 }
 
-
+function onClick() {
+	const popup = document.querySelector("vwc-popup");
+	const button = document.querySelector("#button");
+	if (popup.open) {
+		popup.hide();
+	} else {
+		popup.anchor = button;
+		popup.show();
+	}
+}
