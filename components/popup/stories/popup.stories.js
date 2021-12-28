f67121877bc7219c36d03045bb6dffc4f445df3a
Rina <rina.oksman@vonage.com>
2021-12-28 17:09:01 +0200

    update story

--- components/popup/stories/popup.stories.js
+++ components/popup/stories/popup.stories.js
@@ -22,21 +22,43 @@ const Template = args => html`
 			justify-content: center;
 			background-color: var(--vvd-color-neutral-10);
 		}
+		.content {
+			max-width: 200px;
+			text-align: left;
+		}
+		.line {
+			margin: 5px 0 ;
+   	 		background-color: var(--vvd-color-neutral-40);
+	  		height: 1px;
+		}
 	</style>
 	<div class="popup-wrapper">
-		<vwc-button id="button" layout="outlined" outlined aria-describedby="popup" @click="${onClick}">Click to open popup</vwc-button>
-		<vwc-popup id="popup" ...=${spread(args)}><slot><vwc-text>I'm popup</vwc-text></slot></vwc-popup>
+		<vwc-button id="button" layout="outlined" outlined aria-describedby="popup" @click=${onClick}>Click to open popup</vwc-button>
+		<vwc-popup id="popup" ...=${spread(args)}>
+			<slot>
+				<div class="content">
+					<vwc-text font-face="body-1-bold" tight>Popup title</vwc-text>
+					<div class="line"></div>
+					<vwc-text font-face="body-2" tight>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</vwc-text>
+				</div>
+			</slot>
+		</vwc-popup>
 	</div>`;
 
 export const Basic = Template.bind({});
-Basic.args = { open:false };
+
+export const WithArrow = Template.bind({});
+WithArrow.args = { arrow: true, corner: "right" };
+
+export const Dismissible = Template.bind({});
+Dismissible.args = { dismissible: true, corner: "bottom" };
 
 function onClick() {
 	const popup = document.querySelector("vwc-popup");
 	const button = document.querySelector("#button");
-	if(popup.open){
+	if (popup.open) {
 		popup.hide();
-	}else{
+	} else {
 		popup.anchor = button;
 		popup.show();
 	}
