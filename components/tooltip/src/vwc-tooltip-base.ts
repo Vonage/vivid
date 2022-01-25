6730ee2a7bc08b4083f42f359b55cfd453de0993
rtannenbaum <rachel.tannenbaum@vonage.com>
2022-01-25 17:51:02 +0200

    open & close popup

--- components/tooltip/src/vwc-tooltip-base.ts
+++ components/tooltip/src/vwc-tooltip-base.ts
@@ -1,12 +1,11 @@
 import {
-	html, LitElement, property, query
+	html, LitElement, property, PropertyValues, query
 } from 'lit-element';
 import type { TemplateResult } from 'lit-html';
-import '@vonage/vwc-popup';
-
+import type { VWCPopup } from  '@vonage/vwc-popup';
 
 export class VWCTooltipBase extends LitElement {
-	//@query('.popup') protected popup!: ;
+	@query('.popup') protected popup!: VWCPopup;
 	@query('.iconButton') protected iconButton!: HTMLElement;
 
 	/**
@@ -26,12 +25,12 @@ export class VWCTooltipBase extends LitElement {
 		icon: 'info-line' | 'help-line' = 'help-line';
 
 	/**
-	 * @prop placement - the placement of the tooltip
-	 * accepts auto, auto-start, auto-end, top, top-start, top-end, bottom, bottom-start, bottom-end, right, right-start, right-end, left, left-start, left-end
+	 * @prop corner - the placement of the tooltip
+	 * accepts top, top-start, top-end, bottom, bottom-start, bottom-end, right, right-start, right-end, left, left-start, left-end
 	 * @public
 	 * */
 	@property({ type: String, reflect: true })
-		placement = 'auto';
+		corner = 'top';
 
 	/**
 	 * @prop open - indicates whether the tip is open
@@ -48,24 +47,27 @@ export class VWCTooltipBase extends LitElement {
 	@property({ type: Boolean, reflect: true })
 		dismissible?: boolean;
 
+
+	protected override firstUpdated(changedProperties: PropertyValues): void {
+		super.firstUpdated(changedProperties);
+		this.popup.anchor = this.iconButton;
+	}
+
 	protected override updated(): void {
-		// if(this.popup.open){
-		// 	this.popup.anchor = this.iconButton;
-		// 	this.popup.show();
-		// }
-		// else{
-		// 	this.popup.hide();
-		// }
+		this.popup.open = this.open;
+		if (this.popup.open) {
+			this.popup.show();
+		} else {
+			this.popup.hide();
+		}
 	}
 
 	private clickHandler() {
-		// this.popup.anchor = this.iconButton;
-		// this.open = true;
-		// this.popup.show();
+		this.popup.open = !this.popup.open;
 	}
 
 	private clickCloseHandler() {
-		//this.hide();
+		this.popup.hide();
 	}
 
 
@@ -79,7 +81,7 @@ export class VWCTooltipBase extends LitElement {
 	protected override render(): TemplateResult {
 		return html`
 				<vwc-icon-button icon=${this.icon} class="iconButton"  shape="circled" aria-describedby="tooltip" aria-haspopup="true" @click="${this.clickHandler}"></vwc-icon-button>
-					<vwc-popup corner="bottom" arrow open alternate="true" exportparts="vvd-scheme-alternate" placement=${this.placement}>
+					<vwc-popup arrow alternate="true" exportparts="vvd-scheme-alternate" corner=${this.corner} class="popup">
 						<div class="tooltip">
 							<header part="vvd-scheme-alternate" class="tooltip-header">
 								<div class="tooltip-title">${this.content}</div>
