f3aee57cc3c5fcb326a602203a6b8ad447b45a3d
rachelbt <rachelbt@users.noreply.github.com>
2022-03-09 10:11:51 +0200

    fix(select): option position when no label is set (#1251)
    * fix(select): option position when no label is set
    
    * update ui test
    
    * update ui test
    
    * update snapshot
    
    Co-authored-by: yinonov <yinon@hotmail.com>

--- ui-tests/tests/vwc-select/index.js
+++ ui-tests/tests/vwc-select/index.js
@@ -53,6 +53,56 @@ export async function createElementVariations(wrapper) {
   </vwc-list-item>
 </vwc-select>
 <hr>
+<div>
+	<vwc-select helper="select with no label">
+  <vwc-list-item
+    mwc-list-item=""
+    tabindex="0"
+    aria-disabled="false"
+    role="option"
+    aria-selected="true"
+    selected=""
+    activated=""
+  >First option</vwc-list-item>
+  <vwc-list-item
+    value="0"
+    mwc-list-item=""
+    tabindex="-1"
+    aria-disabled="false"
+    role="option"
+  >
+    Item 0
+  </vwc-list-item>
+  <vwc-list-item
+    value="1"
+    mwc-list-item=""
+    tabindex="-1"
+    aria-disabled="false"
+    role="option"
+  >
+    Item 1
+  </vwc-list-item>
+  <vwc-list-item
+    value="2"
+    mwc-list-item=""
+    tabindex="-1"
+    aria-disabled="false"
+    role="option"
+  >
+    Item 2
+  </vwc-list-item>
+  <vwc-list-item
+    value="3"
+    mwc-list-item=""
+    tabindex="-1"
+    aria-disabled="false"
+    role="option"
+  >
+    Item 3
+  </vwc-list-item>
+</vwc-select>
+</div>
+<hr>
 <div style="background-color: rebeccapurple">
 	<vwc-select label="VWC Select" helper="Helper Text" dense>
   <vwc-list-item
@@ -100,7 +150,7 @@ export async function createElementVariations(wrapper) {
   >
     Item 3
   </vwc-list-item>
-</vwc-select de>
+</vwc-select>
 </div>
 <hr>
 <div style="display: flex; align-items: flex-start;">
