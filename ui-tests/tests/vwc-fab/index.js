489fc70744b1f56e8c9610add2c67aefdcfafbfa
yinonov <yinon@hotmail.com>
2022-03-23 00:08:48 +0200

    disable length-zero-no-unit

--- ui-tests/tests/vwc-fab/index.js
+++ ui-tests/tests/vwc-fab/index.js
@@ -8,7 +8,7 @@ export async function createElementVariations(wrapper) {
 		<vwc-fab icon="wallet" label="Add to cart" extended></vwc-fab>
 		<vwc-fab icon="store" connotation="primary"></vwc-fab>
 		<vwc-fab icon="store" connotation="cta"></vwc-fab>
-		`;
+	`;
 	wrapper.appendChild(textElementWrapper);
 }
 
