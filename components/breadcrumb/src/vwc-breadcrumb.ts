b597cf26526483c9f50bade287cad25824a830a9
yonatankra <yonatan.kra@vonage.com>
2022-05-02 13:21:03 +0300

    Fix the style

--- components/breadcrumb/src/vwc-breadcrumb.ts
+++ components/breadcrumb/src/vwc-breadcrumb.ts
@@ -1,3 +1,12 @@
 import '@vonage/vvd-core';
 import '@vonage/vwc-icon';
 import '@vonage/vivid/breadcrumb/index.js';
+
+const style = document.createElement('style');
+style.innerHTML = `
+	vwc-breadcrumb {
+		--icon-size: 14px;
+	}
+`;
+document.head.appendChild(style);
+
