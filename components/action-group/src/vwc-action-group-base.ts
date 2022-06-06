6d9f567facfa2f9b4592c8fabfd3ab5d8dbccc1c
Rachel BT <rachel.tannenbaum@vonage.com>
2022-06-06 15:10:50 +0300

    feat(action-group): new component

---
+++ components/action-group/src/vwc-action-group-base.ts
@@ -0,0 +1,43 @@
+import {
+	html, LitElement, property, TemplateResult
+} from 'lit-element';
+import { classMap } from 'lit-html/directives/class-map.js';
+import type { ClassInfo } from 'lit-html/directives/class-map.js';
+import type { Shape, Layout } from '@vonage/vvd-foundation/constants.js';
+
+type ActionGroupLayout = Extract<
+	Layout,
+	Layout.Outlined | Layout.Ghost
+>;
+
+type ActionGroupShape = Extract<Shape, Shape.Rounded | Shape.Pill>;
+
+
+/**
+ * @slot - This is a default/unnamed slot to assign text content. *deprecated* please use _text_ property instead
+ */
+export class VWCActionGroupBase extends LitElement {
+	@property({ type: String, reflect: true })
+		shape?: ActionGroupShape;
+
+	@property({ type: String, reflect: true })
+		layout?: ActionGroupLayout;
+
+	@property({ type: String, reflect: true })
+		text?: string;
+
+	protected getRenderClasses(): ClassInfo {
+		return {
+			[`shape-${this.shape}`]: !!this.shape,
+			[`layout-${this.layout}`]: !!this.layout
+		};
+	}
+
+	protected override render(): TemplateResult {
+		return html`
+			<div class="vwc-action-group ${classMap(this.getRenderClasses())}">
+				<slot>
+				</slot>
+			</div>`;
+	}
+}
