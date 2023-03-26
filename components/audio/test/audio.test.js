7022adb8e6f8deb8344a2e067716d0f124109c83
Yonatan Kra <yonatan.kra@vonage.com>
2023-03-26 23:22:30 +0300

    chore(vwc-audio): forgotten fixes (#1379)

--- components/audio/test/audio.test.js
+++ components/audio/test/audio.test.js
@@ -44,29 +44,31 @@ describe('vwc-audio', () => {
 		expect(vwcAudioEl.shadowRoot.querySelector('.playhead-position')).to.exist;
 	});
 
-	it('should cover audio methods', async function () {
-		const [audioElement] = addElements(textToDomToParent(`<vwc-audio></vwc-audio>`));
-		await waitNextTask();
-		audioElement.currentTime = 5;
-		audioElement.pause();
-		audioElement.play();
-	});
-
 	describe('play', () => {
-		it('should return to stop mode when src changes', async function () {
+		let originalPlay = Audio.prototype.play;
+		before(function () {
 			Audio.prototype.play = function () {
 				this.dispatchEvent(new Event('play'));
 			};
+		});
+
+		after(function () {
+			Audio.prototype.play = originalPlay;
+		});
+
+		it('should return to stop mode when src changes', async function () {
+
 			const [audioElement] = (textToDomToParent(`<vwc-audio></vwc-audio>`));
 			await waitNextTask();
 			audioElement.src = 'https://download.samplelib.com/mp3/sample-9s.mp3';
-
 			await audioElement.updateComplete;
+
 			audioElement.play();
 			await audioElement.updateComplete;
 			audioElement.src = 'https://download.samplelib.com/mp3/sample-6s.mp3';
 			await audioElement.updateComplete;
 			await waitNextTask();
+
 			expect(audioElement.shadowRoot.querySelector('vwc-icon').type.includes('play')).to.equal(true);
 		});
 	});
