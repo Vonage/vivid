d492679e2d6a8a458a9b8f2a34554d22d5192fb0
Yonatan Kra <yonatan.kra@vonage.com>
2023-04-17 17:51:06 +0300

    New audio screenshot

--- ui-tests/tests/vwc-audio/index.js
+++ ui-tests/tests/vwc-audio/index.js
@@ -1,6 +1,24 @@
 import vvdCore from '@vonage/vvd-core';
 import '@vonage/vwc-audio';
 
+function setAudioSrc(audios) {
+	audios.forEach(audio => {
+		if (audio.id) return;
+		audio.src = 'fake';
+		Object.defineProperty(audio._audio, 'error', {value: null});
+		audio._audio.dispatchEvent(new Event('loadedmetadata'));
+	});
+}
+
+async function setAudioSrcWithInfinityDuration() {
+	const audio = document.querySelector('#infinity-duration');
+	Object.defineProperty(audio._audio, 'duration', {value: Infinity});
+
+	audio._audio.dispatchEvent(new Event('loadedmetadata'));
+
+	await audio.updateComplete;
+}
+
 export async function createElementVariations(wrapper) {
 	const testWrapper = document.createElement('div');
 	wrapper.classList.add('grid');
@@ -8,6 +26,7 @@ export async function createElementVariations(wrapper) {
 	testWrapper.innerHTML =
 		`
 <vwc-audio timestamp></vwc-audio>
+<vwc-audio id="badSrc" timestamp></vwc-audio>
 <vwc-audio connotation="primary"></vwc-audio>
 <vwc-audio id="infinity-duration" timestamp></vwc-audio>
 <vwc-audio timestamp connotation="primary"></vwc-audio>
@@ -17,13 +36,11 @@ export async function createElementVariations(wrapper) {
 `;
 
 	wrapper.appendChild(testWrapper);
+	const audios = Array.from(document.querySelectorAll('vwc-audio'));
+	await Promise.all(audios.map(audio => audio.updateComplete));
+	setAudioSrc(audios);
 
-	const audio = document.querySelector('#infinity-duration');
-	await audio.updateComplete;
-	Object.defineProperty(audio._audio, 'duration', {value: Infinity});
-	audio._audio.dispatchEvent(new Event('loadedmetadata'));
-
-	await audio.updateComplete;
+	await setAudioSrcWithInfinityDuration();
 
 	await vvdCore.settled;
 }
